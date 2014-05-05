97ee9076a1f0c71b5fbcc5873beecbb447ffdcd7
Daniel Darabos <darabos.daniel@gmail.com>
2014-05-05 17:54:30 +0200

    Delete old webapp setup.

--- public/graph.js
+++
@@ -1,7 +0,0 @@
-angular.module('graphs')
-  .controller('GraphViewCtrl', function($scope, $routeParams, $resource) {
-    var id = $routeParams.graph;
-    var Graph = $resource('/ajax/graph/:graph');
-    $scope.graph = Graph.get({graph: id});
-  })
-;
