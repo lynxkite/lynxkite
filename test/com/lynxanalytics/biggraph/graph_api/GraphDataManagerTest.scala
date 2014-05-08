07fd5895bf1e9c658d7da3a30d364f37fc624618
Andras Nemeth <xxandreww@gmail.com>
2014-05-08 16:50:50 +0200

    Two more tests for the GraphDataManager

--- test/com/lynxanalytics/biggraph/graph_api/GraphDataManagerTest.scala
+++ test/com/lynxanalytics/biggraph/graph_api/GraphDataManagerTest.scala
@@ -27,5 +27,31 @@ class GraphDataManagerTest extends FunSuite with TestBigGraphManager with TestGr
              "((2,Bob),(0,Adam),Bob envies Adam)\n" +
              "((2,Bob),(1,Eve),Bob loves Eve)")
   }
+
+  test("We can reload a graph from memory without recomputing it") {
+    val graphManager = cleanGraphManager("memorycaching")
+    val dataManager = cleanDataManager("memorycaching")
+    val operation = new InstantiateSimpleGraph
+    val myGraph = graphManager.deriveGraph(Seq(), operation)
+    val myData = dataManager.obtainData(myGraph)
+    val myData2 = dataManager.obtainData(myGraph)
+    assert(myData eq myData2)
+    assert(operation.executionCounter == 1)
+  }
+
+  test("We can reload a graph from disk without recomputing it") {
+    val graphManager = cleanGraphManager("diskcaching")
+    val dataManager1 = cleanDataManager("diskcaching")
+    val dataManager2 = GraphDataManager(sparkContext, dataManager1.repositoryPath)
+    val operation = new InstantiateSimpleGraph
+    val myGraph = graphManager.deriveGraph(Seq(), operation)
+    val myData = dataManager1.obtainData(myGraph)
+    dataManager1.saveDataToDisk(myGraph)
+    val myData2 = dataManager2.obtainData(myGraph)
+    assert(!(myData eq myData2))
+    assert(TestUtils.RDDToSortedString(myData.triplets) ==
+             TestUtils.RDDToSortedString(myData2.triplets))
+    assert(operation.executionCounter == 1)
+  }
 }
 
