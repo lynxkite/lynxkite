03e70a6d2dc69a92d27a13452e1517d4aa4d8c6d
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-05-09 14:40:44 +0200

    Implements graph walk with the EdgeGraph operation.

--- app/com/lynxanalytics/biggraph/controllers/BigGraphController.scala
+++ app/com/lynxanalytics/biggraph/controllers/BigGraphController.scala
@@ -1,7 +1,11 @@
 package com.lynxanalytics.biggraph.controllers
 
+import com.lynxanalytics.biggraph.BigGraphSingleton
 import com.lynxanalytics.biggraph.graph_api
+import com.lynxanalytics.biggraph.graph_api.BigGraph
+import com.lynxanalytics.biggraph.graph_operations
 import com.lynxanalytics.biggraph.serving
+import java.util.UUID
 
 case class BigGraphRequest(id: String)
 
@@ -20,15 +24,82 @@ case class BigGraphResponse(
  */
 
 object BigGraphController {
-  def process(request: BigGraphRequest): BigGraphResponse = {
+  val bigGraphManager = BigGraphSingleton.bigGraphManager
+
+  // TODO(forevian): remove this block once standalone graph stats ready.
+  val graphDataManager = BigGraphSingleton.graphDataManager
+  def fakeGraphStats(graph: BigGraph): String = {
+    val data = graphDataManager.obtainData(graph)
+    "Vertices: %d Edges: %s".format(data.vertices.count, data.edges.count)
+  }
+
+  def basicDataFromGraph(graph: BigGraph): GraphBasicData = {
+    GraphBasicData(graph.toLongString, graph.gUID.toString)
+  }
+
+  private def responseFromGraph(graph: BigGraph): BigGraphResponse = {
     BigGraphResponse(
-      title = request.id,
-      stats = "Stats of " + request.id,
-      sources = Seq(
-          GraphBasicData(request.id + "pre1", request.id + "pre1"),
-          GraphBasicData(request.id + "pre2", request.id + "pre2")),
-      ops = Seq(
-          GraphBasicData(request.id + "post1", request.id + "post1"),
-          GraphBasicData(request.id + "post2", request.id + "post2")))
+      title = graph.toLongString,
+      stats = fakeGraphStats(graph),
+      sources = graph.sources.map(basicDataFromGraph(_)),
+      ops = Seq(basicDataFromGraph(bigGraphManager.deriveGraph(
+                                 Seq(graph), new graph_operations.EdgeGraph))))
+  }
+
+  def process(request: BigGraphRequest): BigGraphResponse = {
+    if (request.id == "x") {
+      responseFromGraph(bigGraphManager.deriveGraph(Seq(), new InstantiateSimpleGraph2))
+    } else {
+      responseFromGraph(bigGraphManager.graphForGUID(UUID.fromString(request.id)).get)
+    }
   }
 }
+
+
+// TODO: remove this once we have some more sane starting operations.
+// For now this is a copy of the same class created in GraphTestUtils. The reason for the copy
+// is that prod code cannot (or at least should not) depend on test code, but I don't want
+// to move this out to some permanent non-test location as it will go away from prod code
+// but will stick around in test code.
+import graph_api._
+import org.apache.spark.graphx.Edge
+import attributes.AttributeSignature
+import attributes.DenseAttributes
+
+class InstantiateSimpleGraph2 extends GraphOperation {
+  @transient var executionCounter = 0
+
+  def isSourceListValid(sources: Seq[BigGraph]) = (sources.size == 0)
+
+  def execute(target: BigGraph, manager: GraphDataManager): GraphData = {
+    val sc = manager.runtimeContext.sparkContext
+    val vertexSig = vertexAttributes(target.sources)
+    val edgeSig = edgeAttributes(target.sources)
+
+    val vertexMaker = vertexSig.maker
+    val nameIdx = vertexSig.writeIndex[String]("name")
+    val vertices = Seq(
+        (0l, vertexMaker.make.set(nameIdx, "Adam")),
+        (1l, vertexMaker.make.set(nameIdx, "Eve")),
+        (2l, vertexMaker.make.set(nameIdx, "Bob")))
+
+    val edgeMaker = edgeSig.maker
+    val commentIdx = edgeSig.writeIndex[String]("comment")
+    val edges = Seq(
+        new Edge(0l, 1l, edgeMaker.make.set(commentIdx, "Adam loves Eve")),
+        new Edge(1l, 0l, edgeMaker.make.set(commentIdx, "Eve loves Adam")),
+        new Edge(2l, 0l, edgeMaker.make.set(commentIdx, "Bob envies Adam")),
+        new Edge(2l, 1l, edgeMaker.make.set(commentIdx, "Bob loves Eve")))
+
+    executionCounter += 1
+
+    return new SimpleGraphData(target, sc.parallelize(vertices), sc.parallelize(edges))
+  }
+
+  def vertexAttributes(sources: Seq[BigGraph]): AttributeSignature =
+    AttributeSignature.empty.addAttribute[String]("name").signature
+
+  def edgeAttributes(sources: Seq[BigGraph]): AttributeSignature =
+    AttributeSignature.empty.addAttribute[String]("comment").signature
+}
+
