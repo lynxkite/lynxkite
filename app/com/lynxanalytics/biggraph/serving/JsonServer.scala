33e8f3e236e9798c29993d6e8cd7dfaa166eef5b
Forevian <andras.barjak@gmail.com>
2014-05-19 16:22:16 +0200

    better formatting for test log & example logs for Json Server

--- app/com/lynxanalytics/biggraph/serving/JsonServer.scala
+++ app/com/lynxanalytics/biggraph/serving/JsonServer.scala
@@ -8,27 +8,24 @@ import com.lynxanalytics.biggraph.controllers
 import com.lynxanalytics.biggraph.controllers._
 import play.api.libs.functional.syntax.toContraFunctorOps
 import play.api.libs.json.Json.toJsFieldJsValueWrapper
+import org.slf4j.LoggerFactory
 
 class JsonServer extends mvc.Controller {
-
-  def jsonPost[I : json.Reads, O : json.Writes](action: I => O) =
+  def jsonPost[I : json.Reads, O : json.Writes](action: I => O) = {
+    JsonServer.jsonLogger.info("JSON POST event received")
     mvc.Action(parse.json) {
       request => request.body.validate[I].fold(
-        errors => BadRequest(json.Json.obj(
-          "status" -> "Error",
-          "message" -> "Bad JSON",
-          "details" -> json.JsError.toFlatJson(errors))),
+        errors => JsonBadRequest("Error", "Invalid JSON key", errors),
         result => Ok(json.Json.toJson(action(result))))
     }
+  }
 
-  def jsonGet[I : json.Reads, O : json.Writes](action: I => O, key: String) =
+  def jsonGet[I : json.Reads, O : json.Writes](action: I => O, key: String) = {
     mvc.Action { request =>
+      JsonServer.jsonLogger.info("JSON GET event received")
       request.getQueryString(key) match {
         case Some(s) => Json.parse(s).validate[I].fold(
-            errors => BadRequest(json.Json.obj(
-              "status" -> "Error",
-              "message" -> "Bad JSON",
-              "details" -> json.JsError.toFlatJson(errors))),
+            errors => JsonBadRequest("Error", "Invalid JSON key", errors),
             result => Ok(json.Json.toJson(action(result))))
         case None => BadRequest(json.Json.obj(
               "status" -> "Error",
@@ -36,6 +33,22 @@ class JsonServer extends mvc.Controller {
               "details" -> "You need to specify query parameter %s with a JSON value".format(key)))
       }
     }
+  }
+
+  def JsonBadRequest(
+      status: String,
+      message: String,
+      details: Seq[(play.api.libs.json.JsPath, Seq[play.api.data.validation.ValidationError])]) = {
+    JsonServer.jsonLogger.error("Bad request: " + message)
+    BadRequest(json.Json.obj(
+      "status" -> status,
+      "message" -> message,
+      "details" -> json.JsError.toFlatJson(details)))
+  }
+}
+
+object JsonServer {
+  val jsonLogger = LoggerFactory.getLogger("JSON logger")
 }
 
 object ProductionJsonServer extends JsonServer {
