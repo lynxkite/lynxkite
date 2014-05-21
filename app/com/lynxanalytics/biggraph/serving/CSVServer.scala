335c5f2574ee4577fb49999c6389c6404aaec26a
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-05-21 14:29:03 +0200

    Small style fix

--- app/com/lynxanalytics/biggraph/serving/CSVServer.scala
+++ app/com/lynxanalytics/biggraph/serving/CSVServer.scala
@@ -8,14 +8,14 @@ import com.lynxanalytics.biggraph.graph_api.GraphData
 import com.lynxanalytics.biggraph.graph_util.CSVExport
 
 object CSVServer extends mvc.Controller {
-  def verticesCSVGet = dataGet{data =>
+  def verticesCSVGet = dataGet { data =>
       Ok(CSVExport.exportVertices(data).toString)
         .as("text/csv")
         .withHeaders("Content-disposition" -> "attachment; filename=vertices-%s.csv"
                        .format(data.bigGraph.gUID));
     }
 
-  def edgesCSVGet = dataGet{data =>
+  def edgesCSVGet = dataGet { data =>
       Ok(CSVExport.exportEdges(data).toString)
         .as("text/csv")
         .withHeaders("Content-disposition" -> "attachment; filename=edges-%s.csv"
