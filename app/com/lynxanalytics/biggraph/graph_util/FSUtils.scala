7158b0fc49557ca753df6526b7a33c2453d58174
Forevian <andras.barjak@gmail.com>
2014-05-23 19:05:38 +0200

    started implementing csv-header-stringonly importer

---
+++ app/com/lynxanalytics/biggraph/graph_util/FSUtils.scala
@@ -0,0 +1,43 @@
+package com.lynxanalytics.biggraph.graph_util
+
+import org.apache.hadoop
+import org.apache.spark
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+
+case class Filename(
+  val filename: String,
+  val awsAccessKeyId: String = "",
+  val awsSecretAccessKey: String = ""
+) {
+  override def toString() = filename
+  def hadoopConfiguration(): hadoop.conf.Configuration = {
+    val conf = new hadoop.conf.Configuration()
+    conf.set("fs.s3n.awsAccessKeyId", awsAccessKeyId)
+    conf.set("fs.s3n.awsSecretAccessKey", awsSecretAccessKey)
+    return conf
+  }
+  def fs() = hadoop.fs.FileSystem.get(uri, hadoopConfiguration)
+  def uri() = new java.net.URI(filename)
+  def path() = new hadoop.fs.Path(filename)
+  def open() = fs.open(path) // shouldn't we close this file after using it?
+
+  def saveAsTextFile(lines: spark.rdd.RDD[String]): Unit = {
+    // RDD.saveAsTextFile does not take a hadoop.conf.Configuration argument. So we struggle a bit.
+    val hadoopLines = lines.map(x => (hadoop.io.NullWritable.get(), new hadoop.io.Text(x)))
+    hadoopLines.saveAsHadoopFile(
+      filename,
+      keyClass = classOf[hadoop.io.NullWritable],
+      valueClass = classOf[hadoop.io.Text],
+      outputFormatClass = classOf[hadoop.mapred.TextOutputFormat[hadoop.io.NullWritable, hadoop.io.Text]],
+      conf = new hadoop.mapred.JobConf(hadoopConfiguration)
+    )
+  }
+
+  def addSuffix(suffix: String): Filename = {
+    Filename(filename + suffix, awsAccessKeyId, awsSecretAccessKey)
+  }
+
+  val fileSize: Long = fs.getFileStatus(path).getLen
+
+}
+
