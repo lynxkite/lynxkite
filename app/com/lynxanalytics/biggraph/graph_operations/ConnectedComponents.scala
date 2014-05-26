b42411360904ebb3f570135ed2ac492d6ed9c086
Daniel Darabos <darabos.daniel@gmail.com>
2014-05-26 19:33:29 +0200

    Add ConnectedComponents GraphOperation.

---
+++ app/com/lynxanalytics/biggraph/graph_operations/ConnectedComponents.scala
@@ -0,0 +1,141 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import scala.util.Random
+
+import org.apache.spark
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+import org.apache.spark.graphx.Edge
+import org.apache.spark.graphx.VertexId
+import org.apache.spark.rdd.RDD
+
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.attributes.AttributeSignature
+import com.lynxanalytics.biggraph.graph_api.attributes.DenseAttributes
+
+// Generates a new vertex attribute, that is a component ID.
+case class ConnectedComponents(
+    attribute: String,
+    // This parameter is basically for testing.
+    maxEdgesProcessedLocally: Int = 100000) extends GraphOperation {
+  type ComponentId = VertexId
+
+  @transient lazy val outputSig = AttributeSignature
+      .empty.addAttribute[Long](attribute).signature
+  @transient lazy val outputCloner = AttributeSignature
+      .empty.addAttribute[Long](attribute).cloner
+  @transient lazy val outputIdx = outputSig.writeIndex[Long](attribute)
+
+  def isSourceListValid(sources: Seq[BigGraph]): Boolean = sources.size == 1
+
+  def execute(target: BigGraph,
+              manager: GraphDataManager): GraphData = {
+    val inputGraph = target.sources.head
+    val inputData = manager.obtainData(inputGraph)
+    val runtimeContext = manager.runtimeContext
+    val sc = runtimeContext.sparkContext
+    val cores = runtimeContext.numAvailableCores
+    val partitioner = new spark.HashPartitioner(cores * 5)
+    // Graph as edge lists. Empty lists are generated for degree-0 nodes.
+    val edges = inputData.edges.map(e => (e.srcId, e.dstId))
+    val graph = edges.groupByKey()
+                     .mapValues(_.toSet)
+                     .rightOuterJoin(inputData.vertices)
+                     .mapValues(_._1.getOrElse(Set[VertexId]()))
+    // Get VertexId -> ComponentId map.
+    val components = getComponents(graph)
+    // Put ComponentId in the new attribute.
+    val vertices = inputData.vertices.join(components).mapValues({
+      case (da, cid) =>
+        outputCloner.clone(da).set(outputIdx, cid)
+    })
+    return new SimpleGraphData(target, vertices, inputData.edges)
+  }
+
+  def getComponents(
+    graph: RDD[(VertexId, Set[VertexId])]): RDD[(VertexId, ComponentId)] = {
+    // Need to take a count of edges, and then operate on the graph.
+    // We best cache it here.
+    graph.cache()
+    if (graph.count == 0) {
+      return new spark.rdd.EmptyRDD[(VertexId, ComponentId)](graph.sparkContext)
+    }
+    val edgeCount = graph.map(_._2.size).reduce(_ + _)
+    if (edgeCount <= maxEdgesProcessedLocally) {
+      return getComponentsLocal(graph)
+    } else {
+      return getComponentsDist(graph)
+    }
+  }
+
+  def getComponentsDist(
+    graph: RDD[(VertexId, Set[VertexId])]): RDD[(VertexId, ComponentId)] = {
+    // Each node decides if it is hosting a party or going out as a guest.
+    val invitations = graph.flatMap({ case (n, edges) =>
+      if (Random.nextBoolean()) { // Host. All the neighbors are invited.
+        (edges.map((_, n))
+         ++ Seq((n, -1l)))  // -1 is note to self: stay at home, host party.
+      } else {  // Guest. Can always stay at home at least.
+        Seq((n, n))
+      }
+    })
+    // Accept invitations.
+    val parties = invitations.groupByKey().map({ case (n, invitations) =>
+      if (invitations.size == 1 || invitations.contains(-1l)) {
+        // Nowhere to go, or we are hosting a party. Stay at home.
+        (n, n)
+      } else {
+        // Free to go. Go somewhere else.
+        (n, invitations.find(_ != n).get)
+      }
+    })
+    // Update edges. First, update the source (n->c) and flip the direction.
+    val halfDone = graph.join(parties).flatMap({ case (n, (edges, c)) =>
+      edges.map((_, c))
+    }).groupByKey().mapValues(_.toSet)
+    // Second, update the source, and merge the edge lists.
+    val almostDone = halfDone.join(parties).map({ case (n, (edges, c)) =>
+      (c, edges)
+    }).groupByKey().map({ case (c, edges) => (c, edges.toSet.flatten - c) })
+    // Third, remove finished components.
+    val newGraph = almostDone.filter({ case (n, edges) => edges.nonEmpty })
+    // Recursion.
+    val newComponents: RDD[(VertexId, ComponentId)] = getComponents(newGraph)
+    // We just have to map back the component IDs to the vertices.
+    val guests = parties.map({ case (n, party) => (party, n) }).groupByKey()
+    val components = guests.leftOuterJoin(newComponents).flatMap({
+      case (party, (guests, component)) =>
+        guests.map((_, component.getOrElse(party)))
+    })
+    return components
+  }
+
+  def getComponentsLocal(
+    rdd: RDD[(VertexId, Set[VertexId])]): RDD[(VertexId, ComponentId)] = {
+    import scala.collection.mutable
+    val graph = rdd.collect.toMap
+    val components = mutable.Map[Long, Long]()
+    var idx = 0
+    // Breadth-first search.
+    for (node <- graph.keys) {
+      if (!components.contains(node)) {
+        components(node) = node
+        val todo = mutable.Queue(node)
+        while (todo.size > 0) {
+          val v = todo.dequeue()
+          for (u <- graph(v)) {
+            if (!components.contains(u)) {
+              components(u) = node
+              todo.enqueue(u)
+            }
+          }
+        }
+      }
+    }
+    assert(components.size == graph.size, s"${components.size} != ${graph.size}")
+    return rdd.sparkContext.parallelize(components.toSeq)
+  }
+
+  def vertexAttributes(input: Seq[BigGraph]) = outputSig
+
+  def edgeAttributes(input: Seq[BigGraph]) = input.head.edgeAttributes
+}
---
+++ test/com/lynxanalytics/biggraph/graph_operations/ConnectedComponentsTest.scala
@@ -0,0 +1,94 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import org.apache.spark
+import org.apache.spark.graphx.VertexId
+import org.apache.spark.graphx.Edge
+import org.scalatest.FunSuite
+
+import com.lynxanalytics.biggraph.TestUtils
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.attributes._
+
+// Quick way to make a graph with no attributes from edge lists.
+case class GraphByEdgeLists(nodes: Seq[(Int, Seq[Int])]) extends GraphOperation {
+  def isSourceListValid(sources: Seq[BigGraph]) = (sources.size == 0)
+  def vertexAttributes(sources: Seq[BigGraph]): AttributeSignature =
+    AttributeSignature.empty
+  def edgeAttributes(sources: Seq[BigGraph]): AttributeSignature =
+    AttributeSignature.empty
+
+  def execute(target: BigGraph, manager: GraphDataManager): GraphData = {
+    val sc = manager.runtimeContext.sparkContext
+    val sig = vertexAttributes(Seq())
+    val maker = sig.maker
+    val vertices = nodes.map({ case (n, edges) => (n.toLong, maker.make()) })
+    val edges = nodes.flatMap({ case (n, edges) =>
+      edges.map(e => Edge(n.toLong, e.toLong, maker.make()))
+    })
+    return new SimpleGraphData(
+      target,
+      sc.parallelize(vertices),
+      sc.parallelize(edges)
+    )
+  }
+}
+
+class ConnectedComponentsTest extends FunSuite with TestBigGraphManager
+  with TestGraphDataManager {
+  // Creates the graph specified by `nodes` and applies ConnectedComponents to it.
+  // Returns the resulting component attributes in an easy-to-use format.
+  def getComponents(nodes: Seq[(Int, Seq[Int])], local: Boolean): Map[Int, Int] = {
+    val graphManager = cleanGraphManager("SetOverlapTest")
+    val dataManager = cleanDataManager("SetOverlapTest")
+    val inputGraph = graphManager.deriveGraph(Seq(), GraphByEdgeLists(nodes))
+    val limit = if (local) 100000 else 0
+    val outputGraph = graphManager.deriveGraph(
+      Seq(inputGraph), ConnectedComponents("component", limit))
+    val idx = outputGraph.vertexAttributes.readIndex[Long]("component")
+    val vertices = dataManager.obtainData(outputGraph).vertices
+    return vertices.map({ case (n, da) => (n.toInt, da(idx).toInt) }).collect.toMap
+  }
+
+  def assertSameComponents(comp1: Map[Int, Int], comp2: Map[Int, Int]): Unit = {
+    val mapping = scala.collection.mutable.Map[Int, Int]()
+    assert(comp1.size == comp2.size, "Unexpected size")
+    for (k <- comp1.keys) {
+      assert(comp2.contains(k), s"Missing key: $k")
+      val c1 = comp1(k)
+      val c2 = comp2(k)
+      if (mapping.contains(c1)) {
+        assert(mapping(c1) == c2, s"Unable to match components $c1 and $c2")
+      } else {
+        mapping(c1) = c2
+      }
+    }
+  }
+
+  test("three islands") {
+    val nodes = Seq(0 -> Seq(), 1 -> Seq(), 2 -> Seq())
+    val expectation = Map(0 -> 0, 1 -> 1, 2 -> 2)
+    assertSameComponents(getComponents(nodes, local = true), expectation)
+    assertSameComponents(getComponents(nodes, local = false), expectation)
+  }
+
+  test("triangle") {
+    val nodes = Seq(0 -> Seq(1, 2), 1 -> Seq(0, 2), 2 -> Seq(0, 1))
+    val expectation = Map(0 -> 0, 1 -> 0, 2 -> 0)
+    assertSameComponents(getComponents(nodes, local = true), expectation)
+    assertSameComponents(getComponents(nodes, local = false), expectation)
+  }
+
+  test("island and line") {
+    val nodes = Seq(0 -> Seq(), 1 -> Seq(2), 2 -> Seq(1))
+    val expectation = Map(0 -> 0, 1 -> 1, 2 -> 1)
+    assertSameComponents(getComponents(nodes, local = true), expectation)
+    assertSameComponents(getComponents(nodes, local = false), expectation)
+  }
+
+  test("long line") {
+    val nodes = Seq(0 -> Seq(1), 1 -> Seq(0, 2), 2 -> Seq(1, 3), 3 -> Seq(2))
+    val expectation = Map(0 -> 0, 1 -> 0, 2 -> 0, 3 -> 0)
+    assertSameComponents(getComponents(nodes, local = true), expectation)
+    assertSameComponents(getComponents(nodes, local = false), expectation)
+  }
+}
