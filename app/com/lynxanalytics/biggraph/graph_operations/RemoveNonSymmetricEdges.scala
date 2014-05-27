78793d20439f967556270e6b9f3ac0cab8b3fe66
Andras Nemeth <xxandreww@gmail.com>
2014-05-27 17:54:03 +0200

    Adds BigGraphProperties. Adds RemoveNonSymmetricEdges operation. Adds FE support for connected components, remove non-symmetric edges and set overlap.

---
+++ app/com/lynxanalytics/biggraph/graph_operations/RemoveNonSymmetricEdges.scala
@@ -0,0 +1,33 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import org.apache.spark
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+import org.apache.spark.graphx
+
+import com.lynxanalytics.biggraph.graph_api
+
+import graph_api._
+import graph_api.attributes._
+
+case class RemoveNonSymmetricEdges() extends GraphOperation {
+  def isSourceListValid(sources: Seq[BigGraph]) = (sources.size == 1)
+
+  def execute(target: BigGraph, manager: GraphDataManager): GraphData = {
+    val sc = manager.runtimeContext.sparkContext
+    val source = target.sources.head
+    val sourceData = manager.obtainData(source)
+    val bySource = sourceData.edges.map(e => (e.srcId, e)).groupByKey()
+    val byDest = sourceData.edges.map(e => (e.dstId, e.srcId)).groupByKey().mapValues(_.toSet)
+    val edges = bySource.join(byDest).flatMap { case (vertexId, (outEdges, inEdgeSources)) =>
+        outEdges.filter(outEdge => inEdgeSources.contains(outEdge.dstId))
+      }
+    return new SimpleGraphData(target, sc.union(sourceData.vertices), edges)
+  }
+
+  def vertexAttributes(sources: Seq[BigGraph]) = sources.head.edgeAttributes
+
+  def edgeAttributes(sources: Seq[BigGraph]) = sources.head.vertexAttributes
+
+  override def targetProperties(sources: Seq[BigGraph]) =
+    new BigGraphProperties(symmetricEdges = true)
+}
