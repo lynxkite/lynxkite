4f0e203ba7c6b6f4a995cf3c607dd41a1fa09dab
Andras Nemeth <xxandreww@gmail.com>
2014-05-27 17:54:22 +0200

    Adds BigGraphProperties. Adds RemoveNonSymmetricEdges operation. Adds FE support for connected components, remove non-symmetric edges and set overlap.

--- app/com/lynxanalytics/biggraph/graph_api/BigGraph.scala
+++ app/com/lynxanalytics/biggraph/graph_api/BigGraph.scala
@@ -32,10 +32,24 @@ class BigGraph private[graph_api] (val sources: Seq[BigGraph], val operation: Gr
   @transient lazy val edgeAttributes: AttributeSignature =
       operation.edgeAttributes(sources)
 
+  @transient lazy val properties: BigGraphProperties =
+      operation.targetProperties(sources)
+
   @transient lazy val toLongString: String = "[%s](%s)".format(
       operation.toString,
       sources.map(_.toLongString).mkString(","))
 }
+
+/*
+ * Class used to characterize certain special classes of BigGraphs.
+ *
+ * If a property below is true, it means the graph is guaranteed to have that propery. False
+ * does not mean that the graph is guaranteed to not have the property, it only means it is not
+ * known.
+ */
+case class BigGraphProperties(
+  // If there is an edge A->B then there is also an edge B->A.
+  symmetricEdges: Boolean = false)
 
 /*
  * Interface for a repository of BigGraph objects.
