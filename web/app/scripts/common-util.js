166af2014dbc38279653ac73a24bd26793f1201c
Daniel Darabos <darabos.daniel@gmail.com>
2014-06-03 18:39:39 +0200

    Move "utility" functions into utility "libraries".

---
+++ web/app/scripts/common-util.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var commonUtil = {
+  minmax: function(xs) {
+    var Inf = parseFloat('Infinity');
+    var min = Inf, max = -Inf;
+    for (var i = 0; i < xs.length; ++i) {
+      if (xs[i] < min) { min = xs[i]; }
+      if (xs[i] > max) { max = xs[i]; }
+    }
+    return {min: min, max: max, span: max - min};
+  },
+
+  normalize: function(x, minmax) {
+    return (x - minmax.min) / minmax.span - 0.5;
+  },
+};
