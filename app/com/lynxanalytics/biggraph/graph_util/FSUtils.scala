ecdc755ce17dfd01ea8c566eb0698e9df97bbd3c
Forevian <andras.barjak@gmail.com>
2014-06-03 12:23:17 +0200

    works with aws s3

--- app/com/lynxanalytics/biggraph/graph_util/FSUtils.scala
+++ app/com/lynxanalytics/biggraph/graph_util/FSUtils.scala
@@ -23,6 +23,27 @@ case class Filename(
   def open() = fs.open(path)
   def reader() = new BufferedReader(new InputStreamReader(open))
 
+  /**
+   * Read a text file from HDFS, a local file system (available on all nodes), or any
+   * Hadoop-supported file system URI, and return it as an RDD of Strings.
+   */
+  /*
+  def textFile(path: String, minSplits: Int = sc.defaultMinSplits): RDD[String] = {
+    hadoopFile(path, classOf[TextInputFormat], classOf[LongWritable], classOf[Text],
+      minSplits).map(pair => pair._2.toString)
+  }*/
+
+  // TODO: set default splits?
+  def loadAsTextFile(sc: spark.SparkContext): spark.rdd.RDD[String] = {
+    sc.newAPIHadoopFile(
+      filename,
+      kClass = classOf[hadoop.io.LongWritable],
+      vClass = classOf[hadoop.io.Text],
+      fClass = classOf[hadoop.mapreduce.lib.input.TextInputFormat],
+      conf = hadoopConfiguration)
+      .map(pair => pair._2.toString)
+  }
+
   def saveAsTextFile(lines: spark.rdd.RDD[String]): Unit = {
     // RDD.saveAsTextFile does not take a hadoop.conf.Configuration argument. So we struggle a bit.
     val hadoopLines = lines.map(x => (hadoop.io.NullWritable.get(), new hadoop.io.Text(x)))
@@ -31,8 +52,7 @@ case class Filename(
       keyClass = classOf[hadoop.io.NullWritable],
       valueClass = classOf[hadoop.io.Text],
       outputFormatClass = classOf[hadoop.mapred.TextOutputFormat[hadoop.io.NullWritable, hadoop.io.Text]],
-      conf = new hadoop.mapred.JobConf(hadoopConfiguration)
-    )
+      conf = new hadoop.mapred.JobConf(hadoopConfiguration))
   }
 
   def addSuffix(suffix: String): Filename = {
