5a7feefcce6d25f06129d61046b9e3d0422bb63f
Andras Nemeth <xxandreww@gmail.com>
2014-06-11 17:22:00 +0200

    Implements meta graph manager.

--- app/com/lynxanalytics/biggraph/graph_api/MetaGraphManagerImpl.scala
+++ app/com/lynxanalytics/biggraph/graph_api/MetaGraphManagerImpl.scala
@@ -8,6 +8,8 @@ import java.io.ObjectOutputStream
 import java.util.UUID
 import scala.collection.mutable
 
+import com.lynxanalytics.biggraph.bigGraphLogger
+
 class MetaGraphManagerImpl(val repositoryPath: String) extends MetaGraphManager {
   def apply(operationInstance: MetaGraphOperationInstance): Unit = {
     internalApply(operationInstance)
@@ -21,31 +23,52 @@ class MetaGraphManagerImpl(val repositoryPath: String) extends MetaGraphManager 
   def edgeAttribute(gUID: UUID): EdgeAttribute[_] =
     entities(gUID).asInstanceOf[EdgeAttribute[_]]
 
-  def incommingBundles(vertexSet: VertexSet): Seq[EdgeBundle] =
-    incommingBundlesMap.getOrElse(vertexSet.gUID, Seq())
-  def outgoingBundles(vertexSet: VertexSet): Seq[EdgeBundle]
+  def incomingBundles(vertexSet: VertexSet): Seq[EdgeBundle] =
+    incomingBundlesMap.getOrElse(vertexSet.gUID, Seq())
+  def outgoingBundles(vertexSet: VertexSet): Seq[EdgeBundle] =
     outgoingBundlesMap.getOrElse(vertexSet.gUID, Seq())
-  def attributes(vertexSet: VertexSet): Seq[VertexAttribute[_]]
-    vertexAttribute.getOrElse(vertexSet.gUID, Seq())
-  def attributes(edgeBundle: EdgeBundle): Seq[EdgeAttribute[_]]
-    edgeAttribute.getOrElse(vertexSet.gUID, Seq())
+  def attributes(vertexSet: VertexSet): Seq[VertexAttribute[_]] =
+    vertexAttributesMap.getOrElse(vertexSet.gUID, Seq())
+  def attributes(edgeBundle: EdgeBundle): Seq[EdgeAttribute[_]] =
+    edgeAttributesMap.getOrElse(edgeBundle.gUID, Seq())
 
-  def dependentOperations(component: MetaGraphComponent): Seq[MetaGraphOperationInstance] =
+  def dependentOperations(component: MetaGraphEntity): Seq[MetaGraphOperationInstance] =
     dependentOperationsMap.getOrElse(component.gUID, Seq())
 
   initializeFromDisk()
 
   private val entities = mutable.Map[UUID, MetaGraphEntity]()
 
-  private val outgoingBundlesMap = mutable.Map[UUID, mutable.Buffer[EdgeBundle]]
-  private val incommingBundlesMap = mutable.Map[UUID, mutable.Buffer[EdgeBundle]]
-  private val vertexAttributes = mutable.Map[UUID, mutable.Buffer[VertexAttribute[_]]]
-  private val edgeAttributes = mutable.Map[UUID, mutable.Buffer[EdgeAttribute[_]]]
+  private val outgoingBundlesMap = mutable.Map[UUID, mutable.Buffer[EdgeBundle]]()
+  private val incomingBundlesMap = mutable.Map[UUID, mutable.Buffer[EdgeBundle]]()
+  private val vertexAttributesMap = mutable.Map[UUID, mutable.Buffer[VertexAttribute[_]]]()
+  private val edgeAttributesMap = mutable.Map[UUID, mutable.Buffer[EdgeAttribute[_]]]()
 
   private val dependentOperationsMap =
     mutable.Map[UUID, mutable.Buffer[MetaGraphOperationInstance]]()
 
-  def internalApply(operationInstance: MetaGraphOperationInstance): Unit = ???
+  def internalApply(operationInstance: MetaGraphOperationInstance): Unit = {
+    operationInstance.outputs.all.values.foreach { entity =>
+      val gUID = entity.gUID
+      assert(
+        entities.contains(gUID),
+        "Fatal conflict %s <=> %s".format(entity, entities(gUID)))
+      entities(gUID) = entity
+    }
+    operationInstance.outputs.edgeBundles.values.foreach { eb =>
+      outgoingBundlesMap.getOrElseUpdate(eb.srcVertexSet.gUID, mutable.Buffer()) += eb
+      incomingBundlesMap.getOrElseUpdate(eb.dstVertexSet.gUID, mutable.Buffer()) += eb
+    }
+    operationInstance.outputs.vertexAttributes.values.foreach { va =>
+      vertexAttributesMap.getOrElseUpdate(va.vertexSet.gUID, mutable.Buffer()) += va
+    }
+    operationInstance.outputs.edgeAttributes.values.foreach { ea =>
+      edgeAttributesMap.getOrElseUpdate(ea.edgeBundle.gUID, mutable.Buffer()) += ea
+    }
+    operationInstance.inputs.all.values.foreach { entity =>
+      dependentOperationsMap.getOrElseUpdate(entity.gUID, mutable.Buffer()) += operationInstance
+    }
+  }
 
   private def saveInstanceToDisk(inst: MetaGraphOperationInstance): Unit = {
     val time = scala.compat.Platform.currentTime
@@ -59,8 +82,8 @@ class MetaGraphManagerImpl(val repositoryPath: String) extends MetaGraphManager 
 
   private def initializeFromDisk(): Unit = {
     val repo = new File(repositoryPath)
-    val operations = repo.list.filter(_.startsWith("save-")).sorted
-    snapshots.foreach{ fileName =>
+    val operationFileNames = repo.list.filter(_.startsWith("save-")).sorted
+    operationFileNames.foreach { fileName =>
       try {
         val file = new File(repo, fileName)
         val stream = new ObjectInputStream(new FileInputStream(file))
@@ -79,10 +102,23 @@ class MetaGraphManagerImpl(val repositoryPath: String) extends MetaGraphManager 
   }
 }
 
-private class SerializedOperation(operation: MetaGraphOperation,
-                                  inputs: Map[String, UUID]) extends Serializable = {
-  def toInstance(manager: MetaGraphManager): MetaGraphOperationInstance = ???
+private case class SerializedOperation(operation: MetaGraphOperation,
+                                       inputs: Map[Symbol, UUID]) extends Serializable {
+  def toInstance(manager: MetaGraphManager): MetaGraphOperationInstance = {
+    MetaGraphOperationInstance(
+      operation,
+      MetaDataSet(
+        operation.inputVertexSets.map(n => n -> manager.vertexSet(inputs(n))).toMap,
+        operation.inputEdgeBundles.keys.map(n => n -> manager.edgeBundle(inputs(n))).toMap,
+        operation.inputVertexAttributes.keys
+          .map(n => n -> manager.vertexAttribute(inputs(n))).toMap,
+        operation.inputEdgeAttributes.keys
+          .map(n => n -> manager.edgeAttribute(inputs(n))).toMap))
+  }
 }
-object SerializedOperation {
-  def apply(inst: MetaGraphOperationInstance): SerializedOperation = ???
+private object SerializedOperation {
+  def apply(inst: MetaGraphOperationInstance): SerializedOperation = {
+    SerializedOperation(inst.operation,
+      inst.inputs.all.mapValues(_.gUID))
+  }
 }
