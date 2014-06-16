32b39786e7a771ff77970041c58c37deca908a7d
Daniel Darabos <darabos.daniel@gmail.com>
2014-06-16 16:19:44 +0200

    Remove frontend code with dependencies on BigGraphController for the time being.

--- app/com/lynxanalytics/biggraph/controllers/PersistenceController.scala
+++ app/com/lynxanalytics/biggraph/controllers/PersistenceController.scala
@@ -6,10 +6,6 @@ import com.lynxanalytics.biggraph.serving
 case class SaveGraphRequest(id: String)
 
 class PersistenceController(enviroment: BigGraphEnvironment) {
-  def saveGraph(request: SaveGraphRequest): serving.Empty = {
-    val graph = BigGraphController.getBigGraphForId(request.id, enviroment)
-    enviroment.graphDataManager.saveDataToDisk(graph)
-    serving.Empty()
-  }
+  def saveGraph(request: SaveGraphRequest): serving.Empty = ???
 }
 
