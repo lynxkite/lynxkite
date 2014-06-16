bf87787df543aa2ee612051ff29894ab34eeac74
Daniel Darabos <darabos.daniel@gmail.com>
2014-06-16 14:17:14 +0200

    Let AddConstantEdgeAttribute work on cross-graph edges too.

--- app/com/lynxanalytics/biggraph/graph_operations/AddConstantEdgeAttribute.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/AddConstantEdgeAttribute.scala
@@ -7,7 +7,7 @@ import com.lynxanalytics.biggraph.graph_api._
 
 abstract class AddConstantEdgeAttribute[T] extends MetaGraphOperation {
   def signature = newSignature
-    .inputGraph('ignored, 'edges)
+    .inputEdgeBundle('edges, 'ignoredSrc -> 'ignoredDst)
     .outputEdgeAttribute[T]('attr, 'edges)
   implicit def tt: TypeTag[T]
 
