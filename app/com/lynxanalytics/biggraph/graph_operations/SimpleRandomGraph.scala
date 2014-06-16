de5da1c6e1b31ab93a11d0cd552fa1129a3d82ee
Forevian <andras.barjak@gmail.com>
2014-06-16 15:21:26 +0200

    Reimplement SimpleRandomGraph as CreateVertexSet & SimpleRandomEdgeBundle

--- app/com/lynxanalytics/biggraph/graph_operations/SimpleRandomGraph.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/SimpleRandomGraph.scala
@@ -5,6 +5,34 @@ import scala.util.Random
 
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_api.attributes.AttributeSignature
+import com.lynxanalytics.biggraph.spark_util._
+
+case class CreateVertexSet(size: Long) extends MetaGraphOperation {
+  def signature = newSignature
+    .outputVertexSet('vs)
+
+  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = {
+    val vertices: Seq[(ID, Unit)] = Seq.range[ID](0, size).map(x => (x, ()))
+    outputs.putVertexSet('vs, rc.sparkContext.parallelize(vertices))
+  }
+}
+
+case class SimpleRandomEdgeBundle(seed: Int, density: Float) extends MetaGraphOperation {
+  def signature = newSignature
+    .inputVertexSet('vsSrc)
+    .inputVertexSet('vsDst)
+    .outputEdgeBundle('es, 'vsSrc -> 'vsDst)
+
+  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = {
+    val rand = new Random(seed)
+    val edges = for {
+      src <- inputs.vertexSets('vsSrc).rdd
+      dst <- inputs.vertexSets('vsDst).rdd.collect
+      if rand.nextFloat < density
+    } yield Edge(src._1, dst._1)
+    outputs.putEdgeBundle('es, RDDUtils.fastNumbered(edges))
+  }
+}
 
 case class SimpleRandomGraph(size: Int, seed: Int, density: Float) extends GraphOperation {
   def isSourceListValid(sources: Seq[BigGraph]): Boolean = sources.isEmpty
