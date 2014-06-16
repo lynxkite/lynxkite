83a6436bfe92298c0ac73a1abb04264afd9bfe3b
Andras Nemeth <xxandreww@gmail.com>
2014-06-16 18:40:30 +0200

    Vertex export with toString works.

--- app/com/lynxanalytics/biggraph/graph_util/CSVExport.scala
+++ app/com/lynxanalytics/biggraph/graph_util/CSVExport.scala
@@ -2,6 +2,7 @@ package com.lynxanalytics.biggraph.graph_util
 
 import org.apache.commons.lang.StringEscapeUtils
 import org.apache.hadoop
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
 import org.apache.spark.graphx
 import org.apache.spark.rdd
 import scala.reflect.runtime.universe._
@@ -24,19 +25,37 @@ object CSVData {
 }
 
 object CSVExport {
-  def exportVertexAttributes(attributes: Seq[VertexAttribute],
+  def exportVertexAttributes(attributes: Seq[VertexAttribute[_]],
+                             attributeLabels: Seq[String],
                              dataManager: DataManager): CSVData = {
     assert(attributes.size > 0)
+    assert(attributes.size == attributeLabels.size)
     val vertexSet = attributes.head.vertexSet
-    assert(data.forall(_.vertexSet == vertexSet))
-    val readers = graphData.bigGraph.vertexAttributes.getReadersForOperation(CSVCellConverter)
+    assert(attributes.forall(_.vertexSet == vertexSet))
+    var indexedData: rdd.RDD[(Long, Seq[String])] =
+      dataManager.get(vertexSet).rdd.mapPartitions(it =>
+        it.map {
+          case (id, _) =>
+            (id, List(id.toString))
+        },
+        preservesPartitioning = true)
+    for (attribute <- attributes) {
+      indexedData = indexedData
+        .leftOuterJoin(dataManager.get(attribute).rdd)
+        .mapValues {
+          case (prev, current) =>
+            current match {
+              case Some(value) => prev.toString +: prev
+              case None => "" +: prev
+            }
+        }
+    }
+
     CSVData(
-      ("vertexId" +: graphData.bigGraph.vertexAttributes.attributeSeq).map(quoteString),
-      graphData.vertices.map {
-        case (id, attr) => id.toString +: readers.map(_.readFrom(attr))
-      })
+      ("vertexId" +: attributeLabels).map(quoteString),
+      indexedData.values)
   }
-  def exportEdges(graphData: GraphData): CSVData = {
+  /*  def exportEdges(graphData: GraphData): CSVData = {
     val readers = graphData.bigGraph.edgeAttributes.getReadersForOperation(CSVCellConverter)
     CSVData(
       ("srcVertexId" +: "dstVertexId" +: graphData.bigGraph.edgeAttributes.attributeSeq)
@@ -58,11 +77,11 @@ object CSVExport {
     val edgeCsvData = exportEdges(graphData)
     directoryPath.addPathElement("edge-header").createFromStrings(CSVData.lineToString(edgeCsvData.header))
     edgeCsvData.saveDataToDir(directoryPath.addPathElement("edge-data"))
-  }
+  }*/
 
   private def quoteString(s: String) = "\"" + StringEscapeUtils.escapeJava(s) + "\""
 
-  private object CSVCellConverter extends TypeDependentOperation[String] {
+  /*private object CSVCellConverter extends TypeDependentOperation[String] {
     def getReaderForIndex[S: TypeTag](idx: AttributeReadIndex[S]): AttributeReader[String] = {
       new ConvertedAttributeReader[S, String](
         idx,
@@ -74,5 +93,5 @@ object CSVExport {
           objectValue => objectValue.toString
         })
     }
-  }
+  }*/
 }
