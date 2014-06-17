e408ac530e5d408065865a7be3eb4afbe97b127c
Andras Nemeth <xxandreww@gmail.com>
2014-06-17 16:39:51 +0200

    Prettification

--- test/com/lynxanalytics/biggraph/graph_operations/FindMaxCliquesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/FindMaxCliquesTest.scala
@@ -7,24 +7,11 @@ import scala.collection.mutable
 import com.lynxanalytics.biggraph.TestSparkContext
 import com.lynxanalytics.biggraph.graph_api._
 
-object TestWizard extends TestSparkContext {
-  def rc = RuntimeContext(sparkContext, numAvailableCores = 1, availableCacheMemoryGB = 100.0)
-  def run(op: MetaGraphOperation, inputs: DataSet): DataSet = {
-    val outputs = new DataSetBuilder(MetaGraphOperationInstance(op, inputs.metaDataSet))
-    op.execute(inputs, outputs, rc)
-    return outputs.toDataSet
-  }
-}
-
 class FindMaxCliquesTest extends FunSuite with TestGraphOperation {
   test("triangle") {
     val helper = cleanHelper
     val (sgv, sge) = helper.smallGraph(Map(0 -> Seq(1, 2), 1 -> Seq(0, 2), 2 -> Seq(0, 1)))
-    val fmcOut = helper.apply(
-      FindMaxCliques(3),
-      Map(
-        'vsIn -> sgv,
-        'esIn -> sge))
+    val fmcOut = helper.apply(FindMaxCliques(3), Map('vsIn -> sgv, 'esIn -> sge))
     assert(helper.localData(fmcOut.vertexSets('cliques)).size == 1)
   }
 }
