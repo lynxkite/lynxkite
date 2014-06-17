569e51edede5995edbe758fe66d85a03f4151dd4
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-06-17 16:39:51 +0200

    Prettification

--- test/com/lynxanalytics/biggraph/graph_operations/AddConstantEdgeAttributeTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/AddConstantEdgeAttributeTest.scala
@@ -9,7 +9,7 @@ class AddConstantEdgeAttributeTest extends FunSuite with TestGraphOperation {
     val helper = cleanHelper
     val (vs, es) = helper.smallGraph(Map(0 -> Seq(1), 1 -> Seq(2), 2 -> Seq(0)))
     val out = helper.apply(AddConstantDoubleEdgeAttribute(100.0), Map('edges -> es))
-    assert(helper.localData(out.edgeAttributes('attr)) == Map(
-      (0l, 1l) -> 100.0, (1l, 2l) -> 100.0, (2l, 0l) -> 100.0))
+    assert(helper.localData(out.edgeAttributes('attr)) ==
+      Map((0l, 1l) -> 100.0, (1l, 2l) -> 100.0, (2l, 0l) -> 100.0))
   }
 }
