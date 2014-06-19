e39f24dc3b6eb43a69623cd0d90eb4f919a40ee4
Andras Nemeth <xxandreww@gmail.com>
2014-06-19 23:09:13 +0200

    Seems to work...

--- web/app/scripts/controllers/metaGraph.js
+++ web/app/scripts/controllers/metaGraph.js
@@ -157,22 +157,22 @@ angular.module('biggraph')
     $scope.left.setNewVS = setNewVS($scope.left);
     $scope.right.setNewVS = setNewVS($scope.right);
 
-    $scope.left.addEBToPath = function(eb, isReserved) {
-      $scope.state.leftToRightPath.unshift({eb: eb, isReserved: !isReserved});
+    $scope.left.addEBToPath = function(eb, pointsTowardsMySide) {
+      $scope.state.leftToRightPath.unshift({eb: eb, pointsLeft: pointsTowardsMySide});
     };
-    $scope.right.addEBToPath = function(eb, isReserved) {
-      $scope.state.leftToRightPath.push({eb: eb, isReserved: isReserved});
+    $scope.right.addEBToPath = function(eb, pointsTowardsMySide) {
+      $scope.state.leftToRightPath.push({eb: eb, pointsLeft: !pointsTowardsMySide});
     };
 
     function followEB(side) {
-      return function(eb, isReversed) {
+      return function(eb, pointsTowardsMySide) {
 	if ($scope.state.leftToRightPath !== undefined) {
-	  side.addEBToPath(eb, isReversed);
+	  side.addEBToPath(eb, pointsTowardsMySide);
 	}
-	if (isReversed) {
-	  side.setVS(eb.source.id);
-	} else {
+	if (pointsTowardsMySide) {
 	  side.setVS(eb.destination.id);
+	} else {
+	  side.setVS(eb.source.id);
 	}
       };
     }
@@ -180,10 +180,10 @@ angular.module('biggraph')
     $scope.right.followEB = followEB($scope.right);
 
     function showEB(side) {
-      return function(eb, isReversed) {
+      return function(eb, pointsTowardsMySide) {
 	$scope.state.leftToRightPath = [];
-	side.addEBToPath(eb, isReversed);
-	if (isReversed) {
+	side.addEBToPath(eb, pointsTowardsMySide);
+	if (pointsTowardsMySide) {
 	  side.other.setVS(eb.source.id);
 	} else {
 	  side.other.setVS(eb.destination.id);
@@ -196,7 +196,7 @@ angular.module('biggraph')
     $scope.cutPathLeft = function(idx) {
       $scope.state.leftToRightPath.splice(0, idx);
       var firstStep = $scope.state.leftToRightPath[0];
-      if (firstStep.isReversed) {
+      if (firstStep.pointsLeft) {
 	$scope.left.setVS(firstStep.eb.destination.id);
       } else {
 	$scope.left.setVS(firstStep.eb.source.id);
@@ -205,7 +205,7 @@ angular.module('biggraph')
     $scope.cutPathRight = function(idx) {
       $scope.state.leftToRightPath.splice(idx + 1);
       var lastStep = $scope.state.leftToRightPath[$scope.state.leftToRightPath.length - 1];
-      if (lastStep.isReversed) {
+      if (lastStep.pointsLeft) {
 	$scope.right.setVS(lastStep.eb.source.id);
       } else {
 	$scope.right.setVS(lastStep.eb.destination.id);
