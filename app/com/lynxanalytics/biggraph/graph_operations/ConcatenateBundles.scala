8317abc9d1afc52515bb2d744ce1d37d17eaae8b
Forevian <andras.barjak@gmail.com>
2014-06-20 13:05:19 +0200

    EdgeBundle concatenation implemented (TODO: test)

--- app/com/lynxanalytics/biggraph/graph_operations/ConcatenateBundles.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/ConcatenateBundles.scala
@@ -3,17 +3,41 @@ package com.lynxanalytics.biggraph.graph_operations
 import org.apache.spark.SparkContext.rddToPairRDDFunctions
 
 import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.spark_util.RDDUtils
 
-case class ConcatenateBundles() extends MetaGraphOperation {
+case class ConcatenateBundles(
+    concatWeightsFn: (Double, Double) => Double = { (a, b) => a * b })
+    extends MetaGraphOperation {
   def signature = newSignature
     .inputVertexSet('vsA)
     .inputVertexSet('vsB)
     .inputVertexSet('vsC)
     .inputEdgeBundle('edgesAB, 'vsA -> 'vsB)
     .inputEdgeAttribute[Double]('weightsAB, 'edgesAB)
-    .inputEdgeBundle('edgesBA, 'vsB -> 'vsC)
+    .inputEdgeBundle('edgesBC, 'vsB -> 'vsC)
     .inputEdgeAttribute[Double]('weightsBC, 'edgesBC)
     .outputEdgeBundle('edgesAC, 'vsA -> 'vsC)
     .outputEdgeAttribute[Double]('weightsAC, 'edgesAC)
-  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = ???
+  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = {
+    val edgesAB = inputs.edgeBundles('edgesAB).rdd
+    val edgesBC = inputs.edgeBundles('edgesBC).rdd
+    val weightsAB = inputs.edgeAttributes('weightsAB).runtimeSafeCast[Double].rdd
+    val weightsBC = inputs.edgeAttributes('weightsBC).runtimeSafeCast[Double].rdd
+    val weightedEdgesAB = edgesAB.join(weightsAB)
+    val weightedEdgesBC = edgesBC.join(weightsBC)
+
+    val partitioner = inputs.vertexSets('vsB).rdd.partitioner.getOrElse(rc.defaultPartitioner)
+    val BA = weightedEdgesAB.map { case (_, (edge, weight)) => edge.dst -> (edge.src, weight) }.partitionBy(partitioner)
+    val BC = weightedEdgesBC.map { case (_, (edge, weight)) => edge.src -> (edge.dst, weight) }.partitionBy(partitioner)
+
+    val AC = BA.join(BC).map {
+      case (_, ((edgeA, weightA), (edgeB, weightB))) => (Edge(edgeA, edgeB), concatWeightsFn(weightA, weightB))
+    }
+    val numberedAC = RDDUtils.fastNumbered(AC)
+
+    outputs.putEdgeBundle(
+      'edgesAC, numberedAC.map { case (eId, (edge, weight)) => eId -> edge })
+    outputs.putEdgeAttribute(
+      'weightsAC, numberedAC.map { case (eId, (edge, weight)) => eId -> weight })
+  }
 }
