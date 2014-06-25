8ff6f8e7e03ce27d03c1f1d888678867af2ef7df
Daniel Darabos <darabos.daniel@gmail.com>
2014-06-25 12:05:51 +0200

    CreateExampleGraphOperation -> ExampleGraph in UpperBoundFilterTest

--- test/com/lynxanalytics/biggraph/graph_operations/UpperBoundFilterTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/UpperBoundFilterTest.scala
@@ -5,12 +5,12 @@ import com.lynxanalytics.biggraph.graph_api._
 
 class UpperBoundFilterTest extends FunSuite with TestGraphOperation {
   test("vertex filtering works") {
-    val graph = helper.apply(CreateExampleGraphOperation())
+    val graph = helper.apply(ExampleGraph())
     val filtered = helper.apply(UpperBoundFilter(21), graph.mapNames('age -> 'attr, 'vertices -> 'vs))
     assert(helper.localData(filtered.vertexSets('fvs)) == Set(0L, 1L))
   }
   test("edge filtering works") {
-    val graph = helper.apply(CreateExampleGraphOperation())
+    val graph = helper.apply(ExampleGraph())
     val filtered = helper.apply(UpperBoundFilter(21), graph.mapNames('age -> 'attr, 'vertices -> 'vs))
     val joined = helper.apply(
       InducedEdgeBundle(),
