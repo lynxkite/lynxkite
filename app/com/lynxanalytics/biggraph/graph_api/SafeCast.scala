ff2e24c23bd4cf308d96407f073d6c93d9f535c3
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-06-25 12:15:38 +0200

    classTag method

--- app/com/lynxanalytics/biggraph/graph_api/SafeCast.scala
+++ app/com/lynxanalytics/biggraph/graph_api/SafeCast.scala
@@ -2,6 +2,7 @@ package com.lynxanalytics.biggraph.graph_api
 
 import scala.language.higherKinds
 import scala.reflect.runtime.universe._
+import scala.reflect.ClassTag
 
 trait RuntimeSafeCastable[T, ConcreteKind[T] <: RuntimeSafeCastable[T, ConcreteKind]] {
   implicit val typeTag: TypeTag[T]
@@ -10,6 +11,10 @@ trait RuntimeSafeCastable[T, ConcreteKind[T] <: RuntimeSafeCastable[T, ConcreteK
     if (typeOf[S] =:= typeOf[T]) {
       this.asInstanceOf[ConcreteKind[S]]
     } else throw new ClassCastException("Cannot cast from: %s to: %s".format(typeOf[S], typeOf[T]))
+  }
+
+  def classTag: ClassTag[T] = {
+    ClassTag[T](typeTag.mirror.runtimeClass(typeTag.tpe))
   }
 }
 
