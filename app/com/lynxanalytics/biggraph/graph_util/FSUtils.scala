594ee23b8f16a0788f41bdd200ad38e05afcc0e3
Forevian <andras.barjak@gmail.com>
2014-06-25 13:40:25 +0200

    log deletion of corrupt path

--- app/com/lynxanalytics/biggraph/graph_util/FSUtils.scala
+++ app/com/lynxanalytics/biggraph/graph_util/FSUtils.scala
@@ -80,7 +80,10 @@ case class Filename(
 
     val hadoopData = data.map(x =>
       (hadoop.io.NullWritable.get(), new hadoop.io.BytesWritable(RDDUtils.kryoSerialize(x))))
-    if (fs.exists(path)) fs.delete(path, true)
+    if (fs.exists(path)) {
+      bigGraphLogger.info(s"deleting $path as it already exists (possibly as a result of a failed stage)")
+      fs.delete(path, true)
+    }
     bigGraphLogger.info(s"saving ${data.name} as object file to $filename")
     hadoopData.saveAsNewAPIHadoopFile(
       filename,
