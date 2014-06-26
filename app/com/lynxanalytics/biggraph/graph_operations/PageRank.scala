c44d79cb8fe40128c6dcac40365fe8687ce706ea
Forevian <andras.barjak@gmail.com>
2014-06-26 20:59:29 +0200

    page rank implemented, not tested

--- app/com/lynxanalytics/biggraph/graph_operations/PageRank.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/PageRank.scala
@@ -1,11 +1,6 @@
 package com.lynxanalytics.biggraph.graph_operations
 
-import scala.reflect.runtime.universe._
-
-import org.apache.spark
 import org.apache.spark.SparkContext.rddToPairRDDFunctions
-import org.apache.spark.graphx.VertexId
-import org.apache.spark.rdd.RDD
 
 import com.lynxanalytics.biggraph.graph_api._
 
@@ -13,30 +8,32 @@ case class PageRank(weightAttribute: String,
                     outputAttribute: String,
                     dampingFactor: Double,
                     iterations: Int)
-    extends NewVertexAttributeOperation[Double] {
-  @transient lazy val tt = typeTag[Double]
+    extends MetaGraphOperation {
+  def signature = newSignature
+    .inputVertexSet('vs)
+    .inputEdgeBundle('es, 'vs -> 'vs)
+    .inputEdgeAttribute[Double]('weights, 'es)
+    .outputVertexAttribute[Double]('pagerank, 'vs)
 
-  override def isSourceListValid(sources: Seq[BigGraph]): Boolean =
-    super.isSourceListValid(sources) && sources.head.edgeAttributes.canRead[Double](weightAttribute)
-
-  override def computeHolistically(inputData: GraphData,
-                                   runtimeContext: RuntimeContext,
-                                   vertexPartitioner: spark.Partitioner): RDD[(VertexId, Double)] = {
-    val readIdx = inputData.bigGraph.edgeAttributes.readIndex[Double](weightAttribute)
-    val targetsWithWeights = inputData.edges
-      .map(e => (e.srcId, (e.dstId, e.attr(readIdx))))
+  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = {
+    val edges = inputs.edgeBundles('es).rdd
+    val weights = inputs.edgeAttributes('weights).runtimeSafeCast[Double].rdd
+    val vertices = inputs.vertexSets('vs).rdd
+    val vertexPartitioner = vertices.partitioner.get
+    val targetsWithWeights = edges.join(weights)
+      .map { case (_, (edge, weight)) => edge.src -> (edge.dst, weight) }
       .groupByKey(vertexPartitioner)
       .mapValues { it =>
         val dstW = it.toSeq
-        def sumWeights(s: Seq[(VertexId, Double)]) = s.map({ case (dst, w) => w }).sum
+        def sumWeights(s: Seq[(ID, Double)]) = s.map({ case (dst, w) => w }).sum
         val total = sumWeights(dstW)
         // Collapse parallel edges.
         val collapsed = dstW.groupBy({ case (dst, w) => dst }).mapValues(sumWeights(_))
         collapsed.mapValues(w => w / total).toArray
       }
 
-    var pageRank = inputData.vertices.mapValues(attr => 1.0).partitionBy(vertexPartitioner)
-    val vertexCount = inputData.vertices.count
+    var pageRank = vertices.mapValues(attr => 1.0)
+    val vertexCount = vertices.count
 
     for (i <- 0 until iterations) {
       val incomingRank = pageRank.join(targetsWithWeights)
@@ -55,6 +52,6 @@ case class PageRank(weightAttribute: String,
           case (oldRank, incoming) => distributedExtraWeight + incoming.getOrElse(0.0)
         }
     }
-    pageRank
+    outputs.putVertexAttribute('pageranks, pageRank)
   }
 }
