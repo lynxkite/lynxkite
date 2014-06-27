06c4c04e9bf63a5eab8b9c0eccc80ab560d3b7ed
Daniel Darabos <darabos.daniel@gmail.com>
2014-06-27 13:16:38 +0200

    Add count badges to sublists.

--- web/app/scripts/sublist.js
+++ web/app/scripts/sublist.js
@@ -9,6 +9,9 @@ angular.module('biggraph').directive('sublist', function () {
     templateUrl: 'sublist.html',
     link: function(scope, element, attrs, ctrl, transclude) {
       element.find('#contents').replaceWith(transclude());
+      scope.count = function() {
+        return element.find('#collapse').children('.list-group-item').length;
+      };
     },
   };
 });
