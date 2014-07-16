4a80189ccda8388315603c2d252bcf3b5389a659
Forevian <andras.barjak@gmail.com>
2014-07-16 19:52:59 +0200

    adjust fixed points of buckets

--- web/app/scripts/graph-view.js
+++ web/app/scripts/graph-view.js
@@ -27,7 +27,8 @@ angular.module('biggraph').directive('graphView', function($window) {
     this.edges = svg.create('g', {'class': 'edges'});
     this.vertices = svg.create('g', {'class': 'nodes'});
     this.root = svg.create('g', {'class': 'root'});
-    this.zoom = 500 * 0.67; // todo: replace 500 with the actual svg height
+    var graphToSVGRatio = 0.67;
+    this.zoom = 500 * graphToSVGRatio; // todo: replace 500 with the actual svg height
     this.root.append([this.edges, this.vertices]);
     this.svg.append(this.root);
   }
@@ -99,8 +100,19 @@ angular.module('biggraph').directive('graphView', function($window) {
     var i, x, y, l, side;
     var labelSpace = 50;
     y = yOff + this.zoom * 0.5 + labelSpace;
+    
+    var xb = util.minmax(data.vertices.map(function(n) { return n.x; }));
+    var yb = util.minmax(data.vertices.map(function(n) { return n.y; }));
+    
+    var xNumBuckets = xb.span + 1;
+    var yNumBuckets = yb.span + 1;
+    
     for (i = 0; i < data.xLabels.length; ++i) {
-      x = xOff + this.zoom * util.normalize(i, data.xLabels.length - 1);
+      if (data.xLabelType === 'between') {
+        x = xOff + this.zoom * util.normalize(i, xNumBuckets);
+      } else {
+        x = xOff + this.zoom * util.normalize(i + 0.5, xNumBuckets);
+      }
       l = new Label(x, y, data.xLabels[i]);
       xLabels.push(l);
       this.vertices.append(l.dom);
@@ -114,21 +126,21 @@ angular.module('biggraph').directive('graphView', function($window) {
       side = 'right';
     }
     for (i = 0; i < data.yLabels.length; ++i) {
-      y = yOff + this.zoom * util.normalize(i, data.yLabels.length - 1);
+      if (data.yLabelType === 'between') {
+        y = yOff + this.zoom * util.normalize(i, yNumBuckets);
+      } else {
+        y = yOff + this.zoom * util.normalize(i + 0.5, yNumBuckets);
+      }
       l = new Label(x, y, data.yLabels[i], side);
       yLabels.push(l);
       this.vertices.append(l.dom);
     }
-    // offset vertices by a half label step if label type is 'between'
-    var xBetween =
-      (data.xLabelType === 'between' && data.xLabels.length > 2) ? (this.zoom / (data.xLabels.length - 1)) / 2 : 0;
-    var yBetween =
-      (data.yLabelType === 'between' && data.yLabels.length > 2) ? (this.zoom / (data.yLabels.length - 1)) / 2 : 0;
+     
     var vertexScale = this.zoom * 2 / util.minmax(data.vertices.map(function(n) { return n.size; })).max;
     for (i = 0; i < data.vertices.length; ++i) {
-      var vertex = data.vertices[i];      
-      var v = new Vertex(xOff + xBetween + this.zoom * (vertex.x / (data.xLabels.length - 1) - 0.5),
-                         yOff + yBetween + this.zoom * (vertex.y / (data.yLabels.length - 1) - 0.5),
+      var vertex = data.vertices[i];
+      var v = new Vertex(xOff + this.zoom * util.normalize(vertex.x + 0.5, xNumBuckets),
+                         yOff + this.zoom * util.normalize(vertex.y + 0.5, yNumBuckets),
                          Math.sqrt(vertexScale * vertex.size),
                          vertex.size);
       vertices.push(v);
