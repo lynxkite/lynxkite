f5aed1c2f35e501065fd7e2e69d0621a599a4c0e
Forevian <andras.barjak@gmail.com>
2014-07-17 17:15:20 +0200

    ConcatenateBundles ported

--- app/com/lynxanalytics/biggraph/graph_operations/ConcatenateBundles.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/ConcatenateBundles.scala
@@ -5,26 +5,41 @@ import org.apache.spark.SparkContext.rddToPairRDDFunctions
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.RDDUtils.Implicit
 
-case class ConcatenateBundles() extends MetaGraphOperation {
-  def signature = newSignature
-    .inputVertexSet('vsA)
-    .inputVertexSet('vsB)
-    .inputVertexSet('vsC)
-    .inputEdgeBundle('edgesAB, 'vsA -> 'vsB)
-    .inputEdgeAttribute[Double]('weightsAB, 'edgesAB)
-    .inputEdgeBundle('edgesBC, 'vsB -> 'vsC)
-    .inputEdgeAttribute[Double]('weightsBC, 'edgesBC)
-    .outputEdgeBundle('edgesAC, 'vsA -> 'vsC)
-    .outputEdgeAttribute[Double]('weightsAC, 'edgesAC)
-  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = {
-    val edgesAB = inputs.edgeBundles('edgesAB).rdd
-    val edgesBC = inputs.edgeBundles('edgesBC).rdd
-    val weightsAB = inputs.edgeAttributes('weightsAB).runtimeSafeCast[Double].rdd
-    val weightsBC = inputs.edgeAttributes('weightsBC).runtimeSafeCast[Double].rdd
+object ConcatenateBundles {
+  class Input extends MagicInputSignature {
+    val vsA = vertexSet
+    val vsB = vertexSet
+    val vsC = vertexSet
+    val edgesAB = edgeBundle(vsA, vsB)
+    val edgesBC = edgeBundle(vsB, vsC)
+    val weightsAB = edgeAttribute[Double](edgesAB)
+    val weightsBC = edgeAttribute[Double](edgesBC)
+  }
+  class Output(implicit instance: MetaGraphOperationInstance,
+               inputs: Input) extends MagicOutput(instance) {
+    val edgesAC = edgeBundle(inputs.vsA.entity, inputs.vsC.entity)
+    val weightsAC = edgeAttribute[Double](edgesAC)
+  }
+}
+import ConcatenateBundles._
+case class ConcatenateBundles() extends TypedMetaGraphOp[Input, Output] {
+  @transient override lazy val inputs = new Input()
+
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val id = inputDatas
+    val edgesAB = inputs.edgesAB.rdd
+    val edgesBC = inputs.edgesBC.rdd
+    val weightsAB = inputs.weightsAB.rdd
+    val weightsBC = inputs.weightsBC.rdd
     val weightedEdgesAB = edgesAB.join(weightsAB)
     val weightedEdgesBC = edgesBC.join(weightsBC)
 
-    val partitioner = inputs.vertexSets('vsB).rdd.partitioner.getOrElse(rc.defaultPartitioner)
+    val partitioner = inputs.vsB.rdd.partitioner.getOrElse(rc.defaultPartitioner)
     val BA = weightedEdgesAB.map { case (_, (edge, weight)) => edge.dst -> (edge.src, weight) }.partitionBy(partitioner)
     val BC = weightedEdgesBC.map { case (_, (edge, weight)) => edge.src -> (edge.dst, weight) }.partitionBy(partitioner)
 
@@ -34,9 +49,7 @@ case class ConcatenateBundles() extends MetaGraphOperation {
 
     val numberedAC = AC.fastNumbered(rc.defaultPartitioner)
 
-    outputs.putEdgeBundle(
-      'edgesAC, numberedAC.mapValues { case (edge, weight) => edge })
-    outputs.putEdgeAttribute(
-      'weightsAC, numberedAC.mapValues { case (edge, weight) => weight })
+    output(o.edgesAC, numberedAC.mapValues { case (edge, weight) => edge })
+    output(o.weightsAC, numberedAC.mapValues { case (edge, weight) => weight })
   }
 }
