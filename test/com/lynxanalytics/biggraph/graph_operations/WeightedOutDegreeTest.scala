b699583ea6aae2b2b7ea2df466d4b7ccad670425
Daniel Darabos <darabos.daniel@gmail.com>
2014-07-21 15:21:44 +0200

    Convert WeightedOutDegree to type-safe API.

--- test/com/lynxanalytics/biggraph/graph_operations/WeightedOutDegreeTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/WeightedOutDegreeTest.scala
@@ -3,12 +3,14 @@ package com.lynxanalytics.biggraph.graph_operations
 import org.scalatest.FunSuite
 
 import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.Scripting._
 
-class WeightedOutDegreeTest extends FunSuite with TestGraphOperation {
+class WeightedOutDegreeTest extends FunSuite with TestGraphOp {
   test("example graph") {
-    val g = helper.apply(ExampleGraph())
-    val out = helper.apply(WeightedOutDegree(), 'weights -> g.edgeAttributes('weight))
-    assert(helper.localData(out.vertexAttributes('outdegrees)) ===
+    val g = ExampleGraph()().result
+    val op = WeightedOutDegree()
+    val out = op(op.attr, g.weight).result.outDegree
+    assert(out.rdd.collect.toMap ==
       Map(0 -> 1.0, 1 -> 2.0, 2 -> 7.0, 3 -> 0.0))
   }
 }
