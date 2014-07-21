de2ad139929ffb784a99976ec8d976ef1d4071e2
Forevian <andras.barjak@gmail.com>
2014-07-21 18:23:02 +0200

    VertexBucketGrid ported

--- app/com/lynxanalytics/biggraph/graph_operations/Bucketing.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/Bucketing.scala
@@ -5,48 +5,61 @@ import org.apache.spark.SparkContext.rddToPairRDDFunctions
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_util._
 
-case class VertexBucketGrid[S, T](xBucketer: Bucketer[S],
-                                  yBucketer: Bucketer[T]) extends MetaGraphOperation {
-  def signature = {
-    var sig = newSignature
-      .inputVertexSet('vertices)
-    if (xBucketer.numBuckets > 1) {
-      implicit val tt = xBucketer.tt
-      sig = sig.inputVertexAttribute[S]('xAttribute, 'vertices)
-    }
-    if (yBucketer.numBuckets > 1) {
-      implicit val tt = yBucketer.tt
-      sig = sig.inputVertexAttribute[T]('yAttribute, 'vertices)
-    }
-    sig
-      .outputScalar[Map[(Int, Int), Int]]('bucketSizes)
-      .outputVertexAttribute[Int]('xBuckets, 'vertices)
-      .outputVertexAttribute[Int]('yBuckets, 'vertices)
-      .outputVertexAttribute[Int]('feIdxs, 'vertices)
+object VertexBucketGrid {
+  class Input[S, T] extends MagicInputSignature {
+    val vertices = vertexSet
+    val xAttribute = vertexAttribute[S](vertices)
+    val yAttribute = vertexAttribute[T](vertices)
   }
+  class Output[S, T](implicit instance: MetaGraphOperationInstance,
+                     inputs: Input[S, T]) extends MagicOutput(instance) {
+    implicit val xtt = inputs.xAttribute.typeTag
+    implicit val ytt = inputs.yAttribute.typeTag
+    val bucketSizes = scalar[Map[(Int, Int), Int]]
+    val xBuckets = vertexAttribute[Int](inputs.vertices.entity)
+    val yBuckets = vertexAttribute[Int](inputs.vertices.entity)
+    val feIdxs = vertexAttribute[Int](inputs.vertices.entity)
+  }
+}
+import VertexBucketGrid._
+case class VertexBucketGrid[S, T](xBucketer: Bucketer[S],
+                                  yBucketer: Bucketer[T])
+    extends TypedMetaGraphOp[Input[S, T], Output[S, T]] {
 
-  def execute(inputs: DataSet, outputs: DataSetBuilder, rc: RuntimeContext): Unit = {
-    val vertices = inputs.vertexSets('vertices).rdd
+  @transient override lazy val inputs = new Input[S, T]
+
+  def outputMeta(instance: MetaGraphOperationInstance) =
+    new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output[S, T],
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val id = inputDatas
+    implicit val xtt = inputs.xAttribute.data.typeTag
+    implicit val xct = inputs.xAttribute.data.classTag
+    implicit val ytt = inputs.yAttribute.data.typeTag
+    implicit val yct = inputs.yAttribute.data.classTag
+    val vertices = inputs.vertices.rdd
     val xBuckets = if (xBucketer.numBuckets == 1) {
       vertices.mapValues(_ => 0)
     } else {
       implicit val tt = xBucketer.tt
-      val xAttr = inputs.vertexAttributes('xAttribute).runtimeSafeCast[S].rdd
+      val xAttr = inputs.xAttribute.rdd
       vertices.join(xAttr).mapValues { case (_, value) => xBucketer.whichBucket(value) }
     }
     val yBuckets = if (yBucketer.numBuckets == 1) {
       vertices.mapValues(_ => 0)
     } else {
       implicit val tt = yBucketer.tt
-      val yAttr = inputs.vertexAttributes('yAttribute).runtimeSafeCast[T].rdd
+      val yAttr = inputs.yAttribute.rdd
       vertices.join(yAttr).mapValues { case (_, value) => yBucketer.whichBucket(value) }
     }
-    outputs.putVertexAttribute('xBuckets, xBuckets)
-    outputs.putVertexAttribute('yBuckets, yBuckets)
-    outputs.putVertexAttribute(
-      'feIdxs,
+    output(o.xBuckets, xBuckets)
+    output(o.yBuckets, yBuckets)
+    output(o.feIdxs,
       xBuckets.join(yBuckets).mapValues { case (x, y) => x * yBucketer.numBuckets + y })
-    outputs.putScalar('bucketSizes,
+    output(o.bucketSizes,
       xBuckets.join(yBuckets)
         .map { case (vid, (xB, yB)) => ((xB, yB), 1) }
         .reduceByKey(_ + _)
