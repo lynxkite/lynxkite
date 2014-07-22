b8b34af74aecf7c9b1dcee1fccf81f8a632ddb10
Forevian <andras.barjak@gmail.com>
2014-07-22 19:11:23 +0200

    MaximalCliques ported

--- test/com/lynxanalytics/biggraph/graph_operations/FindMaxCliquesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/FindMaxCliquesTest.scala
@@ -1,16 +1,18 @@
 package com.lynxanalytics.biggraph.graph_operations
 
-import org.apache.spark.SparkContext
 import org.scalatest.FunSuite
-import scala.collection.mutable
 
-import com.lynxanalytics.biggraph.TestSparkContext
 import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.GraphTestUtils._
+import com.lynxanalytics.biggraph.graph_api.Scripting._
 
-class FindMaxCliquesTest extends FunSuite with TestGraphOperation {
+class FindMaxCliquesTest extends FunSuite with TestGraphOp {
   test("triangle") {
-    val (sgv, sge) = helper.smallGraph(Map(0 -> Seq(1, 2), 1 -> Seq(0, 2), 2 -> Seq(0, 1)))
-    val fmcOut = helper.apply(FindMaxCliques(3), 'vsIn -> sgv, 'esIn -> sge)
-    assert(helper.localData(fmcOut.vertexSets('cliques)).size == 1)
+    val g = SmallTestGraph(Map(0 -> Seq(1, 2), 1 -> Seq(0, 2), 2 -> Seq(0, 1))).result
+    val op = FindMaxCliques(3)
+    val fmcOut = op(op.vs, g.vs)(op.es, g.es).result
+    assert(fmcOut.cliques.rdd.count == 1)
   }
+
+  //TODO: some more creative tests
 }
