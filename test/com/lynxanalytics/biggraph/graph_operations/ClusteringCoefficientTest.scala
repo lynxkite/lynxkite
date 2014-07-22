d2ff8b838d1511d16f54b7851a30fb8aaf364439
Forevian <andras.barjak@gmail.com>
2014-07-22 13:32:57 +0200

    test updated

--- test/com/lynxanalytics/biggraph/graph_operations/ClusteringCoefficientTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/ClusteringCoefficientTest.scala
@@ -3,12 +3,13 @@ package com.lynxanalytics.biggraph.graph_operations
 import org.scalatest.FunSuite
 
 import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.Scripting._
 
-class ClusteringCoefficientTest extends FunSuite with TestGraphOperation {
+class ClusteringCoefficientTest extends FunSuite with TestGraphOp {
   test("example graph") {
-    val g = helper.apply(ExampleGraph())
-    val out = helper.apply(ClusteringCoefficient(), 'vs -> g.vertexSets('vertices), 'es -> g.edgeBundles('edges))
-    assert(helper.localData(out.vertexAttributes('clustering)) ===
-      Map(0 -> 0.5, 1 -> 0.5, 2 -> 1.0, 3 -> 1.0))
+    val g = ExampleGraph()().result
+    val op = ClusteringCoefficient()
+    val out = op(op.vs, g.vertices)(op.es, g.edges).result
+    assert(out.clustering.rdd.collect.toMap == Map(0 -> 0.5, 1 -> 0.5, 2 -> 1.0, 3 -> 1.0))
   }
 }
