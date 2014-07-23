1d14d44288ec41aba31a363125d85ae8b0a8a50b
Forevian <andras.barjak@gmail.com>
2014-07-23 16:24:03 +0200

    addressed code review

--- test/com/lynxanalytics/biggraph/graph_operations/UpperBoundFilterTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/UpperBoundFilterTest.scala
@@ -10,15 +10,16 @@ class UpperBoundFilterTest extends FunSuite with TestGraphOp {
 
   test("vertex and edge filtering works") {
     val op = UpperBoundFilter(21.0)
-    val filtered = op(op.attr, g.age)(op.vs, g.vertices).result
-    assert(filtered.fvs.toSet == Set(0L, 1L, 3L))
+    val f = op(op.attr, g.age)(op.vs, g.vertices).result
+    assert(f.fvs.toSet == Set(0L, 1L, 3L))
     val ind = InducedEdgeBundle()
-    val joined = (ind(ind.srcEdges, g.edges)(ind.src, g.vertices)(ind.dst, g.vertices)(ind.srcSubset, filtered.fvs.entity)(ind.dstSubset, filtered.fvs.entity)).result
+    val joined = ind(ind.edges, g.edges)(ind.srcSubset, f.fvs.entity)(ind.dstSubset, f.fvs.entity)
+      .result
     assert(joined.induced.toMap == Map(0L -> 1L, 1L -> 0L))
   }
   test("filtering on strings works") {
     val op = UpperBoundFilter("Bz")
-    val filtered = op(op.attr, g.name)(op.vs, g.vertices).result
-    assert(filtered.fvs.toSet == Set(0L, 2L))
+    val f = op(op.attr, g.name)(op.vs, g.vertices).result
+    assert(f.fvs.toSet == Set(0L, 2L))
   }
 }
