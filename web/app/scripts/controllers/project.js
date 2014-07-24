2f2ce475d08fc39318dc6cbcda4192b17b59044c
Daniel Darabos <darabos.daniel@gmail.com>
2014-07-24 14:56:23 +0200

    State management in project view. "vertexAttribute" and "histogramButton" directives.

--- web/app/scripts/controllers/project.js
+++ web/app/scripts/controllers/project.js
@@ -8,9 +8,59 @@ angular.module('biggraph')
       });
       return req;
     }
+
+    function defaultSideState() {
+      return {
+        vs: undefined,
+        filters: {},
+        graphMode: undefined,
+        bucketCount: 4,
+        sampleRadius: 1,
+        center: undefined,
+      };
+    }
+
+    function getState() {
+      return {
+        leftToRightPath: $scope.leftToRightPath,
+        left: $scope.left.state,
+        right: $scope.right.state,
+      };
+    }
+
     $scope.left = {};
     $scope.right = {};
-    $scope.left.data = get('/ajax/project', {project: $routeParams.project});
+    $scope.left.data = get('/ajax/project', { project: $routeParams.project });
+    $scope.left.state = defaultSideState();
+    $scope.right.state = defaultSideState();
+
+    deepWatch(
+      $scope,
+      function() { return $location.search(); },
+      function(search) {
+        if (!search.q) {
+          $scope.leftToRightPath = undefined;
+          $scope.left.state = defaultSideState();
+          $scope.right.state = defaultSideState();
+        } else {
+          var state = JSON.parse(search.q);
+          // The parts of the template that depend on 'state' get re-rendered
+          // when we replace it. So we only do this if there is an actual
+          // difference.
+          if (!angular.equals(state, getState())) {
+            $scope.leftToRightPath = state.leftToRightPath;
+            $scope.left.state = state.left;
+            $scope.right.state = state.right;
+          }
+        }
+      });
+
+    deepWatch(
+      $scope,
+      getState,
+      function(state) {
+        $location.search({ q: JSON.stringify(state) });
+      });
 
     var ISO = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
     $scope.human = function(x) {
