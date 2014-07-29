30c227c9638e5e14ef1419fc911cf2a2f0e964b6
Daniel Darabos <darabos.daniel@gmail.com>
2014-07-29 18:50:44 +0200

    Add CreateScalar operation.

---
+++ test/com/lynxanalytics/biggraph/graph_operations/CreateScalarTest.scala
@@ -0,0 +1,12 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import org.scalatest.FunSuite
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.Scripting._
+
+class CreateScalarTest extends FunSuite with TestGraphOp {
+  test("CreateStringScalar") {
+    val op = CreateStringScalar("hello world")
+    assert(op().result.value.value == "hello world")
+  }
+}
