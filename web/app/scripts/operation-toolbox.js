60f86195c9e766fddc00036c4cdbdfb5a897c8a4
Daniel Darabos <darabos.daniel@gmail.com>
2014-07-29 12:43:56 +0200

    Move operations to a separate Ajax call. Define categories on the backend.

--- web/app/scripts/operation-toolbox.js
+++ web/app/scripts/operation-toolbox.js
@@ -3,28 +3,27 @@
 angular.module('biggraph').directive('operationToolbox', function($resource) {
   return {
     restrict: 'E',
-    scope: { side: '=' },
+    scope: { ops: '=', side: '=' },
     replace: true,
     templateUrl: 'operation-toolbox.html',
     link: function(scope) {
       var ApplyOp = $resource('/ajax/applyOp');
-      scope.categories = [
-        { icon: 'A', color: 'yellow', title: 'Operations for attributes',
-          ops: scope.side.data.attributeOps },
-        { icon: 'E', color: 'orange', title: 'Operations for ' + scope.side.data.edgeNames,
-          ops: scope.side.data.edgeOps },
-        { icon: 'V', color: 'green', title: 'Operations for ' + scope.side.data.vertexNames,
-          ops: scope.side.data.vertexOps },
-        { icon: 'S', color: 'blue', title: 'Operations for segmentations',
-          ops: scope.side.data.segmentationOps },
-      ];
+      var colors = ['yellow', 'orange', 'green', 'blue'];
+      angular.deepWatch(scope, 'ops', function() {
+        scope.categories = [];
+        for (var i = 0; i < scope.ops.categories.length; ++i) {
+          var data = scope.ops.categories[i];
+          var cat = { title: data.title, ops: data.ops };
+          cat.icon = cat.title[0];
+          cat.color = colors[i];
+          scope.categories.push(cat);
+        }
+      });
       function close() {
         scope.active = undefined;
-        scope.ops = undefined;
       }
       function open(cat) {
         scope.active = cat;
-        scope.ops = cat.ops;
       }
       scope.clicked = function(cat) {
         if (scope.active === cat) {
