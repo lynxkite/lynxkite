2418100f4a03c58ef425f8775747af1f72bb7653
Daniel Darabos <darabos.daniel@gmail.com>
2014-07-30 12:00:47 +0200

    Pass TypeTag in CreateScalar explicitly.

--- app/com/lynxanalytics/biggraph/graph_operations/CreateScalar.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/CreateScalar.scala
@@ -4,16 +4,16 @@ import scala.reflect.runtime.universe._
 import com.lynxanalytics.biggraph.graph_api._
 
 object CreateScalar {
-  class Output[T: TypeTag](implicit instance: MetaGraphOperationInstance) extends MagicOutput(instance) {
+  class Output[T](implicit instance: MetaGraphOperationInstance, tt: TypeTag[T]) extends MagicOutput(instance) {
     val value = scalar[T]
   }
 }
 import CreateScalar._
 abstract class CreateScalar[T] extends TypedMetaGraphOp[NoInput, Output[T]] {
-  implicit def tt: TypeTag[T]
+  def tt: TypeTag[T]
   val value: T
   @transient override lazy val inputs = new NoInput()
-  def outputMeta(instance: MetaGraphOperationInstance) = { implicit val i = instance; new Output[T]() }
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output[T]()(instance, tt)
   def execute(inputDatas: DataSet,
               o: Output[T],
               output: OutputBuilder,
