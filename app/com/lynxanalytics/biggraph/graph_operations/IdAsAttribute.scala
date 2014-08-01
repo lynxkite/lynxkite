b7f62d2f367ad0ef0bc10cd93b4a3bd122b99e58
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-08-01 16:15:07 +0200

    Some missing files for sampled view

---
+++ app/com/lynxanalytics/biggraph/graph_operations/IdAsAttribute.scala
@@ -0,0 +1,29 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import com.lynxanalytics.biggraph.graph_api._
+
+object IdAsAttribute {
+  class Input extends MagicInputSignature {
+    val vertices = vertexSet
+  }
+  class Output(implicit instance: MetaGraphOperationInstance,
+               inputs: Input) extends MagicOutput(instance) {
+    val vertexIds = vertexAttribute[ID](inputs.vertices.entity)
+  }
+}
+import IdAsAttribute._
+case class IdAsAttribute() extends TypedMetaGraphOp[Input, Output] {
+
+  @transient override lazy val inputs = new Input
+
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet, o: Output, output: OutputBuilder, rc: RuntimeContext) = {
+    implicit val id = inputDatas
+    output(
+      o.vertexIds,
+      inputs.vertices.rdd.mapPartitions(
+        it => it.map { case (k, _) => (k, k) },
+        preservesPartitioning = true))
+  }
+}
