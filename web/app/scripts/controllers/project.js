60110305d676cec9e9e2ad2d76b6cdce7f3ab33f
Daniel Darabos <darabos.daniel@gmail.com>
2014-08-12 17:00:08 +0200

    Add project name to the state, reload project if it changes.

--- web/app/scripts/controllers/project.js
+++ web/app/scripts/controllers/project.js
@@ -4,6 +4,7 @@ angular.module('biggraph')
   .controller('ProjectViewCtrl', function ($scope, $routeParams, $resource, $location, util) {
     function defaultSideState() {
       return {
+        projectName: undefined,
         vs: undefined,
         filters: {},
         graphMode: undefined,
@@ -21,14 +22,25 @@ angular.module('biggraph')
       };
     }
 
-    $scope.left = {};
-    $scope.right = {};
-    $scope.left.reload = function() {
-      $scope.left.project = util.nocache('/ajax/project', { name: $routeParams.project });
+    function Side() {
+      this.state = defaultSideState();
+    }
+    Side.prototype.reload = function() {
+      if (this.state.projectName) {
+        this.project = util.nocache('/ajax/project', { name: this.state.projectName });
+      } else{
+        this.project = undefined;
+      }
     };
-    $scope.left.reload();
-    $scope.left.state = defaultSideState();
-    $scope.right.state = defaultSideState();
+    function clearState() {
+      $scope.leftToRightPath = undefined;
+      $scope.left = new Side();
+      $scope.right = new Side();
+      $scope.left.state.projectName = $routeParams.project;
+    }
+    clearState();
+    $scope.$watch('left.state.projectName', function() { $scope.left.reload(); });
+    $scope.$watch('right.state.projectName', function() { $scope.right.reload(); });
 
     function setSideSetting(side, setting, value) {
       if (side.state[setting] === value) {
@@ -46,12 +58,13 @@ angular.module('biggraph')
     };
 
     util.deepWatch($scope, 'left.project', function(project) {
+      if (project === undefined) { return; }
       // Put vertex set and edge bundle in the state.
       // This is for compatibility with the metaGraph.js-related code in graph-view.js
       // and could be removed later.
       $scope.left.state.vertexSet = { id: project.vertexSet };
       if (project.edgeBundle !== '') {
-       $scope.left.state.edgeBundle = { id: project.edgeBundle };
+        $scope.left.state.edgeBundle = { id: project.edgeBundle };
       } else {
         $scope.left.state.edgeBundle = undefined;
       }
@@ -62,9 +75,7 @@ angular.module('biggraph')
       function() { return $location.search(); },
       function(search) {
         if (!search.q) {
-          $scope.leftToRightPath = undefined;
-          $scope.left.state = defaultSideState();
-          $scope.right.state = defaultSideState();
+          clearState();
         } else {
           var state = JSON.parse(search.q);
           // The parts of the template that depend on 'state' get re-rendered
