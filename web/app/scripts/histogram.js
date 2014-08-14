a2df0e4fbdad85a58a637ebe05f6008f699f4c0d
Andras Nemeth <xxandreww@gmail.com>
2014-08-14 15:56:45 +0200

    Everything seems to work

--- web/app/scripts/histogram.js
+++ web/app/scripts/histogram.js
@@ -10,23 +10,23 @@ angular.module('biggraph').directive('histogram', function() {
       scope.$watch('model', function(model) {
         if (model === undefined || !model.$resolved) { return; }
         var max = 1;
-        for (var i = 0; i < model.vertices.length; ++i) {
-          var v = model.vertices[i];
-          if (v.size > max) { max = v.size; }
+        for (var i = 0; i < model.sizes.length; ++i) {
+          var s = model.sizes[i];
+          if (s > max) { max = s; }
         }
         scope.max = max;
-        if (model.xLabelType === 'between') {
+        if (model.labelType === 'between') {
           var histoLabels = [];
-          for (var j = 1; j < model.xLabels.length; ++j) {
-            histoLabels[j-1] = model.xLabels[j-1] + '-' + model.xLabels[j];
+          for (var j = 1; j < model.labels.length; ++j) {
+            histoLabels[j-1] = model.labels[j-1] + '-' + model.labels[j];
           }
           scope.histoLabels = histoLabels;
         } else {
-          scope.histoLabels = model.xLabels;
+          scope.histoLabels = model.labels;
         }
       }, true); // Watch contents of model.
-      scope.height = function(v) {
-        return Math.floor(100 * v.size / scope.max) + '%';
+      scope.height = function(s) {
+        return Math.floor(100 * s / scope.max) + '%';
       };
     },
   };
