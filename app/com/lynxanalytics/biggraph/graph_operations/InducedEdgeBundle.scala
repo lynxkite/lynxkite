a2fc9389961944d9e20708809d6ff454eeb86874
Daniel Darabos <darabos.daniel@gmail.com>
2014-08-14 15:28:54 +0200

    Allow InducedEdgeBundle to take a subset on one side only.

--- app/com/lynxanalytics/biggraph/graph_operations/InducedEdgeBundle.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/InducedEdgeBundle.scala
@@ -7,28 +7,32 @@ import com.lynxanalytics.biggraph.spark_util.Implicits._
 import com.lynxanalytics.biggraph.spark_util.RDDUtils
 
 object InducedEdgeBundle {
-  class Input extends MagicInputSignature {
+  class Input(induceSrc: Boolean, induceDst: Boolean) extends MagicInputSignature {
     val src = vertexSet
     val dst = vertexSet
-    val srcSubset = vertexSet
-    val dstSubset = vertexSet
+    val srcSubset = if (induceSrc) vertexSet else null
+    val dstSubset = if (induceDst) vertexSet else null
     // There is no fundamental reason this couldn't work with injections. We can implement that
     // if/when we need it.
-    val srcInjection = edgeBundle(srcSubset, src, EdgeBundleProperties.embedding)
-    val dstInjection = edgeBundle(dstSubset, dst, EdgeBundleProperties.embedding)
+    val srcInjection = if (induceSrc) edgeBundle(srcSubset, src, EdgeBundleProperties.embedding) else null
+    val dstInjection = if (induceDst) edgeBundle(dstSubset, dst, EdgeBundleProperties.embedding) else null
     val edges = edgeBundle(src, dst)
   }
-  class Output(implicit instance: MetaGraphOperationInstance,
-               inputs: Input) extends MagicOutput(instance) {
-    val induced = edgeBundle(inputs.srcSubset.entity, inputs.dstSubset.entity)
+  class Output(induceSrc: Boolean, induceDst: Boolean)(implicit instance: MetaGraphOperationInstance, inputs: Input) extends MagicOutput(instance) {
+    val induced = {
+      val src = if (induceSrc) inputs.srcSubset else inputs.src
+      val dst = if (induceDst) inputs.dstSubset else inputs.dst
+      edgeBundle(src.entity, dst.entity)
+    }
   }
 }
 import InducedEdgeBundle._
-case class InducedEdgeBundle() extends TypedMetaGraphOp[Input, Output] {
-  @transient override lazy val inputs = new Input
+case class InducedEdgeBundle(induceSrc: Boolean = true, induceDst: Boolean = true)
+    extends TypedMetaGraphOp[Input, Output] {
+  @transient override lazy val inputs = new Input(induceSrc, induceDst)
 
   def outputMeta(instance: MetaGraphOperationInstance) =
-    new Output()(instance, inputs)
+    new Output(induceSrc, induceDst)(instance, inputs)
 
   def execute(inputDatas: DataSet,
               o: Output,
@@ -36,19 +40,25 @@ case class InducedEdgeBundle() extends TypedMetaGraphOp[Input, Output] {
               rc: RuntimeContext): Unit = {
     implicit val id = inputDatas
     val edges = inputs.edges.rdd
-    val srcSubset = inputs.srcSubset.rdd
-    val dstSubset = inputs.dstSubset.rdd
-    val bySrc = edges
-      .map { case (id, edge) => (edge.src, (id, edge)) }
-      .toSortedRDD(srcSubset.partitioner.get)
-      .sortedJoin(srcSubset)
-      .mapValues { case (idEdge, _) => idEdge }
-    val byDst = bySrc
-      .map { case (vid, (id, edge)) => (edge.dst, (id, edge)) }
-      .toSortedRDD(dstSubset.partitioner.get)
-      .sortedJoin(dstSubset)
-      .mapValues { case (idEdge, _) => idEdge }
-    output(o.induced, byDst.values.toSortedRDD(edges.partitioner.get))
+    val srcInduced = if (!induceSrc) edges else {
+      val srcSubset = inputs.srcSubset.rdd
+      val bySrc = edges
+        .map { case (id, edge) => (edge.src, (id, edge)) }
+        .toSortedRDD(srcSubset.partitioner.get)
+        .sortedJoin(srcSubset)
+        .mapValues { case (idEdge, _) => idEdge }
+      bySrc.values
+    }
+    val dstInduced = if (!induceDst) srcInduced else {
+      val dstSubset = inputs.dstSubset.rdd
+      val byDst = srcInduced
+        .map { case (id, edge) => (edge.dst, (id, edge)) }
+        .toSortedRDD(dstSubset.partitioner.get)
+        .sortedJoin(dstSubset)
+        .mapValues { case (idEdge, _) => idEdge }
+      byDst.values
+    }
+    output(o.induced, dstInduced.toSortedRDD(edges.partitioner.get))
   }
 
   override val isHeavy = true
---
+++ test/com/lynxanalytics/biggraph/graph_operations/InducedEdgeBundleTest.scala
@@ -0,0 +1,42 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import org.scalatest.FunSuite
+
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.GraphTestUtils._
+import com.lynxanalytics.biggraph.graph_api.Scripting._
+
+class InducedEdgeBundleTest extends FunSuite with TestGraphOp {
+  val example = ExampleGraph()().result
+  val adamless = {
+    val op = VertexAttributeFilter(new Filter[String] {
+      def matches(name: String) = name != "Adam"
+    })
+    op(op.attr, example.name).result
+  }
+
+  test("example graph induce src & dst") {
+    val op = InducedEdgeBundle()
+    val induced = op(
+      op.edges, example.edges)(
+        op.srcInjection, adamless.identity)(
+          op.dstInjection, adamless.identity).result.induced.toPairSet
+    assert(induced == Set(2 -> 1))
+  }
+
+  test("example graph induce src") {
+    val op = InducedEdgeBundle(induceDst = false)
+    val induced = op(
+      op.edges, example.edges)(
+        op.srcInjection, adamless.identity).result.induced.toPairSet
+    assert(induced == Set(2 -> 1, 1 -> 0, 2 -> 0))
+  }
+
+  test("example graph induce dst") {
+    val op = InducedEdgeBundle(induceSrc = false)
+    val induced = op(
+      op.edges, example.edges)(
+        op.dstInjection, adamless.identity).result.induced.toPairSet
+    assert(induced == Set(2 -> 1, 0 -> 1))
+  }
+}
