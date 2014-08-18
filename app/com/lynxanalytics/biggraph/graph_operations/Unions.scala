f897ca8f6bb219687c6622c066bd6ed728e64b9a
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-08-18 14:34:52 +0200

    Implements vertex set union

---
+++ app/com/lynxanalytics/biggraph/graph_operations/Unions.scala
@@ -0,0 +1,58 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+import org.apache.spark.rdd.RDD
+import scala.reflect.runtime.universe._
+
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.spark_util.Implicits._
+import com.lynxanalytics.biggraph.spark_util.SortedRDD
+
+object VertexSetUnion {
+  class Input(numVertexSets: Int) extends MagicInputSignature {
+    val vss = Range(0, numVertexSets).map {
+      i => vertexSet(Symbol("vs" + i))
+    }.toList
+  }
+  class Output(numVertexSets: Int)(
+      implicit instance: MetaGraphOperationInstance,
+      input: Input) extends MagicOutput(instance) {
+
+    val union = vertexSet
+    // An embedding of the intersection into the first vertex set.
+    val injections = Range(0, numVertexSets)
+      .map(i => edgeBundle(
+        input.vss(i).entity, union, EdgeBundleProperties.injection, Symbol("injection" + i)))
+  }
+}
+case class VertexSetUnion(numVertexSets: Int)
+    extends TypedMetaGraphOp[VertexSetUnion.Input, VertexSetUnion.Output] {
+
+  import VertexSetUnion._
+  assert(numVertexSets >= 1)
+  @transient override lazy val inputs = new Input(numVertexSets)
+
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output(numVertexSets)(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val id = inputDatas
+    val unionWithOldIds = inputs.vss
+      .map(_.rdd)
+      .zipWithIndex
+      .map { case (rdd, idx) => rdd.mapValues(_ => idx) }
+      .reduce[RDD[(ID, Int)]]((rdd1, rdd2) => rdd1 ++ rdd2)
+      .randomNumbered(rc.defaultPartitioner)
+    output(o.union, unionWithOldIds.mapValues(_ => ()))
+    for (idx <- 0 until numVertexSets) {
+      output(
+        o.injections(idx),
+        unionWithOldIds
+          .filter { case (newId, (oldId, sourceIdx)) => sourceIdx == idx }
+          .map { case (newId, (oldId, sourceIdx)) => Edge(oldId, newId) }
+          .randomNumbered(rc.defaultPartitioner))
+    }
+  }
+}
