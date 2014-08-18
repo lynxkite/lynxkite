825a8a86ba5174804a1c3c8f16853c2bc879c86a
Daniel Darabos <darabos.daniel@gmail.com>
2014-08-18 12:10:20 +0200

    Create String/Double vertex attribute based on detected JS type.

--- test/com/lynxanalytics/biggraph/controllers/OperationsTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/OperationsTest.scala
@@ -12,11 +12,19 @@ class OperationsTest extends FunSuite with TestGraphOp with BigGraphEnvironment 
   def run(op: String, params: Map[String, String] = Map()) =
     ops.apply(ProjectOperationRequest("project", FEOperationSpec(op.replace(" ", "-"), params)))
 
-  test("Derived vertex attribute") {
+  test("Derived vertex attribute (Double)") {
     run("Example Graph")
     run("Derived vertex attribute",
-      Map("output" -> "output", "expr" -> "100 + age + 10 * name.length", "type" -> "Number"))
+      Map("output" -> "output", "expr" -> "100 + age + 10 * name.length"))
     val attr = project.vertexAttributes("output").runtimeSafeCast[Double]
     assert(attr.rdd.collect.toMap == Map(0 -> 160.3, 1 -> 148.2, 2 -> 180.3, 3 -> 222.0))
+  }
+
+  test("Derived vertex attribute (String)") {
+    run("Example Graph")
+    run("Derived vertex attribute",
+      Map("output" -> "output", "expr" -> "gender == 'Male' ? 'Mr ' + name : 'Ms ' + name"))
+    val attr = project.vertexAttributes("output").runtimeSafeCast[String]
+    assert(attr.rdd.collect.toMap == Map(0 -> "Mr Adam", 1 -> "Ms Eve", 2 -> "Mr Bob", 3 -> "Mr Isolated Joe"))
   }
 }
