e7abf590d8ed0d2992077a1c1fc70f488191c3bf
Daniel Darabos <darabos.daniel@gmail.com>
2014-08-19 17:35:47 +0200

    Mode and Majority LocalAggregators.

--- app/com/lynxanalytics/biggraph/controllers/Operations.scala
+++ app/com/lynxanalytics/biggraph/controllers/Operations.scala
@@ -564,9 +564,11 @@ class Operations(env: BigGraphEnvironment) extends OperationRepository(env) {
     attrs.toSeq.map {
       case (name, attr) =>
         val options = if (attr.is[Double]) {
-          UIValue.seq(Seq("ignore", "sum", "average", "first", "count"))
+          UIValue.seq(Seq("ignore", "sum", "average", "mode", "count"))
+        } else if (attr.is[String]) {
+          UIValue.seq(Seq("ignore", "mode", "majority-50", "majority-100", "count"))
         } else {
-          UIValue.seq(Seq("ignore", "first", "count"))
+          UIValue.seq(Seq("ignore", "mode", "count"))
         }
         Param(s"aggregate-$name", name, options = options)
     }
@@ -574,10 +576,12 @@ class Operations(env: BigGraphEnvironment) extends OperationRepository(env) {
   // Converts the string identifiers used by aggregateParams to Aggregator objects.
   def aggregate[T](connection: EdgeBundle, attr: VertexAttribute[T], choice: String): VertexAttribute[_] = {
     choice match {
-      case "first" => aggregate(connection, attr, graph_operations.Aggregator.First[T]())
       case "sum" => aggregate(connection, attr.runtimeSafeCast[Double], graph_operations.Aggregator.Sum())
       case "count" => aggregate(connection, attr, graph_operations.Aggregator.Count[T]())
       case "average" => aggregate(connection, attr.runtimeSafeCast[Double], graph_operations.Aggregator.Average())
+      case "mode" => aggregate(connection, attr, graph_operations.Aggregator.Mode[T]())
+      case "majority-50" => aggregate(connection, attr.runtimeSafeCast[String], graph_operations.Aggregator.Majority(0.5))
+      case "majority-100" => aggregate(connection, attr.runtimeSafeCast[String], graph_operations.Aggregator.Majority(1.0))
     }
   }
   // Performs AggregateByEdgeBundle.
