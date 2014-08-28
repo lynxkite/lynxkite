d523e2d1a08224eeb0d2e7af61aef92d9c2629a1
Daniel Darabos <darabos.daniel@gmail.com>
2014-08-28 18:04:05 +0200

    tools/alpha improvements.

---
+++ tools/alpha/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xue
+
+DIR=$(dirname $0)
+source $DIR/settings.sh
+if [ "${CREDENTIALS:-}" = "" ]; then
+  echo "CREDENTIALS has to be set to <key name>:<secret key>"
+  exit 1
+fi
+
+echo "Staging..."
+REMOTE_HOST=ec2-user@$ELASTIC_IP $DIR/../../remote_stage.sh -i "$SSH_KEY"
+
+echo "Starting..."
+ssh \
+  -i "$SSH_KEY" \
+  -o UserKnownHostsFile=/dev/null \
+  -o CheckHostIP=no \
+  -o StrictHostKeyChecking=no \
+  -t -t \
+  ec2-user@$ELASTIC_IP \
+  biggraphstage/alpha/setup.sh $CREDENTIALS
