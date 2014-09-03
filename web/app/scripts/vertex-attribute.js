b2b9c81e6d864e65b12a20d713933f4e8ecb18e5
Daniel Darabos <darabos.daniel@gmail.com>
2014-09-03 18:00:33 +0200

    Restrict bucketing and filtering to Double and String.

--- web/app/scripts/vertex-attribute.js
+++ web/app/scripts/vertex-attribute.js
@@ -4,5 +4,10 @@ angular.module('biggraph').directive('vertexAttribute', function() {
   return {
     scope: { attr: '=vertexAttribute', side: '=' },
     templateUrl: 'vertex-attribute.html',
+    link: function(scope) {
+      var type = scope.attr.typeName;
+      scope.canBucket = type === 'Double' || type === 'String';
+      scope.canFilter = type === 'Double' || type === 'String';
+    }
   };
 });
