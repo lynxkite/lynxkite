45e5e32fe0e06eb532bf9b632f1237a53a566b03
Daniel Darabos <darabos.daniel@gmail.com>
2014-09-05 16:06:02 +0200

    Weighted aggregation now works.

---
+++ app/com/lynxanalytics/biggraph/graph_operations/JoinAttribute.scala
@@ -0,0 +1,35 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import scala.reflect.runtime.universe._
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+
+import com.lynxanalytics.biggraph.graph_api._
+
+object JoinAttributes {
+  class Input[A, B] extends MagicInputSignature {
+    val vs = vertexSet
+    val a = vertexAttribute[A](vs)
+    val b = vertexAttribute[B](vs)
+  }
+  class Output[A, B](implicit instance: MetaGraphOperationInstance,
+                     inputs: Input[A, B])
+      extends MagicOutput(instance) {
+    implicit val tta = inputs.a.typeTag
+    implicit val ttb = inputs.b.typeTag
+    val attr = vertexAttribute[(A, B)](inputs.vs.entity)
+  }
+}
+import JoinAttributes._
+case class JoinAttributes[A, B]()
+    extends TypedMetaGraphOp[Input[A, B], Output[A, B]] {
+  @transient override lazy val inputs = new Input[A, B]
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output[A, B],
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val id = inputDatas
+    output(o.attr, inputs.a.rdd.sortedJoin(inputs.b.rdd))
+  }
+}
