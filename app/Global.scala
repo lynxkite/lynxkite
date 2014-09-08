589a4dac2c4669902ae25fa5779aa08a48efdbd0
Daniel Darabos <darabos.daniel@gmail.com>
2014-09-08 19:14:14 +0200

    Minimal error reporting.

---
+++ app/Global.scala
@@ -0,0 +1,21 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+
+object Global extends GlobalSettings {
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    concurrent.Future.successful(BadRequest(error))
+  }
+  override def onError(request: RequestHeader, throwable: Throwable) = {
+    concurrent.Future.successful(InternalServerError(format(throwable)))
+  }
+  override def onHandlerNotFound(request: RequestHeader) = {
+    concurrent.Future.successful(NotFound(request.toString))
+  }
+  private def rootCause(t: Throwable): Throwable = Option(t.getCause).map(rootCause(_)).getOrElse(t)
+  private val assertionFailed = "assertion failed: (.*)".r
+  private def format(t: Throwable): String = rootCause(t).getMessage match {
+    case assertionFailed(e) => e
+    case e => e
+  }
+}
