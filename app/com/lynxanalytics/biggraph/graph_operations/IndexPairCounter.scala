ea4e832185fb5c4b47b366177dc9e14030ad413d
Andras Nemeth <xxandreww@gmail.com>
2014-09-11 12:35:00 +0200

    Adds some comments on index related operations

--- app/com/lynxanalytics/biggraph/graph_operations/IndexPairCounter.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/IndexPairCounter.scala
@@ -8,6 +8,14 @@ import com.lynxanalytics.biggraph.graph_util._
 import com.lynxanalytics.biggraph.spark_util.Implicits._
 import com.lynxanalytics.biggraph.spark_util.RDDUtils
 
+/* Give two integer index attributes, this operation counts the number of occurances of each
+ * possible pairs of index values.
+ *
+ * This is used (at least) to count edges diagrams: for each edge, the first index is the node
+ * index in the source vertex diagram and the second index is the node index in the destination
+ * vertex diagram. So in this case this operation in effect computes how many edges go between two
+ * nodes in two vertex diagrams.
+ */
 object IndexPairCounter {
   class Input extends MagicInputSignature {
     val original = vertexSet
