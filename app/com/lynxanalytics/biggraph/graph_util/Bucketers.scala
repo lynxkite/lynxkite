977f16e78a2a16493c997df99eb9245a051ac0f3
Daniel Darabos <darabos.daniel@gmail.com>
2014-09-19 16:14:00 +0200

    Only worry about number formatting for DoubleBucketer.

--- app/com/lynxanalytics/biggraph/graph_util/Bucketers.scala
+++ app/com/lynxanalytics/biggraph/graph_util/Bucketers.scala
@@ -51,22 +51,9 @@ abstract class NumericBucketer[T: Numeric](
   @transient lazy val bounds: Seq[T] =
     (1 until nb).map(idx => min + num.fromInt(idx) * bucketSize)
 
-  def fmt(num: T, decimals: Int): String =
-    s"%.${decimals}f".format(num)
-  def fmt(nums: Seq[T], decimals: Int): Seq[String] =
-    nums.map(fmt(_, decimals))
   def bucketLabels: Seq[String] = {
-    val labels = min +: bounds :+ max
-    if (min == max) {
-      fmt(labels, 2)
-    } else {
-      val maxDecimals = 10
-      // Use enough decimals that all the labels are different.
-      val decimals = (0 to maxDecimals).find { decimals =>
-        fmt(labels, decimals).toSet.size == labels.size
-      }
-      fmt(labels, decimals.getOrElse(2))
-    }
+    var labels = min +: bounds :+ max
+    labels.map(_.toString)
   }
 }
 
@@ -82,10 +69,30 @@ case class StringBucketer(options: Seq[String], hasOther: Boolean)
     extends EnumBucketer[String](options, hasOther) {
   val labelType = "bucket"
 }
+
 case class DoubleBucketer(min: Double, max: Double, numBuckets: Int)
     extends FractionalBucketer[Double](min, max, numBuckets) {
   val labelType = "between"
+
+  def fmt(num: Double, decimals: Int): String =
+    s"%.${decimals}f".format(num)
+  def fmt(nums: Seq[Double], decimals: Int): Seq[String] =
+    nums.map(fmt(_, decimals))
+  override def bucketLabels: Seq[String] = {
+    val labels = min +: bounds :+ max
+    if (min == max) {
+      fmt(labels, 2)
+    } else {
+      val maxDecimals = 10
+      // Use enough decimals that all the labels are different.
+      val decimals = (0 to maxDecimals).find { decimals =>
+        fmt(labels, decimals).toSet.size == labels.size
+      }
+      fmt(labels, decimals.getOrElse(2))
+    }
+  }
 }
+
 case class LongBucketer(min: Long, max: Long, numBuckets: Int)
     extends NumericBucketer[Long](min, max, numBuckets) {
   val labelType = if ((max - min) / numBuckets == 0) "bucket" else "between"
