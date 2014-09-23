9603bf5fc6d6f60c0bfebb2921a62c7654f47286
Daniel Darabos <darabos.daniel@gmail.com>
2014-09-23 18:04:59 +0200

    Make sure icons.svg gets copied into web/dist.

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -292,7 +292,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,svg,txt}',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
