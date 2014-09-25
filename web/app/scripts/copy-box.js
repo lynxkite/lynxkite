ac0a6b6dbada5e0998fc0a0ea85be26161b53a2d
Daniel Darabos <darabos.daniel@gmail.com>
2014-09-25 13:28:22 +0200

    Make sure ZeroClipboard works in production deployment too.

--- web/app/scripts/copy-box.js
+++ web/app/scripts/copy-box.js
@@ -7,6 +7,7 @@ angular.module('biggraph').directive('copyBox', function() {
     scope: { data: '@', description: '@' },
     templateUrl: 'copy-box.html',
     link: function(scope, element) {
+      ZeroClipboard.config({ swfPath: 'bower_components/zeroclipboard/dist/ZeroClipboard.swf' });
       var client = new ZeroClipboard(element.find('.clicky'));
       client.on('error', function() {
         // No flash, no copy.
