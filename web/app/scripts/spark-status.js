45a81ca0fe6d4a054b7c10888b0d982155713abd
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-06 19:54:55 +0200

    Delayed retry for /ajax/spark-status on error.

--- web/app/scripts/spark-status.js
+++ web/app/scripts/spark-status.js
@@ -1,26 +1,30 @@
 'use strict';
 
-angular.module('biggraph').directive('sparkStatus', function(util) {
+angular.module('biggraph').directive('sparkStatus', function($timeout, util) {
   return {
     restrict: 'E',
     scope: {},
     templateUrl: 'spark-status.html',
     link: function(scope) {
-      scope.status = util.nocache('/ajax/spark-status', { timestamp: 0 });
-      scope.$watch('status', repeat);
-      scope.$watch('status.$resolved', repeat);
-      function repeat() {
-        if (scope.status && scope.status.$resolved) {
-          scope.update = util.nocache('/ajax/spark-status', { timestamp: scope.status.timestamp });
-        }
+      scope.status = { timestamp: 0 };
+      load();
+      function load() {
+        scope.update = util.nocache('/ajax/spark-status', { timestamp: scope.status.timestamp });
       }
       scope.$watch('update', update);
       scope.$watch('update.$resolved', update);
       function update() {
         if (scope.update && scope.update.$resolved) {
-          scope.status = scope.update;
+          // -1 is used for the timestamp in Grunt testing. We throttle in this case.
+          if (scope.update.error && scope.update.timestamp === -1) {
+            $timeout(load, 1000);  // Try again in a second.
+          } else {
+            scope.status = scope.update;
+            load();
+          }
         }
       }
+
       scope.kill = function() {
         util.nocache('/ajax/spark-cancel-jobs');
       };
