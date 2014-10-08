963a6115f80307615e20f07bac7f460d98bdbd6a
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-08 16:50:31 +0200

    Remove leftover code.

--- web/app/scripts/scalar.js
+++ web/app/scripts/scalar.js
@@ -1,16 +1,8 @@
 'use strict';
 
-angular.module('biggraph').directive('scalar', function($timeout) {
+angular.module('biggraph').directive('scalar', function() {
   return {
     scope: { scalar: '=', value: '@', side: '=' },
     templateUrl: 'scalar.html',
-    link: function(scope, element) {
-      scope.toggleRenaming = function() {
-        scope.renaming = !scope.renaming;
-        scope.newName = scope.scalar.title;
-        // Focus #renameBox once it has appeared.
-        $timeout(function() { element.find('#renameBox').focus(); });
-      };
-    },
   };
 });
--- web/app/scripts/segmentation.js
+++ web/app/scripts/segmentation.js
@@ -1,16 +1,8 @@
 'use strict';
 
-angular.module('biggraph').directive('segmentation', function($timeout) {
+angular.module('biggraph').directive('segmentation', function() {
   return {
     scope: { segmentation: '=', side: '=' },
     templateUrl: 'segmentation.html',
-    link: function(scope, element) {
-      scope.toggleRenaming = function() {
-        scope.renaming = !scope.renaming;
-        scope.newName = scope.segmentation.name;
-        // Focus #renameBox once it has appeared.
-        $timeout(function() { element.find('#renameBox').focus(); });
-      };
-    },
   };
 });
