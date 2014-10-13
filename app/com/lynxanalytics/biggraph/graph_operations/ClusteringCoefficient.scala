736852a31af87163f31aae0abff500d72010e227
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-13 18:34:45 +0200

    Deduplicate code shared between ClusteringCoefficient and Embeddedness.

--- app/com/lynxanalytics/biggraph/graph_operations/ClusteringCoefficient.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/ClusteringCoefficient.scala
@@ -6,67 +6,14 @@ import scala.collection.mutable
 
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.Implicits._
+import com.lynxanalytics.biggraph.spark_util.SortedRDD
 
 object ClusteringCoefficient {
   class Output(implicit instance: MetaGraphOperationInstance, inputs: GraphInput) extends MagicOutput(instance) {
     val clustering = vertexAttribute[Double](inputs.vs.entity)
   }
-}
-import ClusteringCoefficient._
-case class ClusteringCoefficient() extends TypedMetaGraphOp[GraphInput, Output] {
-  override val isHeavy = true
-  @transient override lazy val inputs = new GraphInput
 
-  def outputMeta(instance: MetaGraphOperationInstance) =
-    new Output()(instance, inputs)
-
-  def execute(inputDatas: DataSet,
-              o: Output,
-              output: OutputBuilder,
-              rc: RuntimeContext): Unit = {
-    implicit val id = inputDatas
-    val nonLoopEdges = inputs.es.rdd.filter { case (_, e) => e.src != e.dst }
-    val vertices = inputs.vs.rdd
-    val vertexPartitioner = vertices.partitioner.get
-
-    val inNeighbors = nonLoopEdges
-      .map { case (_, e) => e.dst -> e.src }
-      .groupBySortedKey(vertexPartitioner)
-      .mapValues(it => SortedSet(it.toSeq: _*).toArray)
-
-    val outNeighbors = nonLoopEdges
-      .map { case (_, e) => e.src -> e.dst }
-      .groupBySortedKey(vertexPartitioner)
-      .mapValues(it => SortedSet(it.toSeq: _*).toArray)
-
-    val neighbors = vertices.sortedLeftOuterJoin(outNeighbors).sortedLeftOuterJoin(inNeighbors)
-      .mapValues {
-        case ((_, outs), ins) => sortedUnion(outs.getOrElse(Array()), ins.getOrElse(Array()))
-      }
-
-    val outNeighborsOfNeighbors = neighbors.sortedJoin(outNeighbors).flatMap {
-      case (vid, (all, outs)) => all.map((_, outs))
-    }.groupBySortedKey(vertexPartitioner)
-
-    val clusteringCoeff = neighbors.sortedLeftOuterJoin(outNeighborsOfNeighbors).mapValues {
-      case (mine, theirs) =>
-        val numNeighbors = mine.size
-        if (numNeighbors > 1) {
-          theirs match {
-            case Some(ns) =>
-              val edgesInNeighborhood = ns.map(his => sortedIntersectionSize(his, mine)).sum
-              edgesInNeighborhood * 1.0 / numNeighbors / (numNeighbors - 1)
-            case None => 0.0
-          }
-        } else {
-          1.0
-        }
-    }
-
-    output(o.clustering, clusteringCoeff)
-  }
-
-  private def sortedUnion(a: Array[ID], b: Array[ID]): Array[ID] = {
+  private[graph_operations] def sortedUnion(a: Array[ID], b: Array[ID]): Array[ID] = {
     val builder = new mutable.ArrayBuilder.ofLong
     var ai = 0
     var bi = 0
@@ -88,7 +35,7 @@ case class ClusteringCoefficient() extends TypedMetaGraphOp[GraphInput, Output] 
     builder.result()
   }
 
-  private def sortedIntersectionSize(a: Array[ID], b: Array[ID]): Int = {
+  private[graph_operations] def sortedIntersectionSize(a: Array[ID], b: Array[ID]): Int = {
     var ai = 0
     var bi = 0
     var result = 0
@@ -104,5 +51,64 @@ case class ClusteringCoefficient() extends TypedMetaGraphOp[GraphInput, Output] 
       }
     }
     result
+  }
+
+  private[graph_operations] case class Neighbors(vertices: VertexSetRDD, nonLoopEdges: EdgeBundleRDD) {
+    val vertexPartitioner = vertices.partitioner.get
+
+    val in = nonLoopEdges
+      .map { case (_, e) => e.dst -> e.src }
+      .groupBySortedKey(vertexPartitioner)
+      .mapValues(it => SortedSet(it.toSeq: _*).toArray)
+
+    val out = nonLoopEdges
+      .map { case (_, e) => e.src -> e.dst }
+      .groupBySortedKey(vertexPartitioner)
+      .mapValues(it => SortedSet(it.toSeq: _*).toArray)
+
+    val all = vertices.sortedLeftOuterJoin(out).sortedLeftOuterJoin(in)
+      .mapValues {
+        case ((_, outs), ins) => sortedUnion(outs.getOrElse(Array()), ins.getOrElse(Array()))
+      }
+  }
+}
+import ClusteringCoefficient._
+case class ClusteringCoefficient() extends TypedMetaGraphOp[GraphInput, Output] {
+  override val isHeavy = true
+  @transient override lazy val inputs = new GraphInput
+
+  def outputMeta(instance: MetaGraphOperationInstance) =
+    new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val id = inputDatas
+    val nonLoopEdges = inputs.es.rdd.filter { case (_, e) => e.src != e.dst }
+    val vertices = inputs.vs.rdd
+    val vertexPartitioner = vertices.partitioner.get
+    val neighbors = Neighbors(vertices, nonLoopEdges)
+
+    val outNeighborsOfNeighbors = neighbors.all.sortedJoin(neighbors.out).flatMap {
+      case (vid, (all, outs)) => all.map((_, outs))
+    }.groupBySortedKey(vertexPartitioner)
+
+    val clusteringCoeff = neighbors.all.sortedLeftOuterJoin(outNeighborsOfNeighbors).mapValues {
+      case (mine, theirs) =>
+        val numNeighbors = mine.size
+        if (numNeighbors > 1) {
+          theirs match {
+            case Some(ns) =>
+              val edgesInNeighborhood = ns.map(his => sortedIntersectionSize(his, mine)).sum
+              edgesInNeighborhood * 1.0 / numNeighbors / (numNeighbors - 1)
+            case None => 0.0
+          }
+        } else {
+          1.0
+        }
+    }
+
+    output(o.clustering, clusteringCoeff)
   }
 }
