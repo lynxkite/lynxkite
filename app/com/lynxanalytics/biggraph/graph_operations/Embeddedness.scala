b093bfecad9ffce2972fd2d66855ce465d450191
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-13 18:34:45 +0200

    Deduplicate code shared between ClusteringCoefficient and Embeddedness.

--- app/com/lynxanalytics/biggraph/graph_operations/Embeddedness.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/Embeddedness.scala
@@ -28,21 +28,7 @@ case class Embeddedness() extends TypedMetaGraphOp[GraphInput, Output] {
     val nonLoopEdges = edges.filter { case (_, e) => e.src != e.dst }
     val vertices = inputs.vs.rdd
     val vertexPartitioner = vertices.partitioner.get
-
-    val inNeighbors = nonLoopEdges
-      .map { case (_, e) => e.dst -> e.src }
-      .groupBySortedKey(vertexPartitioner)
-      .mapValues(it => SortedSet(it.toSeq: _*).toArray)
-
-    val outNeighbors = nonLoopEdges
-      .map { case (_, e) => e.src -> e.dst }
-      .groupBySortedKey(vertexPartitioner)
-      .mapValues(it => SortedSet(it.toSeq: _*).toArray)
-
-    val neighbors = vertices.sortedLeftOuterJoin(outNeighbors).sortedLeftOuterJoin(inNeighbors)
-      .mapValues {
-        case ((_, outs), ins) => sortedUnion(outs.getOrElse(Array()), ins.getOrElse(Array()))
-      }
+    val neighbors = ClusteringCoefficient.Neighbors(vertices, nonLoopEdges).all
 
     val bySrc = edges.map {
       case (eid, e) => e.src -> (eid, e)
@@ -55,48 +41,8 @@ case class Embeddedness() extends TypedMetaGraphOp[GraphInput, Output] {
 
     val embeddedness = dstJoined.map {
       case (dst, ((eid, srcNeighbors), dstNeighbors)) =>
-        eid -> sortedIntersectionSize(srcNeighbors, dstNeighbors).toDouble
+        eid -> ClusteringCoefficient.sortedIntersectionSize(srcNeighbors, dstNeighbors).toDouble
     }.toSortedRDD(edges.partitioner.get)
     output(o.embeddedness, embeddedness)
-  }
-
-  private def sortedUnion(a: Array[ID], b: Array[ID]): Array[ID] = {
-    val builder = new mutable.ArrayBuilder.ofLong
-    var ai = 0
-    var bi = 0
-    while (ai < a.size && bi < b.size) {
-      if (a(ai) == b(bi)) {
-        builder += a(ai)
-        ai += 1
-        bi += 1
-      } else if (a(ai) > b(bi)) {
-        builder += b(bi)
-        bi += 1
-      } else {
-        builder += a(ai)
-        ai += 1
-      }
-    }
-    for (i <- ai until a.size) builder += a(i)
-    for (i <- bi until b.size) builder += b(i)
-    builder.result()
-  }
-
-  private def sortedIntersectionSize(a: Array[ID], b: Array[ID]): Int = {
-    var ai = 0
-    var bi = 0
-    var result = 0
-    while (ai < a.size && bi < b.size) {
-      if (a(ai) == b(bi)) {
-        result += 1
-        ai += 1
-        bi += 1
-      } else if (a(ai) > b(bi)) {
-        bi += 1
-      } else {
-        ai += 1
-      }
-    }
-    result
   }
 }
