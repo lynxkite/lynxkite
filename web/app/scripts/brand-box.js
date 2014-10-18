5fc876eeea3aef59ce68fd01dc69ac861aeb718a
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-18 15:56:28 +0200

    Add a few more tips.

--- web/app/scripts/brand-box.js
+++ web/app/scripts/brand-box.js
@@ -11,10 +11,16 @@ angular.module('biggraph').directive('brandBox', function() {
         'The system is busy when you see the gears turning in the bottom right corner.' +
           ' Hover over the gears for the option to abort the calculation.',
         'Click on approximate numbers like 42M to see the exact value.',
-        'Apply custom colors: gender == \'female\' ? \'pink\' : \'lightblue\'',
+        'Apply custom colors by creating an attribute with the color names.' +
+          ' Use the "Derive attribute" operation:' +
+          ' <tt>gender == \'female\' ? \'pink\' : \'lightblue\'</tt>',
         'Click on a histogram bar to zoom in.',
         'Multiple monitors? Enable linked mode at the bottom of the page.',
         'Press / to quickly access operations by their name.',
+        'In concrete vertices mode you can click on a vertex for further options.',
+        'Open the same project on both sides to graph edges between different views of the graph.',
+        'You can copy histogram and graph data to the clipboard with the' +
+          ' <i class="glyphicon glyphicon-th"></i> buttons.',
       ];
       scope.tip = tips[Math.floor(Math.random() * tips.length)];
     },
