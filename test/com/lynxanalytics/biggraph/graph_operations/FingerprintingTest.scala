67c884e34edda4a4fac5af18beec413362d10b14
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-18 15:17:08 +0200

    Neater test.

--- test/com/lynxanalytics/biggraph/graph_operations/FingerprintingTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/FingerprintingTest.scala
@@ -14,51 +14,32 @@ import com.lynxanalytics.biggraph.spark_util.Implicits._
 
 class FingerprintingTest extends FunSuite with TestGraphOp {
   test("two easy pairs") {
-    val graph = SmallTestGraph(Map(
-      10 -> Seq(1, 2, 3), 11 -> Seq(4, 5, 6), 20 -> Seq(1, 2, 3), 21 -> Seq(4, 5, 6))).result
-    val leftName = {
-      val op = AddVertexAttribute(Map(
-        1 -> "L1", 2 -> "L2", 3 -> "L3", 4 -> "L4", 5 -> "L5", 6 -> "L6", 10 -> "L10", 11 -> "L11"))
-      op(op.vs, graph.vs).result.attr
-    }
-    val rightName = {
-      val op = AddVertexAttribute(Map(
-        1 -> "R1", 2 -> "R2", 3 -> "R3", 4 -> "R4", 5 -> "R5", 6 -> "R6", 20 -> "R20", 21 -> "R21"))
-      op(op.vs, graph.vs).result.attr
-    }
-    val weight = AddConstantAttribute.run(graph.es.asVertexSet, 1.0)
-    val candidates = {
-      val op = AddEdgeBundle(Seq(10 -> 20, 10 -> 21, 11 -> 20, 11 -> 21))
-      op(op.vsA, graph.vs)(op.vsB, graph.vs).result.esAB
-    }
-    val fingerprinting = {
-      val op = Fingerprinting(1, 0)
-      op(
-        op.es, graph.es)(
-          op.weight, weight)(
-            op.leftName, leftName)(
-              op.rightName, rightName)(
-                op.candidates, candidates).result
-    }
-    assert(fingerprinting.leftToRight.toPairSet == Set(10L -> 20L, 11L -> 21L))
+    assert(fingerprint(
+      Map(10 -> Seq(1, 2, 3), 11 -> Seq(4, 5, 6)),
+      Map(20 -> Seq(1, 2, 3), 21 -> Seq(4, 5, 6)))
+      == Set(10 -> 20, 11 -> 21))
   }
 
   test("one difficult pair") {
-    val graph = SmallTestGraph(Map(
-      10 -> Seq(1, 2, 3), 11 -> Seq(1, 2, 3, 4), 20 -> Seq(1, 2, 3, 4))).result
+    assert(fingerprint(
+      Map(10 -> Seq(1, 2, 3), 11 -> Seq(1, 2, 3, 4)),
+      Map(20 -> Seq(1, 2, 3, 4)))
+      == Set(11 -> 20))
+  }
+
+  def fingerprint(left: Map[Int, Seq[Int]], right: Map[Int, Seq[Int]]): Set[(Int, Int)] = {
+    val graph = SmallTestGraph(left ++ right).result
     val leftName = {
-      val op = AddVertexAttribute(Map(
-        1 -> "L1", 2 -> "L2", 3 -> "L3", 4 -> "L4", 10 -> "L10", 11 -> "L11"))
+      val op = AddVertexAttribute(left.map { case (k, v) => k -> s"L$k" })
       op(op.vs, graph.vs).result.attr
     }
     val rightName = {
-      val op = AddVertexAttribute(Map(
-        1 -> "R1", 2 -> "R2", 3 -> "R3", 4 -> "R4", 20 -> "L20"))
+      val op = AddVertexAttribute(right.map { case (k, v) => k -> s"R$k" })
       op(op.vs, graph.vs).result.attr
     }
     val weight = AddConstantAttribute.run(graph.es.asVertexSet, 1.0)
     val candidates = {
-      val op = AddEdgeBundle(Seq(10 -> 20, 11 -> 20))
+      val op = AddEdgeBundle(for { l <- left.keys.toSeq; r <- right.keys.toSeq } yield l -> r)
       op(op.vsA, graph.vs)(op.vsB, graph.vs).result.esAB
     }
     val fingerprinting = {
@@ -70,6 +51,6 @@ class FingerprintingTest extends FunSuite with TestGraphOp {
               op.rightName, rightName)(
                 op.candidates, candidates).result
     }
-    assert(fingerprinting.leftToRight.toPairSet == Set(11L -> 20L))
+    fingerprinting.leftToRight.toPairSet.map { case (l, r) => (l.toInt, r.toInt) }
   }
 }
