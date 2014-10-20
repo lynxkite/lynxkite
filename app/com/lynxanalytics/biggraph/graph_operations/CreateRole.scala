9e33a72063dce71cee6ed29973f1cd5173da8fce
Forevian <andras.barjak@gmail.com>
2014-10-20 15:45:12 +0200

    partition attributes based on roles

---
+++ app/com/lynxanalytics/biggraph/graph_operations/CreateRole.scala
@@ -0,0 +1,40 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import scala.util.Random
+
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.spark_util.Implicits._
+
+object CreateRole {
+  class Input extends MagicInputSignature {
+    val vertices = vertexSet
+  }
+  class Output(implicit instance: MetaGraphOperationInstance,
+               inputs: Input) extends MagicOutput(instance) {
+    val role = vertexAttribute[String](inputs.vertices.entity)
+  }
+}
+import CreateRole._
+case class CreateRole(ratio: Double, seed: Int) extends TypedMetaGraphOp[Input, Output] {
+  @transient override lazy val inputs = new Input()
+
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val ds = inputDatas
+    val vertices = inputs.vertices.rdd
+    output(o.role, vertices.mapPartitionsWithIndex(
+      {
+        case (pid, it) =>
+          val rnd = new Random((pid << 16) + seed)
+          it.map {
+            case (vid, _) =>
+              vid -> { if (rnd.nextDouble() < ratio) "test" else "train" }
+          }
+      },
+      preservesPartitioning = true).toSortedRDD)
+  }
+}
