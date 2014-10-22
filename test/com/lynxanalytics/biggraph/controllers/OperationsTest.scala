4f23462bbf30c9877a6a3130aab8e8f3d0677965
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-22 16:25:47 +0200

    Use "no weights" instead of "1.0".

--- test/com/lynxanalytics/biggraph/controllers/OperationsTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/OperationsTest.scala
@@ -153,7 +153,7 @@ class OperationsTest extends FunSuite with TestGraphOp with BigGraphEnvironment 
     run("Fingerprinting", Map(
       "leftName" -> "email",
       "rightName" -> "name",
-      "weight" -> "1.0",
+      "weight" -> "no weights",
       "mrew" -> "0.0",
       "mo" -> "1",
       "ms" -> "0.5"))
