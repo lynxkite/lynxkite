04f80a77e95546a8cca4eeb9e43e7ac2e8a0e173
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-29 13:11:24 +0100

    Use an invisible directive to bind axis-options.

--- web/app/scripts/edge-attribute.js
+++ web/app/scripts/edge-attribute.js
@@ -4,15 +4,5 @@ angular.module('biggraph').directive('edgeAttribute', function() {
   return {
     scope: { attr: '=edgeAttribute', side: '=' },
     templateUrl: 'edge-attribute.html',
-    link: function(scope) {
-      scope.logarithmic = function(log) {  // Setter/getter.
-        var ao = scope.side.axisOptions('edge', scope.attr.title);
-        if (log !== undefined) {
-          ao.logarithmic = log;
-          scope.side.state.axisOptions.edge[scope.attr.title] = ao;
-        }
-        return ao.logarithmic;
-      };
-    },
   };
 });
