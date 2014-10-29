82eeb1b816f6a6c9ed6df5d13dcba9ecce2b0182
Daniel Darabos <darabos.daniel@gmail.com>
2014-10-29 15:13:14 +0100

    Bind axisOptions with a factory instead of a directive.

--- web/app/scripts/edge-attribute.js
+++ web/app/scripts/edge-attribute.js
@@ -1,8 +1,11 @@
 'use strict';
 
-angular.module('biggraph').directive('edgeAttribute', function() {
+angular.module('biggraph').directive('edgeAttribute', function(axisOptions) {
   return {
     scope: { attr: '=edgeAttribute', side: '=' },
     templateUrl: 'edge-attribute.html',
+    link: function(scope) {
+      axisOptions.bind(scope, scope.side, 'edge', scope.attr.title, 'axisOptions');
+    },
   };
 });
