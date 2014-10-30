fe91221c343a879e48627d16916a412f35852e50
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2014-10-30 13:30:14 +0100

    Implements belongs to from matching attr

---
+++ app/com/lynxanalytics/biggraph/graph_operations/EmptyEdgeBundle.scala
@@ -0,0 +1,28 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.spark_util.Implicits._
+
+object EmptyEdgeBundle {
+  class Input extends MagicInputSignature {
+    val src = vertexSet
+    val dst = vertexSet
+  }
+  class Output(implicit instance: MetaGraphOperationInstance,
+               inputs: Input) extends MagicOutput(instance) {
+    val eb = edgeBundle(inputs.src.entity, inputs.dst.entity)
+  }
+}
+import EmptyEdgeBundle._
+case class EmptyEdgeBundle() extends TypedMetaGraphOp[Input, Output] {
+  @transient override lazy val inputs = new Input()
+
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    output(o.eb, rc.sparkContext.emptyRDD[(ID, Edge)].toSortedRDD)
+  }
+}
