6b64ef65f4ca1bba373ce849fa5fc17dabf42e43
Daniel Darabos <darabos.daniel@gmail.com>
2014-11-05 11:36:03 +0100

    Log loading files through FileBasedObjectCache.

--- app/com/lynxanalytics/biggraph/graph_util/FileBasedObjectCache.scala
+++ app/com/lynxanalytics/biggraph/graph_util/FileBasedObjectCache.scala
@@ -2,12 +2,15 @@ package com.lynxanalytics.biggraph.graph_util
 
 import scala.collection.mutable.HashMap
 import scala.ref.WeakReference
+import com.lynxanalytics.biggraph.{ bigGraphLogger => log }
 
 object FileBasedObjectCache {
   private val cache = HashMap[Filename, WeakReference[AnyRef]]()
   def get[T](filename: Filename): T = synchronized {
     cache.get(filename).flatMap(_.get).getOrElse {
+      log.info(s"Loading object from $filename...")
       val value = filename.loadObjectKryo.asInstanceOf[AnyRef]
+      log.info(s"Loaded object from $filename.")
       cache(filename) = WeakReference(value)
       value
     }.asInstanceOf[T]
