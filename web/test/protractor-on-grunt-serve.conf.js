8e8b9979aabf03a0a0976ea2f9599281ffce40d0
Daniel Darabos <darabos.daniel@gmail.com>
2014-11-14 15:57:02 +0100

    Start setting up Protractor tests.

---
+++ web/test/protractor-on-grunt-serve.conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  baseUrl: 'http://localhost:9090/',
+  specs: ['*-test.js'],
+};
---
+++ web/test/splash-test.js
@@ -0,0 +1,16 @@
+describe('splash page', function() {
+  beforeEach(function() {
+    browser.get('/');
+  });
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Lynx PizzaKite');
+  });
+  describe('project selector', function() {
+    it('opens a project when clicked', function() {
+      var melon = element(by.partialLinkText('Project Melon'));
+      melon.click();
+      expect(element(by.css(".project-sides")).isPresent()).toBeTruthy();
+      expect(browser.getCurrentUrl()).toContain('/#/project/Project_Melon');
+    });
+  });
+});
