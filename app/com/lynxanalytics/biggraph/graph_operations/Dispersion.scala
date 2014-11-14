77466256f2ca40da2698e53865408ea1c2355695
Forevian <andras.barjak@gmail.com>
2014-11-14 15:09:34 +0100

    dispersion draft

---
+++ app/com/lynxanalytics/biggraph/graph_operations/Dispersion.scala
@@ -0,0 +1,69 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+import scala.collection.SortedSet
+import scala.collection.mutable
+
+import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.spark_util.Implicits._
+
+object Dispersion {
+  class Output(implicit instance: MetaGraphOperationInstance, inputs: GraphInput)
+      extends MagicOutput(instance) {
+    val dispersion = edgeAttribute[Double](inputs.es.entity)
+  }
+
+  private[graph_operations] def sortedIntersection(a: Seq[ID], b: Seq[ID]): Seq[ID] = {
+    val builder = new mutable.ArrayBuilder.ofLong
+    var ai = 0
+    var bi = 0
+    while (ai < a.size && bi < b.size) {
+      if (a(ai) == b(bi)) {
+        builder += a(ai)
+        ai += 1
+        bi += 1
+      } else if (a(ai) > b(bi)) {
+        bi += 1
+      } else {
+        ai += 1
+      }
+    }
+    builder.result()
+  }
+}
+import Dispersion._
+case class Dispersion() extends TypedMetaGraphOp[GraphInput, Output] {
+  override val isHeavy = true
+  @transient override lazy val inputs = new GraphInput
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val id = inputDatas
+    val edges = inputs.es.rdd
+    val nonLoopEdges = edges.filter { case (_, e) => e.src != e.dst }
+    val vertices = inputs.vs.rdd
+    val vertexPartitioner = vertices.partitioner.get
+    val fullGraph = CompactUndirectedGraph(rc, inputs.es.data, needsBothDirections = false) //nonloop?
+    val dispersion = nonLoopEdges.mapValues {
+      case (edge) =>
+        val srcNeighbors = fullGraph.getNeighbors(edge.src)
+        val dstNeighbors = fullGraph.getNeighbors(edge.dst)
+        val commonNeighbors = sortedIntersection(srcNeighbors, dstNeighbors)
+        commonNeighbors.combinations(2).map {
+          case Seq(a, b) =>
+            val aNeighbors = fullGraph.getNeighbors(a)
+            val bNeighbors = fullGraph.getNeighbors(b)
+            val neighborIntersection =
+              sortedIntersection(
+                sortedIntersection(aNeighbors, bNeighbors),
+                commonNeighbors) // or srcNeighbors in the original doc, but does that make sense?
+            if (neighborIntersection.size == 0) 1.0 else 0.0
+        }.sum
+    }
+    // what about parallel edges or in/out edges of a vertex pair?
+    output(o.dispersion, dispersion)
+  }
+}
