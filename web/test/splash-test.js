72a0446fb61b838b2b6dcd687f375d3621daceb5
Daniel Darabos <darabos.daniel@gmail.com>
2014-11-14 18:07:17 +0100

    More Protractor tests.

--- web/test/splash-test.js
+++ web/test/splash-test.js
@@ -3,14 +3,27 @@ describe('splash page', function() {
     browser.get('/');
     browser.executeScript('window.runningProtractorTests = true;');
   });
+
   it('has a title', function() {
     expect(browser.getTitle()).toEqual('Lynx PizzaKite');
   });
+
+  it('has a version string', function() {
+    expect(element(by.css('.version')).getText()).toBe('Static Ajax Test');
+  });
+
   describe('project selector', function() {
+    it('can search', function() {
+      var list = element.all(by.css('.project-list h1'));
+      expect(list.count()).toBe(3);
+      element(by.css('[placeholder="type to search"]')).sendKeys('v');
+      expect(list.count()).toBe(1);
+      expect(list.first().getText()).toBe('Project Avocado');
+    });
+
     it('opens a project when clicked', function() {
       var melon = element(by.partialLinkText('Project Melon'));
       melon.click();
-      expect(element(by.css(".project-sides")).isPresent()).toBeTruthy();
       expect(browser.getCurrentUrl()).toContain('/#/project/Project_Melon');
     });
   });
