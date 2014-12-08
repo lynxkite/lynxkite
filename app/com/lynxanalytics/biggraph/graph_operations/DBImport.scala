7a6ced566d22bbe2cb12c94e374110bb5ead1f36
Daniel Darabos <darabos.daniel@gmail.com>
2014-12-08 17:15:32 +0100

    Figure out the min/max key automatically in DBTable.

--- app/com/lynxanalytics/biggraph/graph_operations/DBImport.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/DBImport.scala
@@ -1,16 +1,25 @@
 package com.lynxanalytics.biggraph.graph_operations
 
 import com.lynxanalytics.biggraph.graph_api._
+import anorm.SQL
 import java.sql
 import org.apache.spark.rdd.RDD
 
 case class DBTable(
     db: String, table: String, fields: Seq[String],
-    key: String, minKey: Long, maxKey: Long) extends RowInput {
+    key: String) extends RowInput {
   assert(fields.contains(key), s"$key not found in $fields")
 
   def lines(rc: RuntimeContext): RDD[Seq[String]] = {
     val fieldsStr = fields.mkString(", ")
+    val (minKey, maxKey) = {
+      implicit val connection = sql.DriverManager.getConnection("jdbc:" + db)
+      try {
+        val q = SQL(s"SELECT MIN($key) AS min, MAX($key) AS max FROM $table")
+        q().map(r => (r[Long]("min"), r[Long]("max"))).head
+      } finally connection.close()
+    }
+
     new org.apache.spark.rdd.JdbcRDD(
       rc.sparkContext,
       () => sql.DriverManager.getConnection("jdbc:" + db),
