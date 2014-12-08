afbddf37864f355485a2c413023f2ff64f56b174
Daniel Darabos <darabos.daniel@gmail.com>
2014-12-08 17:37:15 +0100

    Have at most 100,000 rows per partition.

--- app/com/lynxanalytics/biggraph/graph_operations/DBImport.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/DBImport.scala
@@ -10,21 +10,32 @@ case class DBTable(
     key: String) extends RowInput {
   assert(fields.contains(key), s"$key not found in $fields")
 
+  case class Stats(table: String)(implicit val connection: sql.Connection) {
+    val (minKey, maxKey) = {
+      val q = SQL(s"SELECT MIN($key) AS min, MAX($key) AS max FROM $table")
+      q().map(r => (r[Long]("min"), r[Long]("max"))).head
+    }
+    val count = {
+      val q = SQL(s"SELECT COUNT(*) AS count FROM $table")
+      q().map(r => r[Long]("count")).head
+    }
+  }
+
   def lines(rc: RuntimeContext): RDD[Seq[String]] = {
     val fieldsStr = fields.mkString(", ")
-    val (minKey, maxKey) = {
-      implicit val connection = sql.DriverManager.getConnection("jdbc:" + db)
-      try {
-        val q = SQL(s"SELECT MIN($key) AS min, MAX($key) AS max FROM $table")
-        q().map(r => (r[Long]("min"), r[Long]("max"))).head
-      } finally connection.close()
+    val stats = {
+      val connection = sql.DriverManager.getConnection("jdbc:" + db)
+      try Stats(table)(connection)
+      finally connection.close()
     }
+    // Have at most 100,000 rows per partition.
+    val numPartitions = rc.defaultPartitioner.numPartitions max (stats.count / 100000).toInt
 
     new org.apache.spark.rdd.JdbcRDD(
       rc.sparkContext,
       () => sql.DriverManager.getConnection("jdbc:" + db),
       s"SELECT $fieldsStr FROM $table WHERE ? <= $key AND $key <= ?",
-      minKey, maxKey, rc.defaultPartitioner.numPartitions,
+      stats.minKey, stats.maxKey, numPartitions,
       row => fields.map(field => row.getString(field))
     )
   }
