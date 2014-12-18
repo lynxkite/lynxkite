0b10aa4db5c0ada39f8295383b479e2baf85c9d0
Andras Nemeth <xxandreww@gmail.com>
2014-12-18 14:56:10 +0100

    Move tests to port 9002

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -103,7 +103,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: 9002,
           base: [
             '.tmp',
             'testdata',
