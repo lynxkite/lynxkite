b6d462a85b5794a271dba77978a4487cf7ecd004
Andras Nemeth <xxandreww@gmail.com>
2015-01-06 16:21:13 +0100

    Allows all configuration needed by pizzakite via .kiterc

--- tools/call_spark_submit.sh
+++ tools/call_spark_submit.sh
@@ -8,10 +8,6 @@ if [[ "$JAVA_OPTS" != "" ]]; then
   java_opts="${JAVA_OPTS}"
 fi
 
-final_app_mem=${app_mem:-1024}
-
-final_java_opts="$(get_mem_opts $final_app_mem) ${java_opts} ${java_args[@]}"
-
 fake_application_jar=${lib_dir}/empty.jar
 
 KITE_SITE_CONFIG=${KITE_SITE_CONFIG:-$HOME/.kiterc}
@@ -23,6 +19,8 @@ popd
 if [ -f ${KITE_SITE_CONFIG} ]; then
   echo "Loading configuration from: ${KITE_SITE_CONFIG}"
   export SPARK_VERSION=`cat ${conf_dir}/SPARK_VERSION`
+  export KITE_RANDOM_SECRET=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+  export KITE_DEPLOYMENT_CONFIG_DIR=${conf_dir}
   source ${KITE_SITE_CONFIG}
 else
   echo "Warning, no Kite Site Config found at: ${KITE_SITE_CONFIG}"
@@ -31,8 +29,27 @@ else
   echo "You can find an example config file at ${conf_dir}/kiterc_template"
 fi
 
+# -mem flag overrides KITE_HEAD_MEMORY_MB and we use 1024 if neither is set.
+final_app_mem=${app_mem:-${KITE_HEAD_MEMORY_MB:-1024}}
+
+addJPropIfNonEmpty () {
+  if [ -n "$2" ]; then
+    addJava "-D$1=$2"
+  fi
+}
+
+addJPropIfNonEmpty http.port "${KITE_HTTP_PORT}"
+addJPropIfNonEmpty https.port "${KITE_HTTPS_PORT}"
+addJPropIfNonEmpty https.keyStore "${KITE_HTTPS_KEYSTORE}"
+addJPropIfNonEmpty https.keyStorePassword "${KITE_HTTPS_KEYSTORE_PWD}"
+addJPropIfNonEmpty application.secret "${KITE_APPLICATION_SECRET}"
+addJPropIfNonEmpty authentication.google.clientSecret "${KITE_GOOGLE_CLIENT_SECRET}"
+addJPropIfNonEmpty hadoop.tmp.dir "${KITE_LOCAL_TMP}"
+final_java_opts="$(get_mem_opts $final_app_mem) ${java_opts} ${java_args[@]}"
+
 export REPOSITORY_MODE=${REPOSITORY_MODE:-"static<$KITE_META_DIR,$KITE_DATA_DIR>"}
 
+set -x
 execRunner ${SPARK_HOME}/bin/spark-submit \
   --class play.core.server.NettyServer \
   --master ${SPARK_MASTER} \
