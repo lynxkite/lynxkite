96c0713d9662134bcbc9c6d6d6f7bdc6a63a24b5
Andras Nemeth <xxandreww@gmail.com>
2015-01-12 15:09:23 +0100

    Pair programmed protractor test for the filter binding

--- web/test/filter-binding-test.js
+++ web/test/filter-binding-test.js
@@ -3,19 +3,30 @@ describe('the all filters button', function() {
   beforeEach(function() {
     mocks.addTo(browser);
     browser.get('/#/project/Project_Strawberry');
-    this.header = element.all(by.css('.project-name')).first();
+    this.header = element(by.css('.project-name'))
     //this.button = this.attr.element(by.css('[tooltip="Show histogram"]'));
     //this.histogram = this.attr.element(by.css('histogram'));
   });
 
+  it('is linked to the vertex attribute filter box', function() {
+    expect(this.header.all(by.css('[ng-click="side.applyFilters()"]')).count())
+      .toBe(0);
+    element.all(by.css('[vertex-attribute] input[placeholder="filter"]')).first().sendKeys('a\n');
+    expect(this.header.all(by.css('[ng-click="side.applyFilters()"]')).count())
+      .toBe(1);
+  });
+  it('is linked to the edge attribute filter box', function() {
+    expect(this.header.all(by.css('[ng-click="side.applyFilters()"]')).count())
+      .toBe(0);
+    element.all(by.css('[edge-attribute] input[placeholder="filter"]')).first().sendKeys('a\n');
+    expect(this.header.all(by.css('[ng-click="side.applyFilters()"]')).count())
+      .toBe(1);
+  });
   it('is linked to the segmentation filter box', function() {
-    expect(this.header.element(by.css('[ng-click="side.applyFilters()"]'))).toBe(undefined)
-
-/*    expect(this.attr.element(by.css('item-name-and-menu > span')).getText()).toBe('age');
-    expect(this.histogram.isDisplayed()).toBe(false);
-    this.button.click();  // Open.
-    expect(this.histogram.isDisplayed()).toBe(true);
-    this.button.click();  // Close.
-    expect(this.histogram.isDisplayed()).toBe(false);*/
+    expect(this.header.all(by.css('[ng-click="side.applyFilters()"]')).count())
+      .toBe(0);
+    element.all(by.css('[segmentation] input[placeholder="filter"]')).first().sendKeys('a\n');
+    expect(this.header.all(by.css('[ng-click="side.applyFilters()"]')).count())
+      .toBe(1);
   });
 });
