8d023249b1628d053676a7c3c8fe4ba49a48f63c
Daniel Darabos <darabos.daniel@gmail.com>
2015-02-16 14:03:59 +0100

    Delete EdgeBundleAsVertexSet.scala.

---
+++ app/com/lynxanalytics/biggraph/graph_operations/EdgeBundleAsAttribute.scala
@@ -0,0 +1,32 @@
+package com.lynxanalytics.biggraph.graph_operations
+
+import com.lynxanalytics.biggraph.graph_api._
+
+object EdgeBundleAsAttribute extends OpFromJson {
+  class Input extends MagicInputSignature {
+    val src = vertexSet
+    val dst = vertexSet
+    val edges = edgeBundle(src, dst)
+  }
+  class Output(
+      implicit instance: MetaGraphOperationInstance,
+      inputs: Input) extends MagicOutput(instance) {
+    val attr = edgeAttribute[(ID, ID)](inputs.edges.entity)
+  }
+  def fromJson(j: JsValue) = EdgeBundleAsAttribute()
+}
+import EdgeBundleAsAttribute._
+case class EdgeBundleAsAttribute() extends TypedMetaGraphOp[Input, Output] {
+
+  @transient override lazy val inputs = new Input()
+
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+
+  def execute(inputDatas: DataSet,
+              o: Output,
+              output: OutputBuilder,
+              rc: RuntimeContext): Unit = {
+    implicit val ds = inputDatas
+    output(o.attr, inputs.edges.rdd.mapValues(edge => (edge.src, edge.dst)))
+  }
+}
