d1c799e427e07d152fec8a14862ae41d95a5e3f4
Daniel Darabos <darabos.daniel@gmail.com>
2015-02-17 16:20:36 +0100

    Use JoinAttributes instead of creating MakePosition.

--- test/com/lynxanalytics/biggraph/graph_operations/JoinAttributesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/JoinAttributesTest.scala
@@ -7,11 +7,11 @@ import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 import org.apache.spark.SparkContext.rddToPairRDDFunctions
 
-class MakePositionTest extends FunSuite with TestGraphOp {
+class JoinAttributesTest extends FunSuite with TestGraphOp {
   test("example graph") {
     val g = ExampleGraph()().result
-    val op = MakePosition()
-    val res = op(op.attrA, g.age)(op.attrB, g.income).result.position
+    val op = JoinAttributes[Double, Double]()
+    val res = op(op.a, g.age)(op.b, g.income).result.attr
     assert(res.rdd.collect.toSeq == Seq(0 -> (20.3, 1000.0), 2 -> (50.3, 2000.0)))
   }
 }
