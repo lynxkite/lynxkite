3190f235c345d5cd2f9ecfa669ca0f8d687d3ee4
Daniel Darabos <darabos.daniel@gmail.com>
2015-03-02 16:03:30 +0100

    Add button for applying history changes.

--- web/app/scripts/project-history.js
+++ web/app/scripts/project-history.js
@@ -8,8 +8,23 @@ angular.module('biggraph').directive('projectHistory', function(util) {
     link: function(scope) {
       scope.history = util.get('/ajax/getHistory', {});
 
-      scope.$watch('show', function() {
-      });
+      function update() {
+        var history = scope.history;
+        console.log('history', history);
+        if (history.$resolved && !history.$error) {
+          console.log('history loaded');
+          for (var i = 0; i < history.steps.length; ++i) {
+            var step = history.steps[i];
+            step.originalParameters = angular.copy(step.parameters);
+          }
+        }
+      }
+      scope.$watch('history', update);
+      scope.$watch('history.$resolved', update);
+
+      scope.unsaved = function(step) {
+        return !angular.equals(step.parameters, step.originalParameters);
+      };
     },
   };
 });
