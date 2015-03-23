3b32eca277bc4c7847a1fb58179b75fadc0724c4
Daniel Darabos <darabos.daniel@gmail.com>
2015-03-23 14:04:01 +0100

    Register ManifestFactory, ClassTag, and Class with Kryo.

--- app/com/lynxanalytics/biggraph/spark_util/BigGraphSparkContext.scala
+++ app/com/lynxanalytics/biggraph/spark_util/BigGraphSparkContext.scala
@@ -88,7 +88,11 @@ class BigGraphKryoRegistrator extends KryoRegistrator {
     kryo.register(classOf[DeadClass2])
     kryo.register(classOf[DeadClass3])
     kryo.register((0.0, 0.0).getClass)
-    kryo.register(math.Numeric.LongIsIntegral.getClass)  // For using NumericRanges with sc.parallelize.
+    kryo.register(math.Numeric.LongIsIntegral.getClass) // For using NumericRanges with sc.parallelize.
+    // The next three are required by some operations after the Spark 1.3.0 updgrade.
+    kryo.register(classOf[scala.reflect.ManifestFactory$$anon$10])
+    kryo.register(classOf[scala.reflect.ClassTag$$anon$1])
+    kryo.register(classOf[Class[_]])
     // Add new stuff just above this line! Thanks.
     // Adding Foo$mcXXX$sp? It is a type specialization. Register the decoded type instead!
     // Z = Boolean, B = Byte, C = Char, D = Double, F = Float, I = Int, J = Long, S = Short.
