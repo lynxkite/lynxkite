feea99ef1029c4b61fd064fd50e53145b00d21c0
Andras Nemeth <xxandreww@gmail.com>
2015-04-01 16:53:26 +0200

    Implements center repick when loading saved visualization

--- app/com/lynxanalytics/biggraph/controllers/UIStatus.scala
+++ app/com/lynxanalytics/biggraph/controllers/UIStatus.scala
@@ -15,6 +15,12 @@ case class UIAxisOptions(
 case class UIAnimation(
   enabled: Boolean,
   labelAttraction: String)
+case class UIAttributeFilter(
+  val attributeName: String,
+  val valueSpec: String)
+case class UICenterRequest(
+  count: Int,
+  filters: Seq[UIAttributeFilter])
 case class UIStatus(
   graphMode: String,
   display: String,
@@ -24,15 +30,23 @@ case class UIStatus(
   sampleRadius: String,
   attributeTitles: Map[String, String],
   animate: UIAnimation,
-  centers: Seq[String])
+  // For explicit center ids entered by the user, this will be set.
+  centers: Option[Seq[String]],
+  // For centers set by a getCenter request, the following parameters will be set
+  // (with vertexSetId set to "") so that we can redo the getCenter request.
+  lastCentersRequest: Option[UICenterRequest])
 object UIStatusSerialization {
   implicit val rUIFilterStatus = json.Json.reads[UIFilterStatus]
   implicit val rUIAxisOptions = json.Json.reads[UIAxisOptions]
   implicit val rUIAnimation = json.Json.reads[UIAnimation]
+  implicit val rUIAttributeFilter = json.Json.reads[UIAttributeFilter]
+  implicit val rUICenterRequest = json.Json.reads[UICenterRequest]
   implicit val rUIStatus = json.Json.reads[UIStatus]
   implicit val wUIFilterStatus = json.Json.writes[UIFilterStatus]
   implicit val wUIAxisOptions = json.Json.writes[UIAxisOptions]
   implicit val wUIAnimation = json.Json.writes[UIAnimation]
+  implicit val wUIAttributeFilter = json.Json.writes[UIAttributeFilter]
+  implicit val wUICenterRequest = json.Json.writes[UICenterRequest]
   implicit val wUIStatus = json.Json.writes[UIStatus]
 }
 
