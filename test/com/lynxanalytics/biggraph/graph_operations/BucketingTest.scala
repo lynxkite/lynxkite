fcacb2a07b7f2f5f6426e37bd22d0415b7f4586b
Daniel Darabos <darabos.daniel@gmail.com>
2015-04-01 17:08:29 +0200

    Export labels from bucketing.

--- test/com/lynxanalytics/biggraph/graph_operations/BucketingTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/BucketingTest.scala
@@ -17,6 +17,7 @@ class BucketingTest extends FunSuite with TestGraphOp {
     assert(bucketing.segments.toSeq.size == 2)
     val segmentSizes = bucketing.belongsTo.toPairSeq.groupBy(_._2).values.map(_.size).toSeq
     assert(segmentSizes.sorted == Seq(1, 3))
+    assert(bucketing.label.rdd.values.collect.toSeq.sorted == Seq("Female", "Male"))
   }
 
   test("example graph by age with overlap") {
@@ -29,5 +30,7 @@ class BucketingTest extends FunSuite with TestGraphOp {
     val segmentSizes = bucketing.belongsTo.toPairSeq.groupBy(_._2).values.map(_.size).toSeq
     assert(segmentSizes.sorted == Seq(1, 1, 1, 1, 2, 2))
     assert(segmentSizes.sum == 4 * 2)
+    assert(bucketing.label.rdd.values.collect.toSeq.sorted ==
+      Seq("-10.0 to 10.0", "0.0 to 20.0", "10.0 to 30.0", "20.0 to 40.0", "40.0 to 60.0", "50.0 to 70.0"))
   }
 }
