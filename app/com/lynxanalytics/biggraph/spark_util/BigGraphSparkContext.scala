87a186c4f3a050786f35c3d8da33853b90d65979
Daniel Darabos <darabos.daniel@gmail.com>
2015-04-24 12:19:48 +0200

    Register RoaringBitmap with Kryo.

--- app/com/lynxanalytics/biggraph/spark_util/BigGraphSparkContext.scala
+++ app/com/lynxanalytics/biggraph/spark_util/BigGraphSparkContext.scala
@@ -101,6 +101,7 @@ class BigGraphKryoRegistrator extends KryoRegistrator {
     kryo.register(classOf[scala.reflect.ManifestFactory$$anon$10])
     kryo.register(classOf[scala.reflect.ClassTag$$anon$1])
     kryo.register(classOf[Class[_]])
+    kryo.register(classOf[org.roaringbitmap.RoaringBitmap]) // #1518
     // Add new stuff just above this line! Thanks.
     // Adding Foo$mcXXX$sp? It is a type specialization. Register the decoded type instead!
     // Z = Boolean, B = Byte, C = Char, D = Double, F = Float, I = Int, J = Long, S = Short.
