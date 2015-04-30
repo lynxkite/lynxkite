cb31f59898cb541cf56f2bf91856626a48b272e2
Svigruha Gergely <gergely.svigruha@lynxanalytics.com>
2015-04-30 16:33:30 +0200

    Init scripts.

--- tools/bare_metal_install_master.sh
+++ tools/bare_metal_install_master.sh
@@ -1,20 +1,20 @@
 echo "Starting LynxKite installation..."
 
-mkdir cloudera
-sudo mount /dev/cdrom cloudera
+DVD_ROOT="$(dirname $0)"
 
 # Install Java
 echo "Installing Java..."
 sudo mkdir -p /usr/java
-sudo tar -xf cloudera/server-jre-7u80-linux-x64.tar.gz -C /usr/java
+sudo tar -xf $DVD_ROOT/server-jre-7u80-linux-x64.tar.gz -C /usr/java
 echo "Java installed successfully."
 
 # Install Cloudera
 echo "Installing Cloudera..."
-sudo tar -xf cloudera/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz -C /opt
+sudo tar -xf $DVD_ROOT/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz -C /opt
 sudo useradd --system --home=/opt/cm-5.3.3/run/cloudera-scm-server --no-create-home cloudera-scm
 sudo chown -R cloudera-scm.cloudera-scm /opt/cm-5.3.3
-sudo cp  /opt/cm-5.3.3/etc/init.d
+sudo cp /opt/cm-5.3.3/etc/init.d/cloudera-scm-agent /etc/init.d/cloudera-scm-agent
+sudo cp /opt/cm-5.3.3/etc/init.d/cloudera-scm-server /etc/init.d/cloudera-scm-server
 echo "Cloudera installed successfully."
 
 # Configure PostgreSQL
--- tools/bare_metal_install_slave.sh
+++ tools/bare_metal_install_slave.sh
@@ -1,16 +1,16 @@
 echo "Starting LynxKite installation..."
 
-mkdir cloudera
-sudo mount /dev/cdrom cloudera
+DVD_ROOT="$(dirname $0)"
 
 # Install Java
 echo "Installing Java..."
 sudo mkdir -p /usr/java
-sudo tar -xf cloudera/server-jre-7u80-linux-x64.tar.gz -C /usr/java
+sudo tar -xf $DVD_ROOT/server-jre-7u80-linux-x64.tar.gz -C /usr/java
 echo "Java installed successfully."
 
 # Install Cloudera
 echo "Installing Cloudera..."
-sudo tar -xf cloudera/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz -C /opt
+sudo tar -xf $DVD_ROOT/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz -C /opt
+sudo cp /opt/cm-5.3.3/etc/init.d/cloudera-scm-agent /etc/init.d/cloudera-scm-agent
 echo "Cloudera installed successfully."
 
