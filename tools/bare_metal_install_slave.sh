fa2f3b4e12989021b17db2131ba1a0142afd1a5a
Svigruha Gergely <gsvigruha@users.noreply.github.com>
2015-04-30 16:11:49 +0200

    Master / slave added.

---
+++ tools/bare_metal_install_slave.sh
@@ -0,0 +1,16 @@
+echo "Starting LynxKite isntallation..."
+
+mkdir cloudera
+sudo mount /dev/cdrom cloudera
+
+# Install Java
+echo "Installing Java..."
+sudo mkdir -p /usr/java
+sudo tar -xf cloudera/server-jre-7u80-linux-x64.tar.gz -C /usr/java
+echo "Java installed successfully."
+
+# Install Cloudera
+echo "Installing Cloudera..."
+sudo tar -xf cloudera/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz -C /opt
+echo "Cloudera installed successfully."
+
