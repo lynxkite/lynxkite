20395012b49dd61fcc66b007a05e5221415ff56b
Daniel Darabos <darabos.daniel@gmail.com>
2015-05-06 16:39:50 +0200

    Help button that displays popup for operation parameters.

--- web/app/help/operations.md
+++ web/app/help/operations.md
@@ -3,3 +3,24 @@
 ## Discard vertices
 
 Throws away all vertices. This implies discarding all edges, attributes, and segmentations too.
+
+## Discard edges
+
+Throws away all edges. This implies discarding all edge attributes too.
+
+## New vertex set
+
+Creates a new vertex set with no edges. Two attributes are generated, `id` and `ordinal`. `id`
+is the internal vertex ID, while `ordinal` is an index for the vertex: it goes from zero to the
+vertex set size.
+
+---
+
+ - <span class="param" name="size">Vertex set size</span>: The number of vertices to create.
+   xx
+
+   ---
+
+   xxx
+   bla
+ - ketto
--- web/app/scripts/help.js
+++ web/app/scripts/help.js
@@ -1,27 +1,139 @@
-// Inserts help content by ID.
+// Displays help content by ID.
 'use strict';
 
+// Loads and preprocesses the help pages.
 angular.module('biggraph').directive('helpContent', function() {
   return {
     restrict: 'E',
     templateUrl: 'help-content.html',
+    link: function(scope, element) {
+      scope.onload = function() {
+        var lastId;
+        // Expand anchor IDs. <a name="X"> inside a section on Y (<h2 id="Y">) will get id="Y-X".
+        // This is used for easy definition and reference of operation parameters.
+        element.find('*').each(function(i, e) {
+          e = angular.element(e);
+          if (e.is(headersUntil(6))) {
+            lastId = e.attr('id') || lastId;
+          }
+          if (e.is('span[name]') && !e.attr('id')) {
+            e.attr('id', lastId + '-' + e.attr('name'));
+          }
+        });
+      };
+    },
   };
 });
 
+// Returns the list of header tags (h1, h2, ...) until and including hn.
+function headersUntil(n) {
+  var list = [];
+  for (var i = 1; i <= n; ++i) {
+    list.push('h' + i);
+  }
+  return list.join(',');
+}
+
+// Returns the header level of a jQuery element.
+function headerLevel(e) {
+  if (e.is(headersUntil(6))) {
+    return parseInt(e[0].tagName[1]);
+  } else {
+    return undefined;
+  }
+}
+
+// Finds a snippet from the help pages by its ID. Replaces the first <hr> with a "read more" link.
 angular.module('biggraph').directive('helpId', function() {
   return {
     restrict: 'A',
     scope: { helpId: '=' },
     link: function(scope, element) {
+      element.addClass('help');
+
       scope.$watch('helpId', function() {
         var id = scope.helpId.toLowerCase();
         var content = angular.element('help-content').find('#' + id).first();
-        var tag = content[0].tagName;
-        if (tag.length === 2 && tag[0] === 'H') {
-          content = content.nextUntil(tag);
+        if (content.is(headersUntil(6))) {
+          // A header. Take all content until the next header on the same level or higher.
+          content = content.nextUntil(headersUntil(headerLevel(content)));
         }
-        element.html(content.html());
+        if (content.is('[name]')) {
+          // An anchor. Take the <li> if this is inside a <li>, otherwise take the parent.
+          var li = content.closest('li');
+          if (li.length === 1) {
+            content = li.children();
+          } else {
+            content = content.parent().children();
+          }
+        }
+        content = content.clone();
+        function expander(e, what) {
+          return function() {
+            e.hide();
+            what.show();
+          };
+        }
+        for (var i = 0; i < content.length; ++i) {
+          if (content[i].tagName === 'HR') {
+            var hr = content.slice(i, i + 1);
+            hr.text('read more');
+            hr.addClass('read-more');
+            var rest = content.slice(i + 1);
+            rest.hide();
+            hr.click(expander(hr, rest));
+          }
+        }
+        element.empty();
+        element.append(content);
       });
+    }
+  };
+});
+
+// A button that displays a help snippet on hover/click.
+angular.module('biggraph').directive('helpPopup', function($rootScope) {
+  return {
+    restrict: 'E',
+    scope: { helpId: '=href' },
+    templateUrl: 'help-popup.html',
+    link: function(scope, element) {
+      var body = angular.element('body');
+      var button = element.find('#help-button');
+      var popup = element.find('#help-popup');
+      popup.hide();
+      var showing = false;
+
+      scope.$on('$destroy', function() {
+        popup.remove();
+      });
+      scope.$on('help popup opened', function(e, source) {
+        if (scope !== source) { // Another popup opened. Hide ourselves.
+          popup.hide();
+        }
+      });
+
+      function show() {
+        $rootScope.$broadcast('help popup opened', scope);
+        // Add the popup at the end of <body> to make sure it's above everything.
+        body.append(popup.detach());
+        popup.show();
+        var offset = button.offset();
+        offset.top += button.height() + 10;
+        offset.left -= popup.width() / 2;
+        popup.offset(offset);
+      }
+
+      scope.on = function() {
+        show();
+      };
+      scope.off = function() {
+        if (!showing) { popup.hide(); }
+      };
+      scope.toggle = function() {
+        showing = !showing;
+        if (showing) { show(); }
+      };
     }
   };
 });
