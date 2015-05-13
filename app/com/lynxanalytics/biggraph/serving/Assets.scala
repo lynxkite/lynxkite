40ed69cfa3f5613575fbedf2cd559ab9956ccecd
gsvigruha <gsvigruha@users.noreply.github.com>
2015-05-13 15:09:15 +0800

    Moving stuff to JsonServer. Checking last file in logs instead of guessing name.

--- app/com/lynxanalytics/biggraph/serving/Assets.scala
+++ app/com/lynxanalytics/biggraph/serving/Assets.scala
@@ -3,11 +3,6 @@ package com.lynxanalytics.biggraph.serving
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
-import play.Play
-
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.io.File
 
 object Assets extends Controller {
   def at(path: String, file: String): Action[AnyContent] = {
@@ -24,20 +19,5 @@ object Assets extends Controller {
     // Redirect e.g. /project/x to /#/project/x.
     // Absolutely bogus URLs get redirected then to the main page by Angular.
     Redirect("/#" + request.path)
-  }
-
-  private val format = new SimpleDateFormat("yyyyMMdd")
-
-  def appLog(): Action[AnyContent] = Action { request =>
-    val dateString = format.format(Calendar.getInstance().getTime())
-    val logFile = new File(Play.application.getFile("logs"),
-      s"application-$dateString.log")
-
-    if (logFile.exists) {
-      Ok.sendFile(logFile, inline = true)
-    } else {
-      // This should never happen unless the log is missing.
-      NotFound
-    }
   }
 }
