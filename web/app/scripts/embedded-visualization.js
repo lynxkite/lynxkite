3d311dac70001d130e5b243b948f8d4d0b753155
Daniel Darabos <darabos.daniel@gmail.com>
2015-05-14 17:25:56 +0200

    Embeddable visualization.

---
+++ web/app/scripts/embedded-visualization.js
@@ -0,0 +1,28 @@
+// A graph visualization that is fully configured using string attributes.
+// This means that it can be embedded in help pages.
+'use strict';
+
+angular.module('biggraph').directive('embeddedVisualization', function(side, loadGraph) {
+  return {
+    restrict: 'E',
+    scope: {
+      width: '=',
+      height: '=',
+    },
+    templateUrl: 'embedded-visualization.html',
+    link: function(scope, element, attrs) {
+      console.log(attrs);
+      scope.side = new side.Side([]);
+      scope.side.state = angular.extend(side.defaultSideState(), attrs);
+      scope.side.reload();
+      scope.graph = new loadGraph.Graph();
+      scope.$watch('side.project.$resolved', function(loaded) {
+        if (loaded) {
+          scope.side.onProjectLoaded();
+          console.log(scope.side.viewData);
+          scope.graph.load(scope.side.viewData);
+        }
+      });
+    },
+  };
+});
