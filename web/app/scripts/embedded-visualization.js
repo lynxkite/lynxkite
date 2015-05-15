36585e3d6b3f7bb048b264d47221634f86c5f78d
Daniel Darabos <darabos.daniel@gmail.com>
2015-05-15 12:10:02 +0200

    Explain crappy code in comment.

--- web/app/scripts/embedded-visualization.js
+++ web/app/scripts/embedded-visualization.js
@@ -12,6 +12,9 @@ angular.module('biggraph').directive('embeddedVisualization', function(util, sid
     templateUrl: 'embedded-visualization.html',
     link: function(scope, element, attrs) {
       scope.side = new side.Side([]);
+      // Copy settings from attributes to the state. This only works for top-level string
+      // settings. We'll have to think up something if we want to set deeper or non-string
+      // settings.
       scope.side.state = angular.extend(side.defaultSideState(), attrs);
       scope.side.reload();
       scope.graph = new loadGraph.Graph();
