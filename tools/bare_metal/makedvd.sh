44ab8b736c1c0bbbc6612be199600edc1e303747
Daniel Darabos <darabos.daniel@gmail.com>
2015-05-22 14:04:13 +0200

    Script for creating cluster install DVD.

---
+++ tools/bare_metal/makedvd.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -xueo pipefail
+
+LYNXKITE_TGZ="$1"
+if [ ! -f "$LYNXKITE_TGZ" ]; then
+  >&2 echo "ERROR: The specified LynxKite archive ($LYNXKITE_TGZ) does not exist."
+  exit 1
+fi
+
+BARE_METAL_DIR=$(dirname "$0")
+REPO_DIR="$BARE_METAL_DIR/../.."
+BUILD="/tmp/bare-metal-dvd"
+SPARK_VERSION=$(cat "$REPO_DIR/conf/SPARK_VERSION")
+rm -rf "$BUILD" || true
+mkdir "$BUILD"
+
+# Add LynxKite and scripts.
+cp "$LYNXKITE_TGZ" "$BUILD"
+cp "$BARE_METAL_DIR"/*.sh "$BUILD"
+
+# Get Spark.
+SPARK_TGZ="spark-$SPARK_VERSION-bin-hadoop2.4.tgz"
+wget --directory-prefix="$BUILD/" \
+  "http://d3kbcqa49mib13.cloudfront.net/$SPARK_TGZ"
+
+# Get Cloudera.
+wget --directory-prefix="$BUILD/" \
+  'http://archive-primary.cloudera.com/cm5/cm/5/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz'
+wget --directory-prefix="$BUILD/" \
+  'http://archive.cloudera.com/cdh5/parcels/5.3.3/CDH-5.3.3-1.cdh5.3.3.p0.5-trusty.parcel'
+wget --directory-prefix="$BUILD/" \
+  'http://archive.cloudera.com/cdh5/parcels/5.3.3/CDH-5.3.3-1.cdh5.3.3.p0.5-trusty.parcel.sha1'
+wget --directory-prefix="$BUILD/" \
+  'http://archive.cloudera.com/cdh5/parcels/5.3.3/manifest.json'
+
+# Get Oracle Server JRE 7.
+wget --directory-prefix="$BUILD/" \
+  'http://download.oracle.com/otn-pub/java/jdk/7u80-b15/server-jre-7u80-linux-x64.tar.gz'
+
+# Create DVD image.
+mkisofs -r -o "bare-metal-dvd.iso" "$BUILD"
+echo "DVD image successfully created as: bare-metal-dvd.iso"
