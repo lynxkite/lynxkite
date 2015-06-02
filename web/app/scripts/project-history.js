4972cec67660bece16475f9377999a4917eda4b7
Daniel Darabos <darabos.daniel@gmail.com>
2015-06-02 17:11:56 +0200

    Remove stray star from regex.

--- web/app/scripts/project-history.js
+++ web/app/scripts/project-history.js
@@ -209,7 +209,7 @@ angular.module('biggraph').directive('projectHistory', function(util) {
             var absoluteName = request.project;
             // Replace the root project name with "!project" so that the
             // workflow can be executed on other projects as well.
-            var relativeName = absoluteName.replace(/^[^/*]*/, '!project');
+            var relativeName = absoluteName.replace(/^[^/]*/, '!project');
             request.project = relativeName;
             return request;
           });
