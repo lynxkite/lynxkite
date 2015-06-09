62b8cf736e44167efd6c41ac3b4f64f474e6f7b6
Daniel Darabos <darabos.daniel@gmail.com>
2015-06-09 16:33:52 +0200

    Indent.

--- benchmarks/testsize.sh
+++ benchmarks/testsize.sh
@@ -9,9 +9,9 @@ cd $DIR/..
 
 RATIO=0.050625
 for i in `seq 10`; do
-rm -r ~/kite_meta
-rm -r ~/kite_data/entities
-rm -r ~/kite_data/scalars
-WORKER_MEMORY_MULT=$RATIO time ./stage/bin/biggraph batch kitescripts/importtest glob:${GLOB}
-RATIO=`echo "scale=10; $RATIO * 1.5" | bc`
+  rm -r ~/kite_meta
+  rm -r ~/kite_data/entities
+  rm -r ~/kite_data/scalars
+  WORKER_MEMORY_MULT=$RATIO time ./stage/bin/biggraph batch kitescripts/importtest glob:${GLOB}
+  RATIO=`echo "scale=10; $RATIO * 1.5" | bc`
 done
