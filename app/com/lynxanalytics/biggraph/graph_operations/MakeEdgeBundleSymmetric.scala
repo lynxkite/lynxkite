8f07860ed23cc37e01b28e868befd4cce87f9f35
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-06-11 11:56:46 +0200

    Implement new behaviour for MakeEdgeBundleSymmetric

--- app/com/lynxanalytics/biggraph/graph_operations/MakeEdgeBundleSymmetric.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/MakeEdgeBundleSymmetric.scala
@@ -1,20 +1,22 @@
 // Discards all A->B edges if there is no B->A edge.
+// If there is at least one B->A edge, exactly one
+// A->B edge will be retained. (That is, no more than one.)
 package com.lynxanalytics.biggraph.graph_operations
 
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.Implicits._
 
-object RemoveNonSymmetricEdges extends OpFromJson {
+object MakeEdgeBundleSymmetric extends OpFromJson {
   class Output(implicit instance: MetaGraphOperationInstance, inputs: GraphInput)
       extends MagicOutput(instance) {
     val symmetric = edgeBundle(inputs.vs.entity, inputs.vs.entity)
     val injection = edgeBundle(
       symmetric.idSet, inputs.es.idSet, EdgeBundleProperties.embedding)
   }
-  def fromJson(j: JsValue) = RemoveNonSymmetricEdges()
+  def fromJson(j: JsValue) = MakeEdgeBundleSymmetric()
 }
-import RemoveNonSymmetricEdges._
-case class RemoveNonSymmetricEdges() extends TypedMetaGraphOp[GraphInput, Output] {
+import MakeEdgeBundleSymmetric._
+case class MakeEdgeBundleSymmetric() extends TypedMetaGraphOp[GraphInput, Output] {
   override val isHeavy = true
   @transient override lazy val inputs = new GraphInput
 
@@ -28,13 +30,18 @@ case class RemoveNonSymmetricEdges() extends TypedMetaGraphOp[GraphInput, Output
     implicit val id = inputDatas
     val vsPart = inputs.vs.rdd.partitioner.get
     val es = inputs.es.rdd
-    val bySource = es.map {
+    val bySourceMulti = es.map {
       case (id, e) => e.src -> (id, e)
     }.groupBySortedKey(vsPart)
+
+    val bySourceUnique = bySourceMulti.map {
+      case (id, e) => (id, e.seq.head)
+    }.groupBySortedKey(vsPart)
+
     val byDest = es.map {
       case (id, e) => e.dst -> e.src
     }.groupBySortedKey(vsPart).mapValues(_.toSet)
-    val edges = bySource.sortedJoin(byDest).flatMap {
+    val edges = bySourceUnique.sortedJoin(byDest).flatMap {
       case (vertexId, (outEdges, inEdgeSources)) =>
         outEdges.collect {
           case (id, outEdge) if inEdgeSources.contains(outEdge.dst) => id -> outEdge
