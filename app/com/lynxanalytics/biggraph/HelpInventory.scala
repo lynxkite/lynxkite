60e2a73e98aecbb3a51e79c475ff887aaa82cfec
Daniel Darabos <darabos.daniel@gmail.com>
2015-06-24 13:53:57 +0200

    Check help document against the inventory.

--- app/com/lynxanalytics/biggraph/HelpInventory.scala
+++ app/com/lynxanalytics/biggraph/HelpInventory.scala
@@ -1,11 +1,14 @@
-// Prints a list of identifiers that must exist in the help pages.
+// Writes to a file a list of identifiers that must exist in the help pages.
 package com.lynxanalytics.biggraph
 
 import com.lynxanalytics.biggraph.controllers.Operations
 
 object HelpInventory extends App {
   val ops = new Operations(null)
+  val fos = new java.io.FileOutputStream(args.head)
   for (op <- ops.operationIds.sorted) {
-    println(op)
+    fos.write(op.getBytes)
+    fos.write('\n')
   }
+  fos.close()
 }
