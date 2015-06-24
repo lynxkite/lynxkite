18d2a2f6ef5e15e6b2849afdc77b2151d890c27b
Daniel Darabos <darabos.daniel@gmail.com>
2015-06-24 14:21:25 +0200

    Fix missing/misplaced documentation.

--- test_everything.sh
+++ test_everything.sh
@@ -18,7 +18,9 @@ cat 'web/.tmp/help.html' | sed -n 's/.*h4 id="\([^"]*\)".*/\1/p' | sort > 'web/.
 sbt --error 'run-main com.lynxanalytics.biggraph.HelpInventory web/.tmp/help-inventory'
 MISSING=$(diff -u 'web/.tmp/help-inventory' 'web/.tmp/help-links' | sed -n 's/^-\(\w\)/  \1/p')
 if [ "$MISSING" != "" ]; then
-  >&2 echo "[ERROR] Documentation is missing for:"
+  >&2 echo '[ERROR] Documentation is missing for:'
   >&2 echo "$MISSING"
   exit 1
+else
+  echo 'Documentation verified.'
 fi
