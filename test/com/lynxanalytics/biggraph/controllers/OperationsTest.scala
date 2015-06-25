049b6c2c6ac57246c56380338970887bbe5caa5b
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-06-25 12:01:11 +0200

    Fix test

--- test/com/lynxanalytics/biggraph/controllers/OperationsTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/OperationsTest.scala
@@ -684,11 +684,12 @@ class OperationsTest extends FunSuite with TestGraphOp with BigGraphEnvironment 
       "direction" -> "incoming edges",
       // "aggregate-comment" -> "", This is now optional
       "aggregate-weight" -> "sum"))
-    run("Aggregate edge attribute to vertices", Map(
-      "prefix" -> "incoming",
-      // "direction" -> "incoming edges", But this is not
-      "aggregate-comment" -> "",
-      "aggregate-weight" -> "sum"))
-
+    intercept[java.lang.AssertionError] {
+      run("Aggregate edge attribute to vertices", Map(
+        "prefix" -> "incoming",
+        // "direction" -> "incoming edges", But this is not
+        "aggregate-comment" -> "",
+        "aggregate-weight" -> "sum"))
+    }
   }
 }
