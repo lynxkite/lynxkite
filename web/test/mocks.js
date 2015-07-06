c1cc4c6ad3418e5adb424b8861af0476fb585cbb
Daniel Darabos <darabos.daniel@gmail.com>
2015-07-06 12:11:31 +0200

    Fix tests.

--- web/test/mocks.js
+++ web/test/mocks.js
@@ -2,8 +2,10 @@ module.exports = {
   addTo: function(browser) {
     browser.addMockModule('biggraph', function() {
       angular.module('biggraph').service('sparkStatusUpdater', function() {
-        this.bind = function() {};
-      });       
+        this.bind = function(scope, name) {
+          scope[name] = { timestamp: 0, activeStages: [], pastStages: [] };
+        };
+      });
     });
   },
 };
