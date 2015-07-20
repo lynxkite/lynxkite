f98a1ea8dbd368069bfa99959b11689bef59c435
Daniel Darabos <darabos.daniel@gmail.com>
2015-07-20 18:21:41 +0200

    Fix InducedEdgeBundleTest.

--- test/com/lynxanalytics/biggraph/graph_operations/InducedEdgeBundleTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/InducedEdgeBundleTest.scala
@@ -8,8 +8,8 @@ import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class InducedEdgeBundleTest extends FunSuite with TestGraphOp {
   val example = ExampleGraph()().result
-  val adamless = {
-    val op = VertexAttributeFilter(NotFilter(OneOf(Set("Adam"))))
+  val bobless = {
+    val op = VertexAttributeFilter(NotFilter(OneOf(Set("Bob"))))
     op(op.attr, example.name).result
   }
 
@@ -17,26 +17,29 @@ class InducedEdgeBundleTest extends FunSuite with TestGraphOp {
     val op = InducedEdgeBundle()
     val induced = op(
       op.edges, example.edges)(
-        op.srcMapping, ReverseEdges.run(adamless.identity))(
-          op.dstMapping, ReverseEdges.run(adamless.identity))
+        op.srcMapping, ReverseEdges.run(bobless.identity))(
+          op.dstMapping, ReverseEdges.run(bobless.identity))
       .result.induced.toPairSeq
-    assert(induced == Seq(2 -> 1))
+    assert(induced == Seq(0 -> 1, 1 -> 0, 4 -> 5, 5 -> 4, 5 -> 4, 5 -> 7, 6 -> 4))
   }
 
   test("example graph induce src") {
     val op = InducedEdgeBundle(induceDst = false)
     val induced = op(
       op.edges, example.edges)(
-        op.srcMapping, ReverseEdges.run(adamless.identity)).result.induced.toPairSeq
-    assert(induced == Seq(1 -> 0, 2 -> 0, 2 -> 1))
+        op.srcMapping, ReverseEdges.run(bobless.identity)).result.induced.toPairSeq
+    assert(induced ==
+      Seq(0 -> 1, 1 -> 0, 4 -> 2, 4 -> 2, 4 -> 5, 5 -> 2, 5 -> 4, 5 -> 4, 5 -> 7, 6 -> 4))
   }
 
   test("example graph induce dst") {
     val op = InducedEdgeBundle(induceSrc = false)
     val induced = op(
       op.edges, example.edges)(
-        op.dstMapping, ReverseEdges.run(adamless.identity)).result.induced.toPairSeq
-    assert(induced == Seq(0 -> 1, 2 -> 1))
+        op.dstMapping, ReverseEdges.run(bobless.identity)).result.induced.toPairSeq
+    assert(induced ==
+      Seq(0 -> 1, 1 -> 0, 2 -> 0, 2 -> 1, 2 -> 4, 2 -> 5, 2 -> 5, 4 -> 5, 5 -> 4,
+        5 -> 4, 5 -> 7, 6 -> 4))
   }
 
   test("example graph induce on merged") {
@@ -52,6 +55,6 @@ class InducedEdgeBundleTest extends FunSuite with TestGraphOp {
             op.dstMapping, merge.belongsTo)
         .result.induced
     }
-    assert(induced.toPairCounts == Map((1, 0) -> 1, (0, 1) -> 2, (0, 0) -> 1))
+    assert(induced.toPairCounts == Map((7, 3) -> 4, (7, 7) -> 4, (3, 3) -> 2, (3, 7) -> 6))
   }
 }
