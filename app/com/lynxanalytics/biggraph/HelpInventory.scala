a7d0352cc38cd38a81b88593dec23c382b7e57c9
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-07-27 19:05:13 +0200

    Allow hidden operations to be speficied in a separate file

--- app/com/lynxanalytics/biggraph/HelpInventory.scala
+++ app/com/lynxanalytics/biggraph/HelpInventory.scala
@@ -1,14 +1,22 @@
 // Writes to a file a list of identifiers that must exist in the help pages.
 package com.lynxanalytics.biggraph
 
+import java.io.FileInputStream
+
 import com.lynxanalytics.biggraph.frontend_operations.Operations
 
 object HelpInventory extends App {
+  import scala.io.Source
+  val hiddenOps = Source.fromFile(args(1)).getLines
+    .map { _.toLowerCase }.toSet
+  println(hiddenOps)
   val ops = new Operations(null)
-  val fos = new java.io.FileOutputStream(args.head)
+  val fos = new java.io.FileOutputStream(args(0))
   for (op <- ops.operationIds.map(_.toLowerCase).sorted) {
-    fos.write(op.getBytes)
-    fos.write('\n')
+    if (!hiddenOps.contains(op)) {
+      fos.write(op.getBytes)
+      fos.write('\n')
+    }
   }
   fos.close()
 }
