bb126ce34883d5cbbceb63bb756a633f9a0babdc
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-07-27 16:32:37 +0200

    Replace example graph with enhanced example graph in some tests; also fix add reversed edges test

--- test/com/lynxanalytics/biggraph/graph_operations/AddReversedEdgesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/AddReversedEdgesTest.scala
@@ -7,10 +7,14 @@ import com.lynxanalytics.biggraph.graph_api.Scripting._
 import com.lynxanalytics.biggraph.graph_api.GraphTestUtils._
 
 class AddReversedEdgesTest extends FunSuite with TestGraphOp {
-  test("example graph") {
-    val g = ExampleGraph()().result
+  test("Enhanced example graph") {
+    val g = EnhancedExampleGraph()().result
     val op = AddReversedEdges()
     val out = op(op.es, g.edges).result
-    assert(out.esPlus.toPairSeq.toSet == Set(0 -> 1, 1 -> 0, 0 -> 2, 2 -> 0, 1 -> 2, 2 -> 1))
+    assert(out.esPlus.toPairSeq ==
+      Seq((0, 1), (0, 1), (0, 2), (1, 0), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2), (2, 2),
+        (2, 4), (2, 4), (2, 4), (2, 5), (2, 5), (2, 5), (2, 5), (2, 5), (4, 2), (4, 2), (4, 2),
+        (4, 5), (4, 5), (4, 5), (4, 5), (4, 6), (5, 2), (5, 2), (5, 2), (5, 2), (5, 2), (5, 4),
+        (5, 4), (5, 4), (5, 4), (5, 7), (6, 4), (7, 5)))
   }
 }
--- test/com/lynxanalytics/biggraph/graph_operations/RemoveNonSymmetricEdgesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/RemoveNonSymmetricEdgesTest.scala
@@ -7,10 +7,12 @@ import com.lynxanalytics.biggraph.graph_api.GraphTestUtils._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class RemoveNonSymmetricEdgesTest extends FunSuite with TestGraphOp {
-  test("example graph") {
-    val g = ExampleGraph()().result
+  test("Enhanced example graph") {
+    val g = EnhancedExampleGraph()().result
     val op = RemoveNonSymmetricEdges()
     val out = op(op.es, g.edges).result
-    assert(out.symmetric.toPairSeq == Seq(0 -> 1, 1 -> 0))
+    assert(out.symmetric.toPairSeq == Seq((0, 1), (1, 0), (2, 2), (2, 4), (2, 5), (2, 5),
+      (2, 5), (4, 2), (4, 2), (4, 5), (5, 2), (5, 2), (5, 4), (5, 4), (5, 4)))
+
   }
 }
