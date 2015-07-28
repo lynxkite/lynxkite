e80150d569d9d764051a5cefb508da2220e9664b
Daniel Darabos <darabos.daniel@gmail.com>
2015-07-28 11:31:29 +0200

    Histograms in batch mode.

--- app/com/lynxanalytics/biggraph/BatchMain.scala
+++ app/com/lynxanalytics/biggraph/BatchMain.scala
@@ -3,34 +3,46 @@
 
 package com.lynxanalytics.biggraph
 
+import scala.io.Source
+import play.api.libs.json
+
 import com.lynxanalytics.biggraph.graph_api.SymbolPath
 import com.lynxanalytics.biggraph.{ bigGraphLogger => log }
-import com.lynxanalytics.biggraph.controllers.Operation
+import com.lynxanalytics.biggraph.controllers._
 import com.lynxanalytics.biggraph.frontend_operations.Operations
-import com.lynxanalytics.biggraph.controllers.Project
-import com.lynxanalytics.biggraph.controllers.WorkflowOperation
-import com.lynxanalytics.biggraph.graph_api.MetaGraphManager
-import com.lynxanalytics.biggraph.graph_api.Scalar
+import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
+import com.lynxanalytics.biggraph.serving.ProductionJsonServer._
 import com.lynxanalytics.biggraph.serving.User
-import scala.io.Source
 
 object BatchMain {
   private val commentRE = "#.*".r
-  private val scalarArgPtrn = raw"\s*\(\s*'([^']+)'\s*,\s*'([^']+)'\s*\)"
-  private val scalarRE = ("GetScalar" + scalarArgPtrn).r
-  private val scalarBenchRE = ("BenchmarkScalar" + scalarArgPtrn).r
+  private val twoArgPtrn = raw"\s*\(\s*'([^']+)'\s*,\s*'([^']+)'\s*\)"
+  private val scalarRE = ("GetScalar" + twoArgPtrn).r
+  private val scalarBenchRE = ("BenchmarkScalar" + twoArgPtrn).r
   private val opsRE = raw"Operations\s*\(\s*'([^']+)'\s*\)".r
   private val opsEnd = "EndOperations"
   private val waitForever = "WaitForever"
   private val resetTimer = "ResetTimer"
+  private val histogramRE = ("Histogram" + twoArgPtrn).r
 
   def getScalarMeta(
-    projectName: String, scalarName: String, params: Map[String, String])(
+    projectName: String, scalarName: String)(
       implicit metaManager: MetaGraphManager): Scalar[_] = {
     Project.validateName(projectName)
     val project = Project.fromName(projectName)
     project.scalars(scalarName)
+  }
+
+  def getAttributeMeta(
+    projectName: String, attributeName: String)(
+      implicit metaManager: MetaGraphManager): Attribute[_] = {
+    Project.validateName(projectName)
+    val project = Project.fromName(projectName)
+    if (project.vertexAttributes.contains(attributeName))
+      project.vertexAttributes(attributeName)
+    else
+      project.edgeAttributes(attributeName)
   }
 
   def main(args: Array[String]) {
@@ -46,7 +58,7 @@ Usage:
 parameter_values is list of items in the format parameter_name:parameter_value
 
 For example:
-./run-kite.sh batch my_script.json seed:42 input_file_name:data1.csv
+./run-kite.sh batch my_script seed:42 input_file_name:data1.csv
 """)
       System.exit(-1)
     }
@@ -62,6 +74,8 @@ For example:
       }
       .toMap
 
+    val drawing = new GraphDrawingController(env)
+    val user = User("Batch User", isAdmin = true)
     val lit = Source.fromFile(scriptFileName).getLines()
     var timer = System.currentTimeMillis
     while (lit.hasNext) {
@@ -72,12 +86,12 @@ For example:
         case "" => ()
         case scalarRE(projectNameSpec, scalarName) =>
           val projectName = WorkflowOperation.substituteUserParameters(projectNameSpec, params)
-          val scalar = getScalarMeta(projectName, scalarName, params)
+          val scalar = getScalarMeta(projectName, scalarName)
           log.info(s"Value of scalar ${scalarName} on project ${projectName}: ${scalar.value}")
           println(s"${projectName}|${scalarName}|${scalar.value}")
         case scalarBenchRE(projectNameSpec, scalarName) =>
           val projectName = WorkflowOperation.substituteUserParameters(projectNameSpec, params)
-          val scalar = getScalarMeta(projectName, scalarName, params)
+          val scalar = getScalarMeta(projectName, scalarName)
           val rc0 = dataManager.runtimeContext
           val t0 = System.nanoTime
           val value = scalar.value
@@ -97,7 +111,6 @@ For example:
         case opsRE(projectNameSpec) =>
           val projectName = WorkflowOperation.substituteUserParameters(projectNameSpec, params)
           Project.validateName(projectName)
-          val user = User("Batch User", isAdmin = true)
           val project = Project.fromName(projectName)
 
           if (!Operation.projects.contains(project)) {
@@ -126,6 +139,16 @@ For example:
               op.validateAndApply(step.op.parameters)
             }
           }
+        case histogramRE(project, attr) =>
+          val req = HistogramSpec(
+            attributeId = getAttributeMeta(project, attr).gUID.toString,
+            vertexFilters = Seq(),
+            numBuckets = 10,
+            axisOptions = AxisOptions(logarithmic = false))
+          val res = drawing.getHistogram(user, req)
+          val j = json.Json.toJson(res).toString
+          log.info(s"Histogram for $attr on project $project: $j")
+          println(s"$project|$attr|$j")
         case `waitForever` =>
           println("Waiting indefinitely...")
           this.synchronized { this.wait() }
