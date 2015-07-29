9c88b99116449717820ebe556e41e305747e1035
gsvigruha <gergely.svigruha@lynxanalytics.com>
2015-07-29 22:27:39 +0800

    Delete orphan files button added.

--- web/app/scripts/controllers/cleaner.js
+++ web/app/scripts/controllers/cleaner.js
@@ -5,11 +5,19 @@ angular.module('biggraph')
   .controller('CleanerCtrl', function ($scope, util) {
     $scope.fileStatus = util.nocache('/ajax/getDataFilesStatus');
 
-    $scope.deleteOrphanFiles = function() {
-      console.log($scope.selectedMethod);
+    $scope.markFilesDeleted = function() {
       util
         .post('/ajax/markFilesDeleted', {
           method: $scope.selectedMethod,
+        }).$status.then(function() {
+          $scope.fileStatus = util.nocache('/ajax/getDataFilesStatus');
+        });
+    };
+
+    $scope.deleteOrphanFiles = function() {
+      util
+        .post('/ajax/deleteOrphanFiles', {
+          fake: 0,
         }).$status.then(function() {
           $scope.fileStatus = util.nocache('/ajax/getDataFilesStatus');
         });
