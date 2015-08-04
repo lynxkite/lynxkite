5e610e02eb96b1f3178c7d78a31a131d8ca37fc4
Andras Nemeth <xxandreww@gmail.com>
2015-08-04 15:37:24 +0200

    Some small comments

--- app/com/lynxanalytics/biggraph/controllers/Project.scala
+++ app/com/lynxanalytics/biggraph/controllers/Project.scala
@@ -584,6 +584,8 @@ class SegmentationEditor(
   implicit val manager = parent.manager
 
   {
+    // If this segmentation editor is called with new segmentationName then we
+    // create the corresponding SegmentationState in the project state's segmentations field.
     val oldSegs = parent.state.segmentations
     if (!oldSegs.contains(segmentationName)) {
       parent.state = parent.state.copy(
@@ -631,7 +633,8 @@ class SegmentationEditor(
     parent.setLastOperationRequest(n.copy(path = segmentationName +: n.path))
 }
 
-// Represents a mutable, named project. State is stored using tags.
+// Represents a mutable, named project. It can be seen as a modifiable pointer into the
+// checkpoint tree with some additional metadata. ProjectFrame's data is persisted using tags.
 class ProjectFrame(val projectPath: SymbolPath)(
     implicit manager: MetaGraphManager) {
   val projectName = projectPath.toString
@@ -670,7 +673,7 @@ class ProjectFrame(val projectPath: SymbolPath)(
   def redo(): Unit = manager.synchronized {
     val next = nextCheckpoint.get
     checkpoint = next
-    lazy val reverseCheckpoints: Stream[String] = farthestCheckpoint #:: reverseCheckpoints.map {
+    val reverseCheckpoints = Stream.iterate(farthestCheckpoint) {
       next => getCheckpointState(next).previousCheckpoint.get
     }
     nextCheckpoint = reverseCheckpoints.takeWhile(_ != next).lastOption
@@ -682,6 +685,8 @@ class ProjectFrame(val projectPath: SymbolPath)(
     nextCheckpoint = None
   }
 
+  // Initializes a new project. One needs to call this to make the initial preparations
+  // for a project in the tag tree.
   def initialize: Unit = manager.synchronized {
     checkpoint = ""
     nextCheckpoint = None
