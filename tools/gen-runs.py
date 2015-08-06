34967b7cbd2571ab9ec6f5384e53331f3c374095
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2015-08-06 16:46:26 +0200

    Tools to run perf tests in batch

---
+++ tools/gen-runs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+def gen_runs(globs, exec_mems, exec_coress, vs_per_partitions, caches):
+  for glob in globs:
+    for exec_mem in exec_mems:
+      for exec_cores in exec_coress:
+        for vs_per_partition in vs_per_partitions:
+          for cache in caches:
+            print "EXECUTOR_MEMORY=%s NUM_CORES_PER_EXECUTOR=%s KITE_VERTICES_PER_PARTITION=%s GLOB='%s' CACHE_IN_IMPORT=%s biggraphstage/tools/one-run.sh" % (exec_mem, exec_cores, vs_per_partition, glob, cache)
+
+"""
+gen_runs(
+  ['S3$kite-benchmark/inputs/fast-random-10M-100M-2/data/part-*'],
+  ['10g', '20g'],
+  [1,8],
+  [200, 400, 800, 1600, 3200],
+  ['false'])
+"""
+gen_runs(
+  ['S3$kite-benchmark/inputs/scale-free-random-1M-10M/data/part-*'],
+  ['10g', '20g'],
+  [1,8],
+  [100000, 10000, 1000],
+  ['false'])
+
+
