ee2890dd1bfb801288e9dbe30e271c8ffa78a896
Daniel Darabos <darabos.daniel@gmail.com>
2015-08-19 15:14:00 +0200

    Fix typo in comment.

--- app/com/lynxanalytics/biggraph/spark_util/BigGraphSparkContext.scala
+++ app/com/lynxanalytics/biggraph/spark_util/BigGraphSparkContext.scala
@@ -179,7 +179,7 @@ object BigGraphSparkContext {
         "spark.scheduler.allocation.file",
         scala.util.Properties.envOrElse("KITE_SCHEDULER_POOLS_CONFIG", "conf/scheduler-pools.xml"))
       // We need a higher akka.frameSize (the Spark default is 10) as when the number of
-      // partitions get into the thousands the map output statuses exeeds this limit.
+      // partitions get into the thousands the map output statuses exceed this limit.
       .setIfMissing(
         "spark.akka.frameSize", "100")
     if (useKryo) {
