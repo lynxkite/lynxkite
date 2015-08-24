37a2a01f513eb0849830d8ba1739f896bc3de360
gsvigruha <gergely.svigruha@lynxanalytics.com>
2015-08-24 16:55:24 +0800

    Backup .kiterc.

--- conf/kite_meta_hdfs_backup.sh
+++ conf/kite_meta_hdfs_backup.sh
@@ -2,16 +2,18 @@
 # Add the script to the crontab e.g. like this:
 # 59 22 * * * $LOCATION_TO_SCRIPT
 
-# export KITE_META=kite_meta
-# export TMP_BACKUP_DIR=/tmp/kite_meta_backup
-# export KITE_META_PARENT_DIR=$HOME
 # export KITERC_FILE=$HOME/.kiterc
+# export TMP_BACKUP_DIR=/tmp/kite_meta_backup
 # export HDFS_BACKUP_DIR=/user/$USER/backup
+
+# Loading $KITE_META_DIR from .kiterc.
+source $KITERC_FILE
+export KITE_META=$(basename $KITE_META_DIR)
 
 # Copy the kite meta dir to a temporary location for cleansing.
 rm -r $TMP_BACKUP_DIR
 mkdir $TMP_BACKUP_DIR
-cp -r $KITE_META_PARENT_DIR/$KITE_META $TMP_BACKUP_DIR/
+cp -r $KITE_META_DIR $TMP_BACKUP_DIR/
 
 # Remove unnecessary lines from tags.journal to save space.
 for FILE in $TMP_BACKUP_DIR/$KITE_META/*/tags.journal
@@ -21,10 +23,12 @@ do
   rm $TMP_BACKUP_DIR/tags.journal.tmp 
 done
 
+tar -czf $TMP_BACKUP_DIR/kite_meta.tgz $TMP_BACKUP_DIR/$KITE_META
+
 # Copy a backup to HDFS.
 export CURRENT_DATE=$(date +"%y%m%d")
 export HDFS_BACKUP_DIR=$HDFS_BACKUP_DIR/$CURRENT_DATE
 hadoop fs -mkdir -p $HDFS_BACKUP_DIR
 hadoop fs -put  $KITERC_FILE $HDFS_BACKUP_DIR/
-hadoop fs -put  $TMP_BACKUP_DIR/$KITE_META $HDFS_BACKUP_DIR/
+hadoop fs -put  $TMP_BACKUP_DIR/kite_meta.tgz $HDFS_BACKUP_DIR/
 
