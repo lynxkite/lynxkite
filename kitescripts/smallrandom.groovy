3da65fcc338bf02a6b3574b88748c30753b958b8
Daniel Darabos <darabos.daniel@gmail.com>
2015-08-25 11:05:11 +0200

    Pass parameters via a "params" map.

--- kitescripts/smallrandom.groovy
+++ kitescripts/smallrandom.groovy
@@ -1,7 +1,7 @@
 // Finds cliques in a small random graph.
-project = new Project(project)
+project = new Project(params['project'])
 project.newVertexSet(size: 10)
-project.createRandomEdgeBundle(degree: 5, seed: seed)
+project.createRandomEdgeBundle(degree: 5, seed: params['seed'])
 project.maximalCliques(name: 'maximal_cliques', bothdir: false, min: 2)
 
 cliques = project.segmentations['maximal_cliques']
