41b46477b012895bc61f1f6147f803e5706db979
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-09-08 12:36:17 +0200

    Remove copy-pasted test

--- test/com/lynxanalytics/biggraph/frontend_operations/ExportImportOperationTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/ExportImportOperationTest.scala
@@ -68,23 +68,7 @@ class ExportImportOperationTest extends OperationsTestBase {
     assert(income.rdd.values.collect.toSeq.sorted == Seq("", "", "1000.0", "2000.0"))
   }
 
-  test("Discard loop edges") {
-    run("Import vertices and edges from single CSV fileset", Map(
-      "files" -> "OPERATIONSTEST$/loop-edges.csv",
-      "header" -> "src,dst,color",
-      "delimiter" -> ",",
-      "src" -> "src",
-      "dst" -> "dst",
-      "omitted" -> "",
-      "allowCorruptLines" -> "yes",
-      "filter" -> ""))
-    def colors =
-      project.edgeAttributes("color").runtimeSafeCast[String].rdd.values.collect.toSeq.sorted
-    assert(colors == Seq("blue", "green", "red"))
-    run("Discard loop edges")
-    assert(colors == Seq("blue", "green")) // "red" was the loop edge.
-  }
-
+  
   def runImport(allowCorruptLines: Boolean) = {
     val allow =
       if (allowCorruptLines) "yes"
