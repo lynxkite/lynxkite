5eec126bcb908f6bf546a49ef406d3cb23ea37a4
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2015-09-17 15:02:25 +0200

    Better names for test declaring methods, better parameter order.

--- web/test/real-backend/example-graph-basics.js
+++ web/test/real-backend/example-graph-basics.js
@@ -1,24 +1,24 @@
 'use strict';
 
 module.exports = function(fw) {
-  fw.newUIState(
-    'empty test-example project',
+  fw.transitionTest(
     'empty splash',
     function(lib) {
       lib.openNewProject('test-example');
     },
+    'empty test-example project',
     function(lib) {
       lib.expectCurrentProjectIs('test-example');
     });
-  fw.newUIState(
-    'test-example project with example graph',
+  fw.transitionTest(
     'empty test-example project',
     function(lib) {
       lib.runOperation('example graph');
     },
+    'test-example project with example graph',
     function() {
     });
-  fw.newIdempotentTest(
+  fw.statePreservingTest(
     'test-example project with example graph',
     'has the proper vertex count',
     function(lib) {
