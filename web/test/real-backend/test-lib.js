a95e792505e2ea98a1ab6807f0d886329fce73f9
Gabor Feher <feherga@gmail.com>
2015-09-18 12:12:19 +0200

    Remove xpath :( from help popup e2e tests

--- web/test/real-backend/test-lib.js
+++ web/test/real-backend/test-lib.js
@@ -7,21 +7,30 @@
 module.exports = (function() {
   var K = protractor.Key;  // Short alias.
   return {
-    openNewProject: function(name) {
-      element(by.id('new-project')).click();
-      element(by.id('new-project-name')).sendKeys(name, K.ENTER);
-    },
     expectCurrentProjectIs: function(name) {
       expect(browser.getCurrentUrl()).toContain('/#/project/' + name);
     },
-    runLeftOperation: function(name) {
-      element(by.css('#operation-toolbox-left #operation-search')).click();
-      element(by.css('#operation-toolbox-left #filter')).sendKeys(name, K.ENTER);
-      element(by.css('#operation-toolbox-left .ok-button')).click();
+    expectHelpPopupVisible: function(helpId, isVisible) {
+      expect(element(by.css('div[help-id="' + helpId + '"]')).isDisplayed()).toBe(isVisible);
     },
     leftVertexCount: function() {
       var asStr = element(by.css('#side-left value.vertex-count span.value')).getText();
       return asStr.then(function(asS) { return parseInt(asS); });
+    },
+    openLeftOperation: function(name) {
+      element(by.css('#operation-toolbox-left #operation-search')).click();
+      element(by.css('#operation-toolbox-left #filter')).sendKeys(name, K.ENTER);
+    },
+    openNewProject: function(name) {
+      element(by.id('new-project')).click();
+      element(by.id('new-project-name')).sendKeys(name, K.ENTER);
+    },
+    runLeftOperation: function(name) {
+      this.openLeftOperation(name);
+      element(by.css('#operation-toolbox-left .ok-button')).click();
+    },
+    toggleLeftSampledVisualization: function() {
+      element(by.css('#side-left label[btn-radio="\'sampled\'"]')).click();
     },
   };
 })();
