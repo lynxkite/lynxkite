d22f2ca2da24abd27f24cec2ed0208b831438755
Andras Nemeth <xxandreww@gmail.com>
2015-09-18 14:07:02 +0200

    Addressed review comments even more

--- web/test/real-backend/test-lib.js
+++ web/test/real-backend/test-lib.js
@@ -7,36 +7,43 @@
 module.exports = (function() {
   var K = protractor.Key;  // Short alias.
   return {
+    evaluateOnLeftSide: function(expr) {
+      return element(by.css('#side-left')).evaluate(expr);
+    },
+
+    expectCurrentProjectIs: function(name) {
+      expect(browser.getCurrentUrl()).toContain('/#/project/' + name);
+    },
+
+    leftApplyFilters: function() {
+      return element(by.css('#side-left #apply-filters-button')).click();
+    },
+
+    leftEdgeCount: function() {
+      var asStr = element(by.css('#side-left value#edge-count span.value')).getText();
+      return asStr.then(function(asS) { return parseInt(asS); });
+    },
+
+    leftVertexCount: function() {
+      var asStr = element(by.css('#side-left value#vertex-count span.value')).getText();
+      return asStr.then(function(asS) { return parseInt(asS); });
+    },
+
     openNewProject: function(name) {
       element(by.id('new-project')).click();
       element(by.id('new-project-name')).sendKeys(name, K.ENTER);
     },
-    expectCurrentProjectIs: function(name) {
-      expect(browser.getCurrentUrl()).toContain('/#/project/' + name);
-    },
+
     runLeftOperation: function(name) {
       element(by.css('#operation-toolbox-left #operation-search')).click();
       element(by.css('#operation-toolbox-left #filter')).sendKeys(name, K.ENTER);
       element(by.css('#operation-toolbox-left .ok-button')).click();
     },
-    leftVertexCount: function() {
-      var asStr = element(by.css('#side-left value#vertex-count span.value')).getText();
-      return asStr.then(function(asS) { return parseInt(asS); });
-    },
-    leftEdgeCount: function() {
-      var asStr = element(by.css('#side-left value#edge-count span.value')).getText();
-      return asStr.then(function(asS) { return parseInt(asS); });
-    },
+
     setLeftAttributeFilter: function(attributeName, filterValue) {
       var filterBox = element(
         by.css('#side-left .attribute input[name="' + attributeName + '"]'));
       filterBox.sendKeys(filterValue, K.ENTER);    
-    },
-    evaluateOnLeftSide: function(expr) {
-      return element(by.css('#side-left')).evaluate(expr);
-    },
-    leftApplyFilters: function() {
-      return element(by.css('#side-left #apply-filters-button')).click();
     },
   };
 })();
