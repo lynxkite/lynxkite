f575d4d5303d4fc5ebf2acca4279221d8c67c46f
Daniel Darabos <darabos.daniel@gmail.com>
2015-09-23 18:01:33 +0200

    Test for directory and project creation, renaming, moving, and deletion.

--- web/test/real-backend/splash-page.js
+++ web/test/real-backend/splash-page.js
@@ -22,4 +22,45 @@ module.exports = function(fw) {
     function() {
       lib.expectCurrentProjectIs('test-example');
     });
+
+  fw.transitionTest(
+    'empty test-example project',
+    'test-example renamed and moved',
+    function() {
+      // Go to project list.
+      lib.left.close();
+      lib.splash.expectProject('test-example');
+
+      // Create directory.
+      lib.splash.newDirectory('test-dir');
+      lib.splash.expectNotProject('test-example');
+
+      // Go back and move the project into the directory.
+      lib.splash.popDirectory();
+      lib.splash.expectProject('test-example');
+      lib.splash.moveProject('test-example', 'test-dir');
+      lib.splash.expectNotProject('test-example');
+
+      // Open directory.
+      lib.splash.openDirectory('test-dir');
+      lib.splash.expectProject('test-example');
+
+      // Rename project.
+      lib.splash.renameProject('test-example', 'test-renamed');
+      lib.splash.expectProject('test-renamed');
+      lib.splash.expectNotProject('test-example');
+
+      // Delete project.
+      lib.splash.deleteProject('test-renamed');
+      lib.splash.expectNotProject('test-renamed');
+
+      // Go back and delete the directory.
+      lib.splash.popDirectory();
+      lib.splash.expectNotProject('test-renamed');
+      lib.splash.expectDirectory('test-dir');
+      lib.splash.deleteDirectory('test-dir');
+      lib.splash.expectNotDirectory('test-dir');
+    },
+    function() {
+    });
 };
