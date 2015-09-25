8c3c6a47858a6e2edc05fd510290c2efbcdb3b8a
xandrew-lynx <xxandreww@gmail.com>
2015-09-25 17:23:21 +0200

    getTheRandomPattern -> theRandomPattern

--- web/test/real-backend/declarative-test.js
+++ web/test/real-backend/declarative-test.js
@@ -95,7 +95,7 @@ fw.runAll();
 var deleteTemporaryFiles = function() {
   var lib = require('./test-lib.js');
   var fs = require('fs');
-  var pattern = lib.getTheRandomPattern;
+  var pattern = lib.theRandomPattern;
 
   fs.readdir('/tmp', function (error, files) {
     if (error) {
