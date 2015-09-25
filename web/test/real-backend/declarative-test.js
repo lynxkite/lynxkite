cc274897c0a05208632228b19fbd577ff930c81f
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-09-25 11:45:55 +0200

    Include unique,random string in downloaded files; delete these after the tests

--- web/test/real-backend/declarative-test.js
+++ web/test/real-backend/declarative-test.js
@@ -90,3 +90,37 @@ require('./center-picker.js')(fw);
 require('./splash-page.js')(fw);
 
 fw.runAll();
+
+
+var deleteTemporaryFiles = function() {
+  var lib = require('./test-lib.js');
+  var fs = require('fs');
+  var pattern = lib.getTheRandomPathPrefix();
+
+  fs.readdir('/tmp', function (error, files) {
+    if (error) {
+      throw error;
+    }
+    for (var i = 0; i < files.length; i++) {
+      var f = files[i];
+      if (f.indexOf(pattern) > -1) {
+        var full = '/tmp/' + f;
+        console.log('Deleting: ' + full);
+        fs.unlink(full);
+      }      
+    }
+  });
+};
+
+//
+// Override the finishCallback so we can add our cleanup function.
+// This is run after all tests have been completed.
+//
+var savedFinishCallback = jasmine.Runner.prototype.finishCallback;
+jasmine.Runner.prototype.finishCallback = function () {
+  // Run the old finishCallback
+  savedFinishCallback.bind(this)();
+
+  console.log('Cleaning up');
+  deleteTemporaryFiles();
+};
