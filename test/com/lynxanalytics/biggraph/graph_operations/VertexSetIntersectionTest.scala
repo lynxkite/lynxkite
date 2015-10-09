bf3303ab8d67029faffa808b676bef76dc45df22
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-10-09 13:00:30 +0200

    Fix VertexSetIntersectionTest

--- test/com/lynxanalytics/biggraph/graph_operations/VertexSetIntersectionTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/VertexSetIntersectionTest.scala
@@ -7,25 +7,30 @@ import com.lynxanalytics.biggraph.graph_api.GraphTestUtils._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class VertexSetIntersectionTest extends FunSuite with TestGraphOp {
-  ignore("intersect 3 sets") {
-    val aVS = 0 to 50
-    val bVS = 5 to 10
-    val cVS = 8 to 100
-    val a = SmallTestGraph(aVS.map(_.toInt -> Seq()).toMap).result.vs
-    val b = SmallTestGraph(bVS.map(_.toInt -> Seq()).toMap).result.vs
-    val c = SmallTestGraph(cVS.map(_.toInt -> Seq()).toMap).result.vs
+  private val parent = CreateVertexSet(100).result
+  private val opToDouble = AddDoubleVertexAttribute((0 until 100).map { a => (a, a.toDouble) }.toMap)
+  private val parentAttr = opToDouble(opToDouble.vs, parent.vs).result.attr
+
+  def getVertexSet(lo: Double, hi: Double) = {
+    val filter = AndFilter(DoubleGE(lo), DoubleLE(hi))
+    val op = VertexAttributeFilter(filter)
+    op(op.attr, parentAttr).result.fvs
+  }
+
+  test("intersect 3 sets") {
+    val aVS = getVertexSet(0, 50)
+    val bVS = getVertexSet(5, 10)
+    val cVS = getVertexSet(8, 99)
     val op = VertexSetIntersection(3)
-    val out = op(op.vss(0), a)(op.vss(1), b)(op.vss(2), c).result
+    val out = op(op.vss(0), aVS)(op.vss(1), bVS)(op.vss(2), cVS).result
     assert(out.intersection.toSeq == Seq(8, 9, 10))
   }
 
-  ignore("empty set") {
-    val aVS = 0 to 5
-    val bVS = 10 to 12
-    val a = SmallTestGraph(aVS.map(_.toInt -> Seq()).toMap).result.vs
-    val b = SmallTestGraph(bVS.map(_.toInt -> Seq()).toMap).result.vs
+  test("empty set") {
+    val aVS = getVertexSet(0, 5)
+    val bVS = getVertexSet(10, 12)
     val op = VertexSetIntersection(2)
-    val out = op(op.vss(0), a)(op.vss(1), b).result
+    val out = op(op.vss(0), aVS)(op.vss(1), bVS).result
     assert(out.intersection.toSeq == Seq())
   }
 }
