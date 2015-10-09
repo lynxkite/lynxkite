f55a520e799da89933b56f2c932d2769bef92027
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-10-09 14:55:14 +0200

    Use SmallTestGraph (which has its starting from 0); use friendlier naming

--- test/com/lynxanalytics/biggraph/graph_operations/VertexSetIntersectionTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/VertexSetIntersectionTest.scala
@@ -7,28 +7,32 @@ import com.lynxanalytics.biggraph.graph_api.GraphTestUtils._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class VertexSetIntersectionTest extends FunSuite with TestGraphOp {
-  private val parent = CreateVertexSet(100).result
-  private val opToDouble = AddDoubleVertexAttribute((0 until 100).map { a => (a, a.toDouble) }.toMap)
-  private val parentAttr = opToDouble(opToDouble.vs, parent.vs).result.attr
 
-  def getVertexSet(lo: Double, hi: Double) = {
+  private val age = {
+    val range = 0 until 100
+    val everybody = SmallTestGraph(range.map { id => (id, Seq[Int]()) }.toMap).result
+    val opToDouble = AddDoubleVertexAttribute(range.map { a => (a, a.toDouble) }.toMap)
+    opToDouble(opToDouble.vs, everybody.vs).result.attr
+  }
+
+  def agedBetween(lo: Double, hi: Double) = {
     val filter = AndFilter(DoubleGE(lo), DoubleLE(hi))
     val op = VertexAttributeFilter(filter)
-    op(op.attr, parentAttr).result.fvs
+    op(op.attr, age).result.fvs
   }
 
   test("intersect 3 sets") {
-    val aVS = getVertexSet(0, 50)
-    val bVS = getVertexSet(5, 10)
-    val cVS = getVertexSet(8, 99)
+    val aVS = agedBetween(0, 50)
+    val bVS = agedBetween(5, 10)
+    val cVS = agedBetween(8, 99)
     val op = VertexSetIntersection(3)
     val out = op(op.vss(0), aVS)(op.vss(1), bVS)(op.vss(2), cVS).result
     assert(out.intersection.toSeq == Seq(8, 9, 10))
   }
 
   test("empty set") {
-    val aVS = getVertexSet(0, 5)
-    val bVS = getVertexSet(10, 12)
+    val aVS = agedBetween(0, 5)
+    val bVS = agedBetween(10, 12)
     val op = VertexSetIntersection(2)
     val out = op(op.vss(0), aVS)(op.vss(1), bVS).result
     assert(out.intersection.toSeq == Seq())
