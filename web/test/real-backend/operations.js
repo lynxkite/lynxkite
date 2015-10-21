ca17d11e0b5fbdd7c2d97d79916ff14608d2235d
Daniel Darabos <darabos.daniel@gmail.com>
2015-10-21 14:12:43 +0200

    Do not expect specific error string.

--- web/test/real-backend/operations.js
+++ web/test/real-backend/operations.js
@@ -8,9 +8,9 @@ module.exports = function(fw) {
     'error reporting',
     function() {
       lib.left.runOperation('Import vertices from CSV files'); // Missing filename.
-      var expectedMessage = 'File name specifications should always start with a registered prefix (XYZ$)';
-      expect(lib.errors()).toEqual([expectedMessage]);
-      // Check that we can try submitting again. #2529
+      // There should be an error message.
+      expect(lib.errors()).toMatch(['.*']);
+      // Check that we can press OK again. (#2529) (It will give the same error.)
       lib.left.submitOperation(lib.left.toolbox);
       lib.closeErrors();
       lib.left.closeOperation();
