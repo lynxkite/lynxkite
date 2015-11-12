070f1b579eb20d44048dcef11ac31d7e4f1595f6
Daniel Darabos <darabos.daniel@gmail.com>
2015-11-12 10:29:50 +0100

    Revert "Let test traits register cleanup operations. Move DataManager.waitAllFutures to TestDataManager."
    This reverts commit efd3271da32b015bb0269df92a2bfce142a7cfad.

--- test/com/lynxanalytics/biggraph/graph_api/io/EntityIOTest.scala
+++ test/com/lynxanalytics/biggraph/graph_api/io/EntityIOTest.scala
@@ -7,8 +7,7 @@ import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_operations.EnhancedExampleGraph
 import com.lynxanalytics.biggraph.graph_util.{ PrefixRepository, HadoopFile }
 
-class EntityIOTest extends FunSuite with TestMetaGraphManager with TestDataManager
-    with TestCleanup {
+class EntityIOTest extends FunSuite with TestMetaGraphManager with TestDataManager {
 
   val resDir = "/graph_api/io/EntityIOTest/"
   val res = getClass.getResource(resDir).toString
