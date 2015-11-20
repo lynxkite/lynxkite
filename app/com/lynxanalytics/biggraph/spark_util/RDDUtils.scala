afa43c01fbbb7a8e5cd97a12baafe7d9eb396a96
Andras Nemeth <andras.nemeth@lynxanalytics.com>
2015-11-20 13:24:04 +0100

    Removes not directly supported case branch

--- app/com/lynxanalytics/biggraph/spark_util/RDDUtils.scala
+++ app/com/lynxanalytics/biggraph/spark_util/RDDUtils.scala
@@ -425,7 +425,6 @@ object Implicits {
       self match {
         case self: SortedRDD[K, V] if partitioner eq self.partitioner.get =>
           self
-        case self if partitioner eq self.partitioner.orNull => ???
         case self =>
           // Use ShuffledRDD instead of partitionBy to avoid re-using an equal but non-identical
           // partitioner.
@@ -441,7 +440,6 @@ object Implicits {
       self match {
         case self: UniqueSortedRDD[K, V] if partitioner eq self.partitioner.get =>
           self
-        case self if partitioner eq self.partitioner.orNull => ???
         case self =>
           // Use ShuffledRDD instead of partitionBy to avoid re-using an equal but non-identical
           // partitioner.
