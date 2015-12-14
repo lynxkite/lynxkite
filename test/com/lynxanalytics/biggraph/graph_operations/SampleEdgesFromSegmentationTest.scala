9a3e2ac922f6916a920b20dd714ee2b472d8ed81
Gabor Olah <gabor.olah@lynxanalytics.com>
2015-12-14 11:51:56 +0100

    Fix compile errors

--- test/com/lynxanalytics/biggraph/graph_operations/SampleEdgesFromSegmentationTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/SampleEdgesFromSegmentationTest.scala
@@ -24,6 +24,7 @@ class SampleEdgesFromSegmentationTest extends FunSuite with TestGraphOp {
       DeriveJS.deriveFromAttributes[String](
         s"(personAttr * ${numPersons}).toFixed()",
         Seq("personAttr" -> personAttr),
+        Seq(),
         vs1).attr
     }
     val segmentation1 = {
