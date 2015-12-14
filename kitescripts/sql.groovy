976f7ecc6d142ffe78e717fd8e80b80b71e22c80
Daniel Darabos <darabos.daniel@gmail.com>
2015-12-14 19:26:39 +0100

    Add Groovy access to DataFrame API.

---
+++ kitescripts/sql.groovy
@@ -0,0 +1,10 @@
+// Demonstration for using the DataFrames API on project RDDs.
+p = lynx.newProject('df-test')
+p.exampleGraph()
+p.df.show()
+p.df.groupBy('gender').count().show()
+p.df.registerTempTable('example')
+df = lynx.sql('select * from example where gender = "Male"')
+df.show()
+df.printSchema()
+p.importDF(df)
