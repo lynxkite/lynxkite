5fe4d9afa199fd1a2a8af0b1c384056ea452ccec
gsvigruha <gergely.svigruha@lynxanalytics.com>
2015-12-15 16:56:03 +0800

    Refactor args.

--- tools/memlog.sh
+++ tools/memlog.sh
@@ -7,24 +7,25 @@
 set -eu
 set -o pipefail
 
-if [ $# -lt 4 ]; then
-    echo "Usage: memlog.sh PROCESS_ID PROCESS_OWNER OUTPUT_FILE TIME_INTERVAL_SEC"
+if [ $# -lt 2 ]; then
+    echo "Usage: memlog.sh PROCESS_ID USER [TIME_INTERVAL_SEC]"
     exit 1
 fi
 
 PROCESS_ID=$1
-PROCESS_OWNER=$2
-OUTPUT_FILE=$3
-TIME_INTERVAL_SEC=$4
+USER=$2
+TIME_INTERVAL_SEC=10
+if [ $# -eq 3 ]; then
+  TIME_INTERVAL_SEC=$3    
+fi
 
-rm -f $OUTPUT_FILE
-echo -e "Eden space utilization\tOld space utilization" >> $OUTPUT_FILE
+echo -e "Eden space utilization\tOld space utilization"
 
 while :
 do
-   sudo -u $PROCESS_OWNER jcmd $PROCESS_ID GC.run
+   sudo -u $USER jcmd $PROCESS_ID GC.run &>/dev/null
    # Skip header and ignore the unimportant columns
-   sudo -u $PROCESS_OWNER jstat -gc $PROCESS_ID | \
-     tail -1 | awk '{print $6 "\t" $8}' >> $OUTPUT_FILE
+   sudo -u $USER jstat -gc $PROCESS_ID 2>/dev/null | \
+     tail -1 | awk '{print $6 "\t" $8}'
    sleep $TIME_INTERVAL_SEC
 done
