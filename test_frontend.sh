38e9129e8fd8c27c4e3206ae71c4a4d7de57d4a2
Daniel Darabos <darabos.daniel@gmail.com>
2015-12-15 16:46:57 +0100

    Split test_everything.sh so we can have two Jenkinses.

---
+++ test_frontend.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -xue
+
+cd `dirname $0`
+
+cd web
+bower install --silent --config.interactive=false
+npm install --silent
+grunt
+cd -
+
+tools/check_documentation.sh
+tools/e2e_test.sh --no-color
