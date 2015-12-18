34e4f550f42c2a5a9f96859c0ca9ddc01689845f
Gabor Feher <gabor.feher@lynxanalytics.com>
2015-12-18 12:50:47 +0100

    Add some more drop tooltips.

--- web/app/scripts/drop-tooltip.js
+++ web/app/scripts/drop-tooltip.js
@@ -5,30 +5,49 @@ angular.module('biggraph').directive('dropTooltip', function() {
   return {
     restrict: 'A',
     scope: {
+      dropTooltipPosition: '@',
       dropTooltip: '@',
+      dropTooltipEnable: '=',
     },
     link: function(scope, element) {
       /* global Drop */
-      var drop = new Drop({
-        target: element[0],
-        content: scope.dropTooltip,
-        position: 'bottom center',
-        openOn: 'hover',
-        classes: 'drop-theme-tooltip',
-        remove: true, // Remove from DOM when closing.
-        tetherOptions: {
-          // Keep within the page.
-          constraints: [{
-            to: 'scrollParent',
-            pin: true,
-          }],
-        },
-      });
+      scope.createDrop = function() {
+        return new Drop({
+          target: element[0],
+          content: scope.dropTooltip,
+          position: scope.dropTooltipPosition || 'top center',
+          openOn: 'hover',
+          classes: 'drop-theme-tooltip',
+          remove: true, // Remove from DOM when closing.
+          tetherOptions: {
+            // Keep within the page.
+            constraints: [{
+              to: 'window',
+              attachment: 'together',
+              pin: true,
+            }],
+          },
+        });
+      };
+      var drop = scope.dropTooltipEnable === false ? undefined : scope.createDrop();
       scope.$watch('dropTooltip', function(tooltip) {
-        drop.content.innerHTML = tooltip;
+        if (drop) {
+          drop.content.innerHTML = tooltip;
+        }
+      });
+      scope.$watch('dropTooltipEnable', function(enabled) {
+        if (enabled === false) {
+          drop.destroy();
+          drop = undefined;
+        } else if (drop === undefined) {
+          drop = scope.createDrop();
+          drop.content.innerHTML = scope.dropTooltip;
+        }
       });
       scope.$on('$destroy', function() {
-        drop.destroy();
+        if (drop) {
+          drop.destroy();
+        }
       });
     },
   };
