fa5194685e17bb6376feda3f3ed0df79b09c8779
Gabor Feher <feherga@gmail.com>
2015-12-23 18:44:31 +0100

    Consolidate redhat and ubuntu dvd maker scripts

--- tools/bare_metal/bare_metal_install_master.sh
+++ tools/bare_metal/bare_metal_install_master.sh
@@ -1,6 +1,8 @@
 set -ueo pipefail
 echo "Starting LynxKite installation..."
 
+. config.sh
+
 DVD_ROOT="$(dirname $0)"
 
 # Install Java
@@ -12,24 +14,24 @@ echo "Java installed successfully."
 
 # Install Cloudera
 echo "Installing Cloudera..."
-sudo tar -xf $DVD_ROOT/cloudera-manager-trusty-cm5.3.3_amd64.tar.gz -C /opt
-sudo sed -i 's/server_host=.*/server_host=localhost/' /opt/cm-5.3.3/etc/cloudera-scm-agent/config.ini
+sudo tar -xf $DVD_ROOT/$CLOUDERA_MANAGER -C /opt
+sudo sed -i 's/server_host=.*/server_host=localhost/' /opt/cm-$CLOUDERA_VERSION/etc/cloudera-scm-agent/config.ini
 sudo useradd --system --home=/opt/cm-5.3.3/run/cloudera-scm-server --no-create-home cloudera-scm
-sudo chown -R cloudera-scm.cloudera-scm /opt/cm-5.3.3
+sudo chown -R cloudera-scm.cloudera-scm /opt/cm-$CLOUDERA_VERSION
 
 # Create parcel directories
 sudo mkdir -p /opt/cloudera/parcel-repo
-sudo cp $DVD_ROOT/CDH-5.3.3-1.cdh5.3.3.p0.5-trusty.parcel /opt/cloudera/parcel-repo/CDH-5.3.3-1.cdh5.3.3.p0.5-trusty.parcel
-sudo cp $DVD_ROOT/CDH-5.3.3-1.cdh5.3.3.p0.5-trusty.parcel.sha1 /opt/cloudera/parcel-repo/CDH-5.3.3-1.cdh5.3.3.p0.5-trusty.parcel.sha1
-sudo cp $DVD_ROOT/manifest.json /opt/cloudera/parcel-repo/manifest.json
+sudo cp $DVD_ROOT/$CLOUDERA_CDH_PARCEL /opt/cloudera/parcel-repo/$CLOUDERA_CDH_PARCEL
+sudo cp $DVD_ROOT/$CLOUDERA_CDH_PARCEL_SHA1 /opt/cloudera/parcel-repo/$CLOUDERA_CDH_PARCEL_SHA1
+sudo cp $DVD_ROOT/$CLOUDERA_MANIFEST /opt/cloudera/parcel-repo/$CLOUDERA_MANIFEST
 # Start a web server in the parcel repo to make Cloudera happy
 cd /opt/cloudera/parcel-repo
 python -m SimpleHTTPServer 8900 > /dev/null 2>&1 &
 cd -
 
 # Config the services to start automatically
-sudo cp /opt/cm-5.3.3/etc/init.d/cloudera-scm-agent /etc/init.d/cloudera-scm-agent
-sudo sed -i 's/CMF_DEFAULTS=.*/CMF_DEFAULTS=\/opt\/cm-5.3.3\/etc\/default/' /etc/init.d/cloudera-scm-agent
+sudo cp /opt/cm-$CLOUDERA_VERSION/etc/init.d/cloudera-scm-agent /etc/init.d/cloudera-scm-agent
+sudo sed -i 's/CMF_DEFAULTS=.*/CMF_DEFAULTS=\/opt\/cm-'$CLOUDERA_VERSION'\/etc\/default/' /etc/init.d/cloudera-scm-agent
 sudo update-rc.d cloudera-scm-agent defaults
 sudo service cloudera-scm-agent start
 
@@ -37,11 +39,11 @@ sudo service cloudera-scm-agent start
 echo "Configuring PostgreSQL..."
 # Feed the following setup SQL script to psql
 echo "create user clouderauser password 'clouderapassword';create database clouderadb;\q\n" | sudo -u postgres psql
-sudo -u cloudera-scm /opt/cm-5.3.3/share/cmf/schema/scm_prepare_database.sh postgresql clouderadb clouderauser clouderapassword 
+sudo -u cloudera-scm /opt/cm-$CLOUDERA_VERSION/share/cmf/schema/scm_prepare_database.sh postgresql clouderadb clouderauser clouderapassword 
 echo "PostgreSQL configured successfully."
 
-sudo cp /opt/cm-5.3.3/etc/init.d/cloudera-scm-server /etc/init.d/cloudera-scm-server
-sudo sed -i 's/CMF_DEFAULTS=.*/CMF_DEFAULTS=\/opt\/cm-5.3.3\/etc\/default/' /etc/init.d/cloudera-scm-server
+sudo cp /opt/cm-$CLOUDERA_VERSION/etc/init.d/cloudera-scm-server /etc/init.d/cloudera-scm-server
+sudo sed -i 's/CMF_DEFAULTS=.*/CMF_DEFAULTS=\/opt\/cm-'$CLOUDERA_VERSION'\/etc\/default/' /etc/init.d/cloudera-scm-server
 sudo update-rc.d cloudera-scm-server defaults
 sudo service cloudera-scm-server start
 echo "Cloudera installed successfully."
