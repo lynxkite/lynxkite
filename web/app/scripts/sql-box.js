b4a7d1d1de498f1b4d9d8ad8b7379cbcb3ab93ac
gsvigruha <gsvigruha@users.noreply.github.com>
2016-01-13 00:10:06 +0800

    Add files.

---
+++ web/app/scripts/sql-box.js
@@ -0,0 +1,24 @@
+// Presents the parameters for saving an operation.
+'use strict';
+
+angular.module('biggraph').directive('sqlBox', function(side, util) {
+  return {
+    restrict: 'E',
+    scope: { side: '=' },
+    templateUrl: 'sql-box.html',
+    link: function(scope) {
+      scope.runQuery = function() {
+        scope.result = util.nocache(
+          '/ajax/runQuery',
+          {
+            project: scope.side.state.projectName,
+            sql: scope.sql,
+          });
+      };
+
+      scope.reportSQLError = function() {
+        util.reportRequestError(scope.result, 'Error executing query.');
+      };
+    }
+  };
+});
