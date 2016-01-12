f0280e93ee870fdc5d01b08c985bff55b57dec39
gsvigruha <gsvigruha@users.noreply.github.com>
2016-01-13 00:36:29 +0800

    Fix tests hopefully.

--- test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
@@ -1,11 +1,24 @@
 package com.lynxanalytics.biggraph.controllers
 
+import scala.reflect.runtime.universe.TypeTag
+import scala.reflect.ClassTag
 import org.scalatest.{ FunSuite, BeforeAndAfterEach }
 
 import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.graph_api.Scripting._
 
-class SQLControllerTest extends BigGraphControllerTest {
+class SQLControllerTest extends FunSuite with TestGraphOp with BeforeAndAfterEach {
+  val controller = new BigGraphController(this)
   val sqlController = new SQLController(this)
+  val projectName = "Test_Project"
+  val projectFrame = ProjectFrame.fromName(projectName)
+  val subProject = projectFrame.subproject
+  val user = com.lynxanalytics.biggraph.serving.User.fake
+
+  def run(op: String, params: Map[String, String] = Map(), on: String = projectName) =
+    controller.projectOp(
+      user,
+      ProjectOperationRequest(on, FEOperationSpec(Operation.titleToID(op), params)))
 
   test("sql on vertices") {
     run("Example Graph")
@@ -13,5 +26,17 @@ class SQLControllerTest extends BigGraphControllerTest {
       sql = "select name from `!vertices` where age < 40"))
     assert(result.header == Array("name"))
     assert(result.data == Array(Seq("Adam"), Seq("Eve"), Seq("Isolated Joe")))
+  }
+
+  override def beforeEach() = {
+    val path = SymbolPath("projects")
+    if (metaGraphManager.tagExists(path)) {
+      for (t <- metaGraphManager.lsTag(path)) {
+        metaGraphManager.rmTag(t)
+      }
+    }
+    controller.createProject(
+      user,
+      CreateProjectRequest(name = projectName, notes = "test project", privacy = "private"))
   }
 }
