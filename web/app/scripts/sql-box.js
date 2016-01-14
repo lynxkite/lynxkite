cf7554ed07ee5cf880d58755bc49ee8db4afa3ed
gsvigruha <gsvigruha@users.noreply.github.com>
2016-01-14 19:19:37 +0800

    SQL box error is a modal window.

--- web/app/scripts/sql-box.js
+++ web/app/scripts/sql-box.js
@@ -8,22 +8,29 @@ angular.module('biggraph').directive('sqlBox', function(side, util) {
     templateUrl: 'sql-box.html',
     link: function(scope) {
       scope.runSQLQuery = function() {
-        scope.inProgress = true;
-        util.nocache(
-          '/ajax/runSQLQuery',
-          {
-            project: scope.side.state.projectName,
-            sql: scope.sql,
-            rownum: 10,
-          }).then(function(result){
-            scope.inProgress = false;
-            scope.result = result;
-          });
+        if (!scope.sql) {
+          util.reportError({ message: 'SQL script must be specified.' });
+        } else {
+
+          scope.inProgress = true;
+          scope.result = util.nocache(
+            '/ajax/runSQLQuery',
+            {
+              project: scope.side.state.projectName,
+              sql: scope.sql,
+              rownum: 10,
+            });
+          scope.result.finally(function() {
+              scope.inProgress = false;
+            });
+        }
       };
 
-      scope.reportSQLError = function() {
-        util.reportRequestError(scope.result, 'Error executing query.');
-      };
+      scope.$watch('result.$error', function(error) {
+        if (error) {
+          util.reportRequestError(scope.result, error);
+        }
+      });
     }
   };
 });
