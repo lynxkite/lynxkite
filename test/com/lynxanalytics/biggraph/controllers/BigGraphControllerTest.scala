929007a9c347cb8a046321955e3e2c455fc1a64a
gsvigruha <gsvigruha@users.noreply.github.com>
2016-01-14 19:53:07 +0800

    Controller tests refactored.

--- test/com/lynxanalytics/biggraph/controllers/BigGraphControllerTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/BigGraphControllerTest.scala
@@ -1,34 +1,8 @@
 package com.lynxanalytics.biggraph.controllers
 
-import scala.reflect.runtime.universe.TypeTag
-import scala.reflect.ClassTag
-import org.scalatest.{ FunSuite, BeforeAndAfterEach }
-
-import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 
-class BigGraphControllerTest extends FunSuite with TestGraphOp with BeforeAndAfterEach {
-  val controller = new BigGraphController(this)
-  val projectName = "Test_Project"
-  def projectFrame = ProjectFrame.fromName(projectName)
-  def subProject = projectFrame.subproject
-  val user = com.lynxanalytics.biggraph.serving.User.fake
-
-  def run(op: String, params: Map[String, String] = Map(), on: String = projectName) =
-    controller.projectOp(
-      user,
-      ProjectOperationRequest(on, FEOperationSpec(Operation.titleToID(op), params)))
-
-  def vattr[T: TypeTag: ClassTag: Ordering](name: String) = {
-    val attr = subProject.viewer.vertexAttributes(name).runtimeSafeCast[T]
-    attr.rdd.values.collect.toSeq.sorted
-  }
-
-  def eattr[T: TypeTag: ClassTag: Ordering](name: String) = {
-    val attr = subProject.viewer.edgeAttributes(name).runtimeSafeCast[T]
-    attr.rdd.values.collect.toSeq.sorted
-  }
-
+class BigGraphControllerTest extends ControllerTestBase {
   test("filtering by vertex attribute") {
     run("Example Graph")
     val filter = ProjectAttributeFilter("age", "<40")
@@ -105,17 +79,5 @@ class BigGraphControllerTest extends FunSuite with TestGraphOp with BeforeAndAft
       controller.createDirectory(user, CreateDirectoryRequest(
         name = projectName + "/bar", privacy = "private"))
     }
-  }
-
-  override def beforeEach() = {
-    val path = SymbolPath("projects")
-    if (metaGraphManager.tagExists(path)) {
-      for (t <- metaGraphManager.lsTag(path)) {
-        metaGraphManager.rmTag(t)
-      }
-    }
-    controller.createProject(
-      user,
-      CreateProjectRequest(name = projectName, notes = "test project", privacy = "private"))
   }
 }
