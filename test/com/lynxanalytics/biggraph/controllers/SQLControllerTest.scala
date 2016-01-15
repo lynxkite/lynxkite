bf5de273f8ba7ae5f3637c8babb80f18d244fad1
Andras Nemeth <xxandreww@gmail.com>
2016-01-15 14:45:05 +0100

    Backend changes for CSV import

--- test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
@@ -1,6 +1,7 @@
 package com.lynxanalytics.biggraph.controllers
 
 import com.lynxanalytics.biggraph.graph_api.Scripting._
+import com.lynxanalytics.biggraph.graph_util
 
 class SQLControllerTest extends BigGraphControllerTestBase {
   val sqlController = new SQLController(this)
@@ -13,5 +14,23 @@ class SQLControllerTest extends BigGraphControllerTestBase {
       rownum = 10))
     assert(result.header == List("name"))
     assert(result.data == List(List("Adam"), List("Eve"), List("Isolated Joe")))
+  }
+
+  test("import from CSV") {
+    val res = getClass.getResource("/graph_operations/ImportGraphTest").toString
+    graph_util.PrefixRepository.registerPrefix("IMPORTGRAPHTEST$", res)
+    val csvFiles = "IMPORTGRAPHTEST$/testgraph/vertex-data/part*"
+    val cpResponse = sqlController.importCSV(
+      user, CSVImportRequest(csvFiles, List("vertexId", "name", "age"), ",", "FAILFAST"))
+    val tableCheckpoint = s"!checkpoint(${cpResponse.checkpoint},)"
+
+    run(
+      "Import vertices from table",
+      Map(
+        "table" -> s"${tableCheckpoint}|!vertices",
+        "id-attr" -> "new_id"))
+    assert(vattr[String]("vertexId") == Seq("0", "1", "2"))
+    assert(vattr[String]("name") == Seq("Adam", "Bob", "Eve"))
+    assert(vattr[String]("age") == Seq("18.2", "20.3", "50.3"))
   }
 }
