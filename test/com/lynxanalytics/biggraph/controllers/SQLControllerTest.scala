65376ae44c1e27ace8b246618a3880e6aad32deb
Daniel Darabos <darabos.daniel@gmail.com>
2016-01-19 13:40:45 +0100

    Update SQLControllerTest.

--- test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
@@ -7,9 +7,8 @@ class SQLControllerTest extends BigGraphControllerTestBase {
 
   test("sql on vertices") {
     run("Example Graph")
-    val result = sqlController.runSQLQuery(user, SQLRequest(
-      project = projectName,
-      sql = "select name from `!vertices` where age < 40",
+    val result = sqlController.runSQLQuery(user, SQLQueryRequest(
+      DataFrameSpec(project = projectName, sql = "select name from `!vertices` where age < 40"),
       maxRows = 10))
     assert(result.header == List("name"))
     assert(result.data == List(List("Adam"), List("Eve"), List("Isolated Joe")))
