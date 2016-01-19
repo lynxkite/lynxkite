99e4eeee7f4d016b4cdd3de6dff8d89357dba32e
Daniel Darabos <darabos.daniel@gmail.com>
2016-01-19 14:06:29 +0100

    Add test for SQL export.

--- test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/SQLControllerTest.scala
@@ -1,6 +1,7 @@
 package com.lynxanalytics.biggraph.controllers
 
 import com.lynxanalytics.biggraph.graph_api.Scripting._
+import com.lynxanalytics.biggraph.graph_util
 
 class SQLControllerTest extends BigGraphControllerTestBase {
   val sqlController = new SQLController(this)
@@ -12,5 +13,14 @@ class SQLControllerTest extends BigGraphControllerTestBase {
       maxRows = 10))
     assert(result.header == List("name"))
     assert(result.data == List(List("Adam"), List("Eve"), List("Isolated Joe")))
+  }
+
+  test("sql export") {
+    run("Example Graph")
+    val result = sqlController.exportSQLQuery(user, SQLExportRequest(
+      DataFrameSpec(project = projectName, sql = "select name from `!vertices` where age < 40"),
+      format = "csv", path = "<download>", options = Map()))
+    val output = graph_util.HadoopFile(result.download.get).loadTextFile(sparkContext)
+    assert(output.collect.sorted.mkString(", ") == "Adam, Eve, Isolated Joe")
   }
 }
