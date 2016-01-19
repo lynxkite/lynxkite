058d788beacfad10948372e183c8e5a2625ba00a
gsvigruha <gergely.svigruha@lynxanalytics.com>
2016-01-19 22:28:44 +0800

    Regression scales labels.

--- app/com/lynxanalytics/biggraph/graph_operations/Regression.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/Regression.scala
@@ -7,6 +7,16 @@ package com.lynxanalytics.biggraph.graph_operations
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.Implicits._
 import org.apache.spark.mllib
+import org.apache.spark.rdd
+
+case class RegressionParams(
+  // Labeled training data points.
+  val points: rdd.RDD[mllib.regression.LabeledPoint],
+  // All data points for the model to evaluate.
+  val vectors: AttributeRDD[mllib.linalg.Vector],
+  // An optional scaler if it was used to scale the labels. It can be used
+  // to scale back the results.
+  val scaler: Option[mllib.feature.StandardScalerModel])
 
 object Regression extends OpFromJson {
   class Input(numFeatures: Int) extends MagicInputSignature {
@@ -33,8 +43,78 @@ case class Regression(method: String, numFeatures: Int) extends TypedMetaGraphOp
               output: OutputBuilder,
               rc: RuntimeContext): Unit = {
     implicit val id = inputDatas
+
+    val (predictions, vectors) = method match {
+      case "Linear regression" =>
+        val p = getParams(forSGD = true)
+        val model = new mllib.regression.LinearRegressionWithSGD().setIntercept(true).run(p.points)
+        (scaleBack(model.predict(p.vectors.values), p.scaler.get), p.vectors)
+      case "Ridge regression" =>
+        val p = getParams(forSGD = true)
+        val model = new mllib.regression.RidgeRegressionWithSGD().setIntercept(true).run(p.points)
+        (scaleBack(model.predict(p.vectors.values), p.scaler.get), p.vectors)
+      case "Lasso" =>
+        val p = getParams(forSGD = true)
+        val model = new mllib.regression.LassoWithSGD().setIntercept(true).run(p.points)
+        (scaleBack(model.predict(p.vectors.values), p.scaler.get), p.vectors)
+      case "Logistic regression" =>
+        val p = getParams(forSGD = false)
+        val model =
+          new mllib.classification.LogisticRegressionWithLBFGS().setNumClasses(10).run(p.points)
+        (model.predict(p.vectors.values), p.vectors)
+      case "Naive Bayes" =>
+        val p = getParams(forSGD = false)
+        val model = mllib.classification.NaiveBayes.train(p.points)
+        (model.predict(p.vectors.values), p.vectors)
+      case "Decision tree" =>
+        val p = getParams(forSGD = false)
+        val model = mllib.tree.DecisionTree.trainRegressor(
+          input = p.points,
+          categoricalFeaturesInfo = Map[Int, Int](), // All continuous.
+          impurity = "variance", // Options: gini, entropy, variance as of Spark 1.6.0.
+          maxDepth = 5,
+          maxBins = 32)
+        (model.predict(p.vectors.values), p.vectors)
+      case "Random forest" =>
+        val p = getParams(forSGD = false)
+        val model = mllib.tree.RandomForest.trainRegressor(
+          input = p.points,
+          categoricalFeaturesInfo = Map[Int, Int](), // All continuous.
+          numTrees = 10,
+          featureSubsetStrategy = "onethird",
+          impurity = "variance", // Options: gini, entropy, variance as of Spark 1.6.0.
+          maxDepth = 4,
+          maxBins = 100,
+          seed = 0)
+        (model.predict(p.vectors.values), p.vectors)
+      case "Gradient-boosted trees" =>
+        val p = getParams(forSGD = false)
+        val boostingStrategy = mllib.tree.configuration.BoostingStrategy.defaultParams("Regression")
+        boostingStrategy.numIterations = 10
+        boostingStrategy.treeStrategy.maxDepth = 5
+        boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]() // All continuous.
+        val model = mllib.tree.GradientBoostedTrees.train(p.points, boostingStrategy)
+        (model.predict(p.vectors.values), p.vectors)
+    }
+    val ids = vectors.keys // We just put back the keys with a zip.
+    output(o.prediction, ids.zip(predictions).asUniqueSortedRDD(vectors.partitioner.get))
+  }
+
+  // Transforms the result RDD using the inverse transformation of the original scaling.
+  private def scaleBack(
+    result: rdd.RDD[Double],
+    scaler: mllib.feature.StandardScalerModel): rdd.RDD[Double] = {
+    result.map { v => v * scaler.std(0) + scaler.mean(0) }
+  }
+
+  // Creates the input for trainign and evaluation.
+  private def getParams(
+    forSGD: Boolean // Whether the data should be prepared for an SGD method.
+    )(implicit id: DataSet): RegressionParams = {
     val vertices = inputs.vertices.rdd
-    val labels = inputs.label.rdd
+    val labelRDD = inputs.label.rdd
+
+    // All scaled data points.
     val vectors = {
       val emptyArrays = vertices.mapValues(l => new Array[Double](numFeatures))
       val numberedFeatures = inputs.features.zipWithIndex
@@ -46,59 +126,34 @@ case class Regression(method: String, numFeatures: Int) extends TypedMetaGraphOp
       }
       val unscaled = fullArrays.mapValues(a => new mllib.linalg.DenseVector(a): mllib.linalg.Vector)
       // Must scale the features or we get NaN predictions. (SPARK-1859)
-      val scaler = new mllib.feature.StandardScaler().fit(unscaled.values)
+      val scaler = new mllib.feature.StandardScaler(
+        withMean = forSGD, // Center the vectors for SGD training methods.
+        withStd = true).fit(
+        // Set the scaler based on only the training vectors, i.e. where we have a label.
+        labelRDD.sortedJoin(unscaled).values.map {
+          case (_, v) => v
+        })
+      // Scale all vectors using the scaler created from the training vectors.
       unscaled.mapValues(v => scaler.transform(v))
     }
+
+    val (labels, labelScaler) = if (forSGD) {
+      // For SGD methods the labels need to be scaled too. Otherwise the optimal stepSize can
+      // vary greatly.
+      val labelVector = labelRDD.mapValues {
+        a => new mllib.linalg.DenseVector(Array(a)): mllib.linalg.Vector
+      }
+      val labelScaler = new mllib.feature.StandardScaler(withMean = true, withStd = true)
+        .fit(labelVector.values)
+      (labelVector.mapValues(v => labelScaler.transform(v)(0)), Some(labelScaler))
+    } else {
+      (labelRDD, None)
+    }
+
     val points = labels.sortedJoin(vectors).values.map {
       case (l, v) => new mllib.regression.LabeledPoint(l, v)
     }
     points.cache
-
-    val predictions = method match {
-      case "Linear regression" =>
-        val model = new mllib.regression.LinearRegressionWithSGD().setIntercept(true).run(points)
-        model.predict(vectors.values)
-      case "Ridge regression" =>
-        val model = new mllib.regression.RidgeRegressionWithSGD().setIntercept(true).run(points)
-        model.predict(vectors.values)
-      case "Lasso" =>
-        val model = new mllib.regression.LassoWithSGD().setIntercept(true).run(points)
-        model.predict(vectors.values)
-      case "Logistic regression" =>
-        val model =
-          new mllib.classification.LogisticRegressionWithLBFGS().setNumClasses(10).run(points)
-        model.predict(vectors.values)
-      case "Naive Bayes" =>
-        val model = mllib.classification.NaiveBayes.train(points)
-        model.predict(vectors.values)
-      case "Decision tree" =>
-        val model = mllib.tree.DecisionTree.trainRegressor(
-          input = points,
-          categoricalFeaturesInfo = Map[Int, Int](), // All continuous.
-          impurity = "variance", // This is the only option at the moment.
-          maxDepth = 5,
-          maxBins = 32)
-        model.predict(vectors.values)
-      case "Random forest" =>
-        val model = mllib.tree.RandomForest.trainRegressor(
-          input = points,
-          categoricalFeaturesInfo = Map[Int, Int](), // All continuous.
-          numTrees = 10,
-          featureSubsetStrategy = "onethird",
-          impurity = "variance", // This is the only option at the moment.
-          maxDepth = 4,
-          maxBins = 100,
-          seed = 0)
-        model.predict(vectors.values)
-      case "Gradient-boosted trees" =>
-        val boostingStrategy = mllib.tree.configuration.BoostingStrategy.defaultParams("Regression")
-        boostingStrategy.numIterations = 10
-        boostingStrategy.treeStrategy.maxDepth = 5
-        boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]() // All continuous.
-        val model = mllib.tree.GradientBoostedTrees.train(points, boostingStrategy)
-        model.predict(vectors.values)
-    }
-    val ids = vectors.keys // We just put back the keys with a zip.
-    output(o.prediction, ids.zip(predictions).asUniqueSortedRDD(vectors.partitioner.get))
+    RegressionParams(points, vectors, labelScaler)
   }
 }
