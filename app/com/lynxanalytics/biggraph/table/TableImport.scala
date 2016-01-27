fe7564f2de2d63f7dc4f76d804c347daaaf68b77
Daniel Darabos <darabos.daniel@gmail.com>
2016-01-27 16:22:51 +0100

    Remove synchronous importDataFrame.

--- app/com/lynxanalytics/biggraph/table/TableImport.scala
+++ app/com/lynxanalytics/biggraph/table/TableImport.scala
@@ -8,30 +8,14 @@ import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_operations
 
 object TableImport {
-  // This is a blocking operation that imports the given dataframe as
-  // LynxKite entities. The imported entities are returned as a Table.
-  def importDataFrame(df: sql.DataFrame)(
-    implicit metaManager: MetaGraphManager, dataManager: DataManager): controllers.Table = {
-    val table = tableFromDataFrame(df)
-    // Enfore the actual import.
-    for (attr <- table.columns.values) dataManager.get(attr)
-    table
-  }
-
-  // Non-blocking version of importDataFrame. This performs the actual import asynchronously.
+  // Imports the given DataFrame as LynxKite entities wrapped in a Table.
+  // The DataManager is used to start the actual import asynchronously.
   def importDataFrameAsync(df: sql.DataFrame)(
     implicit metaManager: MetaGraphManager, dataManager: DataManager): controllers.Table = {
-    val table = tableFromDataFrame(df)
-    // Enfore the actual import.
-    for (attr <- table.columns.values) dataManager.getFuture(attr)
-    table
-  }
-
-  private def tableFromDataFrame(df: sql.DataFrame)(
-    implicit metaManager: MetaGraphManager): controllers.Table = {
     import Scripting._
     val importMetaResult = graph_operations.ImportDataFrame(df).result
     val columnEntities = importMetaResult.columns.mapValues(_.entity)
+    for (attr <- columnEntities.values) dataManager.getFuture(attr)
     controllers.RawTable(importMetaResult.ids, columnEntities)
   }
 }
