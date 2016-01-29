c0a955d27eadcc90c4a4db3a4c905b13e4c09d89
Gabor Feher <gabor.feher@lynxanalytics.com>
2016-01-29 12:25:18 +0100

    Address comments, move perf groovy scripts into own dir

--- test_performance.sh
+++ test_performance.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
+set -ueo pipefail
+trap 'echo Failed.' ERR
+
+cd $(dirname $0)
+
+CLUSTER_NAME="${USER}-test-cluster"
+EMR_TEST_SPEC="/tmp/${CLUSTER_NAME}.emr_test_spec"
+
 ./stage.sh
 
-cd stage/tools
+cp stage/tools/emr_spec_template ${EMR_TEST_SPEC}
+cat >>${EMR_TEST_SPEC} <<EOF
 
-cp emr_spec_template emr_test_spec
-cat >>emr_test_spec <<EOF
 # Override values for the test setup:
-CLUSTER_NAME=${USER}-test-cluster
-NUM_INSTANCES=1
+CLUSTER_NAME=${CLUSTER_NAME}
+NUM_INSTANCES=3
 S3_DATAREPO=""
 
 EOF
 
-CLUSTERID=$(./emr.sh clusterid emr_test_spec)
+CLUSTERID=$(stage/tools/emr.sh clusterid ${EMR_TEST_SPEC})
 if [ -n "$CLUSTERID" ]; then
   echo "Reusing already running cluster instance ${CLUSTERID}."
-  ./emr.sh reset-yes emr_test_spec
+  stage/tools/emr.sh reset-yes ${EMR_TEST_SPEC}
 else
   echo "Starting new cluster instance."
-  ./emr.sh start emr_test_spec
+  stage/tools/emr.sh start ${EMR_TEST_SPEC}
 fi
 
-./emr.sh kite emr_test_spec
-./emr.sh batch emr_test_spec ../kitescripts/jsperf.groovy ../kitescripts/visualization_perf.groovy
+stage/tools/emr.sh kite ${EMR_TEST_SPEC}
+stage/tools/emr.sh batch ${EMR_TEST_SPEC} kitescripts/perf/*.groovy
 
 read -p "Test completed. Terminate cluster? [y/N] " answer
 case ${answer:0:1} in
   y|Y )
-    ./emr.sh terminate-yes emr_test_spec
+    stage/tools/emr.sh terminate-yes ${EMR_TEST_SPEC}
     ;;
   * )
-    echo "Use 'cd stage/tools && ./emr.sh ssh emr_test_spec' to log in to master."
+    echo "Use 'stage/tools/emr.sh ssh ${EMR_TEST_SPEC}' to log in to master."
     echo "Please don't forget to shut down the cluster."
     ;;
 esac
