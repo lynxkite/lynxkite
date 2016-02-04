aeac150612ee38e464791b80e759fd392703b443
Daniel Darabos <darabos.daniel@gmail.com>
2016-02-04 18:36:43 +0100

    Strip headers except the first one.

--- app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
+++ app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
@@ -3201,14 +3201,6 @@ class Operations(env: BigGraphEnvironment) extends OperationRepository(env) {
     op.result.created
   }
 
-  def downloadLink(fn: HadoopFile, name: String) = {
-    val urlPath = java.net.URLEncoder.encode(fn.symbolicName, "utf-8")
-    val urlName = java.net.URLEncoder.encode(name, "utf-8")
-    val url = s"/download?path=$urlPath&name=$urlName"
-    val quoted = '"' + url + '"'
-    newScalar(s"<a href=$quoted>download</a>")
-  }
-
   // Whether a JavaScript expression contains a given identifier.
   // It's a best-effort implementation with no guarantees of correctness.
   def containsIdentifierJS(expr: String, identifier: String): Boolean = {
