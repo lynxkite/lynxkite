40082c0a534bbeaf4037cb37ba1dd7f7b4afcebf
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-02-15 17:53:10 +0100

    Add comments and print filename

--- test_backend.sh
+++ test_backend.sh
@@ -12,9 +12,14 @@ if [ "$?" != "0" ]; then
     exit 1
 fi
 
+# We'll check if the logfile contains 'future failed' lines; these
+# indicate errors that the test framework cannot catch. In case such
+# lines occur, we rename the file logs/test-xxxxxx to logs/failed-xxxxxx
+# so that the next invokation of test_backend.sh will not delete it.
+thelogfile=logs/test-*
+echo logfile: $thelogfile
 grep "future failed" -A1 logs/test-*
 if [ "$?" == "0" ]; then
-    thelogfile=logs/test-*
     failedlog=`echo $thelogfile | sed s:logs/test:logs/failed:`
     mv $thelogfile $failedlog
     exit 1
