8b13a2a042a0c2061ae06862d54811e7139283b4
gsvigruha <gsvigruha@users.noreply.github.com>
2016-02-17 10:13:55 +0800

    Refactor string substitution.

--- app/com/lynxanalytics/biggraph/graph_operations/DeriveJS.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/DeriveJS.scala
@@ -160,7 +160,7 @@ case class DeriveJSString(
   def convert(v: Any, context: => String): String = v match {
     case v: String => v
     case _ => throw new AssertionError(
-      s"$v of ${v.getClass} cannot be converted to String in " + context)
+      s"$v of ${v.getClass} cannot be converted to String in $context")
   }
 }
 
@@ -187,10 +187,10 @@ case class DeriveJSDouble(
   def convert(v: Any, context: => String): Double = v match {
     case v: Double =>
       assert(!v.isNaN() && !v.isInfinite(),
-        context + " did not return a valid number")
+        s"$context did not return a valid number")
       v
     case _ =>
       throw new AssertionError(
-        s"$v of ${v.getClass} cannot be converted to Double in " + context)
+        s"$v of ${v.getClass} cannot be converted to Double in $context")
   }
 }
