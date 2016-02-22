268265dffa3eb79d9a8cf0a747b3942ed1c5d26b
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-02-22 12:22:51 +0100

    Remove temporary debug code

--- app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
+++ app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
@@ -14,24 +14,9 @@ abstract class OperationLogger {
   def close(): Unit
 }
 
-class NullOperationLogger extends OperationLogger {
-  def addOutput(output: SafeFuture[EntityData]): Unit = {}
-  def addInput(input: SafeFuture[EntityData]): Unit = {}
-  def close(): Unit = {}
-}
-
 object OperationLogger {
-  var cnt = 0
-  def getId(): Int = synchronized {
-    cnt += 1
-    cnt
-  }
-
-  val nullLogger = util.Properties.envOrNone("KITE_NO_OPERATION_LOGGER").isDefined
-
   def apply(instance: MetaGraphOperationInstance, ec: ExecutionContextExecutorService): OperationLogger = {
-    if (nullLogger) new NullOperationLogger
-    else new JsonOperationLogger(instance, ec)
+    new JsonOperationLogger(instance, ec)
   }
 }
 
@@ -43,27 +28,11 @@ class JsonOperationLogger(instance: MetaGraphOperationInstance,
 
   private val outputInfoList = scala.collection.mutable.Queue[SafeFuture[OutputInfo]]()
   private val inputInfoList = scala.collection.mutable.Queue[SafeFuture[InputInfo]]()
-  private def outputsLoggedSoFar = outputInfoList.length
-  private var closed = false
   private val creationTime = System.currentTimeMillis
-  private val id = OperationLogger.getId()
-  private val name = s"${instance.operation}[$id]"
-  private val numOutput =
-    if (instance.operation.isHeavy) instance.outputs.nonScalars.size
-    else 0
-  private val numInput =
-    if (instance.operation.isHeavy) instance.inputs.nonScalars.size
-    else 0
 
   private def elapsedMs(): Long = {
     (System.currentTimeMillis() - creationTime)
   }
-  private def threadId() = Thread.currentThread().getId.toString
-  private def info(): String =
-    s"name: $name thread: ${threadId()}" +
-      s" numOutputs: $numOutput written: $outputsLoggedSoFar" +
-      s" numInputs: $numInput" +
-      s" elasped: ${elapsedMs()}"
 
   override def addOutput(output: SafeFuture[EntityData]): Unit = {
     outputInfoList += output.map {
@@ -71,7 +40,6 @@ class JsonOperationLogger(instance: MetaGraphOperationInstance,
         synchronized {
           val rddData = o.asInstanceOf[EntityRDDData[_]]
           val oi = OutputInfo(rddData.entity.name.name, rddData.count, elapsedMs() / 1000)
-          assert(!closed, s"${info()}  output $o  was added after close!")
           oi
         }
     }
@@ -90,10 +58,6 @@ class JsonOperationLogger(instance: MetaGraphOperationInstance,
   }
 
   override def close(): Unit = {
-    assert(outputInfoList.length == numOutput,
-      s"Output info list mismatch: ${outputInfoList.length} != $numOutput")
-    assert(inputInfoList.length == numInput,
-      s"Input info list mismatch: ${inputInfoList.length} != $numInput")
     val outputs = SafeFuture.sequence(outputInfoList)
     val inputs = SafeFuture.sequence(inputInfoList)
 
@@ -103,12 +67,10 @@ class JsonOperationLogger(instance: MetaGraphOperationInstance,
         if (instance.operation.isHeavy) {
           dump(inputs.value.get.get, outputs.value.get.get)
         }
-        closed = true
     }
     both.onFailure {
       case _ =>
-        log.error(s"Error on closing OperationLog ${info()}")
-        closed = true
+        log.error("Error on closing OperationLog")
     }
   }
 
@@ -125,13 +87,5 @@ class JsonOperationLogger(instance: MetaGraphOperationInstance,
       "outputs" -> outputs
     )
     log.info(s"$marker $out")
-  }
-
-  override def finalize() = {
-    if (!closed && numOutput != 0) {
-      val msg = s"${info()}  ***NOT CLOSED***"
-      log.error(msg)
-      println(msg)
-    }
   }
 }
