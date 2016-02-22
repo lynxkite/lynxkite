9d874689c67182938cabb8c9c19e649f4ea02a3c
Daniel Darabos <darabos.daniel@gmail.com>
2016-02-22 15:01:54 +0100

    Avoid popping up a browser for PDF generation.

--- tools/gen_documentation.sh
+++ tools/gen_documentation.sh
@@ -2,13 +2,19 @@
 # Script for generating PDF versions of the documentation pages.
 
 WKHTMLTOPDF="${WKHTMLTOPDF:-wkhtmltopdf}"
+if [ ! -x "$WKHTMLTOPDF" ]; then
+  >&2 echo 'Please install wkhtmltopdf or set WKHTMLTOPDF to point to the binary.'
+  exit 1
+fi
 WKHTML_OPT="--lowquality --footer-center [page] --margin-top 20mm --margin-bottom 20mm"
 
 echo "Starting Grunt server..."
-"$(dirname $0)/../grunt.sh" &
+cd "$(dirname $0)/../web"
+grunt serve:dist &
 GRUNT_PID=$!
+cd -
 function kill_grunt {
-  pkill -term -P $GRUNT_PID
+  kill $GRUNT_PID
 }
 trap kill_grunt EXIT
 # Wait until Grunt is up.
