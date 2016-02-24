3301b2886f6710cf07cc8990eb99198a9e7484d3
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-02-24 09:29:02 +0100

    Address comments; round 1

--- app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
+++ app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
@@ -8,22 +8,8 @@ import scala.concurrent.ExecutionContextExecutorService
 
 import com.lynxanalytics.biggraph.{ bigGraphLogger => log }
 
-abstract class OperationLogger {
-  def addOutput(output: SafeFuture[EntityData]): Unit
-  def addInput(input: SafeFuture[EntityData]): Unit
-  def close(): Unit
-  def start(): Unit
-  def stop(): Unit
-}
-
-object OperationLogger {
-  def apply(instance: MetaGraphOperationInstance, ec: ExecutionContextExecutorService): OperationLogger = {
-    new JsonOperationLogger(instance, ec)
-  }
-}
-
-class JsonOperationLogger(instance: MetaGraphOperationInstance,
-                          implicit val ec: ExecutionContextExecutorService) extends OperationLogger {
+class OperationLogger(instance: MetaGraphOperationInstance,
+                      implicit val ec: ExecutionContextExecutorService) {
   private val marker = "JSON_OPERATION_LOGGER_MARKER"
   case class OutputInfo(name: String, partitions: Int, count: Option[Long])
   case class InputInfo(name: String, partitions: Int, count: Option[Long])
@@ -40,36 +26,32 @@ class JsonOperationLogger(instance: MetaGraphOperationInstance,
     diff
   }
 
-  override def addOutput(output: SafeFuture[EntityData]): Unit = {
+  def addOutput(output: SafeFuture[EntityData]): Unit = {
     outputInfoList += output.map {
       o =>
-        synchronized {
-          val rddData = o.asInstanceOf[EntityRDDData[_]]
-          OutputInfo(rddData.entity.name.name, rddData.rdd.partitions.size, rddData.count)
-        }
+        val rddData = o.asInstanceOf[EntityRDDData[_]]
+        OutputInfo(rddData.entity.name.name, rddData.rdd.partitions.size, rddData.count)
     }
   }
 
-  override def start(): Unit = {
+  def start(): Unit = {
     assert(startTime == -1)
     startTime = System.currentTimeMillis()
   }
 
-  override def stop(): Unit = {
+  def stop(): Unit = {
     assert(stopTime == -1)
     stopTime = System.currentTimeMillis()
   }
-  override def addInput(input: SafeFuture[EntityData]): Unit = {
+  def addInput(input: SafeFuture[EntityData]): Unit = {
     inputInfoList += input.map {
       i =>
-        synchronized {
-          val rddData = i.asInstanceOf[EntityRDDData[_]]
-          InputInfo(rddData.entity.toString, rddData.rdd.partitions.size, rddData.count)
-        }
+        val rddData = i.asInstanceOf[EntityRDDData[_]]
+        InputInfo(rddData.entity.toString, rddData.rdd.partitions.size, rddData.count)
     }
   }
 
-  override def close(): Unit = {
+  def logWhenReady(): Unit = {
     val outputsFuture = SafeFuture.sequence(outputInfoList)
     val inputsFuture = SafeFuture.sequence(inputInfoList)
 
