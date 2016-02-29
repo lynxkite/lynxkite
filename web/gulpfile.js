85a71de7a33142e6a2cc1c1a0df8e80d81a9c16b
Daniel Darabos <darabos.daniel@gmail.com>
2016-02-29 20:03:13 +0100

    Autoload Gulp plugins.

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -5,62 +5,50 @@
 //   gulp serve     # Start an auto-updating server.
 //   gulp quick     # The quick part of the build, creating its output in ".tmp".
 
-var angularFilesort = require('gulp-angular-filesort');
-var autoprefixer = require('gulp-autoprefixer');
-var asciidoctor = require('gulp-asciidoctor');
 var browserSync = require('browser-sync').create();
 var del = require('del');
 var gulp = require('gulp');
-var gulpif = require('gulp-if');
 var httpProxy = require('http-proxy');
-var inject = require('gulp-inject');
-var jshint = require('gulp-jshint');
 var merge = require('merge-stream');
-var ngAnnotate = require('gulp-ng-annotate');
-var rename = require('gulp-rename');
-var rev = require('gulp-rev');
-var revReplace = require('gulp-rev-replace');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var useref = require('gulp-useref');
 var wiredep = require('wiredep').stream;
+var $ = require('gulp-load-plugins')();
 
 gulp.task('asciidoctor', function () {
   var help = gulp.src('app/help/index.asciidoc')
-    .pipe(asciidoctor({
+    .pipe($.asciidoctor({
       base_dir: 'app/help',
       safe: 'safe',
       header_footer: false,
     }))
-    .pipe(rename('help.html'));
+    .pipe($.rename('help.html'));
   var admin = gulp.src('app/admin-manual/index.asciidoc')
-    .pipe(asciidoctor({
+    .pipe($.asciidoctor({
       base_dir: 'app/admin-manual',
       safe: 'safe',
       header_footer: false,
     }))
-    .pipe(rename('admin-manual.html'));
+    .pipe($.rename('admin-manual.html'));
   return merge(help, admin)
     .pipe(gulp.dest('.tmp'));
 });
 
 gulp.task('html', ['css', 'js'], function () {
   var css = gulp.src('.tmp/**/*.css', { read: false });
-  var js = gulp.src('.tmp/**/*.js').pipe(angularFilesort());
+  var js = gulp.src('.tmp/**/*.js').pipe($.angularFilesort());
   return gulp.src('app/index.html')
     .pipe(wiredep())
-    .pipe(inject(css, { ignorePath: '.tmp' }))
-    .pipe(inject(js, { ignorePath: '.tmp' }))
+    .pipe($.inject(css, { ignorePath: '.tmp' }))
+    .pipe($.inject(js, { ignorePath: '.tmp' }))
     .pipe(gulp.dest('.tmp'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('dist', ['clean:dist', 'html'], function () {
   var dynamic = gulp.src('.tmp/**/*.html')
-    .pipe(useref())
-    .pipe(gulpif('*.js', uglify()))
-    .pipe(gulpif(['**/*', '!**/*.html'], rev()))
-    .pipe(revReplace());
+    .pipe($.useref())
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if(['**/*', '!**/*.html'], $.rev()))
+    .pipe($.revReplace());
   var static = gulp.src([
     'app/*.{png,svg}',
     'app/images/*',
@@ -73,29 +61,29 @@ gulp.task('dist', ['clean:dist', 'html'], function () {
 
 gulp.task('sass', function () {
   return gulp.src('app/styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe($.sass().on('error', $.sass.logError))
     .pipe(gulp.dest('.tmp/styles'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('css', ['sass'], function () {
   return gulp.src('app/styles/*.css')
-    .pipe(autoprefixer())
+    .pipe($.autoprefixer())
     .pipe(gulp.dest('.tmp/styles'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('js', function () {
   return gulp.src('app/scripts/**/*.js')
-    .pipe(ngAnnotate())
+    .pipe($.ngAnnotate())
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('jshint', function() {
   return gulp.src('app/scripts/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'));
 });
 
 gulp.task('clean:dist', function() {
