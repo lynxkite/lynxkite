c9ca3bc02f7ed4f8f202aaafb91bd7c557f2e592
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-03-01 10:42:41 +0100

    Make notifyStarterScript async

--- app/Global.scala
+++ app/Global.scala
@@ -5,6 +5,9 @@ import play.api.mvc.Results._
 import play.filters.gzip.GzipFilter
 import play.filters.headers.SecurityHeadersFilter
 import play.twirl.api.HtmlFormat.escape
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 import com.lynxanalytics.biggraph.serving
 
@@ -22,12 +25,20 @@ object Global extends WithFilters(new GzipFilter(), SecurityHeadersFilter()) wit
     concurrent.Future.successful(NotFound(escape(request.toString)))
   }
 
-  def notifyStarterScript(msg: String) = {
-    scala.util.Properties.envOrNone("KITE_READY_PIPE").foreach(pipeName =>
-      org.apache.commons.io.FileUtils.writeStringToFile(
-        new java.io.File(pipeName),
-        msg + "\n",
-        "utf8"))
+  def notifyStarterScript(msg: String): Unit = {
+    val notifier =
+      scala.concurrent.Future[Unit] {
+        scala.util.Properties.envOrNone("KITE_READY_PIPE").foreach(pipeName =>
+          org.apache.commons.io.FileUtils.writeStringToFile(
+            new java.io.File(pipeName),
+            msg + "\n",
+            "utf8"))
+      }
+    try {
+      Await.result(notifier, 5.seconds)
+    } catch {
+      case _: Throwable => println("Timeout - starter script could have been killed")
+    }
   }
 
   override def onStart(app: Application) = {
