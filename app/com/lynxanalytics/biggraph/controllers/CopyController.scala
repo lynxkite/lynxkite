e44e0c2a2dd62ef230ebc768c84c63e0eabd2afa
Gabor Feher <feherga@gmail.com>
2016-03-01 13:11:05 +0100

    Fix the stupidest mistake of the century + add comments

--- app/com/lynxanalytics/biggraph/controllers/CopyController.scala
+++ app/com/lynxanalytics/biggraph/controllers/CopyController.scala
@@ -27,8 +27,8 @@ class CopyController(environment: BigGraphEnvironment, sparkClusterController: S
     val dm = environment.dataManager
     dm.waitAllFutures()
     dm.synchronized {
-      dm.waitAllFutures()  // We don't want other operations to be running during s3copy.
-      sparkClusterController.disableWatchdog()  // Watchdog would fail since we are locking on dataManager.
+      dm.waitAllFutures() // We don't want other operations to be running during s3copy.
+      sparkClusterController.disableWatchdog() // Watchdog would fail since we are locking on dataManager.
       try {
         for (ephemeralPath <- dm.ephemeralPath) {
           log.info(s"Listing contents of $ephemeralPath...")
