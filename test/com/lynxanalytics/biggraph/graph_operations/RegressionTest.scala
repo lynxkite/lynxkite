f7f9107d7d186750e20e1ed20a112242d7b30203
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-03-05 13:12:02 +0100

    Rename gender to beingJoeOrEve

--- test/com/lynxanalytics/biggraph/graph_operations/RegressionTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/RegressionTest.scala
@@ -97,9 +97,9 @@ class RegressionTest extends FunSuite with TestGraphOp {
   test("Logistic regression") {
     val prediction = {
       val g = ExampleGraph()().result
-      val gender = DeriveJS.deriveFromAttributes[Double](
+      val beingJoeOrEve = DeriveJS.deriveFromAttributes[Double](
         "(name == 'Isolated Joe' || name == 'Eve') ? 1 : 0", Seq("name" -> g.name), g.vertices).attr
-      predict("Logistic regression", gender, Seq(g.age))
+      predict("Logistic regression", beingJoeOrEve, Seq(g.age))
     }
     assertRoughly(prediction,
       Map(0L -> 0.0, 1L -> 1.0, 2L -> 0.0, 3L -> 1.0), maxError = 0.1)
