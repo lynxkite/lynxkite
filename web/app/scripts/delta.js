823341372ef5216814936aade1aa95dd1b24c368
Daniel Darabos <darabos.daniel@gmail.com>
2016-03-09 18:09:19 +0100

    Remove unused stuff.

--- web/app/scripts/delta.js
+++ web/app/scripts/delta.js
@@ -1,10 +1,10 @@
 // A scalar that represents a change in another scalar.
 'use strict';
 
-angular.module('biggraph').directive('delta', function(util) {
+angular.module('biggraph').directive('delta', function() {
   return {
     restrict: 'E',
-    scope: { ref: '=', tip: '=' },
+    scope: { ref: '=' },
     templateUrl: 'delta.html',
   };
 });
