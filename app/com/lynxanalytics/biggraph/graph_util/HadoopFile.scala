4dc83124b2a23d8e98c41b9b8ac875bdb2f27972
gsvigruha <gsvigruha@users.noreply.github.com>
2016-03-16 13:32:36 +0800

    More asserts.

--- app/com/lynxanalytics/biggraph/graph_util/HadoopFile.scala
+++ app/com/lynxanalytics/biggraph/graph_util/HadoopFile.scala
@@ -137,6 +137,8 @@ class HadoopFile private (
   def length = fs.getFileStatus(path).getLen
   def globLength = globStatus.map(_.getLen).sum
 
+  def isDir = fs.isDirectory(path)
+
   def listStatus = fs.listStatus(path)
   def getContentSummary = fs.getContentSummary(path)
 
