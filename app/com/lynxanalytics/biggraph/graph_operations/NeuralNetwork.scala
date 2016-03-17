68039d3767afe5388c311b9cd44ea528bb2ef43e
Daniel Darabos <darabos.daniel@gmail.com>
2016-03-17 23:09:31 +0100

    Start on backprop.

--- app/com/lynxanalytics/biggraph/graph_operations/NeuralNetwork.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/NeuralNetwork.scala
@@ -46,35 +46,37 @@ case class NeuralNetwork(featureCount: Int) extends TypedMetaGraphOp[Input, Outp
   }
 
   case class Network(
-      edgeMatrix: DenseMatrix[Double],
-      edgeBias: DenseVector[Double],
-      resetInput: DenseMatrix[Double],
-      resetHidden: DenseMatrix[Double],
-      updateInput: DenseMatrix[Double],
-      updateHidden: DenseMatrix[Double],
-      activationInput: DenseMatrix[Double],
-      activationHidden: DenseMatrix[Double]) {
-  }
+    size: Int,
+    edgeMatrix: DenseMatrix[Double],
+    edgeBias: DenseVector[Double],
+    resetInput: DenseMatrix[Double],
+    resetHidden: DenseMatrix[Double],
+    updateInput: DenseMatrix[Double],
+    updateHidden: DenseMatrix[Double],
+    activationInput: DenseMatrix[Double],
+    activationHidden: DenseMatrix[Double])
   object Network {
-    def zeros(networkSize: Int): Network = new Network(
-      edgeMatrix = DenseMatrix.zeros[Double](networkSize, networkSize),
-      edgeBias = DenseVector.zeros[Double](networkSize),
-      resetInput = DenseMatrix.zeros[Double](networkSize, networkSize),
-      resetHidden = DenseMatrix.zeros[Double](networkSize, networkSize),
-      updateInput = DenseMatrix.zeros[Double](networkSize, networkSize),
-      updateHidden = DenseMatrix.zeros[Double](networkSize, networkSize),
-      activationInput = DenseMatrix.zeros[Double](networkSize, networkSize),
-      activationHidden = DenseMatrix.zeros[Double](networkSize, networkSize))
+    def zeros(size: Int) = new Network(
+      size,
+      edgeMatrix = DenseMatrix.zeros[Double](size, size),
+      edgeBias = DenseVector.zeros[Double](size),
+      resetInput = DenseMatrix.zeros[Double](size, size),
+      resetHidden = DenseMatrix.zeros[Double](size, size),
+      updateInput = DenseMatrix.zeros[Double](size, size),
+      updateHidden = DenseMatrix.zeros[Double](size, size),
+      activationInput = DenseMatrix.zeros[Double](size, size),
+      activationHidden = DenseMatrix.zeros[Double](size, size))
   }
 
   class NetworkOutputs(
       network: Network,
       vertices: Iterable[ID],
       edges: CompactUndirectedGraph,
-      state: Map[ID, DenseVector[Double]]) {
+      state: Map[ID, DenseVector[Double]],
+      visibleState: Map[ID, DenseVector[Double]]) {
     import breeze.numerics._
     val input = vertices.map { id =>
-      val inputs = edges.getNeighbors(id).map(network.edgeMatrix * state(_))
+      val inputs = edges.getNeighbors(id).map(network.edgeMatrix * visibleState(_))
       id -> (inputs.reduce(_ + _) + network.edgeBias)
     }.toMap
     val reset = vertices.map { id =>
@@ -91,6 +93,31 @@ case class NeuralNetwork(featureCount: Int) extends TypedMetaGraphOp[Input, Outp
     }.toMap
   }
 
+  class NetworkGradients(
+      network: Network,
+      vertices: Iterable[ID],
+      edges: CompactUndirectedGraph,
+      stateGradient: Map[ID, DenseVector[Double]],
+      outputs: Seq[NetworkOutputs]) {
+    import breeze.numerics._
+    val state = vertices.map { id =>
+      id -> ((update(id) * (-1.0) + 1.0) :* state(id) + update(id) :* tildeState(id))
+    }.toMap
+    val tildeState = vertices.map { id =>
+      id -> tanh(network.activationInput * input(id) + network.activationHidden * (reset(id) :* state(id)))
+    }.toMap
+    val update = vertices.map { id =>
+      id -> sigmoid(network.updateInput * input(id) + network.updateHidden * state(id))
+    }.toMap
+    val reset = vertices.map { id =>
+      id -> sigmoid(network.resetInput * input(id) + network.resetHidden * state(id))
+    }.toMap
+    val input = vertices.map { id =>
+      val inputs = edges.getNeighbors(id).map(network.edgeMatrix * state(_))
+      id -> (inputs.reduce(_ + _) + network.edgeBias)
+    }.toMap
+  }
+
   val networkSize = 100
   val iterations = 10
   val depth = 10
@@ -103,7 +130,8 @@ case class NeuralNetwork(featureCount: Int) extends TypedMetaGraphOp[Input, Outp
     val labels = labelOpt.flatMap { case (id, labelOpt) => labelOpt.map(id -> _) }.toMap
     val vertices = labelOpt.map(_._1)
     // Initial state contains label. TODO: Also add features.
-    val firstState: Map[ID, DenseVector[Double]] = vertices.map { id =>
+    val blankState = vertices.map(_ -> DenseVector.zeros[Double](networkSize))
+    val trueState: Map[ID, DenseVector[Double]] = vertices.map { id =>
       val state = DenseVector.zeros[Double](networkSize)
       if (labels.contains(id)) {
         state(0) = 1.0
@@ -114,11 +142,23 @@ case class NeuralNetwork(featureCount: Int) extends TypedMetaGraphOp[Input, Outp
 
     var network = Network.zeros(networkSize)
     for (i <- 1 to iterations) {
+      // Forward pass.
       val outputs = (0 until depth).foldLeft {
-        new NetworkOutputs(network, vertices, edges, firstState)
+        // Neighbors can see the labels (trueState) but it is hidden from the node itself (blankState).
+        new NetworkOutputs(network, vertices, edges, blankState, trueState)
       } { (outputs, iteration) =>
-        new NetworkOutputs(network, vertices, edges, outputs.newState)
+        new NetworkOutputs(network, vertices, edges, outputs.newState, outputs.newState)
       }
+
+      // Backward pass.
+      val finalGradient = labels.map {
+        case (id, label) =>
+          val error = DenseVector.zeros[Double](networkSize)
+          error(1) = label - outputs.last.state(1)
+          id -> error
+      }
+      val gradients = new NetworkGradients(network, vertices, edges, finalGradient, outputs)
+
     }
     Iterator()
   }
