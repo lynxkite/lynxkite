f5dc1e7292d80be5de9f14e2a6ed4e45f5fe946f
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-01 14:22:01 +0200

    Use key file for push.

--- test_big_data.sh
+++ test_big_data.sh
@@ -5,11 +5,14 @@ cd $(dirname $0)
 # Run test. (TODO)
 cat kitescripts/perf/last_output.md | sed 's/size/how big/' > kitescripts/perf/last_output.md.new
 
-# Commit and push changed output on PR branch.
-git config user.name 'lynx-jenkins'
-git config user.email 'pizza-support@lynxanalytics.com'
-git checkout "$GIT_BRANCH"
-mv kitescripts/perf/last_output.md{.new,}
-PERF_CHANGE="12% faster"
-git commit -am "Big Data Test: $PERF_CHANGE"
-git push
+if [[ $USERNAME == 'jenkins' ]]; then
+  # Commit and push changed output on PR branch.
+  git config user.name 'lynx-jenkins'
+  git config user.email 'pizza-support@lynxanalytics.com'
+  git config push.default simple
+  git checkout "$GIT_BRANCH"
+  mv kitescripts/perf/last_output.md{.new,}
+  PERF_CHANGE="12% faster"
+  git commit -am "Big Data Test: $PERF_CHANGE"
+  GIT_SSH_COMMAND='ssh -i ~/.ssh/lynx-jenkins' git push
+fi
