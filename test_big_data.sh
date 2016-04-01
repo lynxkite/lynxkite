68d56ef0efb13e9b5f4ab48ce8ffa49fd85f4269
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-01 12:46:47 +0200

    Try committing big data test results into pull request.

--- test_big_data.sh
+++ test_big_data.sh
@@ -1,1 +1,9 @@
-echo 'Performance has improved by 123%.'
+#!/bin/bash -xue
+
+cd $(dirname $0)
+cat kitescripts/perf/last_output.md | sed '/size/how big/' > kitescripts/perf/last_output.md.new
+mv kitescripts/perf/last_output.md{.new,}
+git status
+PERF_CHANGE="12% faster"
+git commit -am "Big Data Test: $PERF_CHANGE"
+git push
