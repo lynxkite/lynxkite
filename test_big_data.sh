0876d034572b43ca1f12e0b3c139939d9d1b97bd
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-01 14:38:36 +0200

    Reset to origin before gitting.

--- test_big_data.sh
+++ test_big_data.sh
@@ -10,9 +10,12 @@ if [[ "$USER" == 'jenkins' ]]; then
   git config user.name 'lynx-jenkins'
   git config user.email 'pizza-support@lynxanalytics.com'
   git config push.default simple
+  export GIT_SSH_COMMAND='ssh -i ~/.ssh/lynx-jenkins'
   git checkout "$GIT_BRANCH"
+  git fetch
+  git reset --hard "origin/$GIT_BRANCH"  # Discard potential local changes from failed runs.
   mv kitescripts/perf/last_output.md{.new,}
   PERF_CHANGE="12% faster"
   git commit -am "Big Data Test: $PERF_CHANGE"
-  GIT_SSH_COMMAND='ssh -i ~/.ssh/lynx-jenkins' git push
+  git push
 fi
