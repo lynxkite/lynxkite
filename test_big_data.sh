a5b65c3de76bbe6c74b5953a33207d76f9d68c4b
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-01 13:54:19 +0200

    There is a GIT_BRANCH env variable.

--- test_big_data.sh
+++ test_big_data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -xue
 
-env
 cd $(dirname $0)
+
+# Run test. (TODO)
 cat kitescripts/perf/last_output.md | sed 's/size/how big/' > kitescripts/perf/last_output.md.new
+
+# Commit and push changed output on PR branch.
+git config user.name 'lynx-jenkins'
+git config user.email 'pizza-support@lynxanalytics.com'
+git checkout "$GIT_BRANCH"
 mv kitescripts/perf/last_output.md{.new,}
-git status
 PERF_CHANGE="12% faster"
-git commit --author="lynx-jenkins <pizza-support@lynxanalytics.com>" -am "Big Data Test: $PERF_CHANGE"
-exit 1
+git commit -am "Big Data Test: $PERF_CHANGE"
 git push
