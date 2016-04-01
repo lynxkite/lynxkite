8360c804506bdabed02700f7021c51df4d5c1ec3
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-01 13:47:54 +0200

    We need the branch name.

--- test_big_data.sh
+++ test_big_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xue
 
+env
 cd $(dirname $0)
 cat kitescripts/perf/last_output.md | sed 's/size/how big/' > kitescripts/perf/last_output.md.new
 mv kitescripts/perf/last_output.md{.new,}
 git status
 PERF_CHANGE="12% faster"
-git commit -am "Big Data Test: $PERF_CHANGE"
+git commit --author="lynx-jenkins <pizza-support@lynxanalytics.com>" -am "Big Data Test: $PERF_CHANGE"
+exit 1
 git push
