e538f4c1b6bdf635747ebd68932cfe84086bc304
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-06 11:11:28 +0200

    Simple tests for features.

--- test/com/lynxanalytics/biggraph/graph_operations/NeuralNetworkTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/NeuralNetworkTest.scala
@@ -8,23 +8,52 @@ import com.lynxanalytics.biggraph.graph_api.Scripting._
 import com.lynxanalytics.biggraph.graph_util.Scripting._
 
 class NeuralNetworkTest extends FunSuite with TestGraphOp {
-  test("features") {
+  def differenceSquareSum(a: Attribute[Double], b: Attribute[Double]): Double = {
+    val diff = DeriveJS.deriveFromAttributes[Double](
+      "(a - b) * (a - b)",
+      Seq("a" -> a, "b" -> b),
+      a.vertexSet).attr
+    diff.rdd.values.sum
+  }
+
+  test("label, trivial") {
+    // The label is a random attribute. It is visible to the vertex.
+    val vs = CreateVertexSet(1000).result.vs
+    val a = vs.randomAttribute(0)
+    val prediction = {
+      val op = NeuralNetwork(
+        featureCount = 0, networkSize = 2, iterations = 100, radius = 0,
+        hideState = false, forgetFraction = 0.0)
+      op(op.edges, vs.emptyEdgeBundle)(op.label, a).result.prediction
+    }
+    assert(differenceSquareSum(prediction, a) < 1)
+  }
+
+  test("feature, trivial") {
+    // The label and the feature are the same random attribute.
+    val vs = CreateVertexSet(1000).result.vs
+    val a = vs.randomAttribute(0)
+    val prediction = {
+      val op = NeuralNetwork(
+        featureCount = 1, networkSize = 3, iterations = 100, radius = 0,
+        hideState = true, forgetFraction = 0.0)
+      op(op.edges, vs.emptyEdgeBundle)(op.label, a)(op.features, Seq(a)).result.prediction
+    }
+    assert(differenceSquareSum(prediction, a) < 1)
+  }
+
+  test("features, simple") {
     val vs = CreateVertexSet(1000).result.vs
     val a = vs.randomAttribute(100)
     val b = vs.randomAttribute(200)
     val c = DeriveJS.deriveFromAttributes[Double]("a - b", Seq("a" -> a, "b" -> b), vs).attr
-
     val prediction = {
       val op = NeuralNetwork(
-        featureCount = 2, networkSize = 20, iterations = 1000, radius = 0,
-        hideState = false, forgetFraction = 0.0)
+        featureCount = 2, networkSize = 4, iterations = 100, radius = 0,
+        hideState = true, forgetFraction = 0.0)
       op(op.edges, vs.emptyEdgeBundle)(op.label, c)(op.features, Seq(a, b)).result.prediction
     }
-    val error = DeriveJS.deriveFromAttributes[Double](
-      "(prediction - c) * (prediction - c)",
-      Seq("prediction" -> prediction, "c" -> c),
-      vs).attr
-    assert(error.rdd.values.collect.sum == 0)
+    assert(differenceSquareSum(prediction, c) < 1)
   }
 
   test("lattice") {
@@ -35,7 +64,7 @@ class NeuralNetworkTest extends FunSuite with TestGraphOp {
       g.vertices).attr
     val prediction = {
       val op = NeuralNetwork(
-        featureCount = 0, networkSize = 20, iterations = 1000, radius = 4,
+        featureCount = 0, networkSize = 20, iterations = 1, radius = 4,
         hideState = false, forgetFraction = 0.0)
       op(op.edges, g.edges)(op.label, sideNum).result.prediction
     }
@@ -43,6 +72,6 @@ class NeuralNetworkTest extends FunSuite with TestGraphOp {
       "var p = prediction < 0 ? 'left' : 'right'; p === truth ? 0.0 : 1.0;",
       Seq("prediction" -> prediction, "truth" -> g.attrs("side_truth")),
       g.vertices).attr
-    assert(isWrong.rdd.values.collect.sum == 0)
+    assert(isWrong.rdd.values.sum == 0)
   }
 }
