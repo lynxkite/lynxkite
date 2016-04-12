5c1b9328576231054967810be48558ee882f93dd
gsvigruha <gergely.svigruha@lynxanalytics.com>
2016-04-12 12:54:45 +0800

    Test add reversed edges.

--- kitescripts/big_data_tests/pagerank.groovy
+++ kitescripts/big_data_tests/pagerank.groovy
@@ -3,6 +3,7 @@
 /// REQUIRE_SCRIPT edge_import.groovy
 
 project = lynx.loadProject('edge_import_result')
+project.addReversedEdges()
 
 // PageRank ignores non-positive weights so let's use Standard Uniform distribution.
 project.addRandomEdgeAttribute(
