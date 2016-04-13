8e6a35f2c11dfda436cae63c06da452aabeb3c68
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-04-13 12:10:12 +0200

    Make cronscript source the same kiterc as the call_spark_submit

--- tools/cron_upload.sh
+++ tools/cron_upload.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 #
-# Runs the bigraph uploadLogs, which uploads the logs and operation performance data
-# to google storage.
+# Runs the bigraph uploadLogs, which will send the logs and operation performance data
+# to LynxAnalytics.
+#
 # This script should be run as a cron job!
-# Copy this file to /etc/cron.monthly/ for automatic monthly uploads.
 
-# Set this variable to the biggraph script log file. (The same as KITE_SCRIPT_LOGS)
-SCRIPTLOGS=${HOME}/biggraph.log
 
-checkFile() {
-    FILE=$1
-    if [ ! -e $FILE ]; then
-        >&2 echo "File '$FILE' does not exist!"
-        exit 1
-    fi
+KITE_SITE_CONFIG=${KITE_SITE_CONFIG:-$HOME/.kiterc}
+source ${KITE_SITE_CONFIG}
 
-    if [ ! -f $FILE ]; then
-        >&2 echo "File '$FILE' is not a regular file!"
-        exit 1
-    fi
-
-    if [ ! -r $FILE ]; then
-        >&2 echo "File '$FILE' cannot be read!"
-        exit 1
-    fi
-}
-
-checkFile $SCRIPTLOGS
-
-COMMAND=`tail -n1 $SCRIPTLOGS | cut -d ' ' -f3`
-
-if [[ ! $COMMAND =~ biggraph$ ]]; then
-    >&2 echo "The command: '$COMMAND' doesn't look like a bigraph script"
+if [ -f "${KITE_SITE_CONFIG_OVERRIDES}" ]; then
+  source ${KITE_SITE_CONFIG_OVERRIDES}
 fi
 
-checkFile $COMMAND
-
-if [ ! -x $COMMAND ]; then
-    >&2 echo "File $COMMAND is not executable"
-fi
+COMMAND=`tail -n1 $KITE_SCRIPT_LOGS | cut -d ' ' -f3`
 
 $COMMAND uploadLogs
 
