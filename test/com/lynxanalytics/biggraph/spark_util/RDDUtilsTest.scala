c9be651d4337ad5bdec1d4226ffeea3e951fefaf
Daniel Darabos <darabos.daniel@gmail.com>
2016-04-18 11:26:19 +0200

    Tests for HybridRDD.

--- test/com/lynxanalytics/biggraph/spark_util/RDDUtilsTest.scala
+++ test/com/lynxanalytics/biggraph/spark_util/RDDUtilsTest.scala
@@ -40,41 +40,6 @@ class RDDUtilsTest extends FunSuite with TestSparkContext {
     }
   }
 
-  test("lookup operations work as expected") {
-    val rnd = new util.Random(0)
-    val localSource = (0 until 1000).map(_ => (rnd.nextInt(100), rnd.nextLong()))
-    val localLookup = (0 until 50).map(x => (x, rnd.nextDouble()))
-    val localLookupMap = localLookup.toMap
-    val localResult = localSource
-      .flatMap {
-        case (key, value) => localLookupMap.get(key).map(lv => key -> (value, lv))
-      }
-      .sorted
-
-    def checkGood(rdd: RDD[(Int, (Long, Double))]) {
-      assert(rdd.collect.toSeq.sorted == localResult)
-    }
-
-    import Implicits._
-    val sourceRDD = sparkContext.parallelize(localSource, 10)
-    val lookupRDD = sparkContext.parallelize(localLookup).sortUnique(new HashPartitioner(10))
-
-    checkGood(RDDUtils.joinLookup(sourceRDD, lookupRDD))
-    checkGood(RDDUtils.smallTableLookup(sourceRDD, localLookupMap))
-    checkGood(RDDUtils.hybridLookup(HybridRDD(sourceRDD, 200), lookupRDD))
-    checkGood(RDDUtils.hybridLookup(HybridRDD(sourceRDD, 0), lookupRDD))
-  }
-
-  test("lookup on empty RDD") {
-    import Implicits._
-    val sourceRDD = sparkContext.emptyRDD[(Int, Long)]
-    val lookupRDD = sparkContext.emptyRDD[(Int, Double)].sortUnique(new HashPartitioner(1))
-    assert(RDDUtils.joinLookup(sourceRDD, lookupRDD).collect.isEmpty)
-    assert(RDDUtils.smallTableLookup(sourceRDD, Map()).collect.isEmpty)
-    assert(RDDUtils.hybridLookup(HybridRDD(sourceRDD, 200), lookupRDD).collect.isEmpty)
-    assert(RDDUtils.hybridLookup(HybridRDD(sourceRDD, 0), lookupRDD).collect.isEmpty)
-  }
-
   test("groupBySortedKey works as expected") {
     import Implicits._
     val rnd = new util.Random(0)
