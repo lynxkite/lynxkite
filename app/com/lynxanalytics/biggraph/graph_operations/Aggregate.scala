cdaef04ae9c9cd8718b073ee3ce9e94e5a468ee0
David Herskovics <david.herskovics@lynxanalytics.com>
2016-04-29 16:30:38 +0200

    New aggregator: median.

--- app/com/lynxanalytics/biggraph/graph_operations/Aggregate.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/Aggregate.scala
@@ -308,6 +308,18 @@ object Aggregator {
     }
   }
 
+  object Median extends LocalAggregatorFromJson { def fromJson(j: JsValue) = Median() }
+  case class Median() extends LocalAggregator[Double, Double] {
+    def outputTypeTag(inputTypeTag: TypeTag[Double]) = inputTypeTag
+    def aggregate(values: Iterable[Double]) = {
+      val cnt: Int = values.size
+      val (halfCount1: Int, halfCount2: Int) = if (cnt % 2 == 0) { if (cnt == 0) (0, 0) else (cnt / 2 - 1, cnt / 2) }
+      else ((cnt - 1) / 2, (cnt - 1) / 2)
+      val sortedValues = values.toSeq.sortWith(_ > _)
+      (sortedValues(halfCount1) + sortedValues(halfCount2)) / 2
+    }
+  }
+
   object MostCommon extends LocalAggregatorFromJson { def fromJson(j: JsValue) = MostCommon() }
   case class MostCommon[T]() extends LocalAggregator[T, T] {
     def outputTypeTag(inputTypeTag: TypeTag[T]) = inputTypeTag
