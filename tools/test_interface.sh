274413668f5acdb8944eb3996a96347485029075
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-05-05 12:37:23 +0200

    Address reviewer's comments

--- tools/test_interface.sh
+++ tools/test_interface.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# Tests that check LynxKite's response/behavior with respect to various http requests.
+
 
 set -xueo pipefail
 
 # Wait till kite can actually serve requests
-sleep 5 
-
+tools/wait_for_port.sh ${PORT}
 
 
 # Dynamic content is escaped in error messages
-curl -v "http://localhost:${PORT}/<script>cross_site_scripting.nasl</script>.asp" | grep "&lt;script&gt;cross_site_scripting.nasl&lt;"
+curl -v "http://localhost:${PORT}/<script>cross_site_scripting.nasl</script>.asp" | \
+    grep "&lt;script&gt;cross_site_scripting.nasl&lt;"
 
 
 
@@ -16,7 +18,7 @@ curl -v "http://localhost:${PORT}/<script>cross_site_scripting.nasl</script>.asp
 LOGFILES1=`ls stage/logs | wc -l`
 EXPECTED=$((LOGFILES1 + 1))
 curl -d '{"fake": 0}' -H "Content-Type: application/json" "http://localhost:${PORT}/forceLogRotate"
-sleep 3
+
 LOGFILES2=`ls stage/logs | wc -l`
 if [ "$EXPECTED" != "$LOGFILES2" ]; then
     exit 1
