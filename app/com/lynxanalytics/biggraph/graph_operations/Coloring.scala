01814353e5e2edb8c983bf96f4424db78c882a0c
David Herskovics <huncros@gmail.com>
2016-05-12 14:49:29 +0200

    cleaned up code

--- app/com/lynxanalytics/biggraph/graph_operations/Coloring.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/Coloring.scala
@@ -1,16 +1,11 @@
 
-/**
- * Created by huncros on 2016.05.02..
- */
-//
 package com.lynxanalytics.biggraph.graph_operations
 
 import org.apache.spark
 import com.lynxanalytics.biggraph.graph_api.{ DataSet, OutputBuilder, RuntimeContext, _ }
 import com.lynxanalytics.biggraph.spark_util.HybridRDD
 import com.lynxanalytics.biggraph.spark_util.Implicits._
-
-import scala.util.control.Breaks
+import org.apache.spark.rdd.RDD
 
 object Coloring extends OpFromJson {
   class Input extends MagicInputSignature {
@@ -31,11 +26,6 @@ case class Coloring()
 
   def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
 
-  // !!!!!!!!!!!!!!!!!!!!!                     EZZEL VALSZEG MEG KENE VALAMIT CSINALNI                    !!!!!!!!!!!!!
-  //                                                            ||
-  //                                                            \/
-  //override def toJson = Json.obj("dampingFactor" -> dampingFactor, "iterations" -> iterations)
-
   def execute(inputDatas: DataSet,
               o: Output,
               output: OutputBuilder,
@@ -46,94 +36,118 @@ case class Coloring()
     val vertexPartitioner = vertices.partitioner.get
     val edgePartitioner = edges.partitioner.get
 
+    val maxIterations = 10
+
     val edgesWithoutID = edges.map { case (id, e) => (e.src, e.dst) }.
       filter { case (src, dst) => src != dst }.distinct()
 
-    val degreeWithoutIsolatedVertices = (edgesWithoutID ++ edgesWithoutID.map { case (src, dst) => (dst, src) }).
-      groupBySortedKey(vertexPartitioner).mapValues(l => l.size.toDouble)
+    val degreeWithoutIsolatedVertices = edgesWithoutID.flatMap { case (src, dst) => Seq(src -> 1.0, dst -> 1.0) }.
+      reduceBySortedKey(edgePartitioner, _ + _)
 
-    val degree = (degreeWithoutIsolatedVertices ++ vertices.mapValues(_ => 0)).reduceByKey(_ max _).asUniqueSortedRDD
-    val convexOrdering = degree.mapValues(degr => { val rnd = scala.util.Random; degr * Math.pow(-1, rnd.nextInt(1)) })
-
-    val randomOrdering: AttributeRDD[Double] = vertices.mapPartitionsWithIndex({
-      (pid, it) =>
-        val rnd = new util.Random(pid)
-        it.map {
-          case (vid, _) => vid -> rnd.nextDouble()
-        }
-    },
-      preservesPartitioning = true).asUniqueSortedRDD
-
-    var bestColoringSoFar = vertices.mapValues(_ => vertices.count().toDouble)
-    var currentNumberOfColors = vertices.count().toDouble
-
-    def updateNumberOfColors() = {
-      currentNumberOfColors = bestColoringSoFar.map { case (id, color) => color }.max()
+    val degree = vertices.sortUnique(edgePartitioner).sortedLeftOuterJoin(degreeWithoutIsolatedVertices).
+      mapValues(_._2.getOrElse(0.0))
+    val convexOrdering: AttributeRDD[Double] = {
+      degree.mapPartitionsWithIndex({
+        (pid, it) =>
+          val rnd = new util.Random(pid)
+          it.map {
+            case (vid, degr) => vid -> degr * (1 - rnd.nextDouble())
+          }
+      },
+        preservesPartitioning = true).asUniqueSortedRDD
     }
 
-    def updateBestColorSoFar(ordering: AttributeRDD[Double]) = {
-      bestColoringSoFar = pertColor(ordering, currentNumberOfColors)
-      updateNumberOfColors()
-    }
-
-    def findColoring() = {
-      val start = System.currentTimeMillis()
-      updateBestColorSoFar(degree)
-      updateBestColorSoFar(convexOrdering)
-      val c1 = System.currentTimeMillis()
-      println("Coloring is done with the two basic ordering in " + (c1 - start) + " ms")
-      updateBestColorSoFar(randomOrdering)
-      val end = System.currentTimeMillis()
-      println("Coloring is finally done in total of " + (end - start) + " ms")
-      bestColoringSoFar
-    }
-
-    /* pertColor gets an ordering:AttributeRDD which tells us how to direct the edges:
-     *   from lower order to the higher one
-     * Calculates [pi(v)  = the length of the longest directed path starting with vertex v] for every vertex v and the
-     *   vertices with same pi will in the same color class
-     * The tooManyColors parameter is optional, by giving it a positive value it can be used to specify a limit
-     *   such that if there would be at least that many color classes then the function terminates and doesn't change
-     *   the coloring. Else it returns a better coloring:AttributeRDD with the colors for each vertex v (i.e. pi(v))
+    /* pertColoring works on a directed acylic graph (DAG) and colors each vertex according to the length of the longest
+     * path starting from that vertex. The DAG is given as an input by a list of its directed edges.
+     * The too many colors parameter is there for stop the pertColoring if we were to have more colors then we want -
+     * it's used when we already have some coloring and so we are only interested in colorings with fewer colors.
+     * If pertColoring is stopped due reaching too many colors then it returns None, otherwise it returns
+     * Some(a new Coloring)
      */
-    def pertColor(ordering: AttributeRDD[Double], tooManyColors: Double = vertices.count()) = {
+    @annotation.tailrec
+    def pertColoring(
+      directedEdges: RDD[(Long, Long)], coloringSoFar: AttributeRDD[Double],
+      nextColor: Double, tooManyColors: Double): Option[AttributeRDD[Double]] = {
+      if (nextColor >= tooManyColors) None
+      else {
+        val notYetColored = directedEdges.mapValues(dst => nextColor).distinct
+        if (notYetColored.isEmpty()) Some(coloringSoFar)
+        else {
+          val newDirectedEdges = directedEdges.map(e => e.swap).join(notYetColored).map { case (dst, (src, color)) => (src, dst) }
+          val newColoringSoFar = coloringSoFar.leftOuterJoin(notYetColored).mapValues {
+            case (oldColor, newColorOpt) => newColorOpt.getOrElse(oldColor)
+          }.sortUnique(vertexPartitioner)
+          pertColoring(newDirectedEdges, newColoringSoFar, nextColor + 1.0, tooManyColors)
+        }
+      }
+    }
+
+    def directEdgesFromOrdering(ordering: AttributeRDD[Double]) = {
       // RDD of (dst, (src, order of src))
-      val directedEdges2 = ordering.join(edgesWithoutID).map { case (src, ordDst) => (ordDst._2, (src, ordDst._1)) }
+      val directedEdges2 = ordering.join(edgesWithoutID).map { case (src, (srcOrd, dst)) => (dst, (src, srcOrd)) }
       // RDD of ((src, order of src), (dst, order of dst)
       val directedEdges1 = ordering.join(directedEdges2).
-        map { case (dst, ordDst_srcOrdSrc) => (ordDst_srcOrdSrc._2, (dst, ordDst_srcOrdSrc._1)) }
+        map { case (dst, (dstOrd, (src, srcOrd))) => ((src, srcOrd), (dst, dstOrd)) }
       // RDD of (src, dst) where edges are directed in such a way that order of src < order of dst
-      var directedEdges = directedEdges1.map {
+      val directedEdges = directedEdges1.map {
         case (srcOrd, dstOrd) => if (srcOrd._2 < dstOrd._2) (srcOrd._1, dstOrd._1)
         else if (srcOrd._2 > dstOrd._2) (dstOrd._1, srcOrd._1)
         else if (srcOrd._1 < dstOrd._1) (srcOrd._1, dstOrd._1)
         else (dstOrd._1, srcOrd._1)
       }
-
-      var coloringForGivenOrder = vertices.mapValues(_ => 1.0)
-      var currentColor = 1.0
-
-      while ((!directedEdges.isEmpty()) && (currentColor < tooManyColors)) {
-        currentColor += 1.0
-        val notYetColored = directedEdges.mapValues(dst => currentColor).distinct
-        coloringForGivenOrder = (coloringForGivenOrder ++ notYetColored).reduceByKey(_ max _).asUniqueSortedRDD
-        directedEdges = directedEdges.map { case (src, dst) => (dst, src) }.join(notYetColored).
-          map { case (dst, srcColor) => (srcColor._1, dst) }
-      }
-      if (directedEdges.isEmpty()) coloringForGivenOrder else bestColoringSoFar
+      directedEdges
     }
 
-    findColoring()
-    output(o.coloring, bestColoringSoFar.sortedRepartition(vertexPartitioner))
+    /* findBetterColoring takes an already calculated coloring and tries to find a better one by trying out
+     * a new ordering. We get the new ordering by taking the color mod (number of colors/2) for each vertex.
+     * The idea behind this is that we want to cut up the long paths and the colors represent the length of the longest
+     * path so by doing this we destroy all previous long paths - and hope that we don't create new ones.
+     * We iterate it for maxIterations steps or until we don't get a worse coloring than the input coloring for that
+     * iteration step.
+     */
+    @annotation.tailrec
+    def findBetterColoring(oldColoring: AttributeRDD[Double], currentNumberOfColors: Double,
+                           iterationsLeft: Int): AttributeRDD[Double] = {
+      if (iterationsLeft > 0) {
+        val newOrdering = oldColoring.mapValues(c => math.floor(c % (currentNumberOfColors / 2)))
+        val directedEdges = directEdgesFromOrdering(newOrdering)
+        val startingColoring = vertices.mapValues(_ => 1.0)
+
+        val newColoring = pertColoring(directedEdges, startingColoring, 2.0, currentNumberOfColors).getOrElse(oldColoring)
+        val newNumberOfColors = newColoring.values.max
+        if (newNumberOfColors > currentNumberOfColors) oldColoring
+        else findBetterColoring(newColoring, newNumberOfColors, iterationsLeft - 1)
+      } else oldColoring
+    }
+
+    /* Tries out to particular orderings for a start: the ordering based on the degrees of the vertices and another one
+     * derived from it called convexOrdering - it basically puts the vertices with big degree to both ends of the
+     * ordering while those with smaller degrees are in the middle.
+     * The idea behind singling out these two orderings is that a long path in the underlying undirected graph
+     * is very likely to go through vertices with high degree. If vertices with high degree are next to each
+     * other on this path then convexOrdering has a 1/2 chance to result in a DAG where this path is not directed path.
+     * On the other hand, if the vertices with high degree have vertices with smaller degree between them along the
+     * path then the ordering based on the degree will cut up such a path.
+     * So we are hoping that one of these orderings will give us a good starting coloring. Then try to improve the
+     * coloring by iterating the findBetterColoring function.
+     */
+    def findColoring(iteration: Int) = {
+      val vertexCount = vertices.count()
+      val startingColoring = vertices.mapValues(_ => 1.0)
+      val directedEdgesToDegreeOrdering = directEdgesFromOrdering(degree)
+      val coloringByDegreeOrdering = pertColoring(directedEdgesToDegreeOrdering, startingColoring, 2, vertexCount).get
+
+      val numberOfColorsSoFar = coloringByDegreeOrdering.values.max
+      val directedEdgesToConvordering = directEdgesFromOrdering(convexOrdering)
+      val coloringAfterTryingConvexOrdering = pertColoring(directedEdgesToConvordering,
+        startingColoring, 2, numberOfColorsSoFar).getOrElse(coloringByDegreeOrdering)
+
+      val numberOfColorsSoFar2 = coloringAfterTryingConvexOrdering.values.max
+
+      findBetterColoring(coloringAfterTryingConvexOrdering, numberOfColorsSoFar2, iteration)
+    }
+
+    val coloring = findColoring(maxIterations)
+    output(o.coloring, coloring.sortedRepartition(vertexPartitioner))
   }
 }
-
-/*randomAttr: AttributeRDD[Double] = vertices.mapPartitionsWithIndex({
-        (pid, it) =>
-          val rnd = util.Random(pid)
-          it.map {
-            case (vid, _) => vid -> rnd.nextValue()
-          }
-      }
-        preservesPartitioning = true).asUniqueSortedRDD
-*/ 
