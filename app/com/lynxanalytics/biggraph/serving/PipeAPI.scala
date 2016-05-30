0aa7e41eaf80270d155cbe369c8f3e51fccd1f73
Daniel Darabos <darabos.daniel@gmail.com>
2016-05-30 15:31:27 +0200

    Working named pipe interface.

--- app/com/lynxanalytics/biggraph/serving/PipeAPI.scala
+++ app/com/lynxanalytics/biggraph/serving/PipeAPI.scala
@@ -5,25 +5,77 @@ import play.api.libs.json
 import com.lynxanalytics.biggraph.graph_util.LoggedEnvironment
 import com.lynxanalytics.biggraph.BigGraphProductionEnvironment
 import com.lynxanalytics.biggraph.{ bigGraphLogger => log }
+import com.lynxanalytics.biggraph.controllers
+import com.lynxanalytics.biggraph.frontend_operations
 import com.lynxanalytics.biggraph.graph_operations.DynamicValue
 
 object PipeAPI {
+  val env = BigGraphProductionEnvironment
+  implicit val metaManager = env.metaGraphManager
+  implicit val dataManager = env.dataManager
+  val ops = new frontend_operations.Operations(env)
 
-  case class Command(kind: String, payload: json.JsObject)
-  case class NewProjectRequest()
-  case class NewCheckpointResponse(id: String)
-  case class OperationRequest(checkpoint: String, operation: String, parameters: Map[String, String])
+  def normalize(operation: String) = operation.replace("-", "").toLowerCase
+  lazy val normalizedIds = ops.operationIds.map(id => normalize(id) -> id).toMap
+
+  case class Command(responsePipe: String, command: String, payload: json.JsObject) {
+    def execute(): Unit = {
+      val response = try {
+        command match {
+          case "newProject" => json.Json.toJson(newProject())
+          case "runOperation" => json.Json.toJson(runOperation(payload.as[OperationRequest]))
+          case "getScalar" => json.Json.toJson(getScalar(payload.as[ScalarRequest]))
+        }
+      } catch {
+        case t: Throwable =>
+          log.error(s"Error while processing $this:", t)
+          json.Json.obj("error" -> t.toString, "request" -> json.Json.toJson(this))
+      }
+      val data = json.Json.asciiStringify(response)
+      org.apache.commons.io.FileUtils.writeStringToFile(
+        new java.io.File(responsePipe), data, "utf-8")
+    }
+  }
+
+  case class CheckpointResponse(checkpoint: String)
+  case class OperationRequest(
+    checkpoint: String,
+    operation: String,
+    parameters: Map[String, String])
   case class ScalarRequest(checkpoint: String, scalar: String)
-  implicit val rCommand = json.Json.reads[Command]
-  //implicit val rNewProjectRequest = json.Json.reads[NewProjectRequest]
-  implicit val wNewCheckpointResponse = json.Json.writes[NewCheckpointResponse]
+  implicit val fCommand = json.Json.format[Command]
+  implicit val wCheckpointResponse = json.Json.writes[CheckpointResponse]
   implicit val rOperationRequest = json.Json.reads[OperationRequest]
   implicit val rScalarRequest = json.Json.reads[ScalarRequest]
-  implicit val wScalarResponse = json.Json.writes[DynamicValue]
+  implicit val wDynamicValue = json.Json.writes[DynamicValue]
 
-  val env = BigGraphProductionEnvironment
-  implicit val metaGraphManager = env.metaGraphManager
-  implicit val dataManager = env.dataManager
+  val user = User("Pipe API User", isAdmin = true)
+
+  def newProject(): CheckpointResponse = {
+    CheckpointResponse("") // Blank checkpoint.
+  }
+
+  def getViewer(cp: String) =
+    new controllers.RootProjectViewer(metaManager.checkpointRepo.readCheckpoint(cp))
+
+  def runOperation(request: OperationRequest): CheckpointResponse = {
+    val normalized = normalize(request.operation)
+    assert(normalizedIds.contains(normalized), s"No such operation: ${request.operation}")
+    val operation = normalizedIds(normalized)
+    val viewer = getViewer(request.checkpoint)
+    val context = controllers.Operation.Context(user, viewer)
+    val spec = controllers.FEOperationSpec(operation, request.parameters)
+    val newState = ops.applyAndCheckpoint(context, spec)
+    CheckpointResponse(newState.checkpoint.get)
+  }
+
+  def getScalar(request: ScalarRequest): DynamicValue = {
+    val viewer = getViewer(request.checkpoint)
+    val scalar = viewer.scalars(request.scalar)
+    implicit val tt = scalar.typeTag
+    import com.lynxanalytics.biggraph.graph_api.Scripting._
+    DynamicValue.convert(scalar.value)
+  }
 
   val pipeFile = LoggedEnvironment.envOrNone("KITE_API_PIPE").map(new java.io.File(_))
 
@@ -49,10 +101,9 @@ class PipeAPI(pipe: java.io.File) extends Thread("Pipe API") {
       log.info(s"Pipe API reading from $pipe.")
       for (line <- input.getLines) {
         try {
-          println(line)
+          log.info(s"Received $line")
           val cmd = json.Json.parse(line).as[Command]
-          println(cmd.kind)
-          println(cmd.payload)
+          cmd.execute()
         } catch {
           case t: Throwable => log.error(s"Error while processing $line:", t)
         }
