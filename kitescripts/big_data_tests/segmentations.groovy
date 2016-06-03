17f20c129c382e85f898932d6532e9106ccb0a7a
Gabor Feher <gabor.feher@lynxanalytics.com>
2016-06-03 23:45:56 +0200

    add groovy tests

---
+++ kitescripts/big_data_tests/segmentations.groovy
@@ -0,0 +1,33 @@
+// Test basic segmentation creation operations.
+
+/// REQUIRE_SCRIPT random_attributes.groovy
+
+project = lynx.loadProject("random_attributes")
+
+project.segmentByDoubleAttribute(
+  name: 'seg',
+  attr: 'rnd_std_normal',
+  'interval-size': '0.01',
+  overlap: 'no')
+s = project.segmentations['seg']
+println "[seg] vertices: ${s.scalars['vertex_count']} edges: ${s.scalars['edge_count']}"
+
+project.segmentByDoubleAttribute(
+  name: 'seg_overlap',
+  attr: 'rnd_std_normal',
+  'interval-size': '0.01',
+  overlap: 'yes')
+s = project.segmentations['seg_overlap']
+println "[seg_overlap] vertices: ${s.scalars['vertex_count']} edges: ${s.scalars['edge_count']}"
+
+project.derivedVertexAttribute(
+  output: 'x',
+  type: 'string',
+  expr: 'rnd_std_uniform.toFixed(7)')
+project.segmentByStringAttribute(
+  name: 'seg_string',
+  attr: 'x')
+s = project.segmentations['seg_string']
+println "[seg_string] vertices: ${s.scalars['vertex_count']} edges: ${s.scalars['edge_count']}"
+
+project.saveAs("segmentations")
