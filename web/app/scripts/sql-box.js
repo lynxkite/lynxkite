372af7a7893e651267933e5c73f1d1de74e4edeb
David Herskovics <huncros@gmail.com>
2016-06-03 18:28:42 +0200

    removed internal SQL header

--- web/app/scripts/sql-box.js
+++ web/app/scripts/sql-box.js
@@ -7,7 +7,6 @@ angular.module('biggraph').directive('sqlBox', function($window, side, util) {
     scope: {
       side: '=?',
       directory: '=?',
-      hideable: '=?'
      },
     templateUrl: 'sql-box.html',
     link: function(scope) {
