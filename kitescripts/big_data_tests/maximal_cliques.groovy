8ef74f5e80d9c532855c71d4f187219266e90a28
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-06-15 15:19:16 +0200

    Add maximal cliques test

---
+++ kitescripts/big_data_tests/maximal_cliques.groovy
@@ -0,0 +1,12 @@
+// Tests the "Maximal cliques" operation
+
+/// REQUIRE_SCRIPT filter_high_degree_vertices.groovy
+
+project = lynx.loadProject('filter_high_degree_vertices_result')
+
+project.maximalCliques('bothdir': 'yes')
+
+s = project.segmentations['maximal_cliques']
+
+println "segmentation vertices: ${s.scalars['vertex_count']} segmentation edges: ${s.scalars['edge_count']}"
+
