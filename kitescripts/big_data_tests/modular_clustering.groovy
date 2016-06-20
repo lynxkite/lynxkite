2424effb95c8323b8ecd7195055b44702953e2d4
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-06-20 11:54:28 +0200

    Add modular clustering test

---
+++ kitescripts/big_data_tests/modular_clustering.groovy
@@ -0,0 +1,16 @@
+// Tests the "Modular clustering" operation
+
+/// REQUIRE_SCRIPT filter_high_degree_vertices.groovy
+
+project = lynx.loadProject('filter_high_degree_vertices_result')
+
+project.maximalCliques(
+  'name': 'modular_clusters',
+  'weight': '!no weight',
+  'max-iterations': '30',
+  'min-increment-per-iteration': '0.001'
+)
+
+s = project.segmentations['modular_clusters']
+
+println "modular clustering: ${s.scalars['modular_clusters']}"
