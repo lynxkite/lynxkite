394066d2e828b9187f82d3200e9726ad33262765
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-06-20 13:50:02 +0200

    Add tests for clustering coeff and rank attribute

---
+++ kitescripts/big_data_tests/clustering_coefficient.groovy
@@ -0,0 +1,19 @@
+// Tests the "Clustering coefficient" operation
+
+/// REQUIRE_SCRIPT filter_high_degree_vertices.groovy
+
+project = lynx.loadProject('filter_high_degree_vertices_result')
+
+name='clustering_coefficient'
+
+project.clusteringCoefficient(
+  'name': name,
+)
+
+histogram = project.vertexAttributes[name].histogram(
+  logarithmic: true,
+  precise: true)
+
+println "$name: $histogram"
+
+
