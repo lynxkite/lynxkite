d42cff684e979327be346a48491636bacbdad683
János Maginecz <wafleeee@gmail.com>
2016-06-20 11:15:32 +0200

    remove all mentions of split target

--- test/com/lynxanalytics/biggraph/frontend_operations/SplitToTrainAndTestSet.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/SplitToTrainAndTestSet.scala
@@ -1,16 +1,15 @@
 package com.lynxanalytics.biggraph.frontend_operations
 
-import com.lynxanalytics.biggraph.graph_api.AttributeRDD
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class SplitToTrainAndTestSet extends OperationsTestBase {
   test("Split to train and test sets") {
-    val target = "age"
+    val source = "age"
     run("Example Graph")
     run("Split to train and test set",
-      Map("source" -> s"$target", "test_set_ratio" -> "0.25", "seed" -> "16"))
-    val trainRDD = project.vertexAttributes(s"${target}_train").rdd
-    val testRDD = project.vertexAttributes(s"${target}_test").rdd
+      Map("source" -> s"$source", "test_set_ratio" -> "0.25", "seed" -> "16"))
+    val trainRDD = project.vertexAttributes(s"${source}_train").rdd
+    val testRDD = project.vertexAttributes(s"${source}_test").rdd
     // train and test should be disjoint
     assert(trainRDD.join(testRDD).isEmpty)
     // these results are determined by the random seed
