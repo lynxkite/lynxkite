e9e6d0d4959f0baef8775d5994e072572602b859
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-06-21 16:47:44 +0200

    Added merge vertices by attribute test

---
+++ kitescripts/big_data_tests/merge_vertices_by_attribute.groovy
@@ -0,0 +1,26 @@
+// Tests the "Merge vertices by attribute" operation
+
+/// REQUIRE_SCRIPT random_attributes_with_constants.groovy
+
+project = lynx.loadProject('random_attributes_with_constants_result')
+
+
+project.derivedVertexAttribute(
+  'output': 'label',
+  'type': 'double',
+  'expr': 'Math.floor(rnd_std_uniform*number_of_vertices*0.01)'
+)
+
+project.mergeVerticesByAttribute(
+  'key': 'label',
+  'aggregate-rnd_std_normal': 'average'
+)
+
+histogram = project
+  .vertexAttributes['rnd_std_normal_average']
+  .histogram(
+    logarithmic: true,
+    precise: true
+  )
+
+println "Merge vertices by attribute: rnd_std_normal_average histogram: $histogram"
