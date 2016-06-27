5be8b0d09918ed7a0f157b29822b2e5ba9b07df9
Gabor Olah <gabor.olah@lynxanalytics.com>
2016-06-27 13:41:25 +0200

    union_with_another_project.groovy

---
+++ kitescripts/big_data_tests/union_with_another_project.groovy
@@ -0,0 +1,18 @@
+// Tests the "Union with another project" operation
+// We're unioning with ourselves
+
+/// REQUIRE_SCRIPT edge_import.groovy
+
+name = 'edge_import_result'
+
+project = lynx.loadProject(name)
+otherName = project.rootCheckpointWithTitle(name)
+
+project.unionWithAnotherProject(
+  'other': otherName,
+  'id-attr': 'new_id'
+)
+
+
+println "vertices: ${project.scalars['vertex_count']} edges ${project.scalars['edge_count']}"
+
