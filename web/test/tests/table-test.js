14edc5569b8d0c104b8dc9c89ff375154ec67e5d
János Maginecz <wafleeee@gmail.com>
2016-06-27 16:30:46 +0200

    add edit import option to frontend

--- web/test/tests/table-test.js
+++ web/test/tests/table-test.js
@@ -3,14 +3,15 @@
 module.exports = function(fw) {
   var lib = require('../test-lib.js');
   var path = require('path');
-
+  var columns = 'name,age';
+  
   fw.transitionTest(
     'empty splash',
     'CSV file imported as table',
     function() {
       var importPath = path.resolve(__dirname, 'import_csv_test.csv');
       lib.splash.startTableImport();
-      lib.splash.importLocalCSVFile('csv imported', importPath);
+      lib.splash.importLocalCSVFile('csv imported', importPath, columns);
     },
     function() {
       lib.splash.expectNumProjects(0);
@@ -29,6 +30,17 @@ module.exports = function(fw) {
     function() {
       expect(lib.left.vertexCount()).toEqual(3);
       expect(lib.left.attributeCount()).toEqual(3);
+    }
+  );
+  fw.transitionTest(
+    'CSV file imported as table',
+    'Editing configuration is possible',
+    function() {
+      lib.splash.editImport();
+    },
+    function() {
+      expect($('import-wizard #table-name input').toEqual('csv imported'));
+      expect($('import-wizard #columns-to-import input').toEqual(columns));
     }
   );
 };
