c6b088717936823e213f4cdb9fcc083dc48cb1cf
vincent <vinchang@mit.edu>
2016-06-27 16:31:52 +0200

    Adapted Github suggesions to modify minor details.

--- test/com/lynxanalytics/biggraph/graph_operations/ReduceDimensionsTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/ReduceDimensionsTest.scala
@@ -37,15 +37,19 @@ class ReduceDimensionsTest extends FunSuite with TestGraphOp {
     import sqlContext.implicits._
 
     val numAttr = 40
-    val attrs = (1 to numAttr).map(i => (0 to 1000).map { case x => x -> (x * i).toDouble }.toMap)
+    val attrs = (1 to numAttr).map(i => (0 to 1000).map {
+      case x => x -> (x * i).toDouble
+    }.toMap)
     val g = SmallTestGraph(attrs(0).mapValues(_ => Seq()), 10).result
     val features = attrs.map(attr => AddVertexAttribute.run[Double](g.vs, attr))
     val op = ReduceDimensions(numAttr)
     val result = op(op.features, features).result
     val attr1 = result.attr1.rdd
     val attr2 = result.attr2.rdd
-    assert((attr1.lookup(500)(0) - 0.0).abs <= 1E-6, "the principal component shall center at the origin")
-    assert(attr1.partitioner.get.numPartitions == 10, "numbers of partitions shall remain the same")
+    assert((attr1.lookup(500)(0) - 0.0).abs <= 1E-6,
+      "the principal component shall center at the origin")
+    assert(attr1.partitioner.get.numPartitions == 10,
+      "numbers of partitions shall remain the same")
     assert(attr1.count == 1001)
     assert(attr2.count == 1001)
   }
