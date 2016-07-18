51f2efba274e51541edf47afa1996ce266dbd5eb
Daniel Darabos <darabos.daniel@gmail.com>
2016-07-18 18:39:13 +0200

    Test with Oracle.

---
+++ kitescripts/big_data_tests/jdbc_oracle.groovy
@@ -0,0 +1,46 @@
+// Writes data through JDBC and then reads it back.
+
+/// REQUIRE_SCRIPT load_edges_from_test_set.groovy
+
+project = lynx.newProject()
+project.importVertices(
+  'id-attr': 'id',
+  table: lynx.openTable('test_edges'))
+before = project.scalars['vertex_count'].toDouble()
+
+start_time = System.currentTimeMillis()
+
+db = System.getenv('ORACLE')
+url = "jdbc:oracle:thin:root/rootroot@$db:1521:orcl"
+println "writing to $url"
+p = new java.util.Properties()
+p.setProperty("driver", "com.mysql.jdbc.Driver")
+p.setProperty("batchsize", "10000")
+project.vertexDF.write().mode('overwrite').jdbc(url, 'test_edges', p)
+
+write_done = System.currentTimeMillis()
+println "JDBC write: ${ (write_done - start_time) / 1000 } seconds"
+
+partitions = project.vertexDF.rdd().partitions().length
+df = lynx.sqlContext.read().jdbc(
+  url,
+  'test_edges',
+  'id',
+  java.lang.Long.MIN_VALUE,
+  java.lang.Long.MAX_VALUE,
+  partitions,
+  p)
+
+if (params.export_from_jdbc) {
+  project.importVertices(
+    'id-attr': 'internal-id',
+    table: lynx.saveAsTable(df, 't'))
+  after = project.scalars['vertex_count'].toDouble()
+} else {
+  // Just iterate over the JDBC results to measure JDBC performance on its own.
+  after = df.javaRDD().count()
+}
+
+read_done = System.currentTimeMillis()
+println "JDBC read: ${ (read_done - write_done) / 1000 } seconds"
+println "vertices before: $before after: $after"
