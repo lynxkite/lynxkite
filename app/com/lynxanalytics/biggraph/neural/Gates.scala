8964d799066a27a085df80f333c85edd0529aa86
Daniel Darabos <darabos.daniel@gmail.com>
2016-07-18 17:49:04 +0200

    Move stuff to a new com.lynxanalytics.biggraph.neural package.

--- app/com/lynxanalytics/biggraph/neural/Gates.scala
+++ app/com/lynxanalytics/biggraph/neural/Gates.scala
@@ -1,37 +1,32 @@
 // Collection of classes for building a neural network.
-package com.lynxanalytics.biggraph.graph_operations
+package com.lynxanalytics.biggraph.neural
 
-import breeze.linalg._
-import breeze.numerics._
 import breeze.stats.distributions.RandBasis
 import com.lynxanalytics.biggraph.graph_api._
 
 trait NetworkOutputs {
-  def apply(output: String): NeuralGates.GraphData
+  def apply(output: String): GraphData
 }
 
-object NeuralGates {
-  type DoubleVector = DenseVector[Double]
-  type DoubleMatrix = DenseMatrix[Double]
-  type Graph[T] = Map[ID, T]
-  type GraphData = Graph[DoubleVector]
-  type GraphVectors = Graph[Iterable[DoubleVector]]
+object Gates {
+  import breeze.linalg._
+  import breeze.numerics._
 
-  trait NetworkElement[Output] extends Product {
+  trait Gate[Output] extends Product {
     // Finds all instances of a type in the network leading up to this node.
     protected def find[T: reflect.ClassTag]: Seq[T] = {
       this.productIterator.flatMap {
         case x if (reflect.classTag[T].runtimeClass.isInstance(x)) => Some(x.asInstanceOf[T])
-        case x: NetworkElement[_] => x.find[T]
+        case x: Gate[_] => x.find[T]
         case _ => None
       }.toSeq
     }
     // Plain toString on case classes is enough to uniquely identify vectors.
-    private[NeuralGates] def id = this.toString
-    private[NeuralGates] def forward(ctx: ForwardContext): Output
+    private[neural] def id = this.toString
+    private[neural] def forward(ctx: ForwardContext): Output
   }
 
-  trait Vector extends NetworkElement[GraphData] {
+  trait Vector extends Gate[GraphData] {
     def *(m: M) = MultiplyWeights(this, m)
     def *(v: Vector) = MultiplyElements(this, v)
     def *(s: Double) = MultiplyScalar(this, s)
@@ -41,7 +36,7 @@ object NeuralGates {
     def +(v: V) = AddWeights(this, v)
   }
 
-  trait Vectors extends NetworkElement[GraphVectors] {
+  trait Vectors extends Gate[GraphVectors] {
     def *(m: M) = MultiplyVectors(this, m)
   }
 
@@ -97,57 +92,58 @@ object NeuralGates {
     def random(size: Int)(implicit r: RandBasis) =
       randn((size, size)) * initialRandomAmplitude
   }
+}
+import Gates._
 
-  private case class ForwardContext(
-      network: Network,
-      vertices: Seq[ID],
-      edges: CompactUndirectedGraph,
-      neighborState: GraphData,
-      inputs: Map[String, GraphData]) {
-    val vectorCache = collection.mutable.Map[String, GraphData]()
-    val vectorsCache = collection.mutable.Map[String, GraphVectors]()
-    def apply(v1: Vector, v2: Vector): Map[ID, (DoubleVector, DoubleVector)] = {
-      val g1 = this(v1)
-      val g2 = this(v2)
-      vertices.map { id => id -> (g1(id), g2(id)) }.toMap
-    }
-    def apply(v: Vector): GraphData = {
-      vectorCache.getOrElseUpdate(v.id, v match {
-        case Input(name) => inputs(name)
-        case v => v.forward(this)
-      })
-    }
-    def apply(vs: Vectors): GraphVectors = {
-      vectorsCache.getOrElseUpdate(vs.id, vs.forward(this))
-    }
-    def apply[T](t: Trained[T]) = network(t)
-    def neighbors: GraphVectors = {
-      vertices.map { id =>
-        id -> edges.getNeighbors(id).map(neighborState(_))
-      }.toMap
-    }
+private case class ForwardContext(
+    network: Network,
+    vertices: Seq[ID],
+    edges: CompactUndirectedGraph,
+    neighborState: GraphData,
+    inputs: Map[String, GraphData]) {
+  val vectorCache = collection.mutable.Map[String, GraphData]()
+  val vectorsCache = collection.mutable.Map[String, GraphVectors]()
+  def apply(v1: Vector, v2: Vector): Map[ID, (DoubleVector, DoubleVector)] = {
+    val g1 = this(v1)
+    val g2 = this(v2)
+    vertices.map { id => id -> (g1(id), g2(id)) }.toMap
   }
-
-  object Network {
-    // The public constructor does not set weights, so they get randomly initialized.
-    def apply(size: Int, outputs: (String, Vector)*) = new Network(size, Map(), outputs.toMap)
+  def apply(v: Vector): GraphData = {
+    vectorCache.getOrElseUpdate(v.id, v match {
+      case Input(name) => inputs(name)
+      case v => v.forward(this)
+    })
   }
-  case class Network private (
-      size: Int, weights: Map[String, Trained[_]], outputs: Map[String, Vector]) {
-    implicit val randBasis = RandBasis.mt0
-    def apply[T](t: Trained[T]): T = weights.getOrElse(t.name, t.random(size)).asInstanceOf[T]
-    def forward(
-      vertices: Seq[ID],
-      edges: CompactUndirectedGraph,
-      neighbors: GraphData,
-      inputs: (String, GraphData)*): NetworkOutputs = ???
-
-    def backward(
-      vertices: Seq[ID],
-      edges: CompactUndirectedGraph,
-      outputs: NetworkOutputs,
-      gradients: (String, GraphData)*): GraphData = ???
-
-    def update(gradients: Iterable[GraphData]): Network = ???
+  def apply(vs: Vectors): GraphVectors = {
+    vectorsCache.getOrElseUpdate(vs.id, vs.forward(this))
   }
+  def apply[T](t: Trained[T]) = network(t)
+  def neighbors: GraphVectors = {
+    vertices.map { id =>
+      id -> edges.getNeighbors(id).map(neighborState(_))
+    }.toMap
+  }
+}
+
+object Network {
+  // The public constructor does not set weights, so they get randomly initialized.
+  def apply(size: Int, outputs: (String, Vector)*) = new Network(size, Map(), outputs.toMap)
+}
+case class Network private (
+    size: Int, weights: Map[String, Trained[_]], outputs: Map[String, Vector]) {
+  implicit val randBasis = RandBasis.mt0
+  def apply[T](t: Trained[T]): T = weights.getOrElse(t.name, t.random(size)).asInstanceOf[T]
+  def forward(
+    vertices: Seq[ID],
+    edges: CompactUndirectedGraph,
+    neighbors: GraphData,
+    inputs: (String, GraphData)*): NetworkOutputs = ???
+
+  def backward(
+    vertices: Seq[ID],
+    edges: CompactUndirectedGraph,
+    outputs: NetworkOutputs,
+    gradients: (String, GraphData)*): GraphData = ???
+
+  def update(gradients: Iterable[GraphData]): Network = ???
 }
