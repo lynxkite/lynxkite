3919f61d836f257064cc0c6437097b24299e49e4
Daniel Darabos <darabos.daniel@gmail.com>
2016-07-20 16:54:20 +0200

    Partitioning for VARCHAR columns.

--- app/com/lynxanalytics/biggraph/graph_util/JDBCUtil.scala
+++ app/com/lynxanalytics/biggraph/graph_util/JDBCUtil.scala
@@ -29,25 +29,84 @@ object JDBCUtil {
       val stats = try TableStats(url, table, keyColumn)
       val numPartitions = RuntimeContext.partitionerForNRows(stats.count).numPartitions
       stats.keyType match {
-        case "String" => ???
-        case "Number" =>
+        case KeyTypes.String =>
+          context.read.jdbc(
+            url,
+            table,
+            stringPartitionClauses(
+              keyColumn,
+              stats.minStringKey.get,
+              stats.maxStringKey.get,
+              numPartitions).toArray,
+            props)
+        case KeyTypes.Number =>
           context.read.jdbc(
             url,
             table,
             keyColumn,
-            stats.minIntKey.get,
-            stats.maxIntKey.get,
+            stats.minLongKey.get,
+            stats.maxLongKey.get,
             numPartitions,
             props)
       }
     }
   }
+
+  val stringPrefixLength =
+    LoggedEnvironment.envOrElse("KITE_JDBC_STRING_PREFIX_LENGTH", "10").toInt
+
+  // Creates a list of numPartitions conditions for WHERE clauses.
+  // It partitions the range between minKey and maxKey by the alphabet.
+  def stringPartitionClauses(
+    keyColumn: String, minKey: String, maxKey: String, numPartitions: Int): Iterable[String] = {
+    assert(minKey < maxKey, s"The database thinks $minKey < $maxKey.")
+    // We assume strings are mostly made up of the following characters.
+    val characters = (('0' to '9') ++ ('A' to 'Z') ++ ('a' to 'z')).sorted.mkString
+    val base: Double = characters.length
+    def indexOf(c: Char) = {
+      val i = characters.indexWhere(c <= _)
+      if (i < 0) characters.length - 1 else i
+    }
+    def toNumber(s: String) = {
+      val values = s.map { c => 1.0 min (indexOf(c) / base) max 0.0 }
+      values.zipWithIndex.map { case (v, i) => v * Math.pow(base, -i) }.sum
+    }
+    def toString(d: Double) = {
+      val numbers = (0 until stringPrefixLength).map {
+        i => (d % Math.pow(base, -i)) * Math.pow(base, i)
+      }
+      numbers.map { n => characters.charAt((n * base).toInt) }.mkString
+    }
+    val minKeyNumber = toNumber(minKey)
+    val maxKeyNumber = toNumber(maxKey)
+    assert(minKeyNumber < maxKeyNumber, s"Could not split the range between $minKey and $maxKey.")
+    val keyRange = maxKeyNumber - minKeyNumber
+    val bounds = (1 until numPartitions).map {
+      i => '"' + toString(minKeyNumber + i * keyRange / numPartitions) + '"'
+    }
+    val k = keyColumn
+    if (bounds.isEmpty) {
+      assert(numPartitions == 1, s"Unexpected partition count: $numPartitions")
+      Array(null)
+    } else {
+      // Only upper bound for the first partition.
+      s"$k < ${bounds.head}" +:
+        bounds.zip(bounds.tail).map { case (a, b) => s"$a <= $k AND $k < $b" } :+
+        // Only lower bound for the last partition.
+        s"${bounds.last} <= $k"
+    }
+  }
+}
+
+object KeyTypes extends Enumeration {
+  val String, Number = Value
 }
 
 case class TableStats(
-  count: Long, keyType: String,
-  minIntKey: Option[Long], maxIntKey: Option[Long],
-  minStringKey: Option[String], maxStringKey: Option[String])
+  count: Long,
+  keyType: KeyTypes.Value,
+  minLongKey: Option[Long] = None, maxLongKey: Option[Long] = None,
+  minStringKey: Option[String] = None, maxStringKey: Option[String] = None)
 object TableStats {
   def apply(url: String, table: String, keyColumn: String): TableStats = {
     val quotedTable = JDBCQuoting.quoteIdentifier(table)
@@ -62,9 +121,19 @@ object TableStats {
         val rs = statement.executeQuery()
         try {
           val md = rs.getMetaData
-          println(md.getColumnType(1))
-          println(md.getColumnTypeName(1))
-          new TableStats(rs.getLong("count"), "???", None, None, None, None)
+          val count = rs.getLong("count")
+          val keyType = md.getColumnType(2)
+          keyType match {
+            case sql.Types.VARCHAR =>
+              new TableStats(
+                count, KeyTypes.String, minStringKey = Some(rs.getString("min")),
+                maxStringKey = Some(rs.getString("max")))
+            case _ =>
+              // Everything else we will try to treat as numbers and see what happens.
+              new TableStats(
+                count, KeyTypes.Number, minLongKey = Some(rs.getLong("min")),
+                maxLongKey = Some(rs.getLong("max")))
+          }
         } finally rs.close()
       } finally statement.close()
     } finally connection.close()
