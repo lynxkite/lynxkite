64151df0d66d657954e7f81c1230b9290ca268f5
Daniel Darabos <darabos.daniel@gmail.com>
2016-07-22 18:16:15 +0200

    Scalariform change.

--- app/com/lynxanalytics/biggraph/graph_operations/RegressionModelTrainer.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/RegressionModelTrainer.scala
@@ -89,18 +89,19 @@ case class RegressionModelTrainer(
     val summary = model.summary
     val r2 = summary.r2
     val MAPE = predictions.select("prediction", "label").map {
-      row => {
-        val prediction = row.getDouble(0)
-        val label = row.getDouble(1)
-        // Return an error of 100% if a zero division error occurs.
-        if (prediction == label) {
-          0.0
-        } else if (prediction == 0.0) {
-          1.0
-        } else {
-          math.abs(prediction / label - 1.0)
+      row =>
+        {
+          val prediction = row.getDouble(0)
+          val label = row.getDouble(1)
+          // Return an error of 100% if a zero division error occurs.
+          if (prediction == label) {
+            0.0
+          } else if (prediction == 0.0) {
+            1.0
+          } else {
+            math.abs(prediction / label - 1.0)
+          }
         }
-      }
     }.mean * 100.0
     // Only compute the t-values for methods with unbiased solvers (when the elastic 
     // net parameter equals to 0).
