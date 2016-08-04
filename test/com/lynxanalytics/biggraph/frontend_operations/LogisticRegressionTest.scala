4a586def653d83224d675724de8d3c3c2fe84bb0
vincent <vinchang@mit.edu>
2016-08-04 16:06:03 +0200

    Added a logistic regression frontend test. Added migration for the regression model trainer.

--- test/com/lynxanalytics/biggraph/frontend_operations/LogisticRegressionTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/LogisticRegressionTest.scala
@@ -10,6 +10,16 @@ class LogisticRegressionTest extends OperationsTestBase {
     run("Train a logistic regression model",
       Map("name" -> "model", "label" -> "label", "features" -> "age", "max-iter" -> "20"))
     run("Classify vertices with a model",
-      Map("name" -> "classification", "model" -> "model"))
+      Map("name" -> "classification", "model" -> """{
+        "modelName" : "model", 
+        "isClassification" : true, 
+        "generatesProbability" : true, 
+        "features" : ["age"]}"""))
+    val classification = project.vertexAttributes("classification").runtimeSafeCast[Double]
+    val probability = project.vertexAttributes("classification_probability").runtimeSafeCast[Double]
+    val probabilityMap = probability.rdd.collect.toMap
+    // Example graph age: 0 -> 20.3, 1 -> 18.2, 2 -> 50.3, 3 -> 2.
+    assert(classification.rdd.collect.toMap == Map(0 -> 0.0, 1 -> 0.0, 2 -> 1.0, 3 -> 0.0))
+    assert(probabilityMap(3) > probabilityMap(1) && probabilityMap(1) > probabilityMap(0))
   }
 }
