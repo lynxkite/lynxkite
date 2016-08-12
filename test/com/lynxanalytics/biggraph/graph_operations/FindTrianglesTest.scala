215e0d834089fe33a42936c042f3c3a9c282eef1
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-08-12 17:27:59 +0200

    asserts fixed, temporarily added some tests for the alternative FindTriangles

--- test/com/lynxanalytics/biggraph/graph_operations/FindTrianglesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/FindTrianglesTest.scala
@@ -1,9 +1,11 @@
 package com.lynxanalytics.biggraph.graph_operations
 
 import org.scalatest.FunSuite
-
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
+
+import scala.collection.mutable
+import scala.util.Random
 
 class FindTrianglesTest extends FunSuite with TestGraphOp {
   test("no triangles 1") {
@@ -16,7 +18,7 @@ class FindTrianglesTest extends FunSuite with TestGraphOp {
       5 -> Seq(6),
       6 -> Seq(3)
     )).result
-    val op = FindTriangles(needsBothDirections = false)
+    val op = FindTrianglesNew(needsBothDirections = false)
     val ftOut = op(op.vs, g.vs)(op.es, g.es).result
     assert(ftOut.segments.rdd.count == 0)
   }
@@ -27,7 +29,7 @@ class FindTrianglesTest extends FunSuite with TestGraphOp {
       1 -> Seq(0, 2, 2),
       2 -> Seq(0, 0, 1)
     )).result
-    val op = FindTriangles(needsBothDirections = true)
+    val op = FindTrianglesNew(needsBothDirections = true)
     val ftOut = op(op.vs, g.vs)(op.es, g.es).result
     assert(ftOut.segments.rdd.count == 0)
   }
@@ -38,11 +40,11 @@ class FindTrianglesTest extends FunSuite with TestGraphOp {
       1 -> Seq(2, 2, 2, 0, 0),
       2 -> Seq(0, 0, 1, 1)
     )).result
-    val opF = FindTriangles(needsBothDirections = false)
+    val opF = FindTrianglesNew(needsBothDirections = false)
     val ftFOut = opF(opF.vs, g.vs)(opF.es, g.es).result
-    val opT = FindTriangles(needsBothDirections = true)
+    val opT = FindTrianglesNew(needsBothDirections = true)
     val ftTOut = opT(opT.vs, g.vs)(opT.es, g.es).result
-    assert(ftFOut.segments.rdd.count == 1 && ftTOut.segments.rdd.count == 1)
+    assert((ftFOut.segments.rdd.count, ftTOut.segments.rdd.count) == (1, 1))
   }
 
   test("5-size clique") {
@@ -53,11 +55,11 @@ class FindTrianglesTest extends FunSuite with TestGraphOp {
       3 -> Seq(4),
       4 -> Seq()
     )).result
-    val opF = FindTriangles(needsBothDirections = false)
+    val opF = FindTrianglesNew(needsBothDirections = false)
     val ftFOut = opF(opF.vs, g.vs)(opF.es, g.es).result
-    val opT = FindTriangles(needsBothDirections = true)
+    val opT = FindTrianglesNew(needsBothDirections = true)
     val ftTOut = opT(opT.vs, g.vs)(opT.es, g.es).result
-    assert(ftFOut.segments.rdd.count == 10 && ftTOut.segments.rdd.count == 0)
+    assert((ftFOut.segments.rdd.count, ftTOut.segments.rdd.count) == (10, 0))
   }
 
   test("5-size clique both directions") {
@@ -68,11 +70,11 @@ class FindTrianglesTest extends FunSuite with TestGraphOp {
       3 -> Seq(0, 1, 2, 4),
       4 -> Seq(0, 1, 2, 3)
     )).result
-    val opF = FindTriangles(needsBothDirections = false)
+    val opF = FindTrianglesNew(needsBothDirections = false)
     val ftFOut = opF(opF.vs, g.vs)(opF.es, g.es).result
-    val opT = FindTriangles(needsBothDirections = true)
+    val opT = FindTrianglesNew(needsBothDirections = true)
     val ftTOut = opT(opT.vs, g.vs)(opT.es, g.es).result
-    assert(ftFOut.segments.rdd.count == 10 && ftTOut.segments.rdd.count == 10)
+    assert((ftFOut.segments.rdd.count, ftTOut.segments.rdd.count) == (10, 10))
   }
 
   test("planar graph neighbouring triangles") {
@@ -84,10 +86,78 @@ class FindTrianglesTest extends FunSuite with TestGraphOp {
       4 -> Seq(3, 5),
       5 -> Seq(3, 4)
     )).result
-    val opF = FindTriangles(needsBothDirections = false)
+    val opF = FindTrianglesNew(needsBothDirections = false)
     val ftFOut = opF(opF.vs, g.vs)(opF.es, g.es).result
-    val opT = FindTriangles(needsBothDirections = true)
+    val opT = FindTrianglesNew(needsBothDirections = true)
     val ftTOut = opT(opT.vs, g.vs)(opT.es, g.es).result
-    assert(ftFOut.segments.rdd.count == 3 && ftTOut.segments.rdd.count == 3)
+    assert((ftFOut.segments.rdd.count, ftTOut.segments.rdd.count) == (3, 3))
+  }
+
+  test("performance test") {
+    println("[info] Performance test started")
+    testPerformance(1000, 0.9, 5, 100)
+    testPerformance(10000, 0.9, 5, 100)
+    testPerformance(100000, 0.9, 5, 100)
+    /*testPerformance(1000000, 0.9, 5, 100)
+    testPerformance(10000, 0.9, 15, 1000)
+    testPerformance(100000, 0.9, 15, 1000)
+    testPerformance(100000, 0.7, 10, 200)*/
+    println("[info] PerformanceNew test started")
+    testPerformanceNew(1000, 0.9, 5, 100)
+    testPerformanceNew(10000, 0.9, 5, 100)
+    testPerformanceNew(100000, 0.9, 5, 100)
+    /*testPerformanceNew(1000000, 0.9, 5, 100)
+    testPerformanceNew(10000, 0.9, 15, 1000)
+    testPerformanceNew(100000, 0.9, 15, 1000)
+    testPerformanceNew(100000, 0.7, 10, 200)*/
+    //assert(true)
+  }
+
+  def testPerformance(n: Int,
+                      ratio: Double,
+                      lowDegree: Int,
+                      highDegree: Int): Unit = {
+    val random = Random
+    val adjacencyArray = mutable.Map[Int, Seq[Int]]()
+    for (i <- 1 to n) {
+      val maxdegree = if (i > n * (1 - ratio)) lowDegree else highDegree
+      val degree = random.nextInt(maxdegree)
+      val neighbours = mutable.ArrayBuffer[Int]()
+      for (j <- 1 to degree) {
+        neighbours += random.nextInt(n)
+      }
+      adjacencyArray += (i -> neighbours)
+    }
+    val g = SmallTestGraph(adjacencyArray.toMap).result
+    val t0 = System.nanoTime()
+    val op = FindTriangles(needsBothDirections = false)
+    val ftOut = op(op.vs, g.vs)(op.es, g.es).result
+    print("[info] - " + ftOut.segments.rdd.count + " triangles found in ")
+    val t1 = System.nanoTime()
+    println((t1 - t0) / 1000000000.0 + " seconds")
+  }
+
+  def testPerformanceNew(n: Int,
+                         ratio: Double,
+                         lowDegree: Int,
+                         highDegree: Int): Unit = {
+    val random = Random
+    val adjacencyArray = mutable.Map[Int, Seq[Int]]()
+    for (i <- 1 to n) {
+      val maxdegree = if (i > n * (1 - ratio)) lowDegree else highDegree
+      val degree = random.nextInt(maxdegree)
+      val neighbours = mutable.ArrayBuffer[Int]()
+      for (j <- 1 to degree) {
+        neighbours += random.nextInt(n)
+      }
+      adjacencyArray += (i -> neighbours)
+    }
+    val g = SmallTestGraph(adjacencyArray.toMap).result
+    val t0 = System.nanoTime()
+    val op = FindTrianglesNew(needsBothDirections = false)
+    val ftOut = op(op.vs, g.vs)(op.es, g.es).result
+    print("[info] - " + ftOut.segments.rdd.count + " triangles found in ")
+    val t1 = System.nanoTime()
+    println((t1 - t0) / 1000000000.0 + " seconds")
   }
 }
