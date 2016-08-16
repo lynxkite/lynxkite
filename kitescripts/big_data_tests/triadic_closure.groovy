1f752784e88ed0f79fd896d86a89a0086f206657
erbenpeter <erben.peter@gmail.com>
2016-08-16 18:14:35 +0200

    big data result degree < 100

--- kitescripts/big_data_tests/triadic_closure.groovy
+++ kitescripts/big_data_tests/triadic_closure.groovy
@@ -1,9 +1,15 @@
 // Tests the "Triadic Closure" FE operation
 
-/// REQUIRE_SCRIPT random_attributes.groovy
-// Random attributes are used for testing the pulling of the edge attributes
+/// REQUIRE_SCRIPT filter_high_degree_vertices.groovy
 
-project = lynx.loadProject('random_attributes')
+// filtering high degree vertices before closure to prevent unmanageable size output
+project = lynx.loadProject('filter_high_degree_vertices_result')
+
+// create random edge attribute for testing the pulling of edge attributes
+project.addRandomEdgeAttribute(
+        name: 'attr',
+        dist: 'Standard Uniform',
+        seed: '123421')
 
 project.triadicClosure()
 
