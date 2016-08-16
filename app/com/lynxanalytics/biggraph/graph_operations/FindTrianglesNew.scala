4e40d78ce89afb4ed856411bfd60661467de8498
Mate Szabo <szmate1618@users.noreply.github.com>
2016-08-16 12:36:48 +0200

    partitioner changed

--- app/com/lynxanalytics/biggraph/graph_operations/FindTrianglesNew.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/FindTrianglesNew.scala
@@ -30,8 +30,7 @@ case class FindTrianglesNew(needsBothDirections: Boolean = false) extends TypedM
     implicit val id = inputDatas
 
     val inputPartitioner = inputs.vs.rdd.partitioner.get
-    val numTasks = (rc.sparkContext.defaultParallelism * 5) max inputPartitioner.numPartitions
-    val outputPartitioner = new HashPartitioner(numTasks)
+    val outputPartitioner = rc.partitionerForNRows(inputs.es.rdd.count())
 
     // remove loop edges
     val filteredEdges = inputs.es.rdd.filter { case (_, Edge(src, dst)) => src != dst }
