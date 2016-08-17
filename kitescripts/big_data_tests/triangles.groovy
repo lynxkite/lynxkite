65e607fa1452836341b173917ebe4df42ba02903
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-08-17 13:07:05 +0200

    big data test for operatin FindTriangles

---
+++ kitescripts/big_data_tests/triangles.groovy
@@ -0,0 +1,14 @@
+// Tests the "Triangles" operation.
+
+/// REQUIRE_SCRIPT filter_high_degree_vertices.groovy
+
+project = lynx.loadProject('filter_high_degree_vertices_result')
+
+project.triangles(
+        'bothdir': 'false',
+        'name': 'triangles',
+)
+
+project.saveAs('triangles_result')
+
+project.computeUncomputed()
