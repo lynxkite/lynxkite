aa41a55b8c5a7b64edca51c4d17ae6f027ea19fe
Mate Szabo <szmate1618@users.noreply.github.com>
2016-08-17 18:23:11 +0200

    doubling the edges turned out to be unnecessary

--- app/com/lynxanalytics/biggraph/graph_operations/FindTriangles.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/FindTriangles.scala
@@ -28,7 +28,7 @@ case class FindTriangles(needsBothDirections: Boolean = false) extends TypedMeta
               output: OutputBuilder,
               rc: RuntimeContext): Unit = {
     implicit val id = inputDatas
-    
+
     val outputPartitioner = inputs.es.rdd.partitioner.get
 
     // remove loop- and parallel edges, keep non-parallel multiple edges
@@ -46,10 +46,9 @@ case class FindTriangles(needsBothDirections: Boolean = false) extends TypedMeta
         filteredEdges.filter(_._2 == 3).map(_._1)
       else
         filteredEdges.map(_._1)
-    val doubledEdges = simpleEdges.flatMap { case (src, dst) => List((src, dst), (dst, src)) }
 
     // construct the adjacencyArray, and join it on the edge set
-    val adjacencyArray = doubledEdges
+    val adjacencyArray = simpleEdges
       .sort(outputPartitioner)
       .groupBySortedKey(outputPartitioner)
     val edgesWithNeighbours = simpleEdges
@@ -60,7 +59,7 @@ case class FindTriangles(needsBothDirections: Boolean = false) extends TypedMeta
       .sortedJoin(adjacencyArray)
       .map {
         case (dst, ((src, nSrc), nDst)) =>
-          ((src, dst), (nSrc.filter(v => v < src), nDst.filter(v => v < dst)))
+          ((src, dst), (nSrc, nDst))
       }
 
     // collect triangles
@@ -70,8 +69,8 @@ case class FindTriangles(needsBothDirections: Boolean = false) extends TypedMeta
         checkNeighbours(
           src,
           dst,
-          nSrc.filter(v => v < dst),
-          nDst.filter(v => v < src),
+          nSrc,
+          nDst,
           collector)
         collector
       }
