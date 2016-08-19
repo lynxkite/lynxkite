095b18e2b519343bbc4e106c0a6267acb9ac31f2
erbenpeter <erben.peter@gmail.com>
2016-08-19 11:02:51 +0200

    tests

--- test/com/lynxanalytics/biggraph/frontend_operations/SnowballSampleTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/SnowballSampleTest.scala
@@ -6,7 +6,7 @@ class SnowballSampleTest extends OperationsTestBase {
   test("Snowball sample with percentage=1.0") {
     run("Example Graph")
     run("Snowball sample",
-      Map("ratio" -> "1.0", "radius" -> "0", "seed" -> "123454321")
+      Map("ratio" -> "1.0", "radius" -> "0", "seed" -> "123454321", "attrName"->"distance_from_start_point")
     )
 
     val size = project.vertexSet.rdd.count.toInt
@@ -16,7 +16,7 @@ class SnowballSampleTest extends OperationsTestBase {
   test("Snowball sample with percentage=0.0") {
     run("Example Graph")
     run("Snowball sample",
-      Map("ratio" -> "0.0", "radius" -> "0", "seed" -> "123454321")
+      Map("ratio" -> "0.0", "radius" -> "0", "seed" -> "123454321", "attrName"->"distance_from_start_point")
     )
 
     val size = project.vertexSet.rdd.count.toInt
@@ -24,9 +24,9 @@ class SnowballSampleTest extends OperationsTestBase {
   }
 
   test("Snowball sample with small graph (1000 vertices, ratio=0.1)") {
-    run("New vertex set", Map("size"->"1000"))
+    run("New vertex set", Map("size" -> "1000"))
     run("Snowball sample",
-      Map("ratio" -> "0.0", "radius" -> "0", "seed" -> "123454321")
+      Map("ratio" -> "0.0", "radius" -> "0", "seed" -> "123454321", "attrName"->"distance_from_start_point")
     )
 
     val size = project.vertexSet.rdd.count.toInt
