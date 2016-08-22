d78e28f05c0d8163124e9930d94b44606ae808b7
David Herskovics <david.herskovics@lynxanalytics.com>
2016-08-22 11:14:42 +0200

    added test for complete graph

--- test/com/lynxanalytics/biggraph/graph_operations/ColoringTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/ColoringTest.scala
@@ -13,4 +13,12 @@ class ColoringTest extends FunSuite with TestGraphOp {
     val color = res.coloring.rdd.collect.toSeq.sorted
     assert(color == Seq(0 -> 2.0, 1 -> 1.0, 2 -> 3.0, 3 -> 1.0))
   }
+
+  test("complete graph") {
+    val g = SmallTestGraph(Map(0 -> Seq(1, 2, 3), 1 -> Seq(2, 3), 2 -> Seq(3), 3 -> Seq())).result
+    val op = Coloring()
+    val res = op(op.es, g.es)().result
+    val color = res.coloring.rdd.collect.toSeq.sorted
+    assert(color == Seq(0 -> 4.0, 1 -> 3.0, 2 -> 2.0, 3 -> 1.0))
+  }
 }
