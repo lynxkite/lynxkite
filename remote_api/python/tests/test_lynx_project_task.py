42b8c2f8fdada7874acce3cd717a8fd85ae128ab
Mate Szabo <szmate1618@users.noreply.github.com>
2016-09-01 14:11:16 +0200

    test project task

---
+++ remote_api/python/tests/test_lynx_project_task.py
@@ -0,0 +1,29 @@
+import unittest
+import lynx.luigi
+from unittest import mock
+import types
+
+
+class TestTask(lynx.luigi.ProjectTask):
+
+  def compute_project(self):
+    return self.lk.new_project().examplegraph()
+
+  def output_name(self):
+    return 'ILoveTestingSoMuch5'
+
+
+class TestProject(unittest.TestCase):
+
+  def test_run(self):
+    tt = TestTask()
+    try:
+      tt.run()
+    except lynx.LynxException:
+      self.assertTrue(True)
+    else:
+      self.assertTrue(tt.output().exists())
+
+
+if __name__ == '__main__':
+  unittest.main()
