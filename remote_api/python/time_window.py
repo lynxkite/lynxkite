6ff24d1232d155beba51ea476bed08919b379945
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-09-08 18:35:34 +0200

    attach decorator dynamically

--- remote_api/python/time_window.py
+++ remote_api/python/time_window.py
@@ -9,47 +9,48 @@ import subprocess
 import types
 
 
-class TimeWindowedTask:
+def time_windowed_method(func):
+  def wrapper(self):
+    now = datetime.datetime.now().time()
+    if (self.earliest_start_time() < now and now < self.latest_start_time()):
+      func(self)
+    else:
+      print("Can't run.")
+  return wrapper
+
+
+class TimeWindowedTask(luigi.Task):
 
   def earliest_start_time(self):
     """Override this to return the earliest time of the day windowed methods can be started,
        as a datetime.time object"""
     #raise NotImplementedError
-    return dateutil.parser.parse('18:04').time()
+    return dateutil.parser.parse('18:35').time()
 
   def latest_start_time(self):
     """Override this to return the latest time of the day windowed methods can be started,
        as a datetime.time object"""
     #raise NotImplementedError
     return dateutil.parser.parse('23:00').time()
+  
+  def run(self):
+    pass
 
-  def time_windowed_method(self, func):
-    def wrapper(self):
-      now = datetime.datetime.now().time()
-      if (self.earliest_start_time() < now and now < self.latest_start_time()):
-        func()
-      else:
-        print("Can't run.")
-    return wrapper(self)
+  def __new__(self):
+    obj = luigi.Task.__new__(self)
+    self.run = time_windowed_method(self.run)
+    return obj
 
-  #def __new__(self):
-   # run = time_windowed_method(getattr(self, 'run'))
-
-
-
-class TestClass:
+class ThisIsANormalTask:
 
   def run(self):
     print("I'm running.")
 
 
-class TestClass2(TestClass, TimeWindowedTask):
-  
-  def whatever(self):
-    return True
+class ActualTask(ThisIsANormalTask, TimeWindowedTask):
+  pass
 
 
 print('testing')
-mtc = TestClass2()
-mtc.run = mtc.time_windowed_method(mtc.run)
-mtc.run()
+at = ActualTask()
+at.run()
