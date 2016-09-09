78f12183d6c9f4a485be071b1aa03b042c93639e
Mate Szabo <szmate1618@users.noreply.github.com>
2016-09-09 17:15:49 +0200

    comments added

--- remote_api/python/time_window.py
+++ remote_api/python/time_window.py
@@ -4,6 +4,9 @@ import luigi
 
 
 class TimeWindowTarget(luigi.target.Target):
+  """
+  A target that represents a time interval.
+  """
 
   def __init__(self, earliest, latest):
     self.earliest_start_time = dateutil.parser.parse(earliest).time()
@@ -15,6 +18,17 @@ class TimeWindowTarget(luigi.target.Target):
 
 
 class InTimeWindow(luigi.task.ExternalTask):
+  """
+  An external task, holding a TimeWindowTarget.
+  Should you need to restrict a task to only run in a certain time interval,
+  add an instance of this class to its 'requires' list.
+
+  Usage example::
+
+    class TestTask(luigi.task.Task):
+      def requires(self):
+        return [InTimeWindow('18:35', '23:00')]
+  """
 
   earliest_start_time = luigi.Parameter()
   latest_start_time = luigi.Parameter()
@@ -22,14 +36,3 @@ class InTimeWindow(luigi.task.ExternalTask):
   def output(self):
     return TimeWindowTarget(self.earliest_start_time, self.latest_start_time)
 
-
-class TestTask(luigi.task.Task):
-
-  def requires(self):
-    return [InTimeWindow('18:35', '23:00')]
-
-  def output(self):
-    return luigi.LocalTarget('testoutput.txt')
-
-  def run(self):
-    print('just testing, actually creating the output is undesired')
