b08d30ca8f00db762ed49c66196300dd8ced0e61
erbenpeter <erben.peter@gmail.com>
2016-09-09 14:51:44 +0200

    SCPtoHDFSTask test added

--- remote_api/python/tests/test_table_overwrite.py
+++ remote_api/python/tests/test_table_overwrite.py
@@ -1,3 +1,6 @@
+# This module tests the exportViewToFile method with the SaveMode.OverWrite setting.
+# The hadoop paths and scp addresses are correct only in the native+local+dockered version.
+
 import unittest
 import lynx.luigi
 import subprocess
@@ -5,17 +8,52 @@ import subprocess
 
 lk = lynx.LynxKite()
 
+class SCPtoHDFSTask(lynx.luigi.SCPtoHDFSTask):
+    def source(self):
+        return 'localhost', '/root/lynx/overwrite_test_dir/file.txt'
+    def destination(self):
+        return 'hdfs://localhost:9000/user/root/lynxkite/overwrite_test_dir/file.txt'
+
+
 class TestTable(unittest.TestCase):
 
-    def test_run1(self):
-        lk._request('/ajax/discardAllReallyIMeanIt')
-        view = lk.sql('SELECT 1')
-        subprocess.call(['hadoop', 'fs', '-rm', '-r', '/user/root/lynxkite/overwrite_test_table'])
-        subprocess.call(['hadoop', 'fs', '-mkdir', '-p', '/user/root/lynxkite/overwrite_test_table'])
-        subprocess.call(['hadoop', 'fs', '-touchz', '/user/root/lynxkite/overwrite_test_table/dummy.txt'])
-        view.export_parquet('DATA$/overwrite_test_table')
+  def test_hadoop_parquet_export(self):
+    lk._request('/ajax/discardAllReallyIMeanIt')
+    view = lk.sql('SELECT 1')
+    subprocess.call(['hadoop', 'fs', '-rm', '-r', '/user/root/lynxkite/overwrite_test_table'])
+    subprocess.call(['hadoop', 'fs', '-mkdir', '-p', '/user/root/lynxkite/overwrite_test_table'])
+    subprocess.call(['hadoop', 'fs', '-touchz',
+                     '/user/root/lynxkite/overwrite_test_table/dummy.txt'])
+    view.export_parquet('DATA$/overwrite_test_table')
 
+
+  def test_scp_to_hdfs(self):
+    lk._request('/ajax/discardAllReallyIMeanIt')
+    # Preparing scp dir and file.
+    subprocess.call(['rm', '-rf', '/root/lynx/overwrite_test_dir'])
+    subprocess.call(['mkdir', '-p', '/root/lynx/overwrite_test_dir'])
+    subprocess.check_call(['touch', '/root/lynx/overwrite_test_dir/file.txt'])
+    # Preparing hdfs dir without _SUCCESS.
+    subprocess.call(['hadoop', 'fs', '-rm', '-r', '/user/root/lynxkite/overwrite_test_dir'])
+    subprocess.call(['hadoop', 'fs', '-mkdir', '-p', '/user/root/lynxkite/overwrite_test_dir'])
+    task = SCPtoHDFSTask()
+    task.run()
+    self.assertTrue(task.output().exists())
+
+
+  def test_scp_to_hdfs(self):
+    lk._request('/ajax/discardAllReallyIMeanIt')
+    # Preparing scp dir and file.
+    subprocess.call(['rm', '-rf', '/root/lynx/overwrite_test_dir'])
+    subprocess.call(['mkdir', '-p', '/root/lynx/overwrite_test_dir'])
+    subprocess.check_call(['touch', '/root/lynx/overwrite_test_dir/file.txt'])
+    # Preparing hdfs dir without _SUCCESS.
+    subprocess.call(['hadoop', 'fs', '-rm', '-r', '/user/root/lynxkite/overwrite_test_dir'])
+    subprocess.call(['hadoop', 'fs', '-mkdir', '-p', '/user/root/lynxkite/overwrite_test_dir'])
+    task = SCPtoHDFSTask()
+    task.run()
+    self.assertTrue(task.output().exists())
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
