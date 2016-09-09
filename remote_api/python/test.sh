24844c3816898b202e2cd10944a80476e51213f6
erbenpeter <erben.peter@gmail.com>
2016-09-09 11:09:10 +0200

    SaveMode.OverWrite test, first version, sqlite fixes, test.sh fix

--- remote_api/python/test.sh
+++ remote_api/python/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Run Python API tests. You need a running LynxKite instance for this.
 
-export LYNXKITE_ADDRESS='http://localhost:2200/'
+# To setup thew environment, you have to run source /root/lynx/config/central
+# before using lynx.
+
 cd $(dirname $0)
 python3 -m unittest discover -s tests
