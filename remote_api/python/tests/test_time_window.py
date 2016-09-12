1db66b8fc5e4d009ce46ef2f0c12a9671fe36161
Mate Szabo <szmate1618@users.noreply.github.com>
2016-09-12 16:46:22 +0200

    overflowing window + test

--- remote_api/python/tests/test_time_window.py
+++ remote_api/python/tests/test_time_window.py
@@ -7,8 +7,8 @@ from unittest import mock
 
 class TestTimeWindow(unittest.TestCase):
 
-  def test_run(self):
-    twt = time_window.DailyWindowTarget('12:00', '12:02')
+  def test_window(self):
+    twt = time_window.DailyWindowTarget('12:00:01', '12:02')
 
     with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('11:59', '%H:%M').time()):
@@ -16,6 +16,10 @@ class TestTimeWindow(unittest.TestCase):
 
     with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('12:00', '%H:%M').time()):
+      self.assertFalse(twt.exists())
+
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('12:01', '%H:%M').time()):
       self.assertTrue(twt.exists())
 
     with mock.patch.object(time_window.DailyWindowTarget, 'now',
@@ -28,6 +32,28 @@ class TestTimeWindow(unittest.TestCase):
 
     with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('12:03', '%H:%M').time()):
+      self.assertFalse(twt.exists())
+
+  def test_inverse_window(self):
+    twt = time_window.DailyWindowTarget('23:00', '02:00')
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('22:59', '%H:%M').time()):
+      self.assertFalse(twt.exists())
+
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('23:00', '%H:%M').time()):
+      self.assertTrue(twt.exists())
+
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('23:01', '%H:%M').time()):
+      self.assertTrue(twt.exists())
+
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('02:00', '%H:%M').time()):
+      self.assertTrue(twt.exists())
+
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('02:01', '%H:%M').time()):
       self.assertFalse(twt.exists())
 
 
