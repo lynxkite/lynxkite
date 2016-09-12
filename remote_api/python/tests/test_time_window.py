4a9d2b4a3d360290df8ae001419e151612ede0d1
Mate Szabo <szmate1618@users.noreply.github.com>
2016-09-12 14:02:08 +0200

    test

---
+++ remote_api/python/tests/test_time_window.py
@@ -0,0 +1,35 @@
+import datetime
+import unittest
+import lynx
+from lynx.luigi import time_window
+from unittest import mock
+
+
+class TestTimeWindow(unittest.TestCase):
+
+  def test_run(self):
+    twt = time_window.TimeWindowTarget('12:00', '12:02')
+
+    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('11:59', '%H:%M').time()):
+      self.assertFalse(twt.exists())
+
+    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('12:00', '%H:%M').time()):
+      self.assertTrue(twt.exists())
+
+    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('12:01', '%H:%M').time()):
+      self.assertTrue(twt.exists())
+
+    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('12:02', '%H:%M').time()):
+      self.assertTrue(twt.exists())
+
+    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+                           return_value=datetime.datetime.strptime('12:03', '%H:%M').time()):
+      self.assertFalse(twt.exists())
+
+
+if __name__ == '__main__':
+  unittest.main()
