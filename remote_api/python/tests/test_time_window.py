f19bd67038ff9f67f6140b3bb694a5e19ea26e4c
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-09-12 14:17:12 +0200

    renamed to daily

--- remote_api/python/tests/test_time_window.py
+++ remote_api/python/tests/test_time_window.py
@@ -8,25 +8,25 @@ from unittest import mock
 class TestTimeWindow(unittest.TestCase):
 
   def test_run(self):
-    twt = time_window.TimeWindowTarget('12:00', '12:02')
+    twt = time_window.DailyWindowTarget('12:00', '12:02')
 
-    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('11:59', '%H:%M').time()):
       self.assertFalse(twt.exists())
 
-    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('12:00', '%H:%M').time()):
       self.assertTrue(twt.exists())
 
-    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('12:01', '%H:%M').time()):
       self.assertTrue(twt.exists())
 
-    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('12:02', '%H:%M').time()):
       self.assertTrue(twt.exists())
 
-    with mock.patch.object(time_window.TimeWindowTarget, 'now',
+    with mock.patch.object(time_window.DailyWindowTarget, 'now',
                            return_value=datetime.datetime.strptime('12:03', '%H:%M').time()):
       self.assertFalse(twt.exists())
 
