a55ca58807d06e56de353a6aa94d4cb3fa2b197c
erbenpeter <erben.peter@gmail.com>
2016-09-13 13:44:49 +0200

    moving test_table_overwrite to ecosystem/tests

--- remote_api/python/test.sh
+++ remote_api/python/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Run Python API tests. You need a running LynxKite instance for this.
 
-# To setup the environment, you have to run source /root/lynx/config/central
-# before using lynx.
 
 cd $(dirname $0)
 python3 -m unittest discover -s tests
