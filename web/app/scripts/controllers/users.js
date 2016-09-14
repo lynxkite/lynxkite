3efd6b7e8db2d810a571cd58ab508911412fd5bc
gsvigruha <gergely.svigruha@lynxanalytics.com>
2016-09-15 00:11:35 +0800

    Make users editable.

--- web/app/scripts/controllers/users.js
+++ web/app/scripts/controllers/users.js
@@ -4,6 +4,8 @@
 angular.module('biggraph')
   .controller('UsersCtrl', function ($scope, util) {
     $scope.list = util.nocache('/ajax/getUsers');
+    $scope.password = {};
+    $scope.userIsAdmin = {};
 
     $scope.createUser = function() {
       util
@@ -12,6 +14,19 @@ angular.module('biggraph')
           password: $scope.newPassword,
           isAdmin: $scope.newUserIsAdmin || false,
         }).$status.then(function() {
+          $scope.list = util.nocache('/ajax/getUsers');
+        });
+    };
+
+    $scope.changeUser = function(email) {
+      console.log($scope.userIsAdmin);
+      util
+        .post('/ajax/changeUser', {
+          email: email,
+          password: $scope.password[email],
+          isAdmin: $scope.userIsAdmin[email],
+        }).$status.then(function() {
+          $scope.password = {};
           $scope.list = util.nocache('/ajax/getUsers');
         });
     };
