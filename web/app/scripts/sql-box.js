42cf42742d25f57b28ad7631e101a1faa7d13593
Mate Szabo <szmate1618@users.noreply.github.com>
2016-09-15 12:06:28 +0200

    nice error message

--- web/app/scripts/sql-box.js
+++ web/app/scripts/sql-box.js
@@ -12,7 +12,7 @@ angular.module('biggraph').directive('sqlBox', function($window, side, util) {
     link: function(scope) {
       scope.inProgress = 0;
       scope.directoryDefined = (typeof scope.directory !== 'undefined');
-      scope.maxRows = 1000;
+      scope.maxRows = '1000';
       if(!!scope.side && scope.directoryDefined) {
         throw 'can not be both defined: scope.side, scope.directory';
       }
@@ -53,6 +53,8 @@ angular.module('biggraph').directive('sqlBox', function($window, side, util) {
       scope.runSQLQuery = function() {
         if (!scope.sql) {
           scope.result = { $error: 'SQL script must be specified.' };
+        } else if (isNaN(parseInt(scope.maxRows))) {
+          scope.result = { $error: 'Input string "' + scope.maxRows + '" is not a number.' };
         } else {
           scope.inProgress += 1;
           scope.result = util.nocache(
@@ -64,7 +66,7 @@ angular.module('biggraph').directive('sqlBox', function($window, side, util) {
                 project: scope.project,
                 sql: scope.sql,
               },
-              maxRows: scope.maxRows,
+              maxRows: parseInt(scope.maxRows),
             });
           scope.result.finally(function() {
             scope.inProgress -= 1;
