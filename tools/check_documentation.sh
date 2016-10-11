884c5e359f9886241014eed7153477b1980c8797
Daniel Darabos <darabos.daniel@gmail.com>
2016-10-11 19:05:24 +0200

    Minor cleanups.

--- tools/check_documentation.sh
+++ tools/check_documentation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ue
 # Make sure all operations are documented.
-export LC_COLLATE=C
+cd $(dirname $0)/..
+export LC_ALL=C
 cat 'web/.tmp/help.html' | sed -n 's/.*h4 id="\([^"]*\)".*/\1/p' | sort > 'web/.tmp/help-links'
 sbt --error 'run-main com.lynxanalytics.biggraph.HelpInventory web/.tmp/help-inventory'
 MISSING=$(diff -u 'web/.tmp/help-inventory' 'web/.tmp/help-links' | sed -n 's/^-\(\w\)/  \1/p')
