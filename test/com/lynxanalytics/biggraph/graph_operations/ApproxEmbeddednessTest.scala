943bd5d4b317a27b0a9085e40bf2666439836a17
gsvigruha <gergely.svigruha@lynxanalytics.com>
2016-10-19 02:21:21 +0800

    Add help and big data test.

--- test/com/lynxanalytics/biggraph/graph_operations/ApproxEmbeddednessTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/ApproxEmbeddednessTest.scala
@@ -20,6 +20,13 @@ class ApproxEmbeddednessTest extends FunSuite with TestGraphOp {
     assert(out.embeddedness.rdd.collect.toMap == Map(0 -> 1, 1 -> 1, 2 -> 1))
   }
 
+  test("bi-directed triangle") {
+    val g = SmallTestGraph(Map(0 -> Seq(1, 2), 1 -> Seq(0, 2), 2 -> Seq(0, 1)))().result
+    val op = ApproxEmbeddedness(bits = 12)
+    val out = op(op.es, g.es).result
+    assert(out.embeddedness.rdd.collect.toMap == Map(0 -> 1, 1 -> 1, 2 -> 1, 3 -> 1, 4 -> 1, 5 -> 1))
+  }
+
   test("line graph") {
     val g = SmallTestGraph(Map(0 -> Seq(1), 1 -> Seq(2), 2 -> Seq(3)))().result
     val op = ApproxEmbeddedness(bits = 12)
@@ -27,10 +34,10 @@ class ApproxEmbeddednessTest extends FunSuite with TestGraphOp {
     assert(out.embeddedness.rdd.collect.toMap == Map(0 -> 0, 1 -> 0, 2 -> 0))
   }
 
-  test("bi-directed triangle") {
-    val g = SmallTestGraph(Map(0 -> Seq(1, 2), 1 -> Seq(0, 2), 2 -> Seq(0, 1)))().result
+  test("loop edge") {
+    val g = SmallTestGraph(Map(0 -> Seq(0), 0 -> Seq(1), 2 -> Seq(0)))().result
     val op = ApproxEmbeddedness(bits = 12)
     val out = op(op.es, g.es).result
-    assert(out.embeddedness.rdd.collect.toMap == Map(0 -> 1, 1 -> 1, 2 -> 1, 3 -> 1, 4 -> 1, 5 -> 1))
+    assert(out.embeddedness.rdd.collect.toMap == Map(0 -> 0, 1 -> 0)) // Undefined for loop edge.
   }
 }
