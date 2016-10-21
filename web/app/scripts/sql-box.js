b61e6bc4477c5232085aadac1d2f7ecc4fe2ef0c
Daniel Darabos <darabos.daniel@gmail.com>
2016-10-21 12:10:49 +0200

    Add comment to weird formula.

--- web/app/scripts/sql-box.js
+++ web/app/scripts/sql-box.js
@@ -282,6 +282,7 @@ angular.module('biggraph').directive('sqlBox', function($rootScope, $window, sid
       });
 
       scope.showMoreRowsIncrement = function() {
+        // Offer increases of 10, 100, 1000, etc. depending on the magnitude of the current limit.
         return Math.max(10, Math.pow(10, Math.floor(Math.log10(scope.maxRows))));
       };
 
