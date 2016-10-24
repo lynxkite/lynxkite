7f9626fb81f1601ece3fcb748fd4bbf9f39fe8eb
Mate Szabo <szmate1618@users.noreply.github.com>
2016-10-24 14:41:01 +0200

    add ViewSchema to remote api

--- app/com/lynxanalytics/biggraph/serving/RemoteAPI.scala
+++ app/com/lynxanalytics/biggraph/serving/RemoteAPI.scala
@@ -14,6 +14,7 @@ import com.lynxanalytics.biggraph.graph_operations.DynamicValue
 import com.lynxanalytics.biggraph.graph_util.HadoopFile
 import com.lynxanalytics.biggraph.serving.FrontendJson._
 import com.lynxanalytics.biggraph.table.TableImport
+import org.apache.calcite.avatica.ColumnMetaData.StructType
 
 object RemoteAPIProtocol {
   case class CheckpointResponse(checkpoint: String)
@@ -125,6 +126,7 @@ object RemoteAPIServer extends JsonServer {
   def getScalar = jsonPost(c.getScalar)
   def getDirectoryEntry = jsonPost(c.getDirectoryEntry)
   def getPrefixedPath = jsonPost(c.getPrefixedPath)
+  def getViewSchema = jsonPost(c.getViewSchema)
   def newProject = jsonPost(c.newProject)
   def loadProject = jsonPost(c.loadProject)
   def removeName = jsonPost(c.removeName)
@@ -308,6 +310,11 @@ class RemoteAPIController(env: BigGraphEnvironment) {
 
   def createView[T <: ViewRecipe: json.Writes](user: User, recipe: T): CheckpointResponse = {
     CheckpointResponse(ViewRecipe.saveAsCheckpoint(recipe))
+  }
+
+  def getViewSchema(user: User, request: CheckpointRequest): String = {
+    val df = viewToDF(user, request.checkpoint)
+    return df.schema.toString()
   }
 
   private def viewToDF(user: User, checkpoint: String): DataFrame = {
