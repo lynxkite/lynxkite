ee333d6017ebe43ac0924ffb8c0443fafe34c5c5
Mate Szabo <szmate1618@users.noreply.github.com>
2016-10-26 15:09:06 +0200

    add test_view_schema

---
+++ remote_api/python/tests/test_view_schema.py
@@ -0,0 +1,24 @@
+import unittest
+import lynx
+from datetime import datetime
+
+
+class TestViewSchema(unittest.TestCase):
+
+  lk = lynx.LynxKite()
+
+  def get_column_names(self, schema_response):
+    return list(map(lambda x:x.name, schema_response.schema))
+
+  def get_data_types(self, schema_response):
+    return list(map(lambda x:x.dataType, schema_response.schema))
+
+  def test_schema(self):
+    '''Used to generate a view and check its schema.'''
+    schema_response = self.lk.sql('''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three''').schema()
+    self.assertEqual(self.get_column_names(self, schema_response), ['zero', 'one', 'two', 'three'])
+    self.assertEqual(self.get_data_types(self, schema_response), ['BooleanType', 'IntegerType', 'DoubleType', 'StringType'])
+
+
+if __name__ == '__main__':
+  unittest.main()
