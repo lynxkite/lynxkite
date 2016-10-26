68d7e9352a3cb9a58de9cc6aee1bff243f28e67b
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-10-26 16:26:51 +0200

    autopep8

--- remote_api/python/tests/test_view_schema.py
+++ remote_api/python/tests/test_view_schema.py
@@ -18,7 +18,9 @@ class TestViewSchema(unittest.TestCase):
     schema_response = self.lk.sql(
         '''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three''').schema()
     self.assertEqual(self.get_column_names(schema_response), ['zero', 'one', 'two', 'three'])
-    self.assertEqual(self.get_data_types(schema_response), ['BooleanType', 'IntegerType', 'DoubleType', 'StringType'])
+    self.assertEqual(
+        self.get_data_types(schema_response), [
+            'BooleanType', 'IntegerType', 'DoubleType', 'StringType'])
 
 
 if __name__ == '__main__':
