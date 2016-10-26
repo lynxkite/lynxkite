23e7c02d6eb3879f3933e56ee21b08287fd3c455
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-10-26 15:14:04 +0200

    remove unnecessary argument

--- remote_api/python/tests/test_view_schema.py
+++ remote_api/python/tests/test_view_schema.py
@@ -8,16 +8,17 @@ class TestViewSchema(unittest.TestCase):
   lk = lynx.LynxKite()
 
   def get_column_names(self, schema_response):
-    return list(map(lambda x:x.name, schema_response.schema))
+    return list(map(lambda x: x.name, schema_response.schema))
 
   def get_data_types(self, schema_response):
-    return list(map(lambda x:x.dataType, schema_response.schema))
+    return list(map(lambda x: x.dataType, schema_response.schema))
 
   def test_schema(self):
     '''Used to generate a view and check its schema.'''
-    schema_response = self.lk.sql('''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three''').schema()
-    self.assertEqual(self.get_column_names(self, schema_response), ['zero', 'one', 'two', 'three'])
-    self.assertEqual(self.get_data_types(self, schema_response), ['BooleanType', 'IntegerType', 'DoubleType', 'StringType'])
+    schema_response = self.lk.sql(
+        '''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three''').schema()
+    self.assertEqual(self.get_column_names(schema_response), ['zero', 'one', 'two', 'three'])
+    self.assertEqual(self.get_data_types(schema_response), ['BooleanType', 'IntegerType', 'DoubleType', 'StringType'])
 
 
 if __name__ == '__main__':
