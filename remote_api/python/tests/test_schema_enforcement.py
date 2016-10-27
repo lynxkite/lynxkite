caad064674c97eb8772bff32891f6a9ea845b33b
Mate Szabo <szmate1618@users.noreply.github.com>
2016-10-27 16:33:19 +0200

    more test cases

--- remote_api/python/tests/test_schema_enforcement.py
+++ remote_api/python/tests/test_schema_enforcement.py
@@ -2,12 +2,13 @@ import unittest
 import lynx.luigi
 from unittest import mock
 import types
-import os
+import shutil
 
 
 lk = lynx.LynxKite()
 
 query = '''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three'''
+directory = r'tests/ThisDirectoryDoesNotExistYet'
 
 
 class TestTask:
@@ -23,8 +24,8 @@ class TestTaskNoCheck(TestTask, lynx.luigi.SchemaEnforcedTask):
   schema_directory = None
 
 
-class TestTaskDoCheckNoDir(TestTask, lynx.luigi.SchemaEnforcedTask):
-  schema_directory = r'ThisDirectoryDoesNotExistYet'
+class TestTaskDoCheck(TestTask, lynx.luigi.SchemaEnforcedTask):
+  schema_directory = directory
 
 
 class TestTable(unittest.TestCase):
@@ -35,10 +36,17 @@ class TestTable(unittest.TestCase):
     tt = TestTaskNoCheck()
     tt.run()
 
-    tt = TestTaskDoCheckNoDir()
+    tt = TestTaskDoCheck()
+    # directory does not exist and should be created
     tt.run()
+    # directory exists
     tt.run()
+    # directory exists, but contains a different scheme
+    shutil.copy(r"tests/TestTaskDoCheck.schema", directory + "/TestTaskDoCheck.schema")
+    self.assertRaises(AssertionError, tt.run)
 
+  def tearDown(self):
+    shutil.rmtree(directory, ignore_errors=True)
 
 if __name__ == '__main__':
   unittest.main()
