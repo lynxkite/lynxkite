f25ad00831086470b8e84c255bc993dba476c430
Mate Szabo <szmate1618@users.noreply.github.com>
2016-10-27 15:48:55 +0200

    schem enforcement test

---
+++ remote_api/python/tests/test_schema_enforcement.py
@@ -0,0 +1,43 @@
+import unittest
+import lynx.luigi
+from unittest import mock
+import types
+import os
+
+
+lk = lynx.LynxKite()
+
+query = '''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three'''
+
+class TestTask:
+
+  def inputview(self):
+    return self.lk.sql(query)
+
+  def compute_view(self):
+    return self.inputview()
+
+
+class TestTaskNoCheck(TestTask, lynx.luigi.SchemaEnforcedTask):
+  schema_directory = None
+
+
+class TestTaskDoCheckNoDir(TestTask, lynx.luigi.SchemaEnforcedTask):
+  schema_directory = r'ThisDirectoryDoesNotExistYet'
+
+
+class TestTable(unittest.TestCase):
+
+  def test_run(self):
+    lk._request('/ajax/discardAllReallyIMeanIt')
+
+    tt = TestTaskNoCheck()
+    tt.run()
+
+    tt = TestTaskDoCheckNoDir()
+    tt.run()
+    tt.run()
+
+
+if __name__ == '__main__':
+  unittest.main()
