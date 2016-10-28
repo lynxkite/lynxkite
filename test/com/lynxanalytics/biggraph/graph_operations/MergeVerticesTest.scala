416b3b73cda20b3a6f6d0e91a34f8eaac6087c5b
gsvigruha <gsvigruha@users.noreply.github.com>
2016-10-29 05:00:39 +0800

    Make MergeVertices scale.

--- test/com/lynxanalytics/biggraph/graph_operations/MergeVerticesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/MergeVerticesTest.scala
@@ -10,7 +10,10 @@ class MergeVerticesTest extends FunSuite with TestGraphOp {
     val g = ExampleGraph()().result
     val op = MergeVertices[String]()
     val res = op(op.attr, g.gender).result
-    assert(res.segments.rdd.keys.collect.toSet == Set(0, 1))
-    assert(res.belongsTo.rdd.values.collect.toSet == Set(Edge(0, 0), Edge(1, 1), Edge(2, 0), Edge(3, 0)))
+    val segments = res.segments.rdd.keys.collect
+    assert(segments.size == 2)
+    val s0 = segments(0)
+    val s1 = segments(1)
+    assert(res.belongsTo.rdd.values.collect.toSet == Set(Edge(0, s0), Edge(1, s1), Edge(2, s0), Edge(3, s0)))
   }
 }
