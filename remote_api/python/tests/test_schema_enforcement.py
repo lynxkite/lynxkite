698dc0b3cab501f7afc6ae4410e562794435fe0c
Mate Szabo <mate.szabo@lynxanalytics.com>
2016-10-28 12:18:28 +0200

    better tests

--- remote_api/python/tests/test_schema_enforcement.py
+++ remote_api/python/tests/test_schema_enforcement.py
@@ -3,13 +3,15 @@ import lynx.luigi
 from unittest import mock
 import types
 import shutil
+import yaml
 
 
 lk = lynx.LynxKite()
 
 query = '''SELECT false AS zero, 1 AS one, 2.0 AS two, 'three' AS three'''
 directory = r'tests/ThisDirectoryDoesNotExistYet'
-
+with open(r"tests/TestTaskDoCheck.schema.correct", 'r') as f:
+  correct_schema = yaml.load(f)
 
 class TestTask:
 
@@ -39,10 +41,14 @@ class TestTable(unittest.TestCase):
     tt = TestTaskDoCheck()
     # directory does not exist and should be created
     tt.run()
+    with open(directory + "/TestTaskDoCheck.schema", 'r') as f:
+      self.assertEquals(correct_schema, yaml.load(f))
     # directory exists
     tt.run()
+    with open(directory + "/TestTaskDoCheck.schema", 'r') as f:
+      self.assertEquals(correct_schema, yaml.load(f))
     # directory exists, but contains a different schema
-    shutil.copy(r"tests/TestTaskDoCheck.schema", directory + "/TestTaskDoCheck.schema")
+    shutil.copy(r"tests/TestTaskDoCheck.schema.incorrect", directory + "/TestTaskDoCheck.schema")
     self.assertRaises(AssertionError, tt.run)
 
   def tearDown(self):
