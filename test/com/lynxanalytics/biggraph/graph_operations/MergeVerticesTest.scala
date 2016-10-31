7550d19d77590f3e46f8faba7898f4e5fdcf9849
gsvigruha <gsvigruha@users.noreply.github.com>
2016-11-01 00:59:58 +0800

    Make tests work.

--- test/com/lynxanalytics/biggraph/graph_operations/MergeVerticesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/MergeVerticesTest.scala
@@ -10,10 +10,8 @@ class MergeVerticesTest extends FunSuite with TestGraphOp {
     val g = ExampleGraph()().result
     val op = MergeVertices[String]()
     val res = op(op.attr, g.gender).result
-    val segments = res.segments.rdd.keys.collect
-    assert(segments.size == 2)
-    val s0 = segments(0)
-    val s1 = segments(1)
-    assert(res.belongsTo.rdd.values.collect.toSet == Set(Edge(0, s0), Edge(1, s1), Edge(2, s0), Edge(3, s0)))
+    val s = res.segments.rdd.keys.collect
+    assert(s.size == 2)
+    assert(res.belongsTo.rdd.values.collect.toSet == Set(Edge(0, s(0)), Edge(1, s(1)), Edge(2, s(0)), Edge(3, s(0))))
   }
 }
