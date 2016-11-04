fc15ef9b217d28699307a7ca7d7e04d7729a8317
erbenpeter <erben.peter@gmail.com>
2016-11-04 15:49:32 +0100

    Test centers with project meta_data().

--- remote_api/python/tests/test_project_metadata.py
+++ remote_api/python/tests/test_project_metadata.py
@@ -7,11 +7,11 @@ class TestProjectMetadata(unittest.TestCase):
   def test_guids_segmentations_and_scalars(self):
     p = lynx.LynxKite().new_project()
     p.newVertexSet(size=123)
-    md=p.meta_data()
+    md = p.meta_data()
     self.assertNotEqual(md.vertexSet, '')
     self.assertEqual(md.edgeBundle, '')
     self.assertEqual(md.segmentations, [])
-    self.assertEqual(md.scalars[0].title,'vertex_count')
+    self.assertEqual(md.scalars[0].title, 'vertex_count')
 
   def test_attributes(self):
     p = lynx.LynxKite().new_project()
@@ -34,6 +34,15 @@ class TestProjectMetadata(unittest.TestCase):
       titles.append(a.title)
     self.assertEqual(titles, ['comment', 'weight'])
 
+  def test_centers(self):
+    p = lynx.LynxKite().new_project()
+    p.newVertexSet(size=123)
+    c = p.centers(4, p.meta_data().vertexSet)
+    self.assertEqual(c,[
+      '-8730854457551617996',
+      '-8599056917853110189',
+      '-8372471039069781959',
+      '-8361175665923325924'])
 
 if __name__ == '__main__':
   unittest.main()
