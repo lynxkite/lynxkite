0bfb68b7bdb2ac4fa4fb2763cf65714df27a7fc9
erbenpeter <erben.peter@gmail.com>
2016-11-09 13:40:29 +0100

    Better test.

--- remote_api/python/tests/test_valkyrie.py
+++ remote_api/python/tests/test_valkyrie.py
@@ -15,12 +15,16 @@ class TestValkyrie(unittest.TestCase):
   @mock.patch('lynx.util.HDFS.list')
   def test_run(self, hdfs_list, hdfs_rm):
     SN = types.SimpleNamespace
-    hdfs_list.return_value = [
+    hdfs_list.side_effect = [[ # For DATA$/table_files
         SN(datetime=datetime.datetime(2016, 8, 1, 15, 32), path='old, no ttl'),
         SN(datetime=datetime.datetime(2016, 8, 1, 15, 32), path='old, long ttl (ttl=7d)'),
         SN(datetime=datetime.datetime(2016, 8, 12, 9, 22), path='new, long ttl (ttl=7d)'),
         SN(datetime=datetime.datetime(2016, 8, 12, 9, 22), path='new, short ttl (ttl=1h)'),
-    ]
+    ],[ # For DATA$/uploads
+      SN(datetime=datetime.datetime(2016, 8, 1, 15, 32), path='uploaded-file, no ttl'),
+      SN(datetime=datetime.datetime(2016, 8, 1, 15, 32), path='uploaded-file-old, long ttl (ttl=7d)'),
+    ]]
+
     v = valkyrie.ValkyrieCleanup(date=datetime.datetime(2016, 8, 12, 12, 5, 0))
     output = mock.MagicMock()
     with mock.patch.multiple(v, output=mock.DEFAULT, lk=mock.DEFAULT) as mocks:
@@ -31,8 +35,9 @@ class TestValkyrie(unittest.TestCase):
     # Assert the right files are deleted.
     hdfs_rm.assert_any_call('old, long ttl (ttl=7d)')
     hdfs_rm.assert_any_call('new, short ttl (ttl=1h)')
+    hdfs_rm.assert_any_call('uploaded-file-old, long ttl (ttl=7d)')
     # Assert no other files are deleted.
-    self.assertEqual(4, hdfs_rm.call_count)
+    self.assertEqual(3, hdfs_rm.call_count)
     # Assert the marker file is created.
     output.open.assert_called_once_with('w')
 
