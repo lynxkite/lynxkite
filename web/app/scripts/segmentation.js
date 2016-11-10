dcb494bcd52ccd011c6a3198401243d113c5f1de
Daniel Darabos <darabos.daniel@gmail.com>
2016-11-10 14:39:37 +0100

    Remove unused files.

--- web/app/scripts/segmentation.js
+++
@@ -1,9 +0,0 @@
-// The entry for a segmentation in the project view.
-'use strict';
-
-angular.module('biggraph').directive('segmentation', function() {
-  return {
-    scope: { segmentation: '=', side: '=' },
-    templateUrl: 'segmentation.html',
-  };
-});
