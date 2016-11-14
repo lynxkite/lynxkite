cbd3fd8a064bee06a4f6a9d6086c87462907cb0d
erbenpeter <erben.peter@gmail.com>
2016-11-14 17:17:25 +0100

    Address review.

--- app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
+++ app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
@@ -427,16 +427,14 @@ class Operations(env: SparkFreeEnvironment) extends OperationRepository(env) {
       assert(attrName != FEOption.unset.id, "The Vertex ID attribute parameter must be set.")
       val idAttr = project.vertexAttributes(attrName).runtimeSafeCast[String]
       val idColumn = table.column(params("id-column")).runtimeSafeCast[String]
-      val prefix = if (params("prefix").nonEmpty) params("prefix") + "_" else ""
       val projectAttrNames = project.vertexAttributeNames
       val tableAttrNames = table.columns
-      for ((name, attr) <- table.columns) {
-        assert(!projectAttrNames.contains(prefix + name),
-          "Vertex attribute name conflict: " + prefix + name)
-      }
       val op = graph_operations.EdgesFromUniqueBipartiteAttributeMatches()
       val res = op(op.fromAttr, idAttr)(op.toAttr, idColumn).result
+      val prefix = if (params("prefix").nonEmpty) params("prefix") + "_" else ""
       for ((name, attr) <- table.columns) {
+        assert(!projectAttrNames.contains(prefix + name),
+          s"Cannot import column ${prefix + name}. Attribute already exists.")
         project.newVertexAttribute(prefix + name, attr.pullVia(res.edges), "imported")
       }
     }
@@ -463,16 +461,14 @@ class Operations(env: SparkFreeEnvironment) extends OperationRepository(env) {
       assert(attrName != FEOption.unset.id, "The Edge ID attribute parameter must be set.")
       val idAttr = project.edgeAttributes(attrName).runtimeSafeCast[String]
       val idColumn = table.column(params("id-column")).runtimeSafeCast[String]
-      val prefix = if (params("prefix").nonEmpty) params("prefix") + "_" else ""
       val projectAttrNames = project.edgeAttributeNames
       val tableAttrNames = table.columns
-      for ((name, attr) <- table.columns) {
-        assert(!projectAttrNames.contains(prefix + name),
-          "Edge attribute name conflict: " + prefix + name)
-      }
       val op = graph_operations.EdgesFromUniqueBipartiteAttributeMatches()
       val res = op(op.fromAttr, idAttr)(op.toAttr, idColumn).result
+      val prefix = if (params("prefix").nonEmpty) params("prefix") + "_" else ""
       for ((name, attr) <- table.columns) {
+        assert(!projectAttrNames.contains(prefix + name),
+          s"Cannot import column ${prefix + name}. Attribute already exists.")
         project.newEdgeAttribute(prefix + name, attr.pullVia(res.edges), "imported")
       }
     }
