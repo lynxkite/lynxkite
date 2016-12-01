434f7e2545d1264905cd78dda502a089fe432d55
Daniel Darabos <darabos.daniel@gmail.com>
2016-12-01 19:24:28 +0100

    Remove ReflectionMutex. Reflection is supposed to be thread-safe in Scala 2.11!

--- app/com/lynxanalytics/biggraph/graph_api/JsonSerialization.scala
+++ app/com/lynxanalytics/biggraph/graph_api/JsonSerialization.scala
@@ -21,14 +21,13 @@ object TypedJson {
         case "Long" => (j \ "data").as[Long].asInstanceOf[T]
         case "Double" => (j \ "data").as[Double].asInstanceOf[T]
         case "String" => (j \ "data").as[String].asInstanceOf[T]
-        case cls => ReflectionMutex.synchronized {
+        case cls =>
           // Find the companion object.
           val sym = reflect.runtime.currentMirror.staticModule(cls)
           val obj = reflect.runtime.currentMirror.reflectModule(sym).instance
           val des = obj.asInstanceOf[FromJson[T]]
           // Ask the companion object to parse the data.
           des.fromJson(j \ "data")
-        }
       }
     } catch {
       // Include more details in the exception.
