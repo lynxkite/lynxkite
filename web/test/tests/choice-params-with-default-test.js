435f2a52bf3b66c346670e5b0e1818b97fa1e67b
gsvigruha <gergely.svigruha@lynxanalytics.com>
2016-12-14 08:04:37 +0800

    Fix test.

--- web/test/tests/choice-params-with-default-test.js
+++ web/test/tests/choice-params-with-default-test.js
@@ -27,22 +27,29 @@ module.exports = function(fw) {
       lib.left.history.close();
     });
 
-  fw.transitionTest(
+  fw.statePreservingTest(
     'test pagerank default choice values',
     'test pagerank default choice values edit history',
     function() {
       lib.left.history.open();
       var op1 = lib.left.history.getOperation(1);
-      lib.left.populateOperation(op1, {direction: 'all edges'});  // change output name
+      lib.left.populateOperation(op1, {direction: 'all edges'});  // change direction
       lib.left.submitOperation(op1);
       var op2 = lib.left.history.getOperation(2);
-      lib.left.populateOperation(op2, {direction: 'all edges'});  // change output name
+      lib.left.populateOperation(op2, {direction: 'all edges'});  // change direction
       lib.left.submitOperation(op2);
       lib.left.history.save();
 
       lib.left.history.open();
       lib.left.history.expectOperationSelectParameter(1, 'direction', 'string:all edges');
       lib.left.history.expectOperationSelectParameter(2, 'direction', 'string:all edges');
+      // Restore original state.
+      var op3 = lib.left.history.getOperation(1);
+      lib.left.populateOperation(op3, {direction: 'outgoing edges'});
+      lib.left.submitOperation(op3);
+      var op4 = lib.left.history.getOperation(2);
+      lib.left.populateOperation(op4, {direction: 'incoming edges'});
+      lib.left.submitOperation(op4);
       lib.left.history.save();
     });
 };
