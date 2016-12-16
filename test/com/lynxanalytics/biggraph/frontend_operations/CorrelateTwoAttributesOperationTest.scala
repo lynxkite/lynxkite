01ed36899c69443487812bad84160b2b496b512f
János Maginecz <wafleeee@gmail.com>
2016-12-16 12:04:16 +0100

    Add failing? test

---
+++ test/com/lynxanalytics/biggraph/frontend_operations/CorrelateTwoAttributesOperationTest.scala
@@ -0,0 +1,17 @@
+package com.lynxanalytics.biggraph.frontend_operations
+
+import com.lynxanalytics.biggraph.graph_api.Scripting._
+import org.scalautils.TolerantNumerics
+
+class CorrelateTwoAttributesOperationTest extends OperationsTestBase {
+  implicit val doubleEq = TolerantNumerics.tolerantDoubleEquality(0.0001)
+
+  test("Aggregate edge attribute to vertices, all directions") {
+    run("Example Graph")
+    run("Correlate two attributes", Map(
+      "attrA" -> "age",
+      "attrB" -> "age"))
+    assert(project.scalars("correlation of age and age").value.asInstanceOf[Double] === 1.0)
+  }
+}
+
