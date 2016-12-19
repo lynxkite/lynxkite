efa5927b0f9d7bd03f67873f5513e861a4794944
Hanna Gábor <gahanna999@gmail.com>
2016-12-19 14:03:51 +0100

    Authentication test (i5287)
    * Run frontend tests with authentication. Log number checking is currently removed!
    
    * Separate authentication tests.
    
    * Add first version of the authentication test.
    
    * Solve problem with self-signed cert and discardAll.
    
    * Hide login-info.
    
    * Remove unnecessary cleanup and auth splash state.
    
    * Add another user.
    
    * Some small changes.
    
    * Nit.
    
    * Remove test_interface.sh.
    
    * yarn
    
    * Tests are passing now.
    
    * Automatic change in yarn.lock.
    
    * Address some review comments.
    
    * Address comments.
    
    * Nit.
    
    * Add console.logs to see where the frontend test fails.
    
    * Add missing semicolons.
    
    * Do not run webdriver-update if the driver is already there.
    
    * Make sure solo works even with the separate test suites.
    
    * Remove some debug logging, add some debug logging.
    
    * More logging. No idea why this fails on Jenkins.
    
    * Try to see what is wrong.
    
    * Remove debug stuff.

--- remote_api/python/test.sh
+++ remote_api/python/test.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/bash -xue
 # Run Python API tests. You can start LynxKite manually, or use tools/with_lk.sh.
 
-export LYNXKITE_ADDRESS="http://localhost:${PORT:-2200}/"
+if [ -n "${HTTPS_PORT:-}" ]; then
+  export LYNXKITE_ADDRESS="https://localhost:$HTTPS_PORT/"
+  export LYNXKITE_PUBLIC_SSL_CERT='../../test/localhost.self-signed.cert.pub'
+else
+  export LYNXKITE_ADDRESS="http://localhost:${PORT:-2200}/"
+fi
+export LYNXKITE_USERNAME="admin"
+export LYNXKITE_PASSWORD="adminpw"
 
 cd $(dirname $0)
 python3 -m unittest discover tests $@
