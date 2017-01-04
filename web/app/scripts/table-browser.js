79881dc15ad694b4bd2d17ede10bf92d8dfddd3e
Gabor Feher <gabor.feher@lynxanalytics.com>
2017-01-04 17:00:15 +0100

    add "show all" link and finalize sorting

--- web/app/scripts/table-browser.js
+++ web/app/scripts/table-browser.js
@@ -1,8 +1,6 @@
 // Presents the parameters for running SQL scripts.
 'use strict';
 
-var MAX_ENTRIES_TO_SHOW = 300;
-
 function charEquals(ch1, ch2) {
   return ch1.toLowerCase() === ch2.toLowerCase();
 }
@@ -132,9 +130,41 @@ angular.module('biggraph').directive('tableBrowser', function(util) {
             }
           },
 
-        };
-      }
+          matchFragmentCount: function() {
+            var cnt = 0;
+            for (var i = 0; i < this.nameMatch.length; ++i) {
+              if (this.nameMatch[i] !== '') {
+                cnt += 1;
+              }
+            }
+            return cnt;
+          },
 
+          // Used for sorting the tables. Sorting is first by number
+          // of match fragments according to user search, and second
+          // by name alphabetically.
+          compareTo: function(other) {
+            var c1 = this.matchFragmentCount();
+            var c2 = other.matchFragmentCount();
+            var n1 = this.relativePath;
+            var n2 = other.relativePath;
+            if (c1 > c2) {
+              return 1;
+            } else if (c1 < c2) {
+              return -1;
+            } else if (n1 > n2) {
+              return 1;
+            } else if (n1 < n2) {
+              return -1;
+            } else {
+              return 0;
+            }
+          },
+
+        };
+      }  // createTable
+
+      scope.maxEntriesToShow = 100;
       var path = scope.projectState ? scope.projectState.projectName : scope.directory;
       scope.tableResponse = util.nocache(
         '/ajax/getAllTables',
@@ -142,24 +172,38 @@ angular.module('biggraph').directive('tableBrowser', function(util) {
           'path': path
         });
       scope.tableFilter = '';
-      scope.$watchGroup(['tableFilter', 'tableResponse.list'], function() {
-        var list = scope.tableResponse.list;
-        if (!list) {
-          return;
-        }
-        var filteredList = [];
-        for (var i = 0; i < list.length; ++i) {
-          var table = createTable(list[i]);
-          table.computeNameMatch(scope.tableFilter);
-          if (table.nameMatch) {
-            filteredList.push(table);
+      // Watch for changes related to the table list:
+      scope.$watchGroup(
+        ['tableFilter', 'tableResponse.list', 'maxEntriesToShow'],
+        function() {
+          var list = scope.tableResponse.list;
+          if (!list) {
+            return;
           }
-        }
-        // Sort by the length of match arrays. This estimates the fragmentedness of matches,
-        // and moves the non-fragmented results early in the list.
-        filteredList.sort(function(a, b) { return a.nameMatch.length - b.nameMatch.length; });
-        scope.list = filteredList.slice(0, MAX_ENTRIES_TO_SHOW);
+          scope.list = [];
+          for (var i = 0; i < list.length; ++i) {
+            var table = createTable(list[i]);
+            table.computeNameMatch(scope.tableFilter);
+            if (table.nameMatch) {
+              scope.list.push(table);
+            }
+          }
+          // Sort by the number of match fragments.
+          scope.list.sort(function(a, b) {
+            return a.compareTo(b);
+          });
+          // JavaScript is OK with handling 10000+ long lists, but
+          // Angular gets really slow with them. So we are only
+          // showing the top 100 results the user here, the rest
+          // gets dropped by default:
+          scope.originalListSize = scope.list.length;
+          if (scope.maxEntriesToShow >= 0) {
+            scope.list = scope.list.slice(0, scope.maxEntriesToShow);
+          }
       });
+      scope.showAll = function() {
+        scope.maxEntriesToShow = -1;
+      };
 
     }
   };
