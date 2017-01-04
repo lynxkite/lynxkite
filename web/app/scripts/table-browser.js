75df2f689f0dc385b7378edc0d6b011e42f4911d
Gabor Feher <feherga@gmail.com>
2017-01-04 15:16:38 +0100

    refactor JS and clean up styling

--- web/app/scripts/table-browser.js
+++ web/app/scripts/table-browser.js
@@ -1,10 +1,26 @@
 // Presents the parameters for running SQL scripts.
 'use strict';
 
+var MAX_ENTRIES_TO_SHOW = 300;
+
 function charEquals(ch1, ch2) {
   return ch1.toLowerCase() === ch2.toLowerCase();
 }
 
+/**
+  Computes wether matcher is a non-contiguous substring of base.
+  If it is, then the result is an array of alternating matching
+  and non-matching portions of base. Example:
+
+  // There is no I in team:
+  computeMatch('team', 'I') = []
+  // 'burg' matches 'hamburger'
+  computeMatch('hamburger', 'burg') = ['', 'ham', 'burg', 'er']
+  // 'apple' matches 'apple'
+  computeMatch('apple', 'apple') = ['apple', '']
+  // '' matches 'skildpadder'
+  computeMatch('', 'skildpadder') = ['', 'skildpadder']
+ */
 function computeMatch(base, matcher) {
   if (matcher === '') {
     return ['', base];
@@ -51,15 +67,81 @@ angular.module('biggraph').directive('tableBrowser', function(util) {
     },
     templateUrl: 'table-browser.html',
     link: function(scope) {
-      var path = scope.projectState ? scope.projectState.projectName : scope.directory;
 
+      // Returns a table object that can be used in the template for
+      // rendering and event handling.
+      function createTable(tableData) {
+        return {
+          absolutePath: tableData.absolutePath,
+          relativePath: tableData.relativePath,
+
+          computeColumnMatches: function() {
+            if (this.columns) {
+              for (var j = 0; j < this.columns.length; ++j) {
+                this.columns[j].nameMatch = computeMatch(
+                    this.columns[j].name,
+                    this.columnFilter);
+              }
+            }
+          },
+
+          computeNameMatch: function(tableFilter) {
+            this.nameMatch = computeMatch(this.relativePath, tableFilter);
+          },
+
+          getSqlColumnName: function(column, fullyQualifyNames) {
+            if (fullyQualifyNames) {
+              return '`' + this.relativePath + '`.`' + column.name + '`';
+            } else {
+              return '`' + column.name + '`';
+            }
+          },
+
+          allColumnsSQL: function(fullyQualifyNames) {
+            var that = this;
+            return this.columns
+                .map(function(column) {
+                    return that.getSqlColumnName(column, fullyQualifyNames);
+                })
+                .join(',\n');
+          },
+
+          toggleOpen: function() {
+            var that = this;
+            if (that.isOpen) {
+              that.isOpen = false;
+              that.deregisterWatch();
+            } else {
+              util.nocache(
+                '/ajax/getColumns',
+                {
+                  absolutePath: that.absolutePath
+                }
+              ).then(function(res) {
+                that.columns = res.columns;
+                that.isOpen = true;
+                that.columnFilter = '';
+                that.deregisterWatch = scope.$watch(
+                  function() {  // What to watch for:
+                    return that.columnFilter;
+                  },
+                  function() {  // What to do when change is detected:
+                    that.computeColumnMatches();
+                  });
+              });
+            }
+          },
+
+        };
+      }
+
+      var path = scope.projectState ? scope.projectState.projectName : scope.directory;
       scope.tableResponse = util.nocache(
         '/ajax/getAllTables',
         {
           'path': path
         });
       scope.tableFilter = '';
-      scope.columnFilter = '';
       scope.$watchGroup(['tableFilter', 'tableResponse.list'], function() {
         var list = scope.tableResponse.list;
         if (!list) {
@@ -67,69 +149,17 @@ angular.module('biggraph').directive('tableBrowser', function(util) {
         }
         var filteredList = [];
         for (var i = 0; i < list.length; ++i) {
-          list[i].nameMatch = computeMatch(
-              list[i].relativePath,
-              scope.tableFilter);
-          if (list[i].nameMatch) {
-            filteredList.push(list[i]);
+          var table = createTable(list[i]);
+          table.computeNameMatch(scope.tableFilter);
+          if (table.nameMatch) {
+            filteredList.push(table);
           }
         }
-        // Sort by the length of match arrays.
-        // The sorter the array is the least fragmented the match is. (Sort of.)
+        // Sort by the length of match arrays. This estimates the fragmentedness of matches,
+        // and moves the non-fragmented results early in the list.
         filteredList.sort(function(a, b) { return a.nameMatch.length - b.nameMatch.length; });
-        scope.list = filteredList.slice(0, 50);
+        scope.list = filteredList.slice(0, MAX_ENTRIES_TO_SHOW);
       });
-      scope.$watchGroup(['columnFilter', 'tableResponse.list', 'openTable'], function() {
-        var list = scope.tableResponse.list;
-        if (!list) {
-          return;
-        }
-        for (var i = 0; i < list.length; ++i) {
-          if (scope.openTable === list[i].relativePath) {
-            scope.computeColumnMatches(list[i]);
-          }
-        }
-      });
-
-      scope.computeColumnMatches = function(table) {
-        var columns = table.columns;
-        if (columns) {
-          for (var j = 0; j < columns.length; ++j) {
-            columns[j].nameMatch = computeMatch(
-                columns[j].name,
-                scope.columnFilter);
-          }
-        }
-      };
-      scope.getColumnName = function(table, column) {
-        if (scope.fullyQualifyNames) {
-          return '`' + table.relativePath + '`.`' + column.name + '`';
-        } else {
-          return '`' + column.name + '`';
-        }
-      };
-      scope.allColumns = function(table) {
-        return table.columns
-            .map(function(column) {
-                return scope.getColumnName(table, column);
-            })
-            .join(',\n');
-      };
-      scope.toggleOpen = function(table) {
-        if (scope.openTable) {
-          scope.openTable = undefined;
-        } else {
-          util.nocache(
-            '/ajax/getColumns',
-            {
-              absolutePath: table.absolutePath
-            }
-          ).then(function(res) {
-            table.columns = res.columns;
-            scope.openTable = table.relativePath;
-          });
-        }
-      };
 
     }
   };
