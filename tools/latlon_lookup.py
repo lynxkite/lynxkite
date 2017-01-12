318bb02bd31cb852993b0670a477c404a21bf4fd
Daniel Darabos <darabos.daniel@gmail.com>
2017-01-12 11:31:24 +0100

    Small tool to look up stuff from a Shapefile until we have an operation.

---
+++ tools/latlon_lookup.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+'''Looks up Shapefile data by lat/long coordinates.'''
+
+import fiona
+import pandas as pd
+import shapely.geometry as sg
+
+with fiona.open('hk.shp') as f:
+  shapes = list(f)
+
+sps = [(sg.asShape(s['geometry']), s['properties']) for s in shapes]
+locs = pd.read_csv('lat-lons.csv')
+
+
+def lookup(id, lat, lon):
+  point = sg.Point(lon, lat)
+  d = {'id': id}
+  hits = [p for (s, p) in sps if s.contains(point)]
+  if hits:
+    d.update(hits[0])
+  return d
+
+mapped = [lookup(id, lat, lon) for (idx, id, lat, lon) in locs.itertuples()]
+mapped = pd.DataFrame(mapped)
+mapped.to_csv('lat-lons-resolved.csv', index=False)
