c88a24a94ddd950f128176b89a2eb0ef4e20bdc4
Gabor Feher <feherga@gmail.com>
2017-01-13 17:56:24 +0100

    comment and css cleanups

--- web/app/scripts/table-browser-entry.js
+++ web/app/scripts/table-browser-entry.js
@@ -1,6 +1,6 @@
 // Represents an entry in the SQL table browser.
 // Parameters:
-//  text: the name of the entry (i.e. table or column name)
+//  text: the user-visible name of the entry (i.e. table or column name)
 //  draggableText: This is the text that will be copied if the
 //    user drags this entry into a text editor field.
 'use strict';
