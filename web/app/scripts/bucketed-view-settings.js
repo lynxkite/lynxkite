c04497f12e839ab0391de2ce40dc3289e9b4c4f7
Daniel Darabos <darabos.daniel@gmail.com>
2017-01-24 15:02:44 +0100

    Tokenize bucketed view settings.

--- web/app/scripts/bucketed-view-settings.js
+++ web/app/scripts/bucketed-view-settings.js
@@ -5,8 +5,44 @@ angular.module('biggraph').directive('bucketedViewSettings', function() {
     scope: { side: '=' },
     restrict: 'E',
     templateUrl: 'bucketed-view-settings.html',
-    link: function(scope) {
-      scope.x = function() {};
+    link: function(scope, element) {
+      var drops = {};
+
+      element.find('.entity').each(function(i, e) {
+        if (!e.id) { return; }
+        var menu = element.find('#menu-' + e.id);
+        /* global Drop */
+        var drop = new Drop({
+          target: e,
+          content: menu[0],
+          openOn: 'click',
+          classes: 'drop-theme-menu',
+          remove: true,
+          position: 'bottom center',
+          tetherOptions: {
+            constraints: [{ to: 'window', attachment: 'together', pin: true, }],
+          },
+        });
+        drops['menu-' + e.id] = drop;
+      });
+
+      function getDrop(e) {
+        e = e.currentTarget;
+        for (var i = 0; i < 20; ++i) {
+          if (drops[e.id] !== undefined) {
+            return drops[e.id];
+          }
+          e = e.parentNode;
+        }
+      }
+
+      scope.everythingDisplayed = function() {
+        return scope.side.state.preciseBucketSizes && scope.side.state.relativeEdgeDensity;
+      };
+
+      scope.closeDrop = function(e) {
+        getDrop(e).close();
+      };
     },
   };
 });
