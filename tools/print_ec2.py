0f4795afcf907f0f12495083bfba42a131efaf8c
hiuy <yenonn.hiu@lynxanalytics.com>
2017-02-01 13:15:49 +0800

    Adding print_ec2.py - boto3 implementation of ec2 report

---
+++ tools/print_ec2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import boto3
+from prettytable import PrettyTable
+
+client = boto3.client('ec2')
+regions = client.describe_regions()['Regions']
+
+
+attributes = ['Region', 'Name', 'Type', 'State', 'Private IP', 'Public IP', 'Launch Time']
+table = PrettyTable(attributes) 
+table.align = 'l'
+
+for region in regions:
+    region_name=region['RegionName']
+    
+    ec2 = boto3.resource('ec2',region_name=region['RegionName'])
+
+    # Get information for all running instances
+    running_instances = ec2.instances.filter(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['running']}])
+
+    
+    for instance in running_instances:
+        instanceinfo = []
+        for tag in instance.tags:
+            if 'Name'in tag['Key']:
+                name = tag['Value']
+
+        instanceinfo.append(region_name.upper())
+        instanceinfo.append(name) 
+        instanceinfo.append(instance.instance_type.upper())
+        instanceinfo.append(instance.state['Name'].upper())
+        instanceinfo.append(instance.private_ip_address)
+        instanceinfo.append(instance.public_ip_address)
+        instanceinfo.append(instance.launch_time)
+
+        if len(instanceinfo) > 0:
+            table.add_row(instanceinfo)
+
+print table
