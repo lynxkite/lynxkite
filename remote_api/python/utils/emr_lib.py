356c501c8c87a32e2df9b24ad2a86b2495e33185
János Maginecz <wafleeee@gmail.com>
2017-02-09 16:19:09 +0100

    Prepare for throttling in emr_lib

--- remote_api/python/utils/emr_lib.py
+++ remote_api/python/utils/emr_lib.py
@@ -70,7 +70,15 @@ class EMRLib:
     while True:
       i = 0
       while i < len(services):
-        if services[i].is_ready():
+        service_ready = False
+        try:
+          service_ready = services[i].is_ready()
+        except botocore.exceptions.ClientError as e:
+          if e.response['Error']['Code'] == 'ThrottlingException':
+            time.sleep(60)
+          else:
+            raise e
+        if service_ready:
           print('{name!s} is ready, waiting for {n!s} more services to start...'.format(
               name=services[i],
               n=(len(services) - 1)
