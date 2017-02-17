eb7361b9afa06876c19b9825c1cc3d239763f348
gsvigruha <gsvigruha@users.noreply.github.com>
2017-02-18 05:07:08 +0800

    Renaming, fix tests.

--- test/com/lynxanalytics/biggraph/graph_operations/SegmentByGeographicalProximityTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/SegmentByGeographicalProximityTest.scala
@@ -6,11 +6,11 @@ import com.lynxanalytics.biggraph.graph_util.Shapefile
 import org.scalatest.FunSuite
 
 class SegmentByGeographicalProximityTest extends FunSuite with TestGraphOp {
-  test("segment the ExampleGraph by GEO data") {
+  test("segment the ExampleGraph by geographical proximity") {
     val shapePath = getClass.getResource("/graph_operations/FindRegionTest/earth.shp").getPath
     val shapeFile = Shapefile(shapePath)
     val ex = ExampleGraph()().result
-    val op = SegmentByGeographicalProximity(shapePath, 0.1, shapeFile.attrNames)
+    val op = SegmentByGeographicalProximity(shapePath, 0.1, shapeFile.attrNames, onlyknownFeatures = true)
     val result = op(op.coordinates, ex.location).result
     assert(result.segments.rdd.count == 418)
     // Test that an overlapping segment was created. The actual values are checked in the
