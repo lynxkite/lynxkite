dbd1d5ce0c8c3df7579d59787a05a1f93a5943d7
Gabor Olah <gabor.olah@lynxanalytics.com>
2017-02-22 09:08:09 +0100

    First shot

--- app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
+++ app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
@@ -4,6 +4,7 @@
 package com.lynxanalytics.biggraph.graph_api
 
 import com.lynxanalytics.biggraph.graph_util.KiteInstanceInfo
+import com.lynxanalytics.biggraph.graph_util.LoggedFutures
 import play.api.libs.json
 import scala.concurrent.ExecutionContextExecutorService
 
@@ -21,8 +22,8 @@ class OperationLogger(instance: MetaGraphOperationInstance,
   private var stopTime = -1L
 
   private def elapsedMs(): Long = {
-    assert(startTime != -1, "elapsedMs() called before startTimer()")
-    assert(stopTime != -1, "elapsedMs() called before stopTimer()")
+    assert(startTime != -1, s"elapsedMs() called before startTimer(), name: ${instance.operation.toString}")
+    assert(stopTime != -1, s"elapsedMs() called before stopTimer(), name: ${instance.operation.toString}")
     stopTime - startTime
   }
 
@@ -47,7 +48,8 @@ class OperationLogger(instance: MetaGraphOperationInstance,
     assert(stopTime == -1, "stopTimer() called more than once")
     stopTime = System.currentTimeMillis()
   }
-  def addInput(name: String, input: EntityData): Unit = {
+  def addInput(name: String, input: EntityData): Unit = inputInfoList.synchronized {
+    //    println(s"Adding $name  input: $input")
     if (instance.operation.isHeavy) input match {
       case rddData: EntityRDDData[_] =>
         inputInfoList +=
@@ -60,34 +62,68 @@ class OperationLogger(instance: MetaGraphOperationInstance,
     }
   }
 
-  def logWhenReady(): Unit = {
+  def logWhenReady(loggedFutures: LoggedFutures): Unit = {
     val outputsFuture = SafeFuture.sequence(outputInfoList)
-
     outputsFuture.map {
-      outputs => dump(outputs)
+      outputs =>
+        try {
+          loggedFutures.register {
+            dump(outputs)
+          }
+        } catch {
+          case t: Throwable => ()
+        }
     }
   }
 
   private def dump(outputs: Seq[OutputInfo]): Unit = {
+    if (outputs.nonEmpty) {
+      try {
+        implicit val formatInput = json.Json.format[InputInfo]
+        implicit val formatOutput = json.Json.format[OutputInfo]
 
-    implicit val formatInput = json.Json.format[InputInfo]
-    implicit val formatOutput = json.Json.format[OutputInfo]
+        val instanceProperties = json.Json.obj(
+          "kiteVersion" -> KiteInstanceInfo.kiteVersion,
+          "sparkVersion" -> KiteInstanceInfo.sparkVersion,
+          "instanceName" -> KiteInstanceInfo.instanceName
+        )
 
-    val instanceProperties = json.Json.obj(
-      "kiteVersion" -> KiteInstanceInfo.kiteVersion,
-      "sparkVersion" -> KiteInstanceInfo.sparkVersion,
-      "instanceName" -> KiteInstanceInfo.instanceName
-    )
+        val inputs = inputInfoList.synchronized { inputInfoList.sortBy(_.name) }
 
-    val out = json.Json.obj(
-      "instanceProperties" -> instanceProperties,
-      "name" -> instance.operation.toString,
-      "timestamp" -> com.lynxanalytics.biggraph.graph_util.Timestamp.toString,
-      "guid" -> instance.operation.gUID.toString,
-      "elapsedMs" -> elapsedMs(),
-      "inputs" -> inputInfoList.sortBy(_.name),
-      "outputs" -> outputs.sortBy(_.name)
-    )
-    log.info(s"$marker $out")
+        val out = json.Json.obj(
+          "instanceProperties" -> instanceProperties,
+          "name" -> instance.operation.toString,
+          "timestamp" -> com.lynxanalytics.biggraph.graph_util.Timestamp.toString,
+          "guid" -> instance.operation.gUID.toString,
+          "elapsedMs" -> elapsedMs(),
+          "inputs" -> inputs,
+          "outputs" -> outputs.sortBy(_.name)
+        )
+        log.info(s"$marker $out")
+      } catch {
+        case t: Throwable =>
+          println("dump failed: " + t)
+          println("name: " + instance.operation.toString)
+          println(s"start time: $startTime")
+          println(s"stop time: $stopTime")
+          println("Outputs: ")
+          for (o <- outputs) {
+            println(s"  $o")
+          }
+          println("Inputs: ")
+          try {
+            for (i <- inputInfoList) {
+              println(s"  $i")
+            }
+          } catch {
+            case x: Throwable =>
+              println("x trace")
+              x.printStackTrace()
+              println("x trace ends")
+          }
+          t.printStackTrace()
+          println("*****")
+      }
+    }
   }
 }
