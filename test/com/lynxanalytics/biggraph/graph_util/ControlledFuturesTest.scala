aac6fe5d73e731b99457bc2f5249adcd9b166836
Gabor Olah <gabor.olah@lynxanalytics.com>
2017-02-23 15:41:20 +0100

    Rename LoggedFutures to ControlledFutures and write some documentation

--- test/com/lynxanalytics/biggraph/graph_util/ControlledFuturesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_util/ControlledFuturesTest.scala
@@ -3,14 +3,14 @@ package com.lynxanalytics.biggraph.graph_util
 import com.lynxanalytics.biggraph.graph_api.{ SafeFuture, ThreadUtil }
 import org.scalatest.FunSuite
 
-class LoggedFuturesTest extends FunSuite {
+class ControlledFuturesTest extends FunSuite {
   val maxParalellism = 11
   val numFutures = 487
   implicit val executionContext =
     ThreadUtil.limitedExecutionContext("LoggedFuturesTest", maxParallelism = maxParalellism)
 
   test("LoggedFutures works") {
-    val loggedFutures = new LoggedFutures()(executionContext)
+    val loggedFutures = new ControlledFutures()(executionContext)
     val counter = new java.util.concurrent.atomic.AtomicInteger(0)
     for (_ <- 1 to numFutures) {
       loggedFutures.register {
