23f3b4f190e306016d5f5eeeaf13b94ef884e462
Daniel Darabos <darabos.daniel@gmail.com>
2017-02-28 15:48:13 +0100

    Dumb script for finding projects with big disk usage.

---
+++ tools/findbigfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ue
+# Find the projects with big data files on PizzaKite.
+# Run from the active metadata directory.
+
+cd /data/partitioned
+du -s * | sort -n | tail -n 100 > /tmp/bigfiles
+cd - > /dev/null
+cat /tmp/bigfiles | while read size guid; do
+  cp=$(grep -l $guid checkpoints/* | tail -n 1 | sed 's/.*-//')
+  if [ -n "$cp" ]; then
+    project=$(grep "$cp" tags.journal | tail -n 1 | sed -n 's/.*projects\/\(.*\)\/\w*heckpoint.*/\1/p')
+    echo "$size kb $guid $cp $project"
+  fi
+done
