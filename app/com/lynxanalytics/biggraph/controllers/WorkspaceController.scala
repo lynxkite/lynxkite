1c1a35ebfb097739f86c4f5638e0393da84e4e3e
Daniel Darabos <darabos.daniel@gmail.com>
2017-03-07 16:03:30 +0100

    getOperation -> getOperationMeta

--- app/com/lynxanalytics/biggraph/controllers/WorkspaceController.scala
+++ app/com/lynxanalytics/biggraph/controllers/WorkspaceController.scala
@@ -9,7 +9,7 @@ import com.lynxanalytics.biggraph.serving
 case class GetWorkspaceRequest(name: String)
 case class SetWorkspaceRequest(name: String, workspace: Workspace)
 case class GetOutputRequest(workspace: String, output: BoxOutput)
-case class GetOperationRequest(workspace: String, box: String)
+case class GetOperationMetaRequest(workspace: String, box: String)
 case class GetOutputResponse(kind: String, project: Option[FEProject] = None)
 case class CreateWorkspaceRequest(name: String, privacy: String)
 case class BoxCatalogResponse(boxes: List[BoxMetadata])
@@ -75,7 +75,7 @@ class WorkspaceController(env: SparkFreeEnvironment) {
     BoxCatalogResponse(ops.operationIds.toList.map(ops.getBoxMetadata(_)))
   }
 
-  def getOperation(user: serving.User, request: GetOperationRequest): FEOperationMeta = {
+  def getOperationMeta(user: serving.User, request: GetOperationMetaRequest): FEOperationMeta = {
     val ws = getWorkspaceByName(user, request.workspace)
     val op = ws.getOperation(user, ops, request.box)
     op.toFE
