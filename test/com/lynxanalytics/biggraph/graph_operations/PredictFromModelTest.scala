cd790cdd543130578f7eb62999c0980ac2a913ef
Hanna Gábor <gahanna999@gmail.com>
2017-03-08 12:04:32 +0100

    Add new test.

--- test/com/lynxanalytics/biggraph/graph_operations/PredictFromModelTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/PredictFromModelTest.scala
@@ -19,7 +19,7 @@ class PredictFromModelTest extends ModelTestBase {
     assertRoughlyEquals(age, 15, 1)
   }
 
-  test("test types of linear regression") {
+  test("test different types of linear regression") {
     checkModel("Linear regression")
     checkModel("Ridge regression")
     checkModel("Lasso")
@@ -29,7 +29,7 @@ class PredictFromModelTest extends ModelTestBase {
     val m = model(
       method = "Decision tree regression",
       labelName = "length of the walk",
-      label = Map(0 -> 1, 1 -> 0, 2 -> 0, 3 -> 7, 4 -> 1, 5 -> 0, 6 -> 1, 7 -> 7),
+      label = Map(0 -> 1, 1 -> 0, 2 -> 0, 3 -> 7, 4 -> 1, 5 -> 0, 6 -> 1, 7 -> 6),
       featureNames = List("temperature", "rain"),
       attrs = Seq(Map(0 -> -15, 1 -> 20, 2 -> -10, 3 -> 20, 4 -> 35, 5 -> 40, 6 -> -15, 7 -> -15),
         Map(0 -> 0, 1 -> 1, 2 -> 1, 3 -> 0, 4 -> 0, 5 -> 1, 6 -> 0, 7 -> 0)),
@@ -40,8 +40,21 @@ class PredictFromModelTest extends ModelTestBase {
       // the cold weather.
       graph(8))
 
-    val s = m.value.statistics.get
-    println(s)
-    // assert(s ==
+    val g = graph(numVertices = 6)
+    val attrs = Seq(Map(0 -> 20.0, 1 -> 42.0, 2 -> 38.0, 3 -> -16.0, 4 -> -20.0, 5 -> 20.0),
+      Map(0 -> 0.0, 1 -> 1.0, 2 -> 0.0, 3 -> 0.0, 4 -> 1.0, 5 -> 1.0))
+    val features = attrs.map(attr => {
+      AddVertexAttribute.run[Double](g.vs, attr)
+    })
+    val op = PredictFromModel(2)
+    val result = op(op.features, features)(op.model, m).result
+    val prediction = result.prediction.rdd.values.collect
+    assert(prediction.size == 6)
+    assert(prediction(0) == 7.0)
+    assert(prediction(1) == 0.0)
+    assert(prediction(2) == 1.0)
+    assert(prediction(3) > 2.66 && prediction(3) < 2.67)
+    assert(prediction(4) == 0.0)
+    assert(prediction(5) == 0.0)
   }
 }
