857585f27350aa347d7d2fb5da01619fbf1e8422
gsvigruha <gsvigruha@users.noreply.github.com>
2017-03-10 23:55:26 +0800

    Migrate tests related to visualization backend.

--- test/com/lynxanalytics/biggraph/graph_operations/TripletAttributesTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/TripletAttributesTest.scala
@@ -16,4 +16,24 @@ class TripletAttributesTest extends FunSuite with TestGraphOp {
     val res = op(op.mapping, mapped.dstEdges)(op.original, g.age)(op.target, g.edges).result
     assert(res.mappedAttribute.rdd.collect.toMap == Map(0 -> 18.2, 1 -> 20.3, 2 -> 20.3, 3 -> 18.2))
   }
+
+  test("edges and neighbors of example graph") {
+    val g = ExampleGraph()().result
+    val mapping = EdgeAndNeighborMapping()
+    val mapped = mapping(mapping.edges, g.edges).result
+    assert(mapped.srcEdges.rdd.mapValues(_.map((e, n) => (e, n)).toSet).collect.toMap ==
+      Map(0 -> Set((0, 1)), 1 -> Set((1, 0)), 2 -> Set((2, 0), (3, 1)), 3 -> Set()))
+    assert(mapped.dstEdges.rdd.mapValues(_.map((e, n) => (e, n)).toSet).collect.toMap ==
+      Map(0 -> Set((1, 1), (2, 2)), 1 -> Set((0, 0), (3, 2)), 2 -> Set(), 3 -> Set()))
+    val res1 = {
+      val op = EdgesForVerticesFromEdgesAndNeighbors(Set(2), None, maxNumEdges = Int.MaxValue)
+      op(op.mapping, mapped.srcEdges).result
+    }
+    assert(res1.edges.value == Some(Vector((2, Edge(2, 0)), (3, Edge(2, 1)))))
+    val res2 = {
+      val op = EdgesForVerticesFromEdgesAndNeighbors(Set(2), Some(Set(1)), maxNumEdges = Int.MaxValue)
+      op(op.mapping, mapped.srcEdges).result
+    }
+    assert(res2.edges.value == Some(Vector((3, Edge(2, 1)))))
+  }
 }
