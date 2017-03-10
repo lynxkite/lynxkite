4497256e39552734aaf26fd7ff0388835e694cbe
Daniel Darabos <darabos.daniel@gmail.com>
2017-03-10 11:06:17 +0100

    Function for creating ID for HTML.

--- app/com/lynxanalytics/biggraph/HelpInventory.scala
+++ app/com/lynxanalytics/biggraph/HelpInventory.scala
@@ -1,14 +1,14 @@
 // Writes to a file a list of identifiers that must exist in the help pages.
 package com.lynxanalytics.biggraph
 
+import com.lynxanalytics.biggraph.controllers.Operation
 import com.lynxanalytics.biggraph.frontend_operations.Operations
 
 object HelpInventory extends App {
   val hiddenOps = Set("create-enhanced-example-graph")
   val ops = new Operations(null)
   val fos = new java.io.FileOutputStream(args.head)
-  def normalize(name: String) = name.toLowerCase.replaceAll("\\W+", "-").replaceFirst("-+$", "")
-  for (op <- ops.operationIds.map(normalize).sorted) {
+  for (op <- ops.operationIds.map(Operation.htmlID).sorted) {
     if (!hiddenOps.contains(op)) {
       fos.write(op.getBytes("UTF-8"))
       fos.write('\n')
