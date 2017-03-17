76d83dc7aec32206395f42715c926fce0c795fca
Gabor Olah <gabor.olah@lynxanalytics.com>
2017-03-17 12:20:10 +0100

    Preserve old belongsTo for segmentations

--- app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
+++ app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
@@ -1973,6 +1973,7 @@ class ProjectOperations(env: SparkFreeEnvironment) extends OperationRegistry {
       val oldEdges = project.edgeBundle
       val oldEAttrs = project.edgeAttributes.toMap
       val oldSegmentations = project.viewer.segmentationMap
+      val oldBelongsTo = if (project.isSegmentation) project.asSegmentation.belongsTo else null
       project.setVertexSet(m.segments, idAttr = "id")
       for ((name, segViewer) <- oldSegmentations) {
         val seg = project.segmentation(name)
@@ -1981,6 +1982,13 @@ class ProjectOperations(env: SparkFreeEnvironment) extends OperationRegistry {
         seg.belongsTo = op(
           op.srcMapping, m.belongsTo)(
             op.edges, seg.belongsTo).result.induced
+      }
+      if (project.isSegmentation) {
+        val seg = project.asSegmentation
+        val op = graph_operations.InducedEdgeBundle(induceSrc = false)
+        seg.belongsTo = op(
+          op.dstMapping, m.belongsTo)(
+            op.edges, oldBelongsTo).result.induced
       }
       for ((attr, choice) <- parseAggregateParams(params)) {
         val result = aggregateViaConnection(
