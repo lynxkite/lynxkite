16ff055ad60582aff23277774f3bf23d8351a262
Matyas Korom <korommatyi@users.noreply.github.com>
2017-03-20 14:47:18 +0100

    Add protractor test

--- web/test/tests/example-graph-test.js
+++ web/test/tests/example-graph-test.js
@@ -33,7 +33,7 @@ module.exports = function(fw) {
 
   fw.transitionTest(
     'test-example workspace with example graph',
-    'test-example workspace with two connected boxes',
+    'test-example workspace with two connected and one independent boxes',
     function() {
       var exampleGraph = lib.workspace.getBox(0);
       lib.workspace.addBox('add reversed edges', 100, 200);
@@ -41,21 +41,43 @@ module.exports = function(fw) {
       lib.workspace.connectBoxes(
           exampleGraph, 'project',
           reversedEdges, 'project');
+      lib.workspace.addBox('create example graph', 350, 100);
     },
     function() {
     });
 
   fw.transitionTest(
-    'test-example workspace with two connected boxes',
-    'has the proper vertex count',
+    'test-example workspace with two connected and one independent boxes',
+    'test-example workspace with reverse edges state selected',
     function() {
       var reversedEdges = lib.workspace.getBox(1);
       lib.workspace.getOutputPlug(reversedEdges, 'project').click();
+    },
+    function() {
+    });
+  
+  fw.statePreservingTest(
+    'test-example workspace with reverse edges state selected',
+    'has the proper vertex count',
+    function() {
       expect(lib.state.vertexCount()).toEqual(4);
       expect(lib.state.edgeCount()).toEqual(8);
       expect(lib.state.attributeCount()).toEqual(8);
-    },
+    });
+
+  fw.statePreservingTest(
+    'test-example workspace with reverse edges state selected',
+    'states has correct fill color',
     function() {
+      let firstExampleGraph = lib.workspace.getBox(0);
+      let outPlugFirstEG = lib.workspace.getOutputPlug(firstExampleGraph, 'project');
+      let reversedEdges = lib.workspace.getBox(1);
+      let outPlugReversedEdges = lib.workspace.getOutputPlug(reversedEdges, 'project');
+      let secondExampleGraph = lib.workspace.getBox(2);
+      let outPlugSecondEG = lib.workspace.getOutputPlug(secondExampleGraph, 'project');
+      expect(outPlugFirstEG.getAttribute('fill')).not.toBeNull();
+      expect(outPlugReversedEdges.getAttribute('fill')).not.toBeNull();
+      expect(outPlugSecondEG.getAttribute('fill')).toBeNull();
     });
 
 };
