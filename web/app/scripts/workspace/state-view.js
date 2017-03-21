85368c7c48a1f4a4fe9834f81746962fb015c250
Gabor Feher <gabor.feher@lynxanalytics.com>
2017-03-21 20:59:18 +0100

    move code into project-state-view

--- web/app/scripts/workspace/state-view.js
+++ web/app/scripts/workspace/state-view.js
@@ -3,41 +3,12 @@
 // Viewer of a state at an output of a box.
 
 angular.module('biggraph')
- .directive('stateView', function(side, util) {
+ .directive('stateView', function() {
     return {
       restrict: 'E',
       templateUrl: 'scripts/workspace/state-view.html',
       scope: {
         state: '='
-      },
-      link: function(scope) {
-        scope.sides = [];
-
-        util.deepWatch(scope, 'state', function() {
-          scope.sides = [];
-          scope.left = new side.Side(scope.sides, 'left', scope.state);
-          scope.right = new side.Side(scope.sides, 'right', scope.state);
-          scope.sides.push(scope.left);
-          scope.sides.push(scope.right);
-
-          scope.sides[0].state.projectPath = '';
-          scope.sides[0].reload();
-        });
-
-        scope.$watch(
-          'left.project.$resolved',
-          function(loaded) {
-            if (loaded) {
-              scope.left.onProjectLoaded();
-            }
-          });
-        scope.$watch(
-          'right.project.$resolved',
-          function(loaded) {
-            if (loaded) {
-              scope.right.onProjectLoaded();
-            }
-          });
-    }
-  };
+      }
+    };
 });
