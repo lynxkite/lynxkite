f7310e481cf4aa6b2801d2c1537b7699e73d11a9
Gabor Feher <feherga@gmail.com>
2017-03-23 10:34:03 +0100

    improve comments

--- web/app/scripts/workspace/workspace-main.js
+++ web/app/scripts/workspace/workspace-main.js
@@ -1,4 +1,12 @@
 // The '/workspace/WorkSpaceName' page.
+//
+// The loaded workspace is stored in a workspaceState that is
+// wrapped in a workspaceManager. The state handles lower-level
+// data structure issues, the manager handles transient GUI
+// state, interaction with GUI and the backend.
+//
+// The components on this page (workspace-drawing-board, box-editor
+// state-view) are all connected to the manager.
 'use strict';
 
 angular.module('biggraph')
