1ec2c9683459e1f874abc4b5c85def31a4b70835
hiuy <yenonn.hiu@lynxanalytics.com>
2017-03-27 20:50:34 +0800

    1. Making send_ses_email as a global function 2. Making get_running_instances as a global function

--- tools/print_ec2.py
+++ tools/print_ec2.py
@@ -40,93 +40,79 @@ class ReportTable():
         return self.rows
 
 
-class Ec2():
+def get_running_instances():
+    table = ReportTable()
+    client = boto3.client('ec2')
+    regions = client.describe_regions()['Regions']
+    for region in regions:
+        region_name = region['RegionName']
+        ec2 = boto3.resource('ec2', region_name=region['RegionName'])
+        # Get information for all running instances
+        running_instances = ec2.instances.filter(Filters=[{
+                                                          'Name':
+                                                              'instance-state-name',
+                                                          'Values': ['running']}])
+        for instance in running_instances:
+            instanceinfo = []
+            tags = {t['Key'].lower(): t['Value'] for t in instance.tags}
+            instanceinfo.append(region_name.upper())
+            instanceinfo.append(tags.get('name', 'undefined'))
+            instanceinfo.append(instance.instance_type.upper())
+            instanceinfo.append(instance.state['Name'].upper())
+            instanceinfo.append(instance.private_ip_address)
+            instanceinfo.append(instance.public_ip_address)
+            instanceinfo.append(instance.launch_time)
+            instanceinfo.append(tags.get('owner', 'undefined'))
+            instanceinfo.append(tags.get('expiry', 'undefined'))
 
-    def __init__(self):
-        self.client = boto3.client('ec2')
-        self.regions = self.client.describe_regions()['Regions']
-
-    def get_running_instances(self):
-        table = ReportTable()
-        for region in self.regions:
-            region_name = region['RegionName']
-            ec2 = boto3.resource('ec2', region_name=region['RegionName'])
-            # Get information for all running instances
-            running_instances = ec2.instances.filter(Filters=[{
-                'Name': 'instance-state-name',
-                                      'Values': ['running']}])
-            for instance in running_instances:
-                instanceinfo = []
-                tags = {t['Key'].lower():t['Value'] for t in instance.tags}
-                instanceinfo.append(region_name.upper())
-                instanceinfo.append(tags.get('name', 'undefined'))
-                instanceinfo.append(instance.instance_type.upper())
-                instanceinfo.append(instance.state['Name'].upper())
-                instanceinfo.append(instance.private_ip_address)
-                instanceinfo.append(instance.public_ip_address)
-                instanceinfo.append(instance.launch_time)
-                instanceinfo.append(tags.get('owner','undefined'))
-                instanceinfo.append(tags.get('expiry','undefined'))
-
-                table.add_row(instanceinfo)
-        return table
+            table.add_row(instanceinfo)
+    return table
 
 
-class SesEmail():
+def send_ses_email(
+        to_address=None, from_address=None, subject=None, html_body=None):
 
-    def __init__(self):
-        self.to = None
-        self.from_addr = None
-        self.subject = None
-        self._html = None
-        self._format = 'html'
+    email_client = boto3.client('ses')
+    email_info = {"to_address": "ec2-users@lynxanalytics.com",
+                  "from_address": "ec2-users@lynxanalytics.com",
+                  "subject": "Not filled subject",
+                  "html_body": None}
 
-    def send_ses_email(
-            self, to_address=None, from_address=None, subject=None, html_body=None):
-        if not to_address:
-            self.to = ["ec2-users@lynxanalytics.com"]
-        else:
-            self.to = [to_address]
+    if to_address:
+        email_info["to_address"] = [to_address]
 
-        if not subject:
-            self.subject = "Not filled subject"
-        else:
-            self.subject = subject
+    if from_address:
+        email_info["from_address"] = from_address
 
-        if not from_address:
-            self.from_addr = 'ec2-users@lynxanalytics.com'
-        else:
-            self.from_addr = from_address
+    if subject:
+        email_info["subject"] = subject
 
-        if not html_body:
-            if not self._html:
-                raise Exception('You must provide a html body')
-        else:
-            self._html = html_body
+    if not html_body:
+        if not email_info["html_body"]:
+            raise Exception('You must provide a html body')
+    else:
+        email_info["html_body"] = html_body
 
-        email_client = boto3.client('ses')
-
-        return email_client.send_email(
-            Source=self.from_addr,
-                    Destination={
-                        'ToAddresses': self.to,
-                    },
-                    Message={
-                        'Subject': {
-                            'Data': self.subject
-                        },
-                        'Body': {
-                            'Html': {
-                                'Data': self._html
-                            }
-                        },
-                    },
-        )
+    return email_client.send_email(
+        Source=email_info["from_address"],
+        Destination={
+            'ToAddresses': email_info["to_address"],
+        },
+        Message={
+            'Subject': {
+                'Data': email_info["subject"],
+            },
+          'Body': {
+              'Html': {
+                  'Data': email_info["html_body"],
+              }
+            },
+        },
+    )
 
 
 if __name__ == '__main__':
-    ec2 = Ec2()
-    table = ec2.get_running_instances()
+    table = get_running_instances()
 
     parser = argparse.ArgumentParser(
         description='Sends report of active AWS instances')
@@ -140,8 +126,7 @@ if __name__ == '__main__':
     if args.ascii:
         table.print_table_ascii()
     else:
-        email = SesEmail()
-        email.send_ses_email(
+        send_ses_email(
             to_address="ec2-users@lynxanalytics.com",
             from_address="ec2-users@lynxanalytics.com",
             subject='AWS Instances Report - %s instances running' % table.get_num_rows(
