e7b066d2a141fc953f7edfa671ebc921d5552458
Daniel Darabos <darabos.daniel@gmail.com>
2017-03-28 16:43:35 +0200

    Move SQLHelper.DataFrameOutput to TableAsAttributes.Output.

--- app/com/lynxanalytics/biggraph/graph_operations/TableToAttributes.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/TableToAttributes.scala
@@ -2,8 +2,11 @@
 package com.lynxanalytics.biggraph.graph_operations
 
 import org.apache.spark
+import org.apache.spark.sql.types
+import scala.reflect.runtime.universe._
 
 import com.lynxanalytics.biggraph.graph_api._
+import com.lynxanalytics.biggraph.protection.Limitations
 import com.lynxanalytics.biggraph.spark_util.SQLHelper
 
 object TableToAttributes extends OpFromJson {
@@ -12,14 +15,63 @@ object TableToAttributes extends OpFromJson {
   }
   def fromJson(j: JsValue) = TableToAttributes()
 
-  def run(table: Table)(implicit m: MetaGraphManager): SQLHelper.DataFrameOutput = {
+  def run(table: Table)(implicit m: MetaGraphManager): Output = {
     import Scripting._
     val op = TableToAttributes()
     op(op.t, table).result
   }
+
+  private def toSymbol(field: types.StructField) = Symbol("imported_column_" + field.name)
+
+  private def toNumberedLines(
+    dataFrame: spark.sql.DataFrame, rc: RuntimeContext): AttributeRDD[Seq[Any]] = {
+    val numRows = dataFrame.count()
+    val maxRows = Limitations.maxImportedLines
+    if (maxRows >= 0) {
+      if (numRows > maxRows) {
+        throw new AssertionError(
+          s"Can't import $numRows lines as your licence only allows $maxRows.")
+      }
+    }
+    val seqRDD = SQLHelper.toSeqRDD(dataFrame)
+    val partitioner = rc.partitionerForNRows(numRows)
+    import com.lynxanalytics.biggraph.spark_util.Implicits._
+    seqRDD.randomNumbered(partitioner)
+  }
+
+  class Output(schema: types.StructType)(implicit instance: MetaGraphOperationInstance)
+      extends MagicOutput(instance) {
+    // Methods for listing the output entities for metagraph building purposes.
+    private def attributeFromTypeTag[T: TypeTag](
+      ids: => EntityContainer[VertexSet], name: scala.Symbol): EntityContainer[Attribute[T]] =
+      vertexAttribute[T](ids, name)
+
+    private def attributeFromField(
+      ids: => EntityContainer[VertexSet],
+      field: types.StructField): EntityContainer[Attribute[_]] = {
+      attributeFromTypeTag(ids, toSymbol(field))(SQLHelper.typeTagFromDataType(field.dataType))
+    }
+
+    val ids = vertexSet
+    val columns = schema.map {
+      field => field.name -> attributeFromField(ids, field)
+    }.toMap
+
+    // Methods for populating this output instance with computed output RDDs.
+    def populateOutput(
+      rc: RuntimeContext,
+      schema: types.StructType,
+      dataFrame: spark.sql.DataFrame) {
+      val entities = this.columns.values.map(_.entity)
+      val entitiesByName = entities.map(e => (e.name, e): (scala.Symbol, Attribute[_])).toMap
+      val inOrder = schema.map(f => entitiesByName(toSymbol(f)))
+
+      rc.ioContext.writeAttributes(inOrder, toNumberedLines(dataFrame, rc))
+    }
+  }
 }
 import TableToAttributes._
-case class TableToAttributes() extends TypedMetaGraphOp[Input, SQLHelper.DataFrameOutput] {
+case class TableToAttributes() extends TypedMetaGraphOp[Input, Output] {
   override val isHeavy = true
   override val hasCustomSaving = true // Single-pass import.
   @transient override lazy val inputs = new Input()
@@ -27,11 +79,11 @@ case class TableToAttributes() extends TypedMetaGraphOp[Input, SQLHelper.DataFra
   def outputMeta(instance: MetaGraphOperationInstance) = {
     implicit val inst = instance
     val schema = inputs.t.entity.schema
-    new SQLHelper.DataFrameOutput(schema)(instance)
+    new Output(schema)(instance)
   }
 
   def execute(inputDatas: DataSet,
-              o: SQLHelper.DataFrameOutput,
+              o: Output,
               output: OutputBuilder,
               rc: RuntimeContext): Unit = {
     implicit val id = inputDatas
