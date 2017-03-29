b47a692a5967a0768ddbeb3f8f0df3fd8bc85c54
Gabor Olah <gabor.olah@lynxanalytics.com>
2017-03-29 14:26:32 +0200

    Temporary commit so that I can work at home

--- app/com/lynxanalytics/biggraph/ScalaScript.scala
+++ app/com/lynxanalytics/biggraph/ScalaScript.scala
@@ -1,6 +1,6 @@
 package com.lynxanalytics.biggraph
 
-import java.security.Permission
+import java.security.{ AccessControlContext, AccessController, Permission }
 import javax.script._
 
 import com.lynxanalytics.biggraph.graph_api.{ SafeFuture, ThreadUtil }
@@ -8,23 +8,50 @@ import com.lynxanalytics.biggraph.graph_api.{ SafeFuture, ThreadUtil }
 import scala.concurrent.duration.Duration
 import scala.tools.nsc.interpreter.IMain
 
-class ScalaScriptSecurityManager(RestrictedThreadName: String) extends SecurityManager {
-  override def checkPermission(permission: Permission): Unit = {
-    if (Thread.currentThread().getName.startsWith(RestrictedThreadName)) {
+class ScalaScriptSecurityManager extends SecurityManager {
+  val logs = scala.collection.mutable.Queue[(String, Long, Long)]()
+  var restrictedThreadID: Long = -1L
+
+  override def getThreadGroup: ThreadGroup = synchronized {
+    val name = Thread.currentThread.getName + "_" + " GET_THREAD_GROUP"
+    val q = (name, restrictedThreadID, Thread.currentThread().getId)
+    logs += q
+    if (Thread.currentThread().getId == restrictedThreadID) {
+      throw new java.security.AccessControlException("Access error: GET_THREAD_GROUP")
+    } else {
+      super.getThreadGroup
+    }
+  }
+  override def checkPermission(permission: Permission): Unit = synchronized {
+    val l = (Thread.currentThread().getName + "_" + permission.toString, restrictedThreadID, Thread.currentThread().getId)
+    logs += l
+    if (Thread.currentThread().getId == restrictedThreadID) {
       super.checkPermission(permission)
+      //     val q = ("ERROR" + Thread.currentThread().getName + "_" + permission.toString, restrictedThreadID, Thread.currentThread().getId)
+      //      logs += q
+      //      Thread.dumpStack()
+      //      throw new java.security.AccessControlException(s"Access error: $permission")
+    }
+  }
+  def disableCurrentThread(): Unit = synchronized {
+    if (restrictedThreadID != -1L) {
+      throw new java.security.AccessControlException(s"RestrictedThreadID access")
+    }
+    restrictedThreadID = Thread.currentThread().getId
+  }
+  def dumpLogs(filter: String = ""): Unit = synchronized {
+    for (v <- logs) {
+      if (v._1 contains (filter)) {
+        println(v)
+      }
     }
   }
 }
 
 object ScalaScript {
-  private val restrictedThreadName = "RestrictedScalaScript"
-  assert(System.getSecurityManager == null)
-  private val sm = new ScalaScriptSecurityManager(restrictedThreadName)
-  System.setSecurityManager(sm)
-
   private val engine = new ScriptEngineManager().getEngineByName("scala").asInstanceOf[IMain]
   private val timeout = Duration(5L, "second")
-  private val executionContext = ThreadUtil.limitedExecutionContext(restrictedThreadName, 1)
+  private val executionContext = ThreadUtil.limitedExecutionContext("RestrictedScalaScript", 1)
 
   private val settings = engine.settings
   settings.usejavacp.value = true
@@ -32,13 +59,19 @@ object ScalaScript {
   def run(code: String): String = {
     // https://issues.scala-lang.org/browse/SI-8521
     val cl = Thread.currentThread().getContextClassLoader
+    val sm = new ScalaScriptSecurityManager
+    assert(System.getSecurityManager == null)
+    System.setSecurityManager(sm)
     val result = try {
       val compiledCode = engine.compile(code)
       SafeFuture {
+        sm.disableCurrentThread()
         compiledCode.eval().toString
       }(executionContext).awaitResult(timeout)
     } finally {
       Thread.currentThread().setContextClassLoader(cl)
+      System.setSecurityManager(null)
+      sm.dumpLogs("")
     }
     result
   }
--- test/com/lynxanalytics/biggraph/ScalaScriptTest.scala
+++ test/com/lynxanalytics/biggraph/ScalaScriptTest.scala
@@ -4,10 +4,31 @@ import org.scalatest.FunSuite
 import java.net.URL
 import java.security.AccessControlException
 
+import scala.reflect.Manifest
+
 class ScalaScriptTest extends FunSuite {
 
-  test("Can't do infinite loop") {
-    intercept[java.util.concurrent.TimeoutException] {
+  def hello(): Unit = {
+    class EvilRun extends Runnable {
+      override def run(): Unit = {
+        val name = Thread.currentThread.getName
+        val id = Thread.currentThread.getId.toString
+        println("Evil " + name + " " + id)
+      }
+    }
+    val r = new EvilRun()
+    val t = new java.lang.Thread(r)
+    t.start()
+  }
+
+  def expect[T <: AnyRef](f: => Any)(implicit manifest: Manifest[T]): Unit = {
+    val e = intercept[T] {
+      f
+    }
+    println(e.asInstanceOf[Throwable].getMessage)
+  }
+  ignore("Can't do infinite loop") {
+    expect[java.util.concurrent.TimeoutException] {
       ScalaScript.run(
         """
         Thread.sleep(10000L);
@@ -15,10 +36,11 @@ class ScalaScriptTest extends FunSuite {
     }
   }
 
-  test("Simple arithmetic works") {
+  ignore("Simple arithmetic works") {
     assert(ScalaScript.run("5 * 5 + 1") == "26")
   }
 
+  /*
   test("Security manager disables file access") {
     val testFile: URL = getClass.getResource("/graph_api/permission_check.txt")
     assert(scala.io.Source.fromFile(testFile.getFile).mkString
@@ -28,5 +50,51 @@ class ScalaScriptTest extends FunSuite {
       ScalaScript.run(s"""scala.io.Source.fromFile("${path}").mkString""")
     }
   }
+  test("Can't replace the security manager") {
+    expect[AccessControlException] {
+      ScalaScript.run(s"""System.setSecurityManager(null)""")
+    }
+  }
 
+  test("Can't tamper with our security manager") {
+    ScalaScript.run(
+      """
+        val s = System.getSecurityManager.asInstanceOf[com.lynxanalytics.biggraph.ScalaScriptSecurityManager]
+        s.disableCurrentThread
+      """)
+  }
+
+  test("Can create a class") {
+    ScalaScript.run(
+      """
+           class C {
+             def run(): Unit = {
+               val name = Thread.currentThread.getName
+               val id = Thread.currentThread.getId.toString
+               println("Evil " + name + " " + id)
+             }
+           }
+             val r = new C()
+             r.run()
+             """)
+  }
+*/
+  test("Can't create a new thread") {
+    //    expect[java.util.concurrent.ExecutionException] {
+    ScalaScript.run(
+      s"""
+           class EvilRun extends Runnable {
+             override def run(): Unit = {
+               val name = Thread.currentThread.getName
+               val id = Thread.currentThread.getId.toString
+               println("Evil " + name + " " + id)
+             }
+           }
+             val r = new EvilRun()
+             val t = new java.lang.Thread(r)
+             t.start()
+
+           """)
+    //    }
+  }
 }
