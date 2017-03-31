979025e056660204cf02cc5b922149621379c0d1
huncros <huncros@gmail.com>
2017-03-31 19:03:10 +0200

    NoInputOutputOperations in separate file

--- app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
+++ app/com/lynxanalytics/biggraph/frontend_operations/Operations.scala
@@ -21,32 +21,6 @@ class Operations(env: SparkFreeEnvironment) extends OperationRepository(env) {
       new ImportOperations(env).operations.toMap
 }
 
-class NoInputOutputOperations(env: SparkFreeEnvironment) extends OperationRegistry {
-  implicit lazy val manager = env.metaGraphManager
-  import Operation.Category
-  import Operation.Context
-  import Operation.Implicits._
-
-  import OperationParams._
-
-  def register(
-    id: String,
-    category: Category)(factory: Context => Operation): Unit = {
-    registerOp(id, category, List(), List(), factory)
-  }
-
-  //Categories
-  val BoxDecorators = Category("Box decorators", "black", icon = "kraken")
-
-  register("Add comment", BoxDecorators)(new DecoratorOperation(_) {
-    def parameters = List(
-      MultilineString("comment", "Comment")
-    )
-    def apply() = {}
-    def enabled = FEStatus.enabled
-  })
-}
-
 class ProjectOperations(env: SparkFreeEnvironment) extends OperationRegistry {
   implicit lazy val manager = env.metaGraphManager
   import Operation.Category
