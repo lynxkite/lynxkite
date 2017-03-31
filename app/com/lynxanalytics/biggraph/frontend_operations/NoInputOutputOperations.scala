802e189a9838880ac325bc9ca49b1e22f7b8bee4
huncros <huncros@gmail.com>
2017-03-31 19:03:10 +0200

    NoInputOutputOperations in separate file

---
+++ app/com/lynxanalytics/biggraph/frontend_operations/NoInputOutputOperations.scala
@@ -0,0 +1,30 @@
+// Frontend operations with no input and output.
+package com.lynxanalytics.biggraph.frontend_operations
+
+import com.lynxanalytics.biggraph.SparkFreeEnvironment
+import com.lynxanalytics.biggraph.controllers._
+
+class NoInputOutputOperations(env: SparkFreeEnvironment) extends OperationRegistry {
+  implicit lazy val manager = env.metaGraphManager
+  import Operation.Category
+  import Operation.Context
+  import OperationParams._
+
+  def register(
+    id: String,
+    category: Category)(factory: Context => Operation): Unit = {
+    registerOp(id, category, List(), List(), factory)
+  }
+
+  //Categories
+  val BoxDecorators = Category("Box decorators", "black", icon = "kraken")
+
+  register("Add comment", BoxDecorators)(new DecoratorOperation(_) {
+    def parameters = List(
+      MultilineString("comment", "Comment")
+    )
+    def apply() = {}
+    def enabled = FEStatus.enabled
+  })
+}
+
