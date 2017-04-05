ae43dece8bf29e99127978ab91a81f1aab2b9229
János Maginecz <wafleeee@gmail.com>
2017-04-05 16:19:11 +0200

    Restore 200 partiton test case

--- remote_api/python/tests/test_parquet_partitioning.py
+++ remote_api/python/tests/test_parquet_partitioning.py
@@ -8,7 +8,7 @@ import shutil
 
 class TestParquetPartitioning(unittest.TestCase):
 
-  def do_test_parquet_partitioning(self, partitions):
+  def do_test_parquet_partitioning(self, partitions, expected_partitions):
     path = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
     lk = lynx.LynxKite()
     p = lk.new_project()
@@ -19,17 +19,14 @@ class TestParquetPartitioning(unittest.TestCase):
     view = lk.sql(sql, p=p)
 
     data_path = "DATA$/" + path
-    if partitions != 200:
-      view.export_parquet(data_path, partitions)
-    else:
-      view.export_parquet(data_path)
+    view.export_parquet(data_path, partitions)
     # Check number of parquet files:
     resolved_path = lk.get_prefixed_path(data_path).resolved
     # Cut file: from the beginning
     raw_path = resolved_path[5:]
     files = os.listdir(raw_path)
     num_files = len(list(filter(lambda file: file.endswith('.snappy.parquet'), files)))
-    self.assertEqual(num_files, partitions)
+    self.assertEqual(num_files, expected_partitions)
 
     # Check data integrity
     view2 = lk.import_parquet(data_path)
@@ -41,8 +38,10 @@ class TestParquetPartitioning(unittest.TestCase):
     shutil.rmtree(raw_path)
 
   def test_parquet_partitioning(self):
-    self.do_test_parquet_partitioning(1)
-    self.do_test_parquet_partitioning(150)
+    self.do_test_parquet_partitioning(None, 1)
+    self.do_test_parquet_partitioning(150, 150)
+    self.do_test_parquet_partitioning(200, 200)
+    self.do_test_parquet_partitioning(230, 230)
 
 if __name__ == '__main__':
   unittest.main()
