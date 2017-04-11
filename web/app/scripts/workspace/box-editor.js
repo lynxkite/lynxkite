66899c9737a24467af3c9ab08209d34d189e2575
Hanna Gábor <gahanna999@gmail.com>
2017-04-11 11:39:25 +0200

    Revert changes in box-editor.js

--- web/app/scripts/workspace/box-editor.js
+++ web/app/scripts/workspace/box-editor.js
@@ -12,16 +12,14 @@ angular.module('biggraph')
       },
       link: function(scope) {
         scope.$watch(
-            'workspace.selectedBoxIds',
+            'workspace.selectedBoxId',
             function() {
               if (!scope.workspace) {
                 return;
               }
               // Make a copy of the parameter values.
               scope.paramValues = Object.assign(
-                  {}, new Map(scope.workspace.selectedBoxes().map(function(box){
-                    return [box.instance.id, box.instance.parameters];
-                  })));
+                  {}, scope.workspace.selectedBox().instance.parameters);
               scope.loadBoxMeta();
             });
         // The metadata (param definition list) of the current box
@@ -44,59 +42,60 @@ angular.module('biggraph')
         scope.paramValues = {};
 
         scope.loadBoxMeta = function() {
-          if (!scope.workspace || !scope.workspace.selectedBoxIds) {
+          if (!scope.workspace || !scope.workspace.selectedBoxId) {
             return;
           }
-          // The below magic makes sure that the response
+          var box = scope.workspace.selectedBox();
+          // Checking currentRequest makes sure that the response
           // to the result of the latest getOperationMetaRequest
           // will be passed to scope.newOpSelected().
           var currentRequest;
-          var success = function(boxId, boxMeta) {
-            if (scope.lastRequest === currentRequest) {
-              scope.newOpSelected(boxMeta);
-            }
-          };
-          var error = function(){
-            if (scope.lastRequest === currentRequest) {
-              scope.newOpSelected(undefined);
-            }
-          };
-          for (var i = 0; i < scope.workspace.selectedBoxIds.length; i ++){
-            scope.lastRequest = currentRequest = util
-              .nocache(
-                '/ajax/getOperationMeta',
-                {
+          scope.lastRequest = currentRequest = util
+            .nocache(
+              '/ajax/getOperationMeta',
+              {
                   workspace: scope.workspace.name,
-                  box: scope.workspace.selectedBoxIds[i],
-                })
-                .then(success.bind(null,scope.workspace.selectedBoxIds[i]),error);
-          }};
+                  box: scope.workspace.selectedBoxId,
+              })
+            .then(
+              function success(boxMeta) {
+                if (scope.lastRequest === currentRequest) {
+                  scope.newOpSelected(box, boxMeta);
+                }
+              },
+              function error() {
+                if (scope.lastRequest === currentRequest) {
+                  scope.newOpSelected(undefined);
+                }
+              });
+        };
 
         // Invoked when the user selects a new operation and its
         // metadata is successfully downloaded.
-        scope.newOpSelected = function(boxId, boxMeta) {
+        scope.newOpSelected = function(box, boxMeta) {
+            scope.box = box;
             scope.boxMeta = boxMeta;
-            if (!scope.boxMeta) {
+            if (!box) {
               return;
             }
 
             // Populate parameter values.
             for (var i = 0; i < boxMeta.parameters.length; ++i) {
               var p = boxMeta.parameters[i];
-              if (scope.paramValues[boxId][p.id] !== undefined) {
+              if (scope.paramValues[p.id] !== undefined) {
                 // Parameter is set externally.
               } else if (p.options.length === 0) {
-                scope.paramValues[boxId][p.id] = p.defaultValue;
+                scope.paramValues[p.id] = p.defaultValue;
               } else if (p.multipleChoice) {
-                scope.paramValues[boxId][p.id] = '';
+                scope.paramValues[p.id] = '';
               } else {
-                scope.paramValues[boxId][p.id] = p.options[0].id;
+                scope.paramValues[p.id] = p.options[0].id;
               }
             }
         };
 
         scope.apply = function() {
-          scope.workspace.updateSelectedBoxes(scope.paramValues);
+          scope.workspace.updateSelectedBox(scope.paramValues);
         };
       },
     };
