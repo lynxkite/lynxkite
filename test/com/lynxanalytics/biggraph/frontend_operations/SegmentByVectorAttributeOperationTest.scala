7ba2f132c807b133dc570a502aae294840d6ab71
gsvigruha <gsvigruha@users.noreply.github.com>
2017-04-12 04:58:37 +0800

    Work around compiler issue.

--- test/com/lynxanalytics/biggraph/frontend_operations/SegmentByVectorAttributeOperationTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/SegmentByVectorAttributeOperationTest.scala
@@ -12,20 +12,20 @@ class SegmentByVectorAttributeOperationTest extends OperationsTestBase {
       "name" -> "segment",
       "attr" -> "vector"))
     val seg = project.segmentation("segment")
-    assert(
-      seg.vertexAttributes("label").runtimeSafeCast[Double].rdd.values.collect.toSet == Set(1, 2))
+    assert(seg.vertexAttributes("vector").runtimeSafeCast[Double].rdd.values.collect.toSet ==
+      Set(1, 2))
   }
 
   test("Segment by vector attribute - vector of strings") {
     run("Example Graph")
     run("Derived vertex attribute",
-      Map("type" -> "vector of doubles", "output" -> "vector", "expr" -> "[name]"))
+      Map("type" -> "vector of strings", "output" -> "vector", "expr" -> "[name]"))
 
     run("Segment by vector attribute", Map(
       "name" -> "segment",
       "attr" -> "vector"))
     val seg = project.segmentation("segment")
-    assert(seg.vertexAttributes("label").runtimeSafeCast[Double].rdd.values.collect.toSet ==
+    assert(seg.vertexAttributes("vector").runtimeSafeCast[String].rdd.values.collect.toSet ==
       Set("Adam", "Eve", "Bob", "Isolated Joe"))
   }
 }
