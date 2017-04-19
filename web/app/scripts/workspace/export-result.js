538d04f2df40cadb7b91b8c620b1220b2153f081
huncros <huncros@gmail.com>
2017-04-19 09:54:56 +0200

    deepWatch stateId on UI

--- web/app/scripts/workspace/export-result.js
+++ web/app/scripts/workspace/export-result.js
@@ -8,40 +8,41 @@ angular.module('biggraph')
       restrict: 'E',
       templateUrl: 'scripts/workspace/export-result.html',
       scope: {
-        workspace: '=',
+        stateId: '=',
       },
       link: function(scope) {
-         var exportResult = util.nocache(
+        util.deepWatch(scope, 'stateId', function() {
+         scope.exportResult = util.nocache(
                                'ajax/getExportResultOutput',
                                {
-                                 stateId: scope.workspace.selectedStateId,
+                                 stateId: scope.stateId,
                                });
-        exportResult.then(
-          function success(exportResult) {
-            scope.alreadyExported = (exportResult.computeProgress === 1.0) ? true : false;
 
-            scope.prettifyCamelCase = function(camelCase) {
-              // insert a space before all caps
-              var split = camelCase.replace(/([A-Z])/g, ' $1');
-              // uppercase the first character
-              var prettified = split.replace(/^./, function(str) {
-                return str.toUpperCase();
-                });
-              return prettified;
-            };
-            scope.fileMetaData =
-              (exportResult.computedValue) ? JSON.parse(exportResult.computedValue.string) : {};
+         scope.exportResult.then(function success(exportResult) {
+           scope.alreadyExported = (exportResult.computeProgress === 1.0) ? true : false;
+           scope.fileMetaData =
+             (exportResult.computedValue) ? JSON.parse(exportResult.computedValue.string) : {};
+             });
+        });
 
-            scope.export = function() {
-              var scalarValue = util.lazyFetchScalarValue(exportResult, true);
+        scope.export = function() {
+            var scalarValue = util.lazyFetchScalarValue(scope.exportResult, true);
+            scope.alreadyExported = true;
+            scalarValue.value.then(function success(value) {
+              scope.fileMetaData = JSON.parse(value.string);
+            });
 
-              scalarValue.value.then(function(value) {
-                scope.alreadyExported = true;
-                scope.fileMetaData = JSON.parse(value.string);
-              });
+          };
 
-            };
-          });
+        scope.prettifyCamelCase = function(camelCase) {
+          // insert a space before all caps
+          var split = camelCase.replace(/([A-Z])/g, ' $1');
+          // uppercase the first character
+          var prettified = split.replace(/^./, function(str) {
+            return str.toUpperCase();
+            });
+          return prettified;
+        };
       },
     };
 });
