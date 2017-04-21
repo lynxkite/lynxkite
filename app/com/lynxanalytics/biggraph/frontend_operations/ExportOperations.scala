276ae2f3bbb160f56493b0014547b9c8b1d2d9e7
huncros <huncros@gmail.com>
2017-04-21 14:07:56 +0200

    No code repetition for registering structured export

--- app/com/lynxanalytics/biggraph/frontend_operations/ExportOperations.scala
+++ app/com/lynxanalytics/biggraph/frontend_operations/ExportOperations.scala
@@ -1,25 +1,15 @@
 package com.lynxanalytics.biggraph.frontend_operations
 
 import com.lynxanalytics.biggraph.SparkFreeEnvironment
-import com.lynxanalytics.biggraph.JavaScript
-import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 import com.lynxanalytics.biggraph.graph_operations
-import com.lynxanalytics.biggraph.graph_util
-import com.lynxanalytics.biggraph.graph_util.JDBCUtil
-import com.lynxanalytics.biggraph.graph_util.Scripting._
 import com.lynxanalytics.biggraph.controllers._
-import com.lynxanalytics.biggraph.graph_util.LoggedEnvironment
-import com.lynxanalytics.biggraph.model
-import com.lynxanalytics.biggraph.serving.FrontendJson
-import play.api.libs.json
 
 class ExportOperations(env: SparkFreeEnvironment) extends OperationRegistry {
   implicit lazy val manager = env.metaGraphManager
 
   import Operation.Category
   import Operation.Context
-  import Operation.Implicits._
 
   private val tableInput = "table"
   private val exportResultConnection = TypedConnection("result", BoxOutputKind.ExportResult)
@@ -30,7 +20,6 @@ class ExportOperations(env: SparkFreeEnvironment) extends OperationRegistry {
   }
 
   import OperationParams._
-  import org.apache.spark
 
   register("Export to CSV")(new ExportOperationToFile(_) {
     lazy val parameters = List(
@@ -52,46 +41,25 @@ class ExportOperations(env: SparkFreeEnvironment) extends OperationRegistry {
     }
   })
 
-  register("Export to JSON")(new ExportOperationToFile(_) {
-    lazy val parameters = List(
-      Param("path", "Path", defaultValue = "<download>"),
-      NonNegInt("version", "Version", default = 0)
-    )
+  registerExportToStructuredFile("Export to JSON")("json")
+  registerExportToStructuredFile("Export to Parquet")("parquet")
+  registerExportToStructuredFile("Export to ORC")("orc")
 
-    def exportResult() = {
-      val op = graph_operations.ExportTableToStructuredFile(
-        params("path"), "json", params("version").toInt
+  def registerExportToStructuredFile(id: String)(format: String) {
+    register(id)(new ExportOperationToFile(_) {
+      lazy val parameters = List(
+        Param("path", "Path", defaultValue = "<download>"),
+        NonNegInt("version", "Version", default = 0)
       )
-      op(op.t, table).result.exportResult
-    }
-  })
 
-  register("Export to Parquet")(new ExportOperationToFile(_) {
-    lazy val parameters = List(
-      Param("path", "Path", defaultValue = "<download>"),
-      NonNegInt("version", "Version", default = 0)
-    )
+      def exportResult = {
+        val op = graph_operations.ExportTableToStructuredFile(
+          params("path"), format, params("version").toInt
+        )
+        op(op.t, table).result.exportResult
+      }
+    })
+  }
 
-    def exportResult = {
-      val op = graph_operations.ExportTableToStructuredFile(
-        params("path"), "parquet", params("version").toInt
-      )
-      op(op.t, table).result.exportResult
-    }
-  })
-
-  register("Export to ORC")(new ExportOperationToFile(_) {
-    lazy val parameters = List(
-      Param("path", "Path", defaultValue = "<download>"),
-      NonNegInt("version", "Version", default = 0)
-    )
-
-    def exportResult = {
-      val op = graph_operations.ExportTableToStructuredFile(
-        params("path"), "orc", params("version").toInt
-      )
-      op(op.t, table).result.exportResult
-    }
-  })
 }
 
