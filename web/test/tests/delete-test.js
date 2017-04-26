ddf589ad9aa4017fb5ba93184f752b6f821a7bb8
Hanna Gábor <gahanna999@gmail.com>
2017-04-26 15:03:11 +0200

    Remove popups with deleted boxes.

--- web/test/tests/delete-test.js
+++ web/test/tests/delete-test.js
@@ -9,15 +9,12 @@ module.exports = function(fw) {
     'test pagerank default choice values',
     'delete boxes',
     function() {
-      lib.workspace.openBoxEditor('ex0').close();
-      lib.workspace.openBoxEditor('pr1').close();
-      lib.workspace.openBoxEditor('pr2').close();
       lib.workspace.deleteBoxes(['pr1', 'ex0']);
     },
     function() {
       expect(lib.workspace.boxExists('ex0')).toEqual(false);
       expect(lib.workspace.boxExists('pr1')).toEqual(false);
       expect(lib.workspace.boxExists('pr2')).toEqual(true);
-    }, 1
+    }
   );
 };
