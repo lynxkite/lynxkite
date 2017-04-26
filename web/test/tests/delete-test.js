cda39f061990e01f8e4532569179037b638bd93d
Hanna Gábor <gahanna999@gmail.com>
2017-04-26 17:18:32 +0200

    Fix popups.

--- web/test/tests/delete-test.js
+++ web/test/tests/delete-test.js
@@ -9,12 +9,16 @@ module.exports = function(fw) {
     'test pagerank default choice values',
     'delete boxes',
     function() {
+      lib.workspace.selectBox('pr2');
       lib.workspace.deleteBoxes(['pr1', 'ex0']);
     },
     function() {
       expect(lib.workspace.boxExists('ex0')).toEqual(false);
       expect(lib.workspace.boxExists('pr1')).toEqual(false);
       expect(lib.workspace.boxExists('pr2')).toEqual(true);
+      expect(lib.workspace.boxPopupExists('ex0')).toEqual(false);
+      expect(lib.workspace.boxPopupExists('pr1')).toEqual(false);
+      expect(lib.workspace.boxPopupExists('pr2')).toEqual(true);
     }
   );
 };
