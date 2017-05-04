bb6e9d2ed0fcc09ad5d24c05b3a8c50323253cd5
Gabor Feher <feherga@gmail.com>
2017-05-04 20:07:47 +0200

    finish off guiMaster

--- web/app/scripts/workspace/workspace-entry-point.js
+++ web/app/scripts/workspace/workspace-entry-point.js
@@ -3,18 +3,7 @@
 'use strict';
 
 angular.module('biggraph')
-  .controller('WorkspaceEntryPointCtrl', function ($scope, $routeParams, util, workspaceGuiMaster) {
-
-
-    // Do not cache box catalog, so that custom boxes are up to date.
+  .controller('WorkspaceEntryPointCtrl', function ($scope, $routeParams, util) {
+    $scope.routeParams = $routeParams;
     $scope.boxCatalog = util.nocache('/ajax/boxCatalog');
-    $scope.$watch(
-      'boxCatalog.$resolved',
-      function(resolved) {
-        if (resolved) {
-          $scope.guiMaster = workspaceGuiMaster(
-            $scope.boxCatalog,
-            $routeParams.workspaceName);
-        }
-      });
   });
