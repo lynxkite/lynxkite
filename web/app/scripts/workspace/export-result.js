11bf0aaff17d991e8690724797c8c0c5e78ebdc4
huncros <huncros@gmail.com>
2017-05-10 19:13:04 +0200

    comment for computeOnCommand field

--- web/app/scripts/workspace/export-result.js
+++ web/app/scripts/workspace/export-result.js
@@ -21,6 +21,9 @@ angular.module('biggraph')
          scope.exportResultOutput.then(function success(exportResultOutput) {
            // scope.status is the scalar value which is shown on the state viewer.
            scope.status = util.lazyFetchScalarValue(scope.exportResultOutput.result, false);
+           // computeOnCommand is used in inline-loading. It makes it so that if the scalar
+           // is not computed yet, it will show a Start computation button instead of the retry
+           // button.
            scope.status.value.computeOnCommand = true;
            scope.parameters = exportResultOutput.parameters;
            // Only exported files can be downloaded, JDBC exports not.
