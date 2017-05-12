12e467125604f10a15ffba6881dc446a77897e30
Matyas Korom <matyas.korom@lynxanalytics.com>
2017-05-12 15:21:53 +0200

    Add an option to transform data during transfer

--- remote_api/python/tests/test_scp.py
+++ remote_api/python/tests/test_scp.py
@@ -3,6 +3,8 @@ import luigi
 import lynx.luigi
 from unittest import mock
 import types
+import os
+import shutil
 
 
 class TestInput(luigi.ExternalTask):
@@ -20,22 +22,37 @@ class TestTask(lynx.luigi.TransferTask):
 class TestSCP(unittest.TestCase):
 
   @mock.patch('subprocess.check_call')
-  @mock.patch('subprocess.Popen')
-  @mock.patch('lynx.util.HDFS.list')
-  def test_run(self, hdfs_list, popen, check_call):
-    SN = types.SimpleNamespace
-    hdfs_list.return_value = [SN(path='input/_SUCCESS'), SN(path='input/one'), SN(path='input/two')]
+  def test_run(self, check_call):
+    local_path = ''
+
+    def hdsf_download(cmd, **kwargs):
+      nonlocal local_path
+      if cmd[:-1] == ['hadoop', 'fs', '-cp', 'input' + '/*']:
+        local_path = cmd[-1].split(':')[1]
+        with open(os.path.join(local_path, 'one'), 'w') as f:
+          f.write('one')
+        with open(os.path.join(local_path, 'two'), 'w') as f:
+          f.write('two')
+    check_call.side_effect = hdsf_download
+
     t = TestTask()
-    t.run()
+
+    def transform(src, dst):
+      for name in os.listdir(src):
+        shutil.copyfile(os.path.join(src, name), os.path.join(dst, name))
+
+    with mock.patch.object(t, 'transform_fn') as m:
+      m.side_effect = transform
+      t.run()
+      self.assertEqual(1, t.transform_fn.call_count)
+
+    check_call.assert_any_call(['hadoop', 'fs', '-cp', 'input/*',
+                                'file:' + local_path], env=mock.ANY)
     check_call.assert_any_call(['ssh', 'dsthost', "mkdir -p 'dstpath'"])
-    popen.assert_any_call(['hadoop', 'fs', '-cat', 'input/one'], env=mock.ANY, stdout=mock.ANY)
     check_call.assert_any_call(['ssh', 'dsthost', "cat > 'dstpath/one'"], stdin=mock.ANY)
-    popen.assert_any_call(['hadoop', 'fs', '-cat', 'input/two'], env=mock.ANY, stdout=mock.ANY)
     check_call.assert_any_call(['ssh', 'dsthost', "cat > 'dstpath/two'"], stdin=mock.ANY)
-    popen.assert_any_call(['hadoop', 'fs', '-cat', 'input/_SUCCESS'], env=mock.ANY, stdout=mock.ANY)
     check_call.assert_any_call(['ssh', 'dsthost', "cat > 'dstpath/_SUCCESS'"], stdin=mock.ANY)
-    self.assertEqual(3, popen.call_count)
-    self.assertEqual(4, check_call.call_count)
+    self.assertEqual(5, check_call.call_count)
 
 if __name__ == '__main__':
   unittest.main()
