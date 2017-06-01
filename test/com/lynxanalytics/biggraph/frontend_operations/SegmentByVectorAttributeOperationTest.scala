c3812e6d456fa1aee6294738c4d40c444fbdf92d
Gergely Svigruha <gergely.svigruha@lynxanalytics.com>
2017-06-01 15:42:45 -0400

    Make tests compile.

--- test/com/lynxanalytics/biggraph/frontend_operations/SegmentByVectorAttributeOperationTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/SegmentByVectorAttributeOperationTest.scala
@@ -4,26 +4,24 @@ import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class SegmentByVectorAttributeOperationTest extends OperationsTestBase {
   test("Segment by vector attribute - vector of doubles") {
-    run("Example Graph")
-    run("Derived vertex attribute",
-      Map("type" -> "vector of doubles", "output" -> "vector", "expr" -> "[1, 2]"))
-
-    run("Segment by vector attribute", Map(
-      "name" -> "segment",
-      "attr" -> "vector"))
+    val project = box("Example Graph")
+      .box("Derived vertex attribute",
+        Map("type" -> "vector of doubles", "output" -> "vector", "expr" -> "[1, 2]"))
+      .box("Segment by Vector attribute", Map(
+        "name" -> "segment",
+        "attr" -> "vector")).project
     val seg = project.segmentation("segment")
     assert(seg.vertexAttributes("vector").runtimeSafeCast[Double].rdd.values.collect.toSet ==
       Set(1, 2))
   }
 
   test("Segment by vector attribute - vector of strings") {
-    run("Example Graph")
-    run("Derived vertex attribute",
-      Map("type" -> "vector of strings", "output" -> "vector", "expr" -> "[name]"))
-
-    run("Segment by vector attribute", Map(
-      "name" -> "segment",
-      "attr" -> "vector"))
+    val project = box("Example Graph")
+      .box("Derived vertex attribute",
+        Map("type" -> "vector of strings", "output" -> "vector", "expr" -> "[name]"))
+      .box("Segment by Vector attribute", Map(
+        "name" -> "segment",
+        "attr" -> "vector")).project
     val seg = project.segmentation("segment")
     assert(seg.vertexAttributes("vector").runtimeSafeCast[String].rdd.values.collect.toSet ==
       Set("Adam", "Eve", "Bob", "Isolated Joe"))
