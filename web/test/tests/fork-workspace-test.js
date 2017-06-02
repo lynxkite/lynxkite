0ee468f78002f5b2e73b4a6379ead66abc556a1a
Mate Szabo <mate.szabo@lynxanalytics.com>
2017-06-02 13:40:31 +0200

    add fork workspace test

---
+++ web/test/tests/fork-workspace-test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function() {};
+
+var lib = require('../test-lib.js');
+
+module.exports = function(fw) {
+  fw.transitionTest(
+    'empty test-example workspace',
+    'empty test-example workspace forked',
+    function() {
+      $('#save-workspace-as-starter-button').click();
+      lib.submitInlineInput($('#save-workspace-as-input'), '-fork');
+    },
+    function() {
+      expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + '#/workspace/test-example-fork');
+      lib.workspace.close();
+      lib.splash.expectWorkspaceListed('test-example-fork');
+    },true);
+};
