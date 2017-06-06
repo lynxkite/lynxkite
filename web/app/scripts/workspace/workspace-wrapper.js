631e34cf9a65fb279921f0e17b313a7746d85b65
Mate Szabo <mate.szabo@lynxanalytics.com>
2017-06-06 17:26:38 +0200

    call _updateBoxCatalog

--- web/app/scripts/workspace/workspace-wrapper.js
+++ web/app/scripts/workspace/workspace-wrapper.js
@@ -129,6 +129,7 @@ angular.module('biggraph').factory('WorkspaceWrapper', function(BoxWrapper, util
       // of backendState will only be notified once the
       // backend is fully aware of the new state.
       this.state = angular.copy(this.backendState);
+      this._updateBoxCatalog();
       this._build();
       this._assignStateInfoToPlugs(response.outputs);
       this._assignSummaryInfoToBoxes(response.summaries);
