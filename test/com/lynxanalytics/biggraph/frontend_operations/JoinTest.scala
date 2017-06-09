9c9f0cd7fe8158181a9f281247aa3ab54c5bb330
Gabor Olah <gabor.olah@lynxanalytics.com>
2017-06-09 11:44:23 +0200

    Add more tests

--- test/com/lynxanalytics/biggraph/frontend_operations/JoinTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/JoinTest.scala
@@ -6,7 +6,8 @@ class JoinTest extends OperationsTestBase {
   test("Simple vertex attribute join works") {
     val root = box("Create example graph")
     val right = root
-      .box("Add constant vertex attribute",
+      .box(
+        "Add constant vertex attribute",
         Map("name" -> "seven", "value" -> "7", "type" -> "Double"))
     val left = root
     val project = box("Join", Map("attr" -> "seven"), Seq(left, right)).project
@@ -15,5 +16,89 @@ class JoinTest extends OperationsTestBase {
 
     assert(values == Seq(7, 7, 7, 7))
   }
+
+  test("Simple edge attribute join works") {
+    val root = box("Create example graph")
+    val right = root
+      .box(
+        "Add constant edge attribute",
+        Map("name" -> "eight", "value" -> "8", "type" -> "Double"))
+    val left = root
+    val project = box(
+      "Join",
+      Map(
+        "apply_to_left" -> "!edges",
+        "apply_to_right" -> "!edges",
+        "attr" -> "eight"), Seq(left, right)).project
+
+    val values = project.edgeAttributes("eight").rdd.collect.toMap.values.toSeq
+
+    assert(values == Seq(8, 8, 8, 8))
+  }
+
+  test("Segmentations can be joined") {
+    val root = box("Create example graph")
+    val left = root
+    val right = root
+      .box(
+        "Segment by Double attribute",
+        Map(
+          "name" -> "bucketing",
+          "attr" -> "age",
+          "interval_size" -> "1",
+          "overlap" -> "no"
+        ))
+      .box(
+        "Create random edge bundle",
+        Map(
+          "apply_to_project" -> "|bucketing",
+          "degree" -> "10",
+          "seed" -> "31415"
+        ))
+      .box(
+        "Add constant edge attribute",
+        Map(
+          "apply_to_project" -> "|bucketing",
+          "name" -> "ten",
+          "value" -> "10",
+          "type" -> "Double"))
+    val project = box("Join",
+      Map(
+        "apply_to_left" -> "",
+        "apply_to_right" -> "",
+        "sg" -> "bucketing"
+      ), Seq(left, right)
+    ).project
+
+    val segm = project.existingSegmentation("bucketing")
+    val values = segm.edgeAttributes("ten").rdd.collect.toMap.values.toSeq
+    val tens = values.count(_ == 10.0)
+    assert(tens > 0 && tens == values.size)
+  }
+
+  test("Edge attributes joined to vertex attributes") {
+    val root = box("Create example graph")
+    val left = root
+    val right = root
+      .box(
+        "Take edges as vertices",
+        Map())
+
+    val project = box("Join",
+      Map(
+        "apply_to_left" -> "!edges",
+        "apply_to_right" -> "",
+        "attr" -> "dst_name,dst_gender"
+      ), Seq(left, right)
+    ).project
+
+    val names = project.edgeAttributes("dst_name")
+      .rdd.collect.toMap.values.toList.map(_.asInstanceOf[String]).sorted
+    val genders = project.edgeAttributes("dst_gender")
+      .rdd.collect.toMap.values.toList.map(_.asInstanceOf[String]).sorted
+    assert(names == List("Adam", "Adam", "Eve", "Eve"))
+    assert(genders == List("Female", "Female", "Male", "Male"))
+  }
+
 }
 
