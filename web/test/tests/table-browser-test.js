ae2c3228122fb25ea694be86e951e717671d1db9
Gabor Feher <feherga@gmail.com>
2017-06-14 13:10:29 +0800

    fix tests more

--- web/test/tests/table-browser-test.js
+++ web/test/tests/table-browser-test.js
@@ -72,19 +72,20 @@ module.exports = function(fw) {
 */
 
   fw.transitionTest(
-    'a few workspaces created',
+    'snapshot created',
     'SQL table browser - global SQL box',
     function() {
+      lib.splash.newDirectory('plum');
+      lib.splash.popDirectory();
       lib.splash.openGlobalSqlBox();
       var tableBrowser = lib.splash.tableBrowser;
       tableBrowser.toggle();
       tableBrowser.expectNode([0], 'plum', '`plum`');
-      tableBrowser.expectNode([1], 'apple', '`apple`');
-      tableBrowser.expectNode([2], 'pear', '`pear`');
-      tableBrowser.toggleNode([0]);
-      tableBrowser.expectNode([0, 1], 'grape', '`plum/grape`');
-      tableBrowser.enterSearchQuery('grape');
-      tableBrowser.expectNode([0], 'plum/grape', '`plum/grape`');
+      tableBrowser.expectNode([1], 'This is a snapshot.', '`This is a snapshot.`');
+      tableBrowser.toggleNode([1]);
+      tableBrowser.expectNode([1, 0], 'vertices', '`This is a snapshot.|vertices`');
+      tableBrowser.enterSearchQuery('snap');
+      tableBrowser.expectNode([0], 'This is a snapshot.', '`This is a snapshot.`');
       tableBrowser.toggle();
     },
     function() {
