3e7c196aa8111a751ef73ee0bd3330a144ba11a7
Daniel Darabos <darabos.daniel@gmail.com>
2017-06-29 18:20:33 +0200

    Return 404 for missing pages.

--- app/com/lynxanalytics/biggraph/serving/Assets.scala
+++ app/com/lynxanalytics/biggraph/serving/Assets.scala
@@ -24,10 +24,6 @@ object Assets extends Controller {
     if (requested.startsWith(static) && file.exists) {
       // Send file from ./static if it exists.
       Ok.sendFile(file)
-    } else {
-      // Redirect e.g. /project/x to /#/project/x.
-      // Absolutely bogus URLs get redirected then to the main page by Angular.
-      Redirect("/#" + request.path)
-    }
+    } else NotFound
   }
 }
