84f7e9bfb815ac553fd9ff33905f2f8136e9c82b
huncros <huncros@gmail.com>
2017-07-03 17:48:58 +0200

    manage project operations

---
+++ app/com/lynxanalytics/biggraph/frontend_operations/ManageProjectOperations.scala
@@ -0,0 +1,337 @@
+package com.lynxanalytics.biggraph.frontend_operations
+
+import com.lynxanalytics.biggraph.SparkFreeEnvironment
+import com.lynxanalytics.biggraph.graph_operations
+import com.lynxanalytics.biggraph.controllers._
+
+class ManageProjectOperations(env: SparkFreeEnvironment) extends ProjectOperations(env) {
+  import Operation.Category
+  import Operation.Context
+  import Operation.Implicits._
+
+  val ManageProjectOperations = Category("Manage project", "blue")
+  val defaultIcon = "p"
+
+  def register(id: String)(factory: Context => ProjectTransformation): Unit = {
+    registerOp(id, defaultIcon, ManageProjectOperations, List(projectOutput),
+      List(projectOutput), factory)
+  }
+
+  import com.lynxanalytics.biggraph.controllers.OperationParams._
+
+  register("Copy edge attribute")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Old name", options = project.edgeAttrList),
+      Param("destination", "New name"))
+    def enabled = FEStatus.assert(project.edgeAttrList.nonEmpty, "No edge attributes")
+    override def summary = {
+      val from = params("name")
+      val to = params("destination")
+      s"Copy edge attribute $from to $to"
+    }
+    def apply() = {
+      project.newEdgeAttribute(
+        params("destination"), project.edgeAttributes(params("name")),
+        project.viewer.getEdgeAttributeNote(params("name")))
+    }
+  })
+
+  register("Copy scalar")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Old name", options = project.scalarList),
+      Param("destination", "New name"))
+    def enabled = FEStatus.assert(project.scalarList.nonEmpty, "No scalars")
+    override def summary = {
+      val from = params("name")
+      val to = params("destination")
+      s"Copy scalar $from to $to"
+    }
+    def apply() = {
+      project.newScalar(
+        params("destination"), project.scalars(params("name")),
+        project.viewer.getScalarNote(params("name")))
+    }
+  })
+
+  register("Copy segmentation")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Old name", options = project.segmentationList),
+      Param("destination", "New name"))
+    def enabled = FEStatus.assert(project.segmentationList.nonEmpty, "No segmentations")
+    override def summary = {
+      val from = params("name")
+      val to = params("destination")
+      s"Copy segmentation $from to $to"
+    }
+    def apply() = {
+      val from = project.existingSegmentation(params("name"))
+      val to = project.segmentation(params("destination"))
+      to.segmentationState = from.segmentationState
+    }
+  })
+
+  register("Copy vertex attribute")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Old name", options = project.vertexAttrList),
+      Param("destination", "New name"))
+    def enabled = FEStatus.assert(project.vertexAttrList.nonEmpty, "No vertex attributes")
+    override def summary = {
+      val from = params("name")
+      val to = params("destination")
+      s"Copy vertex attribute $from to $to"
+    }
+    def apply() = {
+      assert(params("destination").nonEmpty, "Please set the new attribute name.")
+      project.newVertexAttribute(
+        params("destination"), project.vertexAttributes(params("name")),
+        project.viewer.getVertexAttributeNote(params("name")))
+    }
+  })
+
+  register("Discard edge attributes")(new ProjectTransformation(_) {
+    params += Choice("name", "Name", options = project.edgeAttrList, multipleChoice = true)
+    def enabled = FEStatus.assert(project.edgeAttrList.nonEmpty, "No edge attributes")
+    override def summary = {
+      val names = params("name").replace(",", ", ")
+      s"Discard edge attributes: $names"
+    }
+    def apply() = {
+      for (param <- splitParam("name")) {
+        project.deleteEdgeAttribute(param)
+      }
+    }
+  })
+
+  register("Discard scalars")(new ProjectTransformation(_) {
+    params += Choice("name", "Name", options = project.scalarList, multipleChoice = true)
+    def enabled = FEStatus.assert(project.scalarList.nonEmpty, "No scalars")
+    override def summary = {
+      val names = params("name").replace(",", ", ")
+      s"Discard scalars: $names"
+    }
+    def apply() = {
+      for (param <- splitParam("name")) {
+        project.deleteScalar(param)
+      }
+    }
+  })
+
+  register("Discard segmentation")(new ProjectTransformation(_) {
+    params += Choice("name", "Name", options = project.segmentationList)
+    def enabled = FEStatus.assert(project.segmentationList.nonEmpty, "No segmentations")
+    override def summary = {
+      val name = params("name")
+      s"Discard segmentation: $name"
+    }
+    def apply() = {
+      project.deleteSegmentation(params("name"))
+    }
+  })
+
+  register("Discard vertex attributes")(new ProjectTransformation(_) {
+    params += Choice("name", "Name", options = project.vertexAttrList, multipleChoice = true)
+    def enabled = FEStatus.assert(project.vertexAttrList.nonEmpty, "No vertex attributes")
+    override def summary = {
+      val names = params("name").replace(",", ", ")
+      s"Discard vertex attributes: $names"
+    }
+    def apply() = {
+      for (param <- splitParam("name")) {
+        project.deleteVertexAttribute(param)
+      }
+    }
+  })
+
+  register("Rename edge attribute")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("before", "Old name", options = project.edgeAttrList),
+      Param("after", "New name"))
+    def enabled = FEStatus.assert(project.edgeAttrList.nonEmpty, "No edge attributes")
+    override def summary = {
+      val from = params("before")
+      val to = params("after")
+      s"Rename edge attribute $from to $to"
+    }
+    def apply() = {
+      project.edgeAttributes(params("after")) = project.edgeAttributes(params("before"))
+      project.edgeAttributes(params("before")) = null
+    }
+  })
+
+  register("Rename scalar")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("before", "Old name", options = project.scalarList),
+      Param("after", "New name"))
+    def enabled = FEStatus.assert(project.scalarList.nonEmpty, "No scalars")
+    override def summary = {
+      val from = params("before")
+      val to = params("after")
+      s"Rename scalar $from to $to"
+    }
+    def apply() = {
+      project.scalars(params("after")) = project.scalars(params("before"))
+      project.scalars(params("before")) = null
+    }
+  })
+
+  register("Rename segmentation")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("before", "Old name", options = project.segmentationList),
+      Param("after", "New name"))
+    def enabled = FEStatus.assert(project.segmentationList.nonEmpty, "No segmentations")
+    override def summary = {
+      val from = params("before")
+      val to = params("after")
+      s"Rename segmentation $from to $to"
+    }
+    def apply() = {
+      project.segmentation(params("after")).segmentationState =
+        project.existingSegmentation(params("before")).segmentationState
+      project.deleteSegmentation(params("before"))
+    }
+  })
+
+  register("Rename vertex attribute")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("before", "Old name", options = project.vertexAttrList),
+      Param("after", "New name"))
+    def enabled = FEStatus.assert(project.vertexAttrList.nonEmpty, "No vertex attributes")
+    override def summary = {
+      val before = params("before")
+      val after = params("after")
+      s"Rename vertex attribute $before to $after"
+    }
+    def apply() = {
+      assert(params("after").nonEmpty, "Please set the new attribute name.")
+      project.newVertexAttribute(
+        params("after"), project.vertexAttributes(params("before")),
+        project.viewer.getVertexAttributeNote(params("before")))
+      project.vertexAttributes(params("before")) = null
+    }
+  })
+
+  register("Set edge attribute icon")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Name", options = project.edgeAttrList),
+      Param("icon", "Icon name"))
+    def enabled = FEStatus.assert(project.edgeAttrList.nonEmpty, "No vertex attributes")
+    override def summary = {
+      val name = params("name")
+      val icon = if (params("icon").nonEmpty) params("icon") else "nothing"
+      s"Set icon for $name to $icon"
+    }
+    def apply() = {
+      val name = params("name")
+      val icon = params("icon")
+      project.setElementMetadata(
+        EdgeAttributeKind, name, MetadataNames.Icon,
+        if (icon.nonEmpty) icon else null)
+    }
+  })
+
+  register("Set scalar icon")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Name", options = project.scalarList),
+      Param("icon", "Icon name"))
+    def enabled = FEStatus.assert(project.scalarList.nonEmpty, "No scalars")
+    override def summary = {
+      val name = params("name")
+      val icon = if (params("icon").nonEmpty) params("icon") else "nothing"
+      s"Set icon for $name to $icon"
+    }
+    def apply() = {
+      val name = params("name")
+      val icon = params("icon")
+      project.setElementMetadata(
+        ScalarKind, name, MetadataNames.Icon,
+        if (icon.nonEmpty) icon else null)
+    }
+  })
+
+  register("Set segmentation icon")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Name", options = project.segmentationList),
+      Param("icon", "Icon name"))
+    def enabled = FEStatus.assert(project.segmentationList.nonEmpty, "No vertex attributes")
+    override def summary = {
+      val name = params("name")
+      val icon = if (params("icon").nonEmpty) params("icon") else "nothing"
+      s"Set icon for $name to $icon"
+    }
+    def apply() = {
+      val name = params("name")
+      val icon = params("icon")
+      project.setElementMetadata(
+        SegmentationKind, name, MetadataNames.Icon,
+        if (icon.nonEmpty) icon else null)
+    }
+  })
+
+  register("Set vertex attribute icon")(new ProjectTransformation(_) {
+    params ++= List(
+      Choice("name", "Name", options = project.vertexAttrList),
+      Param("icon", "Icon name"))
+    def enabled = FEStatus.assert(project.vertexAttrList.nonEmpty, "No vertex attributes")
+    override def summary = {
+      val name = params("name")
+      val icon = if (params("icon").nonEmpty) params("icon") else "nothing"
+      s"Set icon for $name to $icon"
+    }
+    def apply() = {
+      val name = params("name")
+      val icon = params("icon")
+      project.setElementMetadata(
+        VertexAttributeKind, name, MetadataNames.Icon,
+        if (icon.nonEmpty) icon else null)
+    }
+  })
+
+  register("Take segmentation as base project")(new ProjectTransformation(_) with SegOp {
+    def addSegmentationParameters = {}
+    def enabled = FEStatus.enabled
+    def apply() = {
+      project.rootEditor.state = project.state
+    }
+  })
+
+  register("Take edges as vertices")(new ProjectTransformation(_) {
+    def enabled = project.hasEdgeBundle
+    def apply() = {
+      val edgeBundle = project.edgeBundle
+      val vertexAttrs = project.vertexAttributes.toMap
+      val edgeAttrs = project.edgeAttributes.toMap
+      project.scalars = Map()
+      project.vertexSet = edgeBundle.idSet
+      for ((name, attr) <- vertexAttrs) {
+        project.newVertexAttribute(
+          "src_" + name, graph_operations.VertexToEdgeAttribute.srcAttribute(attr, edgeBundle))
+        project.newVertexAttribute(
+          "dst_" + name, graph_operations.VertexToEdgeAttribute.dstAttribute(attr, edgeBundle))
+      }
+      for ((name, attr) <- edgeAttrs) {
+        project.newVertexAttribute("edge_" + name, attr)
+      }
+    }
+  })
+
+  register("Take segmentation links as base project")(new ProjectTransformation(_) with SegOp {
+    def addSegmentationParameters = {}
+    def enabled = FEStatus.enabled
+    def apply() = {
+      val root = project.rootEditor
+      val baseAttrs = parent.vertexAttributes.toMap
+      val segAttrs = project.vertexAttributes.toMap
+      val belongsTo = seg.belongsTo
+      root.scalars = Map()
+      root.vertexSet = belongsTo.idSet
+      for ((name, attr) <- baseAttrs) {
+        root.newVertexAttribute(
+          "base_" + name, graph_operations.VertexToEdgeAttribute.srcAttribute(attr, belongsTo))
+      }
+      for ((name, attr) <- segAttrs) {
+        root.newVertexAttribute(
+          "segment_" + name, graph_operations.VertexToEdgeAttribute.dstAttribute(attr, belongsTo))
+      }
+    }
+  })
+}
