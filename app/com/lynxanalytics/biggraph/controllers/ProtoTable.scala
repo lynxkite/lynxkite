c6835380fdfdc64686bf6c2b5abb99d23728c2d0
János Maginecz <wafleeee@gmail.com>
2017-07-04 10:27:33 +0200

    Start implementing schema resovler

--- app/com/lynxanalytics/biggraph/controllers/ProtoTable.scala
+++ app/com/lynxanalytics/biggraph/controllers/ProtoTable.scala
@@ -5,13 +5,30 @@ package com.lynxanalytics.biggraph.controllers
 
 import com.lynxanalytics.biggraph._
 import com.lynxanalytics.biggraph.graph_api._
-import org.apache.spark.SparkConf
+import org.apache.spark.sql.catalyst.CatalystConf
+import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.catalyst.analysis.Analyzer
+import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
 import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.analysis.UnresolvedStar
+import org.apache.spark.sql.catalyst.catalog.CatalogDatabase
+import org.apache.spark.sql.catalyst.catalog.CatalogStorageFormat
+import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.catalyst.catalog.CatalogTableType
+import org.apache.spark.sql.catalyst.catalog.InMemoryCatalog
+import org.apache.spark.sql.catalyst.catalog.SessionCatalog
+import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.catalyst.expressions.NamedExpression
+import org.apache.spark.sql.catalyst.optimizer.Optimizer
 import org.apache.spark.sql.catalyst.plans.logical.BinaryNode
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.catalyst.plans.logical.UnaryNode
+import org.apache.spark.sql.catalyst.plans.logical.Filter
 import org.apache.spark.sql.catalyst.plans.logical.LeafNode
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.catalyst.plans.logical.Project
+import org.apache.spark.sql.catalyst.plans.logical.UnaryNode
+import org.apache.spark.sql.execution.OptimizeMetadataOnlyQuery
 import org.apache.spark.sql.execution.SparkSqlParser
+import org.apache.spark.sql.execution.python.ExtractPythonUDFFromAggregate
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types
 
@@ -27,6 +44,17 @@ trait ProtoTable {
   def toTable: Table
 }
 
+class SparkOptimizer(
+                      catalog: SessionCatalog,
+                      conf: SQLConf)
+  extends Optimizer(catalog, conf) {
+
+  override def batches: Seq[Batch] = super.batches :+
+    Batch("Optimize Metadata Only Query", Once, OptimizeMetadataOnlyQuery(catalog, conf)) :+
+    Batch("Extract Python UDF from Aggregate", Once, ExtractPythonUDFFromAggregate)
+//    Batch("Prune File Source Table Partitions", Once, PruneFileSourcePartitions)
+}
+
 object ProtoTable {
   def apply(table: Table) = new TableWrappingProtoTable(table)
   def apply(attributes: Iterable[(String, Attribute[_])])(implicit m: MetaGraphManager) =
@@ -35,22 +63,57 @@ object ProtoTable {
   // Analyzes the given query and restricts the given ProtoTables to their minimal subsets that is
   // necessary to support the query.
   def minimize(sql: String, protoTables: Map[String, ProtoTable]): Map[String, ProtoTable] = {
-    val parser = new SparkSqlParser(new SQLConf())
-    val tables = parsePlanForTableNames(parser.parsePlan(sql))
-    protoTables.filter(p => tables.contains(p._1))
+    val sqlConf = new SQLConf()
+    val parser = new SparkSqlParser(sqlConf)
+    val unanalyzedPlan = parser.parsePlan(sql.replace("|","__"))
+    unanalyzedPlan.allAttributes
+    val catalog = new SessionCatalog(new InMemoryCatalog())
+    val db = CatalogDatabase("default", "", "ot", Map())
+    catalog.createDatabase(db, ignoreIfExists = false)
+    for ((name, protoTable) <- protoTables)
+      catalog.createTable(CatalogTable(
+      TableIdentifier(name.replace("|","__")),
+      CatalogTableType.EXTERNAL,
+      CatalogStorageFormat.empty,
+      protoTable.schema), ignoreIfExists = false)
+    val anal = new Analyzer(catalog, new CatalystConf())
+    val analyzedPlan = anal.execute(unanalyzedPlan)
+    val optimizer = new SparkOptimizer(catalog, sqlConf)
+    val optimizedPlan = optimizer.execute(analyzedPlan)
+    val tables = parsePlanForRequiredTables(optimizedPlan)
+    val selectedTables = tables.map( f => {
+      val table = protoTables(f._1.replace("__", "|"))
+      val columns = f._2.flatMap(parseExpression)
+      val selectedTable = if (columns.contains("*")) {
+        table
+      } else {
+        table.maybeSelect(columns)
+      }
+      f._1.replace("__", "|") -> selectedTable
+    }).toMap
+    selectedTables
   }
 
-  def parsePlanForTableNames(plan: LogicalPlan): List[String] = {
+  def parseExpression(expression: Expression): Seq[String] = expression match {
+    case u: UnresolvedAttribute => Seq(u.name)
+    case u: UnresolvedStar => Seq("*")
+    case exp: Expression => exp.children.flatMap(parseExpression)
+  }
+
+  def parsePlanForRequiredTables(plan: LogicalPlan): List[(String, Seq[NamedExpression])] =
     plan match {
-      case u: UnresolvedRelation => List(u.tableIdentifier.identifier)
+      case Project(projectList, u: UnresolvedRelation) =>
+        List((u.tableIdentifier.identifier, projectList))
+      case Project(projectList, Filter(_, u: UnresolvedRelation)) =>
+        List((u.tableIdentifier.identifier, projectList))
       case l: LeafNode =>
         bigGraphLogger.info(s"$l ignored in ProtoTable minimalization")
         List()
-      case s: UnaryNode => parsePlanForTableNames(s.child)
-      case s: BinaryNode => parsePlanForTableNames(s.left) ++ parsePlanForTableNames(s.right)
+      case s: UnaryNode => parsePlanForRequiredTables(s.child)
+      case s: BinaryNode => parsePlanForRequiredTables(s.left) ++ parsePlanForRequiredTables(s.right)
     }
   }
-}
+
 
 class TableWrappingProtoTable(table: Table) extends ProtoTable {
   def schema = table.schema
