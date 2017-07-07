551f03d0074f19de73f59ffd3ad63d223b6377cf
János Maginecz <wafleeee@gmail.com>
2017-07-07 19:50:28 +0200

    Stop code duplication

--- test/com/lynxanalytics/biggraph/controllers/ProtoTableTest.scala
+++ test/com/lynxanalytics/biggraph/controllers/ProtoTableTest.scala
@@ -1,6 +1,7 @@
 package com.lynxanalytics.biggraph.controllers
 
 import com.lynxanalytics.biggraph.graph_api.Attribute
+import com.lynxanalytics.biggraph.graph_operations.ExecuteSQL
 
 class ProtoTableTest extends BigGraphControllerTestBase {
   private def attr(name: String): (String, Attribute[_]) =
@@ -22,13 +23,16 @@ class ProtoTableTest extends BigGraphControllerTestBase {
     ("select a from one where b < 3", Map("one" -> Set("a", "b"))),
     ("select o.a, t.c from one o cross join two t",
       Map("one" -> Set("a"), "two" -> Set("c"))),
+    ("select o.a, t.c from one o cross join one t",
+      Map("one" -> Set("a"), "one" -> Set("c"))),
     ("select a from (select * from one)", Map("one" -> Set("a"))),
     ("select a from (select * from one) where b=11", Map("one" -> Set("a", "b"))),
     ("select o.a, two.c from one o inner join two on o.b=two.b where o.a=1",
       Map("one" -> Set("a", "b"), "two" -> Set("c", "b")))
   ).foreach(f => {
     test(f._1) {
-      val minimizedTables = ProtoTable.minimize(f._1, protoTables)
+      val (plan, tableLookup) = ExecuteSQL.getOptimizedLogicalPlanWithLookup(f._1, protoTables)
+      val minimizedTables = ProtoTable.minimize(plan, tableLookup)
       compareProto(f._2, minimizedTables)
     }
   })
