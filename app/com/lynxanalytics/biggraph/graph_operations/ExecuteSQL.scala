87103abf5cdf9a7fc3178b9f50ff8b79f0b03ec3
János Maginecz <wafleeee@gmail.com>
2017-07-07 19:53:58 +0200

    Change operation separately to avoid monstrous merge

--- app/com/lynxanalytics/biggraph/graph_operations/ExecuteSQL.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/ExecuteSQL.scala
@@ -3,7 +3,6 @@ package com.lynxanalytics.biggraph.graph_operations
 
 import com.lynxanalytics.biggraph.controllers.ProtoTable
 import com.lynxanalytics.biggraph.graph_api._
-import com.lynxanalytics.biggraph.spark_util.SQLHelper
 import org.apache.spark
 import org.apache.spark.sql.catalyst.catalog.SessionCatalog
 import org.apache.spark.sql.catalyst.optimizer.Optimizer
