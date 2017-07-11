94114795eae285966ce1e1aacefa94cf8afdfbdc
János Maginecz <wafleeee@gmail.com>
2017-07-11 13:54:06 +0200

    Make test statepreserving

--- web/test/tests/operation-selector-test.js
+++ web/test/tests/operation-selector-test.js
@@ -5,13 +5,16 @@ module.exports = function() {};
 var lib = require('../test-lib.js');
 
 module.exports = function(fw) {
-  fw.transitionTest(
+  fw.statePreservingTest(
     'empty test-example workspace',
     'example graph workspace',
     function() {
       lib.workspace.addBoxFromSelector('Create example graph');
-    },
-    function() {
       lib.workspace.expectNumBoxes(2); // With anchor box
+      var K = protractor.Key;
+      browser.actions()
+        .sendKeys(K.ESCAPE + K.DELETE)
+        .perform();
+      lib.workspace.expectNumBoxes(1);
     });
 };
