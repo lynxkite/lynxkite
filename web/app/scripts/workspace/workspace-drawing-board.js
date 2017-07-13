60e89db093fac91eb89450db2e82fdbb23898dc6
huncros <huncros@gmail.com>
2017-07-13 14:06:25 +0200

    change popup height back => fix frontend-test

--- web/app/scripts/workspace/workspace-drawing-board.js
+++ web/app/scripts/workspace/workspace-drawing-board.js
@@ -195,7 +195,7 @@ angular.module('biggraph')
         function placePopup(event) {
           // Avoid the event position, stay on the screen, and try to be close to the event.
           var w = 500;
-          var h = 540;
+          var h = 500;
           var eventX = event.pageX - w / 2;
           var eventY = event.pageY - h / 2;
           var minX = 0;
