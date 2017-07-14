33fa2a581289f4f692f60b6132d78d97969e2b29
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2017-07-14 15:53:37 -0400

    Load builtins for every Kite restart.

--- app/com/lynxanalytics/biggraph/graph_api/MetaGraphManager.scala
+++ app/com/lynxanalytics/biggraph/graph_api/MetaGraphManager.scala
@@ -276,28 +276,35 @@ object MetaGraphManager {
 
 object BuiltIns {
   def createBuiltIns(implicit manager: MetaGraphManager) = {
-    if (!builtInsDirectoryExists) {
-      log.info("Loading built-ins from disk...")
-      val builtInsLocalDir = getBuiltInsLocalDirectory()
-      import com.lynxanalytics.biggraph.controllers.WorkspaceJsonFormatters._
-      for ((file, j) <- loadBuiltIns(builtInsLocalDir)) {
-        try {
-          val ws = j.as[Workspace]
-          val entry = DirectoryEntry.fromName("built-ins/" + file).asNewWorkspaceFrame()
-          val cp = ws.checkpoint()
-          entry.setCheckpoint(cp)
-        } catch {
-          case e: Throwable => throw new Exception(s"failed to load $file.", e)
-        }
-      }
-      log.info("Built-ins loaded from disk.")
+    val builtInsDir = DirectoryEntry.fromName("built-ins")
+    if (!builtInsDir.exists) {
+      builtInsDir.asNewDirectory()
+      builtInsDir.readACL = "*"
+      builtInsDir.writeACL = ""
     }
+    log.info("Loading built-ins from disk...")
+    val builtInsLocalDir = getBuiltInsLocalDirectory()
+    import com.lynxanalytics.biggraph.controllers.WorkspaceJsonFormatters._
+    for ((file, json) <- loadBuiltIns(builtInsLocalDir)) {
+      try {
+        val entry = DirectoryEntry.fromName("built-ins/" + file)
+        // Overwrite existing built-ins with the ones from the disk.
+        if (entry.exists) {
+          entry.remove()
+        }
+        val frame = entry.asNewWorkspaceFrame()
+        val cp = json.as[Workspace].checkpoint()
+        frame.setCheckpoint(cp)
+      } catch {
+        case e: Throwable => throw new Exception(s"failed to load $file.", e)
+      }
+    }
+    log.info("Built-ins loaded from disk.")
   }
 
   private def getBuiltInsLocalDirectory(): String = {
     val stageDir = scala.util.Properties.envOrNone("KITE_STAGE_DIR")
-    // In the backend-tests because there we don't have the KITE_STAGE_DIR environment variable
-    // set.
+    // In the backend-tests we don't have the KITE_STAGE_DIR environment variable set.
     stageDir.getOrElse("stage")
   }
 
@@ -309,9 +316,5 @@ object BuiltIns {
         f.getName() -> Json.parse(FileUtils.readFileToString(f, "utf8"))
       }
     } else Iterable()
-  }
-
-  private def builtInsDirectoryExists(implicit manager: MetaGraphManager): Boolean = {
-    DirectoryEntry.fromName("built-ins").exists
   }
 }
