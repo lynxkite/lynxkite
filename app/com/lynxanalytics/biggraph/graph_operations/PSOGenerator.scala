2347c6a3bce717ca1ae28a9079b840edcb354584
borsim2 <mb16240@bristol.ac.uk>
2017-07-21 11:35:43 +0200

    First working version

--- app/com/lynxanalytics/biggraph/graph_operations/PSOGenerator.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/PSOGenerator.scala
@@ -12,18 +12,15 @@ import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.Implicits._
 
 object PSOGenerator extends OpFromJson {
-  class Input extends MagicInputSignature {
-    val vs = vertexSet
-  }
-  class Output(implicit instance: MetaGraphOperationInstance,
-               inputs: Input) extends MagicOutput(instance) {
-    val (vertices, edges) = graph
-    val radial = vertexAttribute[Double](vertices)
-    val angular = vertexAttribute[Double](vertices)
-    //val reorderedID = vertexAttribute[Int](vertices)
-    //val edgeProbability = edgeAttribute[Double](edges)
+
+  class Output(implicit instance: MetaGraphOperationInstance) extends MagicOutput(instance) {
+
+    val (vs, es) = graph
+    val radial = vertexAttribute[Double](vs)
+    val angular = vertexAttribute[Double](vs)
   }
   def fromJson(j: JsValue) = PSOGenerator(
+    (j \ "size").as[Long],
     (j \ "externalDegree").as[Int],
     (j \ "internalDegree").as[Int],
     (j \ "exponent").as[Double],
@@ -31,13 +28,14 @@ object PSOGenerator extends OpFromJson {
     (j \ "seed").as[Long])
 }
 import PSOGenerator._
-case class PSOGenerator(externalDegree: Int, internalDegree: Int,
-                        exponent: Double, temperature: Double, seed: Long) extends TypedMetaGraphOp[Input, Output] {
+case class PSOGenerator(size: Long, externalDegree: Int, internalDegree: Int, exponent: Double,
+                        temperature: Double, seed: Long) extends TypedMetaGraphOp[NoInput, Output] {
   override val isHeavy = true
-  @transient override lazy val inputs = new Input
+  @transient override lazy val inputs = new NoInput
 
-  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
+  def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance)
   override def toJson = Json.obj(
+    "size" -> size,
     "externalDegree" -> externalDegree,
     "internalDegree" -> internalDegree,
     "exponent" -> exponent,
@@ -49,13 +47,15 @@ case class PSOGenerator(externalDegree: Int, internalDegree: Int,
               output: OutputBuilder,
               rc: RuntimeContext): Unit = {
     implicit val id = inputDatas
+    val partitioner = rc.partitionerForNRows(size)
+    val ordinals = rc.sparkContext.parallelize(0L until size, partitioner.numPartitions)
+    val vertices = ordinals.randomNumbered(partitioner)
     val sc = rc.sparkContext
-    val vs = inputs.vs.rdd
     val masterRandom = new Random(seed)
-    val numVertices = vs.count
+    val numVertices = vertices.count
     val logNumVertices: Double = math.log(numVertices.toDouble)
     // Adds the necessary attributes for the calculations to come
-    val reorderedID = vs.zipWithIndex.map { case ((key, nothing), reID) => (key, reID + 1) }
+    val reorderedID = vertices.zipWithIndex.map { case ((key, nothing), reID) => (key, reID + 1) }
     val radialAdded = reorderedID.map { case (key, reID) => (key, (reID, 2 * math.log(reID.toDouble))) }
     val radial = radialAdded.map { case (key, (reID, radial)) => (key, radial) }
     val angularAdded = radialAdded.map({ case (key, (reID, rad)) => (key, (reID, rad, masterRandom.nextDouble * math.Pi * 2)) })
@@ -64,8 +64,8 @@ case class PSOGenerator(externalDegree: Int, internalDegree: Int,
       case (key, (reID, rad, ang)) => (key, (reID, rad, ang,
         (math.round(logNumVertices * totalExpectedEPSO(exponent, externalDegree, internalDegree, numVertices, reID))).toInt))
     }
-    // key | reorderedID | radial | angular | (expectedDegree)
-    // This replaces the double-linked list found in the local implementation
+    // key | reorderedID | radial | angular | expectedDegree
+    // This replaces the double-linked list found in the local implementation.
     // Groups the samples for each vertex into a list. The first element of these are the vertices, then 
     // angular samples from cloclwise-most to counterclockwise-most, then radial samples.
     // Note: sample list will include the node itself in the middle.
@@ -78,33 +78,35 @@ case class PSOGenerator(externalDegree: Int, internalDegree: Int,
       var resultList: List[List[(Long, Long, Double, Double, Int)]] = Nil
       var remainderList: List[(Long, Long, Double, Double, Int)] = allVerticesList
       var i = numFirstSamples
-      while (i > 0) remainderList = remainderList.tail
+      while (i > 0 && !remainderList.isEmpty) remainderList = remainderList.tail
       var angularSampleList: List[(Long, Long, Double, Double, Int)] = endofVerticesList ++ allVerticesList.take(numFirstSamples)
       var radialSampleList: List[(Long, Long, Double, Double, Int)] = allVerticesList.head :: Nil
       resultList = (allVerticesList.head :: angularSampleList) :: resultList
-      for (vertex <- allVerticesList.tail) {
-        if (!radialSampleList.isEmpty) radialSampleList = radialSampleList.take(vertex._5)
-        radialSampleList = vertex :: radialSampleList
+      if (!allVerticesList.isEmpty) {
+        for (vertex <- allVerticesList.tail) {
+          if (!radialSampleList.isEmpty) radialSampleList = radialSampleList.take(vertex._5)
+          radialSampleList = vertex :: radialSampleList
 
-        if (remainderList.isEmpty) remainderList = allVerticesList.take(numFirstSamples)
-        if (!angularSampleList.isEmpty) angularSampleList = angularSampleList.take(vertex._5 * 2)
-        // Small modification if clause to keep the sample list centered on 'vertex'
-        if (vertex._5 >= remainderList.head._5) angularSampleList = remainderList.head :: angularSampleList
-        remainderList = remainderList.tail
+          if (remainderList.isEmpty) remainderList = allVerticesList.take(numFirstSamples)
+          if (!angularSampleList.isEmpty) angularSampleList = angularSampleList.take(vertex._5 * 2)
+          // Small modification if clause to keep the sample list centered on 'vertex'
+          if (vertex._5 >= remainderList.head._5) angularSampleList = remainderList.head :: angularSampleList
+          remainderList = remainderList.tail
 
-        val vertexResult = vertex :: (angularSampleList ++ radialSampleList)
-        resultList = vertexResult :: resultList
+          val vertexResult = vertex :: (angularSampleList ++ radialSampleList)
+          resultList = vertexResult :: resultList
+        }
       }
       resultList
     }
     // Selects the expectedDegree smallest distance edges from possibility bundles
     val possibilities = sc.parallelize(possibilityList)
-    val edges = sc.parallelize(possibilities.map {
+    val es = sc.parallelize(possibilities.map {
       case (data) =>
         {
           var resultEdges: List[(Long, Long)] = Nil
           val numSelections: Double = totalExpectedEPSO(exponent, externalDegree, internalDegree,
-            numVertices, data.head._1)
+            numVertices, data.head._5)
           val numSamples: Int = (math.log(numVertices) * numSelections).toInt
           val srcTuple = data.head
           // hyperbolicDistance | src key | dst key
@@ -117,8 +119,10 @@ case class PSOGenerator(externalDegree: Int, internalDegree: Int,
               srcTuple._1, dstTuple._1)
           }
           //This could be parallelized and 'for' probably doesn't do it.
-          for (dstTuple <- data.tail) {
-            if (srcTuple != dstTuple) maxHeap += heapElement(srcTuple, dstTuple)
+          if (!data.tail.isEmpty) {
+            for (dstTuple <- data.tail) {
+              if (srcTuple != dstTuple) maxHeap += heapElement(srcTuple, dstTuple)
+            }
           }
           for (j <- 0 until numSelections.toInt) {
             val result = maxHeap.dequeue
@@ -128,9 +132,10 @@ case class PSOGenerator(externalDegree: Int, internalDegree: Int,
         }
     }.reduce(_ ++ _).map { case (edge1, edge2) => Edge(edge1, edge2) })
     // TODO will this contain parallel edges? The algorithm generates them but does sortUnique eliminate?
-    output(o.radial, radial.sortUnique(inputs.vs.rdd.partitioner.get))
-    output(o.angular, angular.sortUnique(inputs.vs.rdd.partitioner.get))
-    output(o.edges, edges.randomNumbered(inputs.vs.rdd.partitioner.get))
+    output(o.vs, vertices.mapValues(_ => ()))
+    output(o.radial, radial.sortUnique(partitioner))
+    output(o.angular, angular.sortUnique(partitioner))
+    output(o.es, es.randomNumbered(partitioner))
   }
   // Returns hyperbolic distance.
   def hyperbolicDistance(rad1: Double, rad2: Double, ang1: Double, ang2: Double): Double = {
