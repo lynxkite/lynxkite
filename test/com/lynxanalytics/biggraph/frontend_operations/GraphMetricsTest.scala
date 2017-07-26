e7753a6813a1b7ad35e2490b124c21cf46cf7e41
ddkatona <ddkatona@gmail.com>
2017-07-26 16:18:27 +0200

    Tests updated for the Modular clusterless version

--- test/com/lynxanalytics/biggraph/frontend_operations/GraphMetricsTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/GraphMetricsTest.scala
@@ -12,21 +12,15 @@ class GraphMetricsTest extends OperationsTestBase {
     }
   }
 
-  // All 3 plot tests use this box
-  def plotBox = box("Create example graph")
+  // All 2 plot tests use this box
+  val plotBox = box("Create example graph")
     .box("built-ins/graph-metrics", Map(
-      "CustomPercentile" -> "0",
-      "MaximumNumberOfIterations" -> "30",
-      "MinimalModularityIncerement" -> "0.001",
-      "ModularClustering" -> "true"))
+      "CustomPercentile" -> "0"))
 
   test("graph metrics works (Output: General Metrics)") {
     val gmbox1 = box("Create example graph")
       .box("built-ins/graph-metrics", Map(
-        "CustomPercentile" -> "0",
-        "MaximumNumberOfIterations" -> "30",
-        "MinimalModularityIncerement" -> "0.001",
-        "ModularClustering" -> "true")).output("General Metrics")
+        "CustomPercentile" -> "0")).output("General Metrics")
 
     val table1 = gmbox1.table
     assert(table1.schema.map(_.name) == Seq("Attribute", "Sum", "Avg", "Min", "5% percentile", "Med",
@@ -39,15 +33,11 @@ class GraphMetricsTest extends OperationsTestBase {
       Seq("In Degree", 4.0, 1.0, 0.0, 0.0, 1.0, 2.0, 2.0, 0.0),
       Seq("Out Degree", 4.0, 1.0, 0.0, 0.15000000000000002, 1.0, 1.8499999999999996, 2.0, 0.0),
       Seq("Sym Degree", 2.0, 0.5, 0.0, 0.0, 0.5, 1.0, 1.0, 0.0),
-      Seq("Connected Components", 2.0, 2.0, 1.0, 1.1, 2.0, 2.8999999999999995, 3.0, 1.0),
-      Seq("Modular Clusternig", 2.0, 2.0, 1.0, 1.1, 2.0, 2.8999999999999995, 3.0, 1.0)))
+      Seq("Connected Components", 2.0, 2.0, 1.0, 1.1, 2.0, 2.8999999999999995, 3.0, 1.0)))
 
     val gmbox2 = box("Create example graph")
       .box("built-ins/graph-metrics", Map(
-        "CustomPercentile" -> "0.7",
-        "MaximumNumberOfIterations" -> "30",
-        "MinimalModularityIncerement" -> "0.001",
-        "ModularClustering" -> "true")).output("General Metrics")
+        "CustomPercentile" -> "0.7")).output("General Metrics")
 
     // Testing schema again, because it's dependent on the input
     val table2 = gmbox2.table
@@ -60,8 +50,7 @@ class GraphMetricsTest extends OperationsTestBase {
       Seq("In Degree", 4.0, 1.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0),
       Seq("Out Degree", 4.0, 1.0, 0.0, 0.15000000000000002, 1.0, 1.8499999999999996, 2.0, 1.0999999999999996),
       Seq("Sym Degree", 2.0, 0.5, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0),
-      Seq("Connected Components", 2.0, 2.0, 1.0, 1.1, 2.0, 2.8999999999999995, 3.0, 2.3999999999999995),
-      Seq("Modular Clusternig", 2.0, 2.0, 1.0, 1.1, 2.0, 2.8999999999999995, 3.0, 2.3999999999999995)))
+      Seq("Connected Components", 2.0, 2.0, 1.0, 1.1, 2.0, 2.8999999999999995, 3.0, 2.3999999999999995)))
   }
 
   test("graph metrics works (Output: Degree Distribution)") {
@@ -72,11 +61,6 @@ class GraphMetricsTest extends OperationsTestBase {
   test("graph metrics works (Output: Component Distribution)") {
     // If the plot creation fails, the test throws an exception here
     val plot = plotBox.output("Component Distribution").plot.value
-  }
-
-  test("graph metrics works (Output: Modular Distribution)") {
-    // If the plot creation fails, the test throws an exception here
-    val plot = plotBox.output("Modular Distribution").plot.value
   }
 
 }
