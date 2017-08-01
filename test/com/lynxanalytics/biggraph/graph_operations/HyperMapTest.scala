3d97a1bf04e0668546fff1deff367759b6c62dcc
borsim2 <mb16240@bristol.ac.uk>
2017-08-01 16:02:15 +0200

    HyperMapTest fixed

--- test/com/lynxanalytics/biggraph/graph_operations/HyperMapTest.scala
+++ test/com/lynxanalytics/biggraph/graph_operations/HyperMapTest.scala
@@ -7,17 +7,21 @@ import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_api.Scripting._
 
 class HyperMapTest extends FunSuite with TestGraphOp {
-  test("example graph") {
-    val g = ExampleGraph()().result
+  test("small example graph") {
+    val g = SmallTestGraph(Map(0 -> Seq(1, 2, 3, 4), 1 -> Seq(0, 2), 2 -> Seq(0, 1), 3 -> Seq(0), 4 -> Seq()))().result
+    val degreeOp = OutDegree()
+    val degree = degreeOp(degreeOp.es, g.es).result.outDegree
     val op = HyperMap(1.5, 0.6, 0.45, 1337)
-    val out = op(op.vs, g.vertices)(op.es, g.edges).result
+    val out = op(op.vs, g.vs)(op.es, g.es)(op.degree, degree).result
     val angulars = out.angular.rdd.collect.map { case (id, ang) => ang }
     // Isolated Joe really is isolated
-    assert(math.abs(angulars(3) - angulars(2)) > 1)
+    assert(math.abs(angulars(4) - angulars(1)) > 1 &&
+      math.abs(2 * math.Pi - math.abs(angulars(4) - angulars(1))) > 1)
     // Radial coordinates are correct
     assert(out.radial.rdd.collect.toMap == Map(0 -> 2 * math.log(1),
       1 -> 2 * math.log(2),
       2 -> 2 * math.log(3),
-      3 -> 2 * math.log(4)))
+      3 -> 2 * math.log(4),
+      4 -> 2 * math.log(5)))
   }
 }
