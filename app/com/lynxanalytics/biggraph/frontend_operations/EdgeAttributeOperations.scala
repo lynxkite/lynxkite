e045bec7999bff67ecf49e05bd79eab761bd2342
erbenpeter <erben.peter@gmail.com>
2017-09-14 12:48:06 +0200

    Remove unused 'type' parameter from Derive edge attribute operation

--- app/com/lynxanalytics/biggraph/frontend_operations/EdgeAttributeOperations.scala
+++ app/com/lynxanalytics/biggraph/frontend_operations/EdgeAttributeOperations.scala
@@ -88,7 +88,6 @@ class EdgeAttributeOperations(env: SparkFreeEnvironment) extends ProjectOperatio
   register("Derive edge attribute")(new ProjectTransformation(_) {
     params ++= List(
       Param("output", "Save as"),
-      Choice("type", "Result type", options = FEOption.jsDataTypes),
       Choice("defined_attrs", "Only run on defined attributes",
         options = FEOption.bools), // Default is true.
       Code("expr", "Value", defaultValue = "", language = "javascript"))
