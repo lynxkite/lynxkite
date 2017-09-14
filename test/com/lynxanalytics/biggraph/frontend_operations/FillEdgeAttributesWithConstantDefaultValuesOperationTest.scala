d8ca15d957c3d80a774542134c399a85cf21ab3b
erbenpeter <erben.peter@gmail.com>
2017-09-14 12:48:06 +0200

    Remove unused 'type' parameter from Derive edge attribute operation

--- test/com/lynxanalytics/biggraph/frontend_operations/FillEdgeAttributesWithConstantDefaultValuesOperationTest.scala
+++ test/com/lynxanalytics/biggraph/frontend_operations/FillEdgeAttributesWithConstantDefaultValuesOperationTest.scala
@@ -6,9 +6,9 @@ class FillEdgeAttributesWithConstantDefaultValuesOperationTest extends Operation
   test("Fill edge attributes with constant default values") {
     val project = box("Create example graph")
       .box("Derive edge attribute",
-        Map("type" -> "Double", "output" -> "src_income_edge", "expr" -> "src$income"))
+        Map("output" -> "src_income_edge", "expr" -> "src$income"))
       .box("Derive edge attribute",
-        Map("type" -> "Double", "output" -> "dst_income_edge", "expr" -> "dst$income"))
+        Map("output" -> "dst_income_edge", "expr" -> "dst$income"))
       .box("Fill edge attributes with constant default values",
         Map("fill_src_income_edge" -> "-1.0", "fill_dst_income_edge" -> "1.0")).project
     val filledSrcIncome = project.edgeAttributes("src_income_edge").runtimeSafeCast[Double]
