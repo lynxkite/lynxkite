950cb22c1bbc7f51e425e36e9d95773ad1208bd5
Matyas Korom <korommatyi@users.noreply.github.com>
2017-10-18 18:10:52 +0200

    Address review commits

--- tools/compare_dataframes.scala
+++ tools/compare_dataframes.scala
@@ -1,49 +1,34 @@
 // Usage:
 //   1) start a spark-shell
 //   2) :load path/to/this/file
-//   3) create a map (name -> (pathOfExpected, pathOfActual)) with the files to compare 
+//   3) create a map (name -> (pathOfExpected, pathOfActual)) with the files to compare
 //   4) val problems = compareAll(map)
 //   5) inspect problems
 
-
 import org.apache.spark.sql.DataFrame
-
-
-case class Problem(problemType: String, details: String = "")
+import util.{ Try, Success, Failure }
 
 def frameHashCode(dataFrame: DataFrame): Int =
-  dataFrame.map(_.hashCode).reduce(_+_)
+  dataFrame.map(_.hashCode).reduce(_ + _)
 
-def compareDataFrames(df1: DataFrame, df2: DataFrame): Option[Problem] = {
+def compareDataFrames(df1: DataFrame, df2: DataFrame): Try[Unit] = {
   val df1Schema = df1.schema.toString.toLowerCase
   val df2Schema = df2.schema.toString.toLowerCase
-  if (df1Schema != df2Schema) {
-    Some(Problem(problemType = "schema mismatch",
-      details = s"Expected:\n$df1Schema\n\nvs.\n\nActual:\n$df2Schema"))
-  } else if (df1.count != df2.count) {
-    Some(Problem(problemType = "count mismatch",
-      details = s"Expected: ${df1.count} vs. Actual: ${df2.count}"))
-  } else if (df1.count == 0) {
-    Some(Problem(problemType = "both emtpy"))
-  } else if (frameHashCode(df1) != frameHashCode(df2)) {
-    Some(Problem(problemType = "hash mismatch"))
-  } else {
-    None
-  }
+  Try({
+    assert(df1Schema == df2Schema, "schema mismatch")
+    assert(df1.count == df2.count, "count mismatch")
+    assert(df1.count != 0, "both emtpy")
+    assert(frameHashCode(df1) == frameHashCode(df2), "hash mismatch")
+  })
 }
 
 // spark is the spark context in the spark-shell, it should be available here
 def readParquet(path: String) = spark.read.parquet(path)
 
-def compareAll(whatToWhat: Map[String, (String, String)]): Map[String, Option[Problem]] =
+def compareAll(whatToWhat: Map[String, (String, String)]): Map[String, Try[Unit]] =
   whatToWhat.mapValues {
     case (path1, path2) =>
-      try {
-        val df1 = readParquet(path1)
-        val df2 = readParquet(path2)
-        compareDataFrames(df1, df2)
-      } catch {
-        case ex: Exception =>
-          Some(Problem(problemType = "exception thrown", details = ex.getMessage))
-      }
+      val df1 = readParquet(path1)
+      val df2 = readParquet(path2)
+      compareDataFrames(df1, df2)
   }
