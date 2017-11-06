1acc6d585c17a9e8459f299d4ec20ac61fec9afc
Matyas Korom <matyas.korom@lynxanalytics.com>
2017-11-06 13:42:46 +0100

    Skip hashing double columns

--- tools/compare_dataframes.scala
+++ tools/compare_dataframes.scala
@@ -7,41 +7,45 @@
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.DoubleType
-import org.apache.spark.sql.functions.udf
-import util.{ Try, Success, Failure }
+import org.apache.spark.sql.functions
+import util.Try
 
-def frameHashCode(dataFrame: DataFrame): Int =
-  dataFrame.map(_.hashCode).reduce(_ + _)
+def isClose(a: Double, b: Double, rtol: Double = 1e-5, atol: Double = 1e-8): Boolean =
+  Math.abs(a - b) < Math.max(atol, Math.abs(a) * rtol)
 
-def roundDoubleValues(dataFrame: DataFrame, precision: Int): DataFrame = {
-  val x = math.pow(10, precision)
-  val round = udf { d: Double => math.rint(d * x) / x }
-  val doubleColumns = dataFrame.schema.filter(_.dataType == DoubleType).map(_.name)
-  doubleColumns.foldLeft(dataFrame) {
-    case (df, columnName) => df.withColumn(columnName, round(df(columnName)))
-  }
-}
+def doubleColSum(df: DataFrame, colName: String): Double =
+  df.select(colName).as[Double].reduce(_ + _)
 
-def compareDataFrames(df1: DataFrame, df2: DataFrame, precision: Int): Unit = {
-  val df1Schema = df1.schema.toString.toLowerCase
-  val df2Schema = df2.schema.toString.toLowerCase
-  assert(df1Schema == df2Schema, "schema mismatch")
+def columnHash(df: DataFrame, colName: String): Int =
+  df.select(hash(df(colName))).as[Int].reduce(_ + _)
+
+def compareDataFrames(df1: DataFrame, df2: DataFrame): Unit = {
+  val df1Schema = df1.schema
+  val df2Schema = df2.schema
+  assert(df1Schema.toString.toLowerCase == df2Schema.toString.toLowerCase, "schema mismatch")
   assert(df1.count == df2.count, "count mismatch")
   assert(df1.count != 0, "both emtpy")
-  val df1Rounded = roundDoubleValues(df1, precision)
-  val df2Rounded = roundDoubleValues(df2, precision)
-  assert(frameHashCode(df1Rounded) == frameHashCode(df2Rounded), "hash mismatch")
+  df1Schema.foreach { col =>
+    val colName = col.name
+    if (col.dataType == DoubleType) {
+      val df1Sum = doubleColSum(df1, colName)
+      val df2Sum = doubleColSum(df2, colName)
+      assert(isClose(df1Sum, df2Sum), s"$colName sum mismatch")
+    } else {
+      assert(columnHash(df1, colName) == columnHash(df2, colName), s"$colName hash mismatch")
+    }
+  }
 }
 
 // spark is the spark context in the spark-shell, it should be available here
 def readParquet(path: String) = spark.read.parquet(path)
 
-def compareAll(whatToWhat: Map[String, (String, String)], precision: Int = 3): Map[String, Try[Unit]] =
+def compareAll(whatToWhat: Map[String, (String, String)]): Map[String, Try[Unit]] =
   whatToWhat.map {
     case (name, (path1, path2)) =>
       name -> Try {
         val df1 = readParquet(path1)
         val df2 = readParquet(path2)
-        compareDataFrames(df1, df2, precision)
+        compareDataFrames(df1, df2)
       }
   }
