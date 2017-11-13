43b6573e00c5bce0d7cf1c301b636260fa054ab0
Daniel Darabos <darabos.daniel@gmail.com>
2017-11-13 17:58:25 +0100

    Shortcut for saving workspace in temporary API.

--- remote_api/python/lynx/kite.py
+++ remote_api/python/lynx/kite.py
@@ -240,6 +240,13 @@ class LynxKite:
         'downloadFile',
         params=dict(q=json.dumps(dict(path=path, stripHeaders=False)))).content
 
+  def set_workspace(self, path, boxes):
+    if not self.get_directory_entry(path).exists:
+      self._send('/ajax/createWorkspace', dict(name=path))
+    return self._send(
+        '/ajax/setWorkspace',
+        dict(reference=dict(top=path, customBoxStack=[]), workspace=dict(boxes=boxes)))
+
 
 class LynxException(Exception):
   '''Raised when LynxKite indicates that an error has occured while processing a command.'''
