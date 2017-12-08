fc7fb0dfa079f2c583a7d2b91e84b27560c0c84f
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2017-12-08 15:45:03 -0500

    Add TableSnapshotSequence.

---
+++ remote_api/python/tests/test_snapshot_sequences.py
@@ -0,0 +1,20 @@
+import unittest
+import lynx.kite
+import json
+import test_workspace
+
+
+class TestSnapshotSequence(unittest.TestCase):
+
+  def test_tables(self):
+    lk = lynx.kite.LynxKite()
+
+    # TODO: Set up test dirs and snapshots.
+
+    tss = lynx.kite.TableSnapshotSequence('test_snapshot_sequence/1', 'yyyy/MM')
+    tables = tss.tables(lk, '2017/11', '2017/12')
+    self.assertEqual(len(tables), 2)
+    self.assertEqual('test_snapshot_sequence/1/2017/11', tables[0].name)
+    self.assertEqual('test_snapshot_sequence/1/2017/12', tables[1].name)
+    self.assertEqual('table', tables[0].icon)
+    self.assertEqual('table', tables[1].icon)
