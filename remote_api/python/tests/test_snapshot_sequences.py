dce5afedf4db59aa3ffde222f83bb4b8d25da544
Gergely Svigruha <gergely.svigruha@lynxanalytics.com>
2017-12-12 13:10:14 -0500

    Add proper tests.

--- remote_api/python/tests/test_snapshot_sequences.py
+++ remote_api/python/tests/test_snapshot_sequences.py
@@ -33,22 +33,47 @@ ANCHOR_EXAMPLE_AND_SQL = '''
 
 class TestSnapshotSequence(unittest.TestCase):
 
+  def _save_snapshots(self, lk, paths, state):
+    for path in paths:
+      lk.save_snapshot('test_snapshot_sequence/' + path, state)
+
   def test_tables_yearly(self):
     lk = lynx.kite.LynxKite()
 
+    lk.remove_name('test_snapshot_sequence', force=True)
     lk.create_dir('test_snapshot_sequence')
     lk.create_dir('test_snapshot_sequence/1')
     outputs = lk.run(json.loads(ANCHOR_EXAMPLE_AND_SQL))
     state = outputs['SQL1_1', 'table'].stateId
 
-    lk.save_snapshot('test_snapshot_sequence/1/2010', state)
-    lk.save_snapshot('test_snapshot_sequence/1/2011', state)
-    lk.save_snapshot('test_snapshot_sequence/1/2012', state)
+    self._save_snapshots(lk, ['1/' + y for y in ['2010', '2011', '2012']], state)
 
     tss = lynx.kite.TableSnapshotSequence('test_snapshot_sequence/1')
     tables = tss.tables_yearly(lk, '2010', '2011')
     self.assertEqual(len(tables), 2)
     self.assertEqual('test_snapshot_sequence/1/2010', tables[0].name)
     self.assertEqual('test_snapshot_sequence/1/2011', tables[1].name)
+    self.assertEqual('table', tables[0].icon)
+    self.assertEqual('table', tables[1].icon)
+
+    self._save_snapshots(lk, ['2/2015/' + '%02d' % m for m in range(1, 13)], state)
+    self._save_snapshots(lk, ['2/2016/' + '%02d' % m for m in range(1, 13)], state)
+
+    tss = lynx.kite.TableSnapshotSequence('test_snapshot_sequence/2')
+    tables = tss.tables_monthly(lk, '2015/05', '2016/10')
+    self.assertEqual(len(tables), 18)
+    self.assertEqual('test_snapshot_sequence/2/2015/05', tables[0].name)
+    self.assertEqual('test_snapshot_sequence/2/2016/10', tables[17].name)
+    self.assertEqual('table', tables[0].icon)
+    self.assertEqual('table', tables[1].icon)
+
+    self._save_snapshots(lk, ['3/2017/03/' + '%02d' % d for d in range(1, 32)], state)
+    self._save_snapshots(lk, ['3/2017/04/' + '%02d' % d for d in range(1, 31)], state)
+
+    tss = lynx.kite.TableSnapshotSequence('test_snapshot_sequence/3')
+    tables = tss.tables_daily(lk, '2017/03/15', '2017/04/15')
+    self.assertEqual(len(tables), 32)
+    self.assertEqual('test_snapshot_sequence/3/2017/03/15', tables[0].name)
+    self.assertEqual('test_snapshot_sequence/3/2017/04/15', tables[31].name)
     self.assertEqual('table', tables[0].icon)
     self.assertEqual('table', tables[1].icon)
