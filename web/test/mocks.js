497ee4ebe788674f5a064b2c90bb5448d4b29686
Daniel Darabos <darabos.daniel@gmail.com>
2017-12-18 16:52:10 +0100

    Update mock.

--- web/test/mocks.js
+++ web/test/mocks.js
@@ -3,9 +3,11 @@ module.exports = {
   addTo: function(browser) {
     browser.addMockModule('biggraph', function() {
       angular.module('biggraph')
-        .service('sparkStatusUpdater', function() {
-          this.bind = function(scope, name) {
-            scope[name] = { timestamp: 0, activeStages: [], pastStages: [] };
+        .service('longPoll', function() {
+          this.onUpdate = function() {};
+          this.lastUpdate = {
+            sparkStatus: { timestamp: 0, activeStages: [], pastStages: [] },
+            progress: {},
           };
         })
         .service('dropTooltipConfig', function() {
