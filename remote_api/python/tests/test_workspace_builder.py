36457e68adff90147e3c25e93958e10775f8a475
erbenpeter <erben.peter@gmail.com>
2017-12-20 16:02:43 +0100

    Address review

--- remote_api/python/tests/test_workspace_builder.py
+++ remote_api/python/tests/test_workspace_builder.py
@@ -17,7 +17,7 @@ class TestWorkspaceBuilder(unittest.TestCase):
 
   def test_simple_chain(self):
     lk = lynx.kite.LynxKite()
-    state = lk.createExampleGraph().computePagerank().sql1(sql='select page_rank from vertices')
+    state = lk.createExampleGraph().computePagerank().sql('select page_rank from vertices')
     table_state = lk.get_state_id(state)
     table = lk.get_table(table_state)
     self.assertEqual(table.header[0].dataType, 'Double')
@@ -28,8 +28,8 @@ class TestWorkspaceBuilder(unittest.TestCase):
   def test_simple_sql_chain(self):
     lk = lynx.kite.LynxKite()
     state = (lk.createExampleGraph()
-             .sql1(sql='select * from vertices where age < 30')
-             .sql1(sql='select name from input where age > 2'))
+             .sql('select * from vertices where age < 30')
+             .sql('select name from input where age > 2'))
     table_state = lk.get_state_id(state)
     table = lk.get_table(table_state)
     values = [row[0].string for row in table.data]
@@ -38,7 +38,7 @@ class TestWorkspaceBuilder(unittest.TestCase):
   def test_multi_input(self):
     lk = lynx.kite.LynxKite()
     eg = lk.createExampleGraph()
-    new_edges = eg.sql1(sql='select * from edges where edge_weight > 1')
+    new_edges = eg.sql('select * from edges where edge_weight > 1')
     new_graph = lk.useTableAsEdges(
         eg, new_edges, attr='id', src='src_id', dst='dst_id')
     project = lk.get_project(lk.get_state_id(new_graph))
@@ -49,7 +49,7 @@ class TestWorkspaceBuilder(unittest.TestCase):
   def test_pedestrian_custom_box(self):
     lk = lynx.kite.LynxKite()
     i = lk.input(name='graph')
-    o = i.sql1(sql='select name from vertices').output(name='vtable')
+    o = i.sql('select name from vertices').output(name='vtable')
     ws = lynx.kite.Workspace('allvs', [o], [i])
     table_state = lk.get_state_id(ws(lk.createExampleGraph()))
     table = lk.get_table(table_state)
@@ -64,27 +64,29 @@ class TestWorkspaceBuilder(unittest.TestCase):
     scalars = {s.title: lk.get_scalar(s.id) for s in project.scalars}
     self.assertEqual(scalars['pi'].string, '3.14')
 
-  def test_parametric_parameters_with_defaults(self):
+  def parametric_ws(self):
     from lynx.kite import pp, text
     lk = lynx.kite.LynxKite()
-    state = lk.createExampleGraph().sql1(
-        sql=pp('select name from `vertices` where age = $ap')).output(name='table')
+    state = lk.createExampleGraph().sql(
+        pp('select name from `vertices` where age = $ap')).output(name='table')
     ws = lynx.kite.Workspace('ws params', [state], ws_parameters=[text('ap', '18.2')])
+    return ws
+
+  def test_parametric_parameters_with_defaults(self):
+    lk = lynx.kite.LynxKite()
+    ws = self.parametric_ws()
     state_id = lk.get_state_id(ws())
     table = lk.get_table(state_id)
     values = [row[0].string for row in table.data]
     self.assertEqual(values, ['Eve'])
 
   def test_parametric_parameters_with_workspace_parameters(self):
-    from lynx.kite import pp, text
     lk = lynx.kite.LynxKite()
-    state = lk.createExampleGraph().sql1(
-        sql=pp('select name from `vertices` where age = $ap')).output(name='table')
-    ws = lynx.kite.Workspace('ws params', [state], ws_parameters=[text('ap')])
-    state_id = lk.get_state_id(ws(ap='18.2'))
+    ws = self.parametric_ws()
+    state_id = lk.get_state_id(ws(ap='20.3'))
     table = lk.get_table(state_id)
     values = [row[0].string for row in table.data]
-    self.assertEqual(values, ['Eve'])
+    self.assertEqual(values, ['Adam'])
 
   def test_wrong_chain_with_multiple_inputs(self):
     lk = lynx.kite.LynxKite()
