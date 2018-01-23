8bcc9d67fb86f545045018ce1084462c5fb828cc
János Maginecz <wafleeee@gmail.com>
2018-01-23 13:12:17 +0100

    Keep prototable if newSchema == old schema

--- app/com/lynxanalytics/biggraph/controllers/ProtoTable.scala
+++ app/com/lynxanalytics/biggraph/controllers/ProtoTable.scala
@@ -63,7 +63,9 @@ object ProtoTable {
       f =>
         val output = f.relation.output
         val newSchema = fields.intersect(output.map(f => (f.name, f.metadata)))
-        if (newSchema.nonEmpty)
+        if (newSchema.map(_._1) == output.map(_.name))
+          f
+        else if (newSchema.nonEmpty)
           f.maybeSelect(newSchema.map(_._1))
         else
           f.maybeSelect(Seq(output.map(_.name).head))
