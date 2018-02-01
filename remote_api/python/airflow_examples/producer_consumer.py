e46083b3684b01845272712af45b97c380d75130
erbenpeter <erben.peter@gmail.com>
2018-02-01 15:53:40 +0100

    Address most of the review comments

--- remote_api/python/airflow_examples/producer_consumer.py
+++ remote_api/python/airflow_examples/producer_consumer.py
@@ -2,24 +2,29 @@
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
+from os import path
 import random
 import lynx.kite
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 2, 1, 10, 20),
-    'local_src_folder': '/home/petererben/test/src/',
+    'start_date': datetime.now() - timedelta(minutes=10),
+    'local_src_folder': path.expanduser('~/test/src/'),
     'lk_src_folder': 'test/src',
     'lk_dst_folder': 'test/dst',
 }
 
-dag = DAG('producer_consumer_tasks', default_args=default_args, schedule_interval='*/10 * * * *')
+scheduling = '*/2 * * * *'
+
+dag = DAG('producer_consumer_tasks', default_args=default_args, schedule_interval=scheduling)
+
+date_format = '%Y-%m-%dT%H%M'
 
 
 def src_name(dt):
-  date_part = dt.strftime('%Y-%m-%dT%H%M')
+  date_part = dt.strftime(date_format)
   return 'source_{}.csv'.format(date_part)
 
 
@@ -28,14 +33,13 @@ def local_src_path(dt):
 
 
 def lk_src_path(dt):
-  return default_args['lk_src_folder'] + dt.strftime('%Y-%m-%dT%H%M')
+  return default_args['lk_src_folder'] + dt.strftime(date_format)
 
 
 def local_producer(ds, **kwargs):
   with open(local_src_path(kwargs['execution_date']), 'a') as f:
     f.write('date_id,val\n')
     f.write(str(kwargs['execution_date']) + ',' + str(random.randint(1, 1234567)) + '\n')
-    f.close()
 
 
 def lk_producer(ds, **kwargs):
@@ -45,7 +49,7 @@ def lk_producer(ds, **kwargs):
   path = lk.upload(data)
   state = lk.importCSV(filename=path)
   state_id = lk.get_state_id(state)
-  tss = lynx.kite.TableSnapshotSequence(default_args['lk_src_folder'], '*/10 * * * *')
+  tss = lynx.kite.TableSnapshotSequence(default_args['lk_src_folder'], scheduling)
   tss.save_to_sequence(lk, state_id, kwargs['execution_date'])
 
 
@@ -55,8 +59,8 @@ def lk_consumer(ds, **kwargs):
   with open(local_src_path(dt)) as f:
     prefixed_path = lk.upload(f)
   table1 = lk.importCSV(filename=prefixed_path)
-  tss = lynx.kite.TableSnapshotSequence(default_args['lk_src_folder'], '*/10 * * * *')
-  tss_result = lynx.kite.TableSnapshotSequence(default_args['lk_dst_folder'], '*/10 * * * *')
+  tss = lynx.kite.TableSnapshotSequence(default_args['lk_src_folder'], scheduling)
+  tss_result = lynx.kite.TableSnapshotSequence(default_args['lk_dst_folder'], scheduling)
   table2 = tss.read_interval(lk, dt, dt)
   result = lk.sql('select * from one union all select * from two', table1, table2)
   result_id = lk.get_state_id(result)
