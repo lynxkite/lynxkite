b3bd676310ba1682753a284f4f01318681b8ea51
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2018-02-14 11:23:49 +0100

    Add comments.

--- app/org/apache/spark/sql/UDFHelper.scala
+++ app/org/apache/spark/sql/UDFHelper.scala
@@ -3,5 +3,6 @@ package org.apache.spark.sql
 import org.apache.spark.sql.catalyst.analysis.FunctionRegistry
 
 object UDFHelper {
+  // This is needed because the constructor of UDFRegistration is package private.
   def udfRegistration(fr: FunctionRegistry) = new UDFRegistration(fr)
 }
