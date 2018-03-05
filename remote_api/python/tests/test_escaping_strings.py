0140770e7ade0b05e9ef86869fcea9091bf6b149
erbenpeter <erben.peter@gmail.com>
2018-03-05 15:14:04 +0100

    Add own escaping, address review

--- remote_api/python/tests/test_escaping_strings.py
+++ remote_api/python/tests/test_escaping_strings.py
@@ -14,37 +14,23 @@ class TestEscapingStrings(unittest.TestCase):
     lk = lynx.kite.LynxKite()
     with open(self.name_of_test_data()) as f:
       csv_path = lk.upload(f)
-    return lk.importCSV(filename=csv_path)
-
-  def test_reading_strings_to_dataframe(self):
-    print(self.import_test_data().df())
-
-  def test_escape_on_test_data(self):
-    with open(self.name_of_test_data()) as f:
-      for line in f:
-        print(lynx.kite.escape(line))
+    return lk.importCSV(filename=csv_path, columns='test_name,raw_value')
 
   def test_escaped_query(self):
     lk = lynx.kite.LynxKite()
     query = {}
     box = {}
+    import csv
     with open(self.name_of_test_data()) as f:
-      for i, line in enumerate(f):
-        # No coma in raw data
-        test_id, test_name, test_raw = line.split(',')
-        query[i] = '''
-          select {} as test_id,
+      csv_reader = csv.reader(f, delimiter=',')
+      for i, row in enumerate(csv_reader):
+        test_name, test_raw = row[:2]
+        query[i] = '''select
           '{}' as test_name,
-          {} as raw_value
-          from vertices
-          '''.format(test_id, test_name, lynx.kite.escape(test_raw))
-        box[i] = lk.createExampleGraph().sql(query[i])
-    print(box)
-    # At most 10 test cases because of the SQL box limit.
-    table_names = {
-        1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-        6: 'six', 7: 'seven', 8: 'eight'}
-    boxes = [box[i] for i in range(1, 9)]
-    result_query = ' union all '.join(
-        ['select * from {}'.format(table_names[i]) for i in range(1, 9)])
-    print(lk.sql(result_query, *boxes).df())
+          '{}' as raw_value
+          '''.format(test_name, lynx.kite.escape(test_raw))
+    result_query = ' union all '.join([q for key, q in query.items()])
+
+    original = self.import_test_data().df()
+    escaped = lk.createExampleGraph().sql(result_query).df()
+    print(original.equals(escaped))
