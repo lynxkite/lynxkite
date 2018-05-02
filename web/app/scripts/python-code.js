6c7be3507ff85a60a89d4844eae27a528fc9bbd5
erbenpeter <erben.peter@gmail.com>
2018-05-02 11:21:50 +0200

    Add modal window for generated Python code

---
+++ web/app/scripts/python-code.js
@@ -0,0 +1,14 @@
+// The modal dialog for generated Python code
+'use strict';
+
+angular.module('biggraph').controller('PythonCodeCtrl', function($scope, $modalInstance, code) {
+  $scope.code = code;
+
+  $scope.close = function() {
+    $modalInstance.dismiss('close');
+  };
+
+  $scope.copyToClipboard = function() {
+
+  };
+});
