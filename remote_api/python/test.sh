12108a84363620439b688572e9c5b4f460bf2f88
Mate Szabo <szmate1618@users.noreply.github.com>
2018-05-16 16:03:36 +0200

    undo incorrect autopep changes

--- remote_api/python/test.sh
+++ remote_api/python/test.sh
@@ -11,5 +11,5 @@ else
 fi
 
 cd $(dirname $0)
-python3 -m mypy lynx --ignore-missing-import
-python3 -m unittest discover tests $@
+python3.6 -m mypy lynx --ignore-missing-import
+python3.6 -m unittest discover tests $@
