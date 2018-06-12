5454ae948fdd0e46954676fefdc9b6505ed60b59
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2018-06-12 12:42:16 -0400

    Refactor test scripts.

---
+++ lk_config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -xue
+
+if [ -n "${HTTPS_PORT:-}" ]; then
+  export LYNXKITE_ADDRESS="https://localhost:$HTTPS_PORT/"
+  export LYNXKITE_PUBLIC_SSL_CERT="$BASEDIR/test/localhost.self-signed.cert.pub"
+  export LYNXKITE_USERNAME="admin"
+  export LYNXKITE_PASSWORD="adminpw"
+else
+  export LYNXKITE_ADDRESS="http://localhost:${PORT:-2200}/"
+fi
