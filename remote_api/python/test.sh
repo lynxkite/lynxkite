8578520dbd049628ac3f40c4aee08a41fcc5903f
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2018-06-12 14:59:02 -0400

    Move lk_config to tools.

--- remote_api/python/test.sh
+++ remote_api/python/test.sh
@@ -2,7 +2,7 @@
 # Run Python API tests. You can start LynxKite manually, or use tools/with_lk.sh.
 
 BASEDIR=$(git rev-parse --show-toplevel)
-source $BASEDIR/lk_config.sh
+source $BASEDIR/tools/lk_config.sh
 
 cd $(dirname $0)
 python3 -m mypy lynx --ignore-missing-import
