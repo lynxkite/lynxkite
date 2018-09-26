33f7a1b1af39f924f8876fdd1bdf57acd2fa5142
Daniel Darabos <darabos.daniel@gmail.com>
2018-09-26 18:52:29 +0200

    Test for external computation.

---
+++ remote_api/python/tests/test_external_computation.py
@@ -0,0 +1,34 @@
+import lynx.kite
+import numpy as np
+import os
+import pandas as pd
+import unittest
+
+
+class TestExternalComputation(unittest.TestCase):
+
+  def test_external_computation(self):
+    lk = lynx.kite.LynxKite()
+
+    def title_names(inpath, outpath):
+      df = pd.read_parquet(inpath.replace('file:', ''))
+      df['name'] = np.where(df.gender == 'Female', 'Ms ' + df.name, 'Mr ' + df.name)
+      op = outpath.replace('file:', '')
+      os.makedirs(op, exist_ok=True)
+      df.to_parquet(op + '/part-00000.gzip.parquet', engine='fastparquet', compression='GZIP')
+      with open(op + '/_SUCCESS', 'w'):
+        pass
+
+    @lynx.kite.subworkspace
+    def titled(data, sec=lynx.kite.SideEffectCollector.AUTO):
+      return data.apply(title_names, sec)
+
+    eg = lk.createExampleGraph().sql('select name, gender from vertices')
+    t = titled(eg)
+    t.trigger()
+    self.assertTrue(t.df().equals(pd.DataFrame([
+        ['Mr Adam', 'Male'],
+        ['Ms Eve', 'Female'],
+        ['Mr Bob', 'Male'],
+        ['Mr Isolated Joe', 'Male'],
+    ], columns=['name', 'gender'])))
