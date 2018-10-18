a670713ff1548758de097d611a643e6fdca6e76b
erbenpeter <erben.peter@gmail.com>
2018-10-18 12:18:13 +0200

    The test asserts now on diff signs

--- remote_api/python/tests/test_cleaner.py
+++ remote_api/python/tests/test_cleaner.py
@@ -11,52 +11,99 @@ def stats(data_status):
 
 
 def count_diff(before, after):
-  return dict(
-      total_cnt_diff=after['total_cnt'] - before['total_cnt'],
-      trash_cnt_diff=after['trash_cnt'] - before['trash_cnt'],
-      entities_diff={
-          method: after['entities'][method] - before['entities'][method]
-          for method in before['entities'].keys()}
-  )
+  diff = dict(
+      total_cnt=after['total_cnt'] - before['total_cnt'],
+      trash_cnt=after['trash_cnt'] - before['trash_cnt'])
+  for method in before['entities'].keys():
+    diff[method] = after['entities'][method] - before['entities'][method]
+  return diff
 
 
 class TestCleaner(unittest.TestCase):
   data_status = None
   lk = lynx.kite.LynxKite()
 
-  def print_info(self, msg):
+  def check(self, msg, expected=None):
+    '''The exact number of files and entities are not always predictable, so
+    the test asserts on the sign of the changes. E.g. when we create
+    a new snapshot, we know that the number of files has to increase, and
+    when we delete a snapshot, which referred to computed entitties,
+    we know that the number of "notSnapshotEntities" has to increase.'''
     before = self.data_status
     after = stats(self.lk.get_data_files_status())
-    print('== ' + msg)
     if before:
-      print('Diff:', count_diff(before, after))
+      diff = count_diff(before, after)
+      print(f'  Diff({msg})')
+      if expected:
+        for key in expected.keys():
+          if expected[key] == '+':
+            self.assertGreater(diff[key], 0)
+          elif expected[key] == '0':
+            self.assertEqual(diff[key], 0)
+          elif expected[key] == '-':
+            self.assertLess(diff[key], 0)
+          else:
+            raise Exception('Wrong sign')
     else:
-      print('New state:', after)
+      print(f'Initial state({msg})')
     self.data_status = after
+
+  def test_not_snapshot_cleaning(self):
+    lk = self.lk
+    self.data_status = None
+    self.check('Not snapshot cleaning')
+    lk.remove_name('cln1', force=True)
+    table = lk.uploadCSVNow('x,y,z\n1,11,111\n2,22,222')
+    table.save_snapshot('cln1')
+    self.check('Snapshot created', {'total_cnt': '+'})
+    lk.remove_name('cln1')
+    self.check('Snapshot deleted', {'notSnapshotEntities': '+'})
+    lk.move_to_cleaner_trash('notSnapshotEntities')
+    self.check('After moving to cleaner', {
+        'total_cnt': '-',
+        'trash_cnt': '+',
+        'notSnapshotEntities': '-'})
+    lk.empty_cleaner_trash()
+    self.check('After running "empty cleaner"', {'trash_cnt': '-'})
 
   def test_import_output_cleaning(self):
     lk = self.lk
-    self.print_info('Initial state')
+    self.data_status = None
+    self.check('Import output cleaning')
     table = lk.uploadCSVNow('x,y\n1,11\n2,22')
-    self.print_info('Table uploaded')
+    self.check('Table uploaded', {'total_cnt': '+'})
     lk.move_to_cleaner_trash('notSnapshotOrImportBoxEntities')
-    self.print_info('After moving to cleaner')
+    self.check('After moving to cleaner', {
+        'total_cnt': '-',
+        'trash_cnt': '+'})
     lk.empty_cleaner_trash()
-    self.print_info('After running "empty cleaner"')
-    print(table.df())
+    self.check('After running "empty cleaner"', {'trash_cnt': '-'})
 
   def test_not_snapshot_or_workspace_entities(self):
     lk = self.lk
-    lk.remove_name('cln1', force=True)
-    self.print_info('Initial state')
+    self.data_status = None
+    lk.remove_name('cln2', force=True)
+    self.check('Not snapshot or workspace cleaning')
     table = lk.createVertices(size=12).sql('select * from vertices')
-    table.save_snapshot('cln1')
-    self.print_info('After saving snapshot')
+    table.save_snapshot('cln2')
+    self.check('After saving snapshot', {
+        'total_cnt': '0',
+        'trash_cnt': '0',
+        'notSnapshotOrWorkspaceEntities': '0'})
     table.compute()
-    self.print_info('After triggering computation')
-    lk.remove_name('cln1')
-    self.print_info('After removing snapshot')
+    self.check('After triggering computation', {
+        'total_cnt': '+',
+        'trash_cnt': '0',
+        'notSnapshotEntities': '+'})
+    lk.remove_name('cln2')
+    self.check('After removing snapshot', {
+        'total_cnt': '0',
+        'trash_cnt': '0',
+        'notSnapshotEntities': '+'})
     lk.move_to_cleaner_trash('notSnapshotOrWorkspaceEntities')
-    self.print_info('After moving to cleaner')
+    self.check('After moving to cleaner', {
+        'total_cnt': '-',
+        'trash_cnt': '+',
+        'notSnapshotEntities': '-'})
     lk.empty_cleaner_trash()
-    self.print_info('After running "empty cleaner"')
+    self.check('After running "empty cleaner"', {'trash_cnt': '-'})
