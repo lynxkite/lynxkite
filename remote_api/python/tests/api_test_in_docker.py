ef0199c3187fd1336bef5a5e385c69e3b217c5ac
erbenpeter <erben.peter@gmail.com>
2019-03-21 11:47:03 +0100

    Simplify instructions

--- remote_api/python/tests/api_test_in_docker.py
+++ remote_api/python/tests/api_test_in_docker.py
@@ -1,23 +1,13 @@
 # How to run this example?
 #
-# Before starting docker image
+# 1. Remove the ecosystem/native/test-mount folder
 #
-# 0. Modify `ecosystem/native/test-mount/config/central` for local run:
-#    a) KITE_DATA_DIR=file:/lynxkite/KITE_DATA
-#    b) SPARK_MASTER=local
-#    c) # NUM_EXECUTORS=1
-# 1. Stop mysql server if running, before start the container
-#    `/etc/init.d/mysql stop`
-# 2. Start docker release
-#    `run_docker_release_test.sh ../native/test-mount/`
-# 3. Open bash in running container
-#    `docker exec -it docker_ecosystem_1 bash`
+# 2. Build a new ecosystem-docker-release
 #
-# In the docker container
+# 3. Start docker release
+#    `run_docker_release_test.sh
 #
-# 4. `source /lynxkite/config/central` to set PYTHONPATH
-# 5. Use Python3.6 in docker:
-#    `/opt/conda/bin/python3.6 /lynxkite/apps/remote_api/python/tests/api_test_in_docker.py`
+# 4. Run the below code in a Python notebook in JupytyerLab
 
 import lynx.kite
 lk = lynx.kite.LynxKite()
