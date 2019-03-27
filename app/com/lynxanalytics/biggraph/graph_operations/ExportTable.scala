c7d80079d1dfbae93f87e0681a0034c7da9c13c6
erbenpeter <erben.peter@gmail.com>
2019-03-27 08:43:11 +0100

    Allow overwrite in export

--- app/com/lynxanalytics/biggraph/graph_operations/ExportTable.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/ExportTable.scala
@@ -3,6 +3,7 @@ package com.lynxanalytics.biggraph.graph_operations
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.graph_util.HadoopFile
 import org.apache.spark
+import org.apache.spark.sql.SaveMode
 
 object ExportTable {
   class Input extends MagicInputSignature {
@@ -46,6 +47,7 @@ object ExportTableToCSV extends OpFromJson {
   val timestampFormatParameter = NewParameter("timestampFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
   val dropLeadingWhiteSpaceParameter = NewParameter("dropLeadingWhiteSpace", false)
   val dropTrailingWhiteSpaceParameter = NewParameter("dropTrailingWhiteSpace", false)
+  val overwriteParameter = NewParameter("overwrite", "no")
   def fromJson(j: JsValue) = ExportTableToCSV(
     (j \ "path").as[String], (j \ "header").as[Boolean],
     (j \ "delimiter").as[String], (j \ "quote").as[String],
@@ -56,26 +58,28 @@ object ExportTableToCSV extends OpFromJson {
     timestampFormatParameter.fromJson(j),
     dropLeadingWhiteSpaceParameter.fromJson(j),
     dropTrailingWhiteSpaceParameter.fromJson(j),
-    (j \ "version").as[Int])
+    (j \ "version").as[Int],
+    overwriteParameter.fromJson(j))
 }
 
 case class ExportTableToCSV(path: String, header: Boolean,
     delimiter: String, quote: String, quoteAll: Boolean,
     escape: String, nullValue: String, dateFormat: String, timestampFormat: String,
     dropLeadingWhiteSpace: Boolean, dropTrailingWhiteSpace: Boolean,
-    version: Int)
+    version: Int, overwrite: String)
   extends ExportTable {
   override def toJson = Json.obj(
     "path" -> path, "header" -> header,
     "delimiter" -> delimiter, "quote" -> quote,
-    "version" -> version) ++
+    "version" -> version, "overwrite" -> overwrite) ++
     ExportTableToCSV.quoteAllParameter.toJson(quoteAll) ++
     ExportTableToCSV.escapeParameter.toJson(escape) ++
     ExportTableToCSV.nullValueParameter.toJson(nullValue) ++
     ExportTableToCSV.dateFormatParameter.toJson(dateFormat) ++
     ExportTableToCSV.timestampFormatParameter.toJson(timestampFormat) ++
     ExportTableToCSV.dropLeadingWhiteSpaceParameter.toJson(dropLeadingWhiteSpace) ++
-    ExportTableToCSV.dropTrailingWhiteSpaceParameter.toJson(dropTrailingWhiteSpace)
+    ExportTableToCSV.dropTrailingWhiteSpaceParameter.toJson(dropTrailingWhiteSpace) ++
+    ExportTableToCSV.overwriteParameter.toJson(overwrite)
 
   def exportDataFrame(df: spark.sql.DataFrame) = {
     val file = HadoopFile(path)
@@ -90,25 +94,27 @@ case class ExportTableToCSV(path: String, header: Boolean,
       "ignoreLeadingWhiteSpace" -> (if (dropLeadingWhiteSpace) "true" else "false"),
       "ignoreTrailingWhiteSpaces" -> (if (dropTrailingWhiteSpace) "true" else "false"),
       "header" -> (if (header) "true" else "false"))
-    df.write.format("csv").options(options).save(file.resolvedName)
+    val saveMode = if (overwrite == "yes") SaveMode.Overwrite else SaveMode.ErrorIfExists
+    df.write.mode(saveMode).format("csv").options(options).save(file.resolvedName)
   }
 }
 
 object ExportTableToStructuredFile extends OpFromJson {
   def fromJson(j: JsValue) = ExportTableToStructuredFile(
     (j \ "path").as[String], (j \ "format").as[String],
-    (j \ "version").as[Int])
+    (j \ "version").as[Int], (j \ "overwrite").as[String])
 }
 
-case class ExportTableToStructuredFile(path: String, format: String, version: Int)
+case class ExportTableToStructuredFile(path: String, format: String, version: Int, overwrite: String)
   extends ExportTable {
 
   override def toJson = Json.obj(
-    "path" -> path, "format" -> format, "version" -> version)
+    "path" -> path, "format" -> format, "version" -> version, "overwrite" -> overwrite)
 
   def exportDataFrame(df: spark.sql.DataFrame) = {
     val file = HadoopFile(path)
-    df.write.format(format).save(file.resolvedName)
+    val saveMode = if (overwrite == "yes") SaveMode.Overwrite else SaveMode.ErrorIfExists
+    df.write.mode(saveMode).format(format).save(file.resolvedName)
   }
 }
 
