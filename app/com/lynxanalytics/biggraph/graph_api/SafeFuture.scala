5a78429c2eb6cae5537df272c5705d4e55f35b26
Gabor Olah <gabor.olah@lynxanalytics.com>
2019-05-22 09:10:08 +0200

    Revert SafeFuture and HadoopFile changes

--- app/com/lynxanalytics/biggraph/graph_api/SafeFuture.scala
+++ app/com/lynxanalytics/biggraph/graph_api/SafeFuture.scala
@@ -59,9 +59,6 @@ class SafeFuture[+T] private (val future: Future[T]) {
   def onFailure[U](pf: PartialFunction[Throwable, U])(implicit ec: ExecutionContext) =
     future.onFailure(pf)
 
-  def onComplete[U](f: Try[T] => U)(implicit executor: ExecutionContext) =
-    future.onComplete(f)
-
   def foreach[U](f: T => U)(implicit ec: ExecutionContext) =
     future.foreach(f)
 
