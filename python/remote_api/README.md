ee7f2ce774066210d5184faed28577d6aaf8609f
Matyas Korom <matyas.korom@lynxanalytics.com>
2019-05-28 11:57:52 +0200

    Address review

---
+++ python/remote_api/README.md
@@ -0,0 +1,4 @@
+# LynxKite Python API
+
+This is the official Python client API for [LynxKite](https://lynxkite.com/) from Lynx Analytics.
+See the LynxKite documentation for usage instructions.
--- python/remote_api/setup.py
+++ python/remote_api/setup.py
@@ -1,12 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='lynx-remote-api',
+    name='lynxkite-python-api',
     version='2.10.0',
     install_requires=[
-        'PyYAML',
         'requests',
-        'croniter',
     ],
     extras_require={
         'dev': [
@@ -19,5 +17,15 @@ setup(
     python_requires='>=3.6',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    zip_safe=False,
+    author='Lynx Analytics',
+    author_email='lynxkite@lynxanalytics.com',
+    description='Python API for LynxKite',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://lynxkite.com/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+    ],
 )
