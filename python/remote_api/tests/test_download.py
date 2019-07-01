1a79dba00cc111aa659e25aeba616fa09cfb7e88
Gabor Olah <gabor.olah@lynxanalytics.com>
2019-07-01 14:56:13 +0200

    Fix test

--- python/remote_api/tests/test_download.py
+++ python/remote_api/tests/test_download.py
@@ -14,7 +14,7 @@ class TestDownload(unittest.TestCase):
   def test_downloaded_parquet_is_healthy(self):
     a = self.lk.createVertices(size=self.num_vertices)
     b = a.sql('select * from `vertices`')
-    path = self.lk.exportToParquetNow(b, path=self.export_path).run_export()
+    path = self.lk.exportToParquetNow(b, path=self.export_path, for_download=True).run_export()
     c = self.lk.download_file(path)
     d = self.lk.uploadParquetNow(c)
     count = self.lk.useTableAsVertices(d).sql('select count(*) from `vertices`').df().iloc[0][0]
