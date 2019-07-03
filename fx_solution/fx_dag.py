e22ca3e9c3b8070370a658b81a1323767ff8c4c4
aceten <aht36@georgetown.edu>
2019-07-03 17:26:52 +0800

    Files for onboarding task solution

---
+++ fx_solution/fx_dag.py
@@ -0,0 +1,58 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.sensors.base_sensor_operator import BaseSensorOperator
+
+from datetime import datetime, timedelta
+
+import fx_functions as fx
+
+
+default_args = {
+    'start_date': datetime(2019, 7, 2),
+    'retries': 2,
+    'retry_delay': timedelta(days=1),}
+
+dag = DAG(
+    dag_id='fx_dag',
+    default_args=default_args,
+    schedule_interval='@once')
+
+class data_availability_sensor(BaseSensorOperator):
+    '''
+    Invokes check_data_availability(), which returns a boolean
+    indicating if the desired data is available.
+    '''
+    def __init__(self, *args, **kwargs):
+        super(data_availability_sensor, self).__init__(*args, **kwargs)
+    def poke(self, context):
+        print('Poking {}'.__str__())
+        return fx.check_data_availability(context)
+
+wait_for_data_availability_sensor = data_availability_sensor(
+    task_id='wait_for_data_availability',
+    dag=dag)
+
+
+make_month_df_operator = PythonOperator(
+    task_id='make_month_df',
+    python_callable=fx.make_month_df,
+    provide_context=True,
+    templates_dict={'date': '{{ ds }}'},
+    dag=dag)
+
+make_min_max_parquet_operator = PythonOperator(
+    task_id='make_min_max_parquet',
+    python_callable=fx.make_min_max_parquet,
+    provide_context=True,
+    templates_dict={'date': '{{ ds }}'},
+    dag=dag)
+
+make_max_profit_parquet_operator = PythonOperator(
+    task_id='make_max_profit_parquet',
+    python_callable=fx.make_max_profit_parquet,
+    provide_context=True,
+    templates_dict={'date': '{{ ds }}'},
+    dag=dag)
+
+wait_for_data_availability_sensor >> make_month_df_operator
+make_month_df_operator >> [make_min_max_parquet_operator, make_max_profit_parquet_operator]
