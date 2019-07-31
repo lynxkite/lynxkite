a44acbde3a1cafc89d7658ec0d92b90abb14a92a
Gabor Olah <gabor.olah@lynxanalytics.com>
2019-07-31 12:41:43 +0200

    Remove unused imports

---
+++ python/remote_api/tests/test_hive_export.py
@@ -0,0 +1,16 @@
+import unittest
+import lynx.kite
+
+
+class TestHive(unittest.TestCase):
+  def test_hive_overwrite_with_partitions(self):
+    lk = lynx.kite.LynxKite()
+    table = lk.createExampleGraph().sql('select * from `vertices`')
+    with self.assertRaises(Exception) as context:
+      table.exportToHiveNow(
+          table='out',
+          mode='Drop the table if it already exists',
+          partition_by="name,gender")
+    self.assertTrue(
+        'overwrite mode cannot be used with partition columns' in str(
+            context.exception))
