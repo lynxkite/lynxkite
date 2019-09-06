e45d37b56b45b9bc359bb5c2fb0ae163b402a937
Daniel Darabos <darabos.daniel@gmail.com>
2019-09-06 18:34:20 +0200

    Uncomment and fix up SparkDomainTests.

--- test/com/lynxanalytics/biggraph/graph_api/SparkDomainTest.scala
+++ test/com/lynxanalytics/biggraph/graph_api/SparkDomainTest.scala
@@ -5,73 +5,73 @@ import org.scalatest.FunSuite
 import com.lynxanalytics.biggraph.TestUtils
 import com.lynxanalytics.biggraph.TestUtils.computeProgress
 import com.lynxanalytics.biggraph.controllers
-import com.lynxanalytics.biggraph.graph_operations
-import com.lynxanalytics.biggraph.graph_operations.ExampleGraph
+import com.lynxanalytics.biggraph.graph_operations.EnhancedExampleGraph
 import com.lynxanalytics.biggraph.graph_util.HadoopFile
 
 class SparkDomainTest extends FunSuite with TestMetaGraphManager with TestDataManager {
-  /*
+  def newDataManager(sd: SparkDomain) = new DataManager(Seq(new ScalaDomain, sd))
+
   test("We can reload a graph from disk without recomputing it") {
     val metaManager = cleanMetaManager
-    val dataManager1 = cleanDataManager
-    val operation = ExampleGraph()
+    val sparkDomain1 = cleanSparkDomain
+    val dataManager1 = newDataManager(sparkDomain1)
+    val operation = EnhancedExampleGraph()
     val instance = metaManager.apply(operation)
     val names = instance.outputs.attributes('name).runtimeSafeCast[String]
     val greeting = instance.outputs.scalars('greeting).runtimeSafeCast[String]
-    val data1: AttributeData[String] = dataManager1.get(names)
-    val scalarData1: ScalarData[String] = dataManager1.get(greeting)
-    val dataManager2 = new DataManager(sparkSession, dataManager1.repositoryPath)
-    val data2 = dataManager2.get(names)
+    val data1 = GraphTestUtils.get(names)(metaManager, dataManager1)
+    val scalarData1 = dataManager1.get(greeting)
+    val sparkDomain2 = new SparkDomain(sparkSession, sparkDomain1.repositoryPath)
+    val dataManager2 = newDataManager(sparkDomain2)
+    val data2 = GraphTestUtils.get(names)(metaManager, dataManager2)
     val scalarData2 = dataManager2.get(greeting)
     assert(data1 ne data2)
-    assert(TestUtils.RDDToSortedString(data1.rdd) ==
-      TestUtils.RDDToSortedString(data2.rdd))
+    assert(data1 == data2)
     assert(scalarData1 ne scalarData2)
-    assert(scalarData1.value == scalarData2.value)
+    assert(scalarData1 == scalarData2)
     assert(operation.executionCounter == 1)
   }
 
   test("Ephemeral repo can read main repo") {
     val metaManager = cleanMetaManager
-    val dataManager1 = cleanDataManager
-    val operation = ExampleGraph()
+    val sparkDomain1 = cleanSparkDomain
+    val dataManager1 = newDataManager(sparkDomain1)
+    val operation = EnhancedExampleGraph()
     val instance = metaManager.apply(operation)
     val names = instance.outputs.attributes('name).runtimeSafeCast[String]
     val greeting = instance.outputs.scalars('greeting).runtimeSafeCast[String]
-    val data1: AttributeData[String] = dataManager1.get(names)
-    val scalarData1: ScalarData[String] = dataManager1.get(greeting)
-    val dataManager2 = {
-      val tmpDM = cleanDataManager
-      new DataManager(
-        sparkSession, dataManager1.repositoryPath,
-        ephemeralPath = Some(tmpDM.repositoryPath))
+    dataManager1.get(greeting)
+    val sparkDomain2 = {
+      val tmpSD = cleanSparkDomain
+      new SparkDomain(
+        sparkSession, sparkDomain1.repositoryPath,
+        ephemeralPath = Some(tmpSD.repositoryPath))
     }
-    assert(computeProgress(dataManager2, names) == 1.0)
-    assert(computeProgress(dataManager2, greeting) == 1.0)
+    assert(sparkDomain2.has(names))
+    assert(sparkDomain2.has(greeting))
   }
 
   test("Ephemeral repo writes to ephemeral directory") {
     val metaManager = cleanMetaManager
-    val dataManager1 = {
-      val dm1 = cleanDataManager
-      val dm2 = cleanDataManager
-      new DataManager(
-        sparkSession, dm1.repositoryPath,
-        ephemeralPath = Some(dm2.repositoryPath))
+    val sparkDomain1 = {
+      val sd1 = cleanSparkDomain
+      val sd2 = cleanSparkDomain
+      new SparkDomain(
+        sparkSession, sd1.repositoryPath,
+        ephemeralPath = Some(sd2.repositoryPath))
     }
-    val operation = ExampleGraph()
+    val operation = EnhancedExampleGraph()
     val instance = metaManager.apply(operation)
     val names = instance.outputs.attributes('name).runtimeSafeCast[String]
     val greeting = instance.outputs.scalars('greeting).runtimeSafeCast[String]
-    val data1: AttributeData[String] = dataManager1.get(names)
-    val scalarData1: ScalarData[String] = dataManager1.get(greeting)
-    val dataManagerMain = new DataManager(sparkSession, dataManager1.repositoryPath)
-    assert(computeProgress(dataManagerMain, names) == 0.0)
-    assert(computeProgress(dataManagerMain, greeting) == 0.0)
-    val dataManagerEphemeral = new DataManager(sparkSession, dataManager1.ephemeralPath.get)
-    assert(computeProgress(dataManagerEphemeral, names) == 1.0)
-    assert(computeProgress(dataManagerEphemeral, greeting) == 1.0)
-  }*/
+    sparkDomain1.await(sparkDomain1.compute(names.source))
+    val sparkDomainMain = new SparkDomain(sparkSession, sparkDomain1.repositoryPath)
+    assert(!sparkDomainMain.has(names))
+    assert(!sparkDomainMain.has(greeting))
+    val sparkDomainEphemeral = new SparkDomain(sparkSession, sparkDomain1.ephemeralPath.get)
+    assert(sparkDomainEphemeral.has(names))
+    assert(sparkDomainEphemeral.has(greeting))
+  }
 
   /*
   // TODO: Adapt to boxes.
