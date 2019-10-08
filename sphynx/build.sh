9fc69e05deee994406182908753b4fb796260264
Hanna Gábor <gahanna999@gmail.com>
2019-10-08 14:37:59 +0200

    Build Sphynx nicer.

---
+++ sphynx/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -xue
+# Generates Java and Go interfaces from proto files. These interfaces are used
+# for LynxKite-Sphynx communication.
+# Compiles Sphynx.
+
+cd $(dirname $0)
+REPO=$(realpath .)
+PROTO_SOURCE_DIR="proto"
+PROTO_SOURCE_FILE="sphynx.proto"
+GRPC_JAVA_VERSION="1.24.0"
+
+# Generate the gRPC Java interfaces.
+GRPC_JAVA=protoc-gen-grpc-java-$GRPC_JAVA_VERSION-linux-x86_32.exe
+wget -nc https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/$GRPC_JAVA_VERSION/$GRPC_JAVA
+chmod +x $REPO/$GRPC_JAVA
+protoc --plugin=protoc-gen-grpc-java=$REPO/$GRPC_JAVA --grpc-java_out=../app \
+	--proto_path=$PROTO_SOURCE_DIR $PROTO_SOURCE_FILE
+protoc -I=$PROTO_SOURCE_DIR --java_out=../app $PROTO_SOURCE_DIR/$PROTO_SOURCE_FILE
+
+GO_PKG=github.com/biggraph/biggraph/sphynx
+export GOPATH=$REPO/go
+export GOCACHE=$REPO/go/.cache
+
+# Generate the gRPC Go interfaces.
+go get -u google.golang.org/grpc
+go get -u github.com/golang/protobuf/protoc-gen-go
+PATH=$GOPATH/bin:$PATH protoc $PROTO_SOURCE_DIR/$PROTO_SOURCE_FILE --go_out=plugins=grpc,import_path=$PROTO_SOURCE_DIR:.
+
+# Compile Sphynx server.
+SERVER=$GOPATH/bin/server
+cd $GOPATH/src/$GO_PKG
+go fmt $GO_PKG/server
+go get -v $GO_PKG/server
