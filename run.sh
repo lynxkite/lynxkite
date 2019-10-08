87dda7a007caebf69bd25509344c78787acafd66
Hanna Gábor <gahanna999@gmail.com>
2019-10-08 12:21:40 +0200

    Sphynxmurderer.

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
-#!/bin/sh -xue
+#!/bin/bash -xue
 
 cd $(dirname $0)
 
 make backend
 tools/install_spark.sh
 sphynx/go/bin/server &
+SPHYNX_PID=$!
+function kill_sphynx {
+  kill -9 $SPHYNX_PID
+}
+trap kill_sphynx EXIT ERR
+
 stage/bin/biggraph "$@" interactive
