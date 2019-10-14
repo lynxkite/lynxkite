4d2083a1cbcd7d2b42bd1fbc68c4058539ad7bfc
Hanna Gábor <gahanna999@gmail.com>
2019-10-14 17:01:29 +0200

    Start Sphynx server at a given port.

--- run.sh
+++ run.sh
@@ -4,6 +4,17 @@ cd $(dirname $0)
 
 make backend
 tools/install_spark.sh
+KITE_SITE_CONFIG=${KITE_SITE_CONFIG:-$HOME/.kiterc}
+
+if [ -f ${KITE_SITE_CONFIG} ]; then
+  >&2 echo "Loading configuration from: ${KITE_SITE_CONFIG}"
+  source ${KITE_SITE_CONFIG}
+else
+  >&2 echo "Warning, no LynxKite Site Config found at: ${KITE_SITE_CONFIG}"
+  >&2 echo "Default location is $HOME/.kiterc, but you can override via the environment variable:"
+  >&2 echo "KITE_SITE_CONFIG"
+fi
+
 sphynx/go/bin/server &
 SPHYNX_PID=$!
 function kill_sphynx {
