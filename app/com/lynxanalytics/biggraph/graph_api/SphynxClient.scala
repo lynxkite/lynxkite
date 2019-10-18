0c0dca1f74dfa73fc5a49d49995d433945b647d5
Hanna Gábor <gahanna999@gmail.com>
2019-10-18 16:13:30 +0200

    Make encryption optional.

--- app/com/lynxanalytics/biggraph/graph_api/SphynxClient.scala
+++ app/com/lynxanalytics/biggraph/graph_api/SphynxClient.scala
@@ -3,13 +3,25 @@ package com.lynxanalytics.biggraph.graph_api
 import _root_.io.grpc.netty.NettyChannelBuilder
 import _root_.io.grpc.netty.GrpcSslContexts
 import _root_.io.grpc.StatusRuntimeException
+import _root_.io.grpc.ManagedChannelBuilder
 import com.lynxanalytics.biggraph.graph_api.proto._
+import com.lynxanalytics.biggraph.graph_util.LoggedEnvironment
 import java.io.File
 
 class SphynxClient(host: String, port: Int) {
-  private val channel = NettyChannelBuilder.forAddress(host, port)
-    .sslContext(GrpcSslContexts.forClient().trustManager(new File("cert.pem")).build())
-    .build();
+
+  private val channel = {
+    val cert = LoggedEnvironment.envOrNone("SPHYNX_CERT")
+    cert match {
+      case Some(cert) => NettyChannelBuilder.forAddress(host, port)
+        .sslContext(GrpcSslContexts.forClient().trustManager(new File(cert)).build())
+        .build();
+      case None => {
+        println("Using unsecure channel to communicate with Sphynx.")
+        ManagedChannelBuilder.forAddress(host, port).usePlaintext().build
+      }
+    }
+  }
 
   private val blockingStub = SphynxGrpc.newBlockingStub(channel)
 
