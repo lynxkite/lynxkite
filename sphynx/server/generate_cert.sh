55f9a5c405657d85f333661759b575c6afb0d815
Hanna Gábor <gahanna999@gmail.com>
2019-11-06 13:59:17 +0100

    Start Sphynx only if needed. Generate certificate automatically.

--- sphynx/server/generate_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xue
-# Generates certificate for the gRPC server.
-
-cd $(dirname $0)
-
-openssl req -x509 -sha256 -newkey rsa:4096 -keyout private-key.pem -out cert.pem -days 365 -nodes \
-  -subj "/C=/ST=/L=/O=Lynx Analytics/OU=Org/CN=localhost"
