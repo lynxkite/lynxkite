f6a90c629c0295b05c58ac7205154ac6d0427997
Gabor Olah <gabor.olah@lynxanalytics.com>
2019-11-07 09:31:51 +0100

    Adding temporary globalSQL2 function

--- app/com/lynxanalytics/biggraph/graph_operations/TableToScalar.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/TableToScalar.scala
@@ -2,11 +2,16 @@ package com.lynxanalytics.biggraph.graph_operations
 
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.SQLHelper
+import org.apache.spark.rdd.RDD
 
 import scala.reflect.runtime.universe.TypeTag
 
+case class TableContents(
+    header: List[(String, TypeTag[Any])],
+    data: List[List[DynamicValue]])
+
 object TableToScalar extends OpFromJson {
-  override def fromJson(j: JsValue) = new TableToScalar()
+  override def fromJson(j: JsValue) = new TableToScalar((j \ "maxRows").as[Int])
   class Input extends MagicInputSignature {
     val tab = table
   }
@@ -14,23 +19,22 @@ object TableToScalar extends OpFromJson {
   class Output(implicit
       instance: MetaGraphOperationInstance,
       inputs: Input) extends MagicOutput(instance) {
-    val header = scalar[List[(String, String)]]
-    val data = scalar[List[List[DynamicValue]]]
+    val tableContents = scalar[TableContents]
   }
 
-  def run(tab: Table): Unit = {
+  def run(tab: Table, maxRows: Int = -1)(implicit m: MetaGraphManager) = {
     import Scripting._
-    val op = TableToScalar()
-
+    val op = TableToScalar(maxRows)
+    op(op.tab, tab).result.tableContents
   }
 }
 
 import TableToScalar._
-case class TableToScalar() extends SparkOperation[Input, Output] {
+case class TableToScalar(maxRows: Int) extends SparkOperation[Input, Output] {
   def outputMeta(instance: MetaGraphOperationInstance) = new Output()(instance, inputs)
   @transient override lazy val inputs = new Input()
   override val isHeavy: Boolean = false
-  override def toJson = Json.obj()
+  override def toJson = Json.obj("maxRows" -> maxRows)
 
   def execute(
     inputDatas: DataSet,
@@ -40,10 +44,19 @@ case class TableToScalar() extends SparkOperation[Input, Output] {
     implicit val ds = inputDatas
     implicit val sd = rc.sparkDomain
     val df = inputs.tab.data.df
-    val columns = df.schema.toList.map { field =>
+    val header = df.schema.toList.map { field =>
       field.name -> SQLHelper.typeTagFromDataType(field.dataType).asInstanceOf[TypeTag[Any]]
     }
-    output(o.data, List(List()))
-    output(o.header, List())
+    def zipper(row: Seq[Any]) = {
+      row.toList.zip(header).map {
+        case (null, _) => DynamicValue("null", defined = false)
+        case (item, (_, tt)) => DynamicValue.convert(item)(tt)
+      }
+    }
+    val rdd = SQLHelper.toSeqRDD(df)
+    val data =
+      if (maxRows < 0) rdd.collect().map { row => zipper(row) }
+      else rdd.take(maxRows).map { row => zipper(row) }
+    output(o.tableContents, TableContents(header, data.toList))
   }
 }
