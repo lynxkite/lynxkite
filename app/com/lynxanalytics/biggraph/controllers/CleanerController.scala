e40e90df7da9976341acf9f64b472476a0e3d4e4
Gabor Olah <gabor.olah@lynxanalytics.com>
2019-11-29 13:16:45 +0100

    Block computations when the cleaner is at work

--- app/com/lynxanalytics/biggraph/controllers/CleanerController.scala
+++ app/com/lynxanalytics/biggraph/controllers/CleanerController.scala
@@ -7,8 +7,6 @@ import java.util.UUID
 import scala.collection.immutable.Set
 import scala.collection.immutable.Map
 import scala.collection.mutable.HashSet
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.Queue
 
 import com.lynxanalytics.biggraph.BigGraphEnvironment
 import com.lynxanalytics.biggraph.graph_util.LoggedEnvironment
@@ -259,14 +257,20 @@ class CleanerController(environment: BigGraphEnvironment, ops: OperationReposito
     log.info(s"${user.email} attempting to move data files to trash using '${req.method}'.")
     val files = getAllFiles(trash = false)
     val filesToKeep = methods.find(m => m.id == req.method).get.filesToKeep()
-    moveToTrash(io.PartitionedDir, files.partitioned.keys.toSet -- filesToKeep)
-    moveToTrash(io.EntitiesDir, files.entities.keys.toSet -- filesToKeep)
-    moveToTrash(io.OperationsDir, files.operations.keys.toSet -- filesToKeep)
-    moveToTrash(io.ScalarsDir, files.scalars.keys.toSet -- filesToKeep)
-    moveToTrash(io.TablesDir, files.tables.keys.toSet -- filesToKeep)
-    moveToTrash(io.BroadcastsDir, files.broadcasts.keys.toSet -- filesToKeep)
-    environment.sparkDomain.clear()
-    environment.dataManager.clear()
+    val filesToDelete = files.all.keys.toSet -- filesToKeep
+    if (filesToDelete.nonEmpty) {
+      environment.dataManager.synchronized {
+        environment.dataManager.waitAllFutures()
+        moveToTrash(io.PartitionedDir, files.partitioned.keys.toSet -- filesToKeep)
+        moveToTrash(io.EntitiesDir, files.entities.keys.toSet -- filesToKeep)
+        moveToTrash(io.OperationsDir, files.operations.keys.toSet -- filesToKeep)
+        moveToTrash(io.ScalarsDir, files.scalars.keys.toSet -- filesToKeep)
+        moveToTrash(io.TablesDir, files.tables.keys.toSet -- filesToKeep)
+        moveToTrash(io.BroadcastsDir, files.broadcasts.keys.toSet -- filesToKeep)
+        environment.sparkDomain.clear()
+        environment.dataManager.clear()
+      }
+    }
   }
 
   private def moveToTrash(dir: String, files: Set[String]): Unit = {
