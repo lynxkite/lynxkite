63191ad7d97000019b00e164f18e9e90dc6e8af2
Gabor Olah <gabor.olah@lynxanalytics.com>
2020-01-06 08:17:23 +0100

    Simplify pulled over code; register output

--- sphynx/server/pulled_over_vertex_attribute.go
+++ sphynx/server/pulled_over_vertex_attribute.go
@@ -12,13 +12,13 @@ func init() {
 			function := ea.getEdgeBundle("function")
 			attributeEntity := ea.inputs["originalAttr"]
 
-			edgeMapping := make(map[int64]int64, len(function.Src))
+			dstIdToSrcId := make(map[int64]int64, len(function.Src))
 			for i, _ := range function.Src {
-				edgeMapping[function.Src[i]] = function.Dst[i]
+				dstIdToSrcId[function.Src[i]] = function.Dst[i]
 			}
-			vertexMapping := make(map[int64]int64, len(originalVS.Mapping))
+			srcIdToIndex := make(map[int64]int64, len(originalVS.Mapping))
 			for i, k := range originalVS.Mapping {
-				vertexMapping[k] = int64(i)
+				srcIdToIndex[k] = int64(i)
 			}
 			switch attr := attributeEntity.(type) {
 			case *DoubleAttribute:
@@ -26,12 +26,10 @@ func init() {
 					make([]float64, len(destinationVS.Mapping)),
 					make([]bool, len(destinationVS.Mapping)),
 				}
-				for i, k := range destinationVS.Mapping {
-					a := edgeMapping[k]
-					b := vertexMapping[a]
-					srcIdx := b
-					dstIdx := int64(i)
-					dst.Values[dstIdx] = attr.Values[srcIdx]
+				for dstIdx, dstId := range destinationVS.Mapping {
+					srcId := dstIdToSrcId[dstId]
+					index := srcIdToIndex[srcId]
+					dst.Values[dstIdx] = attr.Values[index]
 					dst.Defined[dstIdx] = true
 				}
 				ea.output("pulledAttr", &dst)
@@ -40,29 +38,27 @@ func init() {
 					make([]string, len(destinationVS.Mapping)),
 					make([]bool, len(destinationVS.Mapping)),
 				}
-				for i, k := range destinationVS.Mapping {
-					a := edgeMapping[k]
-					b := vertexMapping[a]
-					srcIdx := b
-					dstIdx := int64(i)
-					dst.Values[dstIdx] = attr.Values[srcIdx]
+				for dstIdx, dstId := range destinationVS.Mapping {
+					srcId := dstIdToSrcId[dstId]
+					index := srcIdToIndex[srcId]
+					dst.Values[dstIdx] = attr.Values[index]
 					dst.Defined[dstIdx] = true
 				}
+				ea.output("pulledAttr", &dst)
 			case *DoubleTuple2Attribute:
 				dst := DoubleTuple2Attribute{
 					make([]float64, len(destinationVS.Mapping)),
 					make([]float64, len(destinationVS.Mapping)),
 					make([]bool, len(destinationVS.Mapping)),
 				}
-				for i, k := range destinationVS.Mapping {
-					a := edgeMapping[k]
-					b := vertexMapping[a]
-					srcIdx := b
-					dstIdx := int64(i)
-					dst.Values1[dstIdx] = attr.Values1[srcIdx]
-					dst.Values2[dstIdx] = attr.Values2[srcIdx]
+				for dstIdx, dstId := range destinationVS.Mapping {
+					srcId := dstIdToSrcId[dstId]
+					index := srcIdToIndex[srcId]
+					dst.Values1[dstIdx] = attr.Values1[index]
+					dst.Values2[dstIdx] = attr.Values2[index]
 					dst.Defined[dstIdx] = true
 				}
+				ea.output("pulledAttr", &dst)
 			default:
 				return fmt.Errorf("Not attribute type: %v", attr)
 			}
