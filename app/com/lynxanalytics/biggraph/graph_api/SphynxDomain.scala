568d0cdd7ba20cffd8f92c7db185cc4254951652
Hanna Gábor <gahanna999@gmail.com>
2020-01-06 14:43:06 +0100

    Relocate vertex set from Spark to Unordered Sphynx.

--- app/com/lynxanalytics/biggraph/graph_api/SphynxDomain.scala
+++ app/com/lynxanalytics/biggraph/graph_api/SphynxDomain.scala
@@ -4,6 +4,12 @@ package com.lynxanalytics.biggraph.graph_api
 
 import com.lynxanalytics.biggraph.graph_util
 import play.api.libs.json.Json
+import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import java.nio.file.{ Paths, Files }
+import java.io.SequenceInputStream
+import scala.collection.JavaConversions.asJavaEnumeration
 
 abstract class SphynxDomain(host: String, port: Int, certDir: String) extends Domain {
   implicit val executionContext =
@@ -40,6 +46,7 @@ class SphynxMemory(host: String, port: Int, certDir: String) extends SphynxDomai
   override def canRelocateFrom(source: Domain): Boolean = {
     source match {
       case _: OrderedSphynxDisk => true
+      case _: UnorderedSphynxDisk => true
       case _ => false
     }
   }
@@ -110,6 +117,7 @@ class UnorderedSphynxDisk(host: String, port: Int, certDir: String, val dataDir:
   override def canRelocateFrom(source: Domain): Boolean = {
     source match {
       case _: SphynxMemory => true
+      case _: SparkDomain => true
       case _ => false
     }
   }
@@ -123,6 +131,35 @@ class UnorderedSphynxDisk(host: String, port: Int, certDir: String, val dataDir:
           case a: Attribute[_] => client.writeToUnorderedDisk(a)
           case _ => throw new AssertionError(s"Cannot fetch $e from $source")
         }
+      }
+      case source: SparkDomain => {
+        val middlePath = source.repositoryPath / "to_sphynx" / e.gUID.toString
+        // TODO: check if middlePath contains something or not.
+        val future = SafeFuture.async(source.getData(e) match {
+          case v: VertexSetData => {
+            val rdd = v.rdd.map {
+              case (k, _) => Row(k)
+            }
+            val schema = StructType(Seq(StructField("Id", LongType, false)))
+            val df = source.sparkSession.createDataFrame(rdd, schema)
+            df.show()
+            df.write.parquet(middlePath.resolvedName)
+            val dstPath = Paths.get(s"${dataDir}/${e.gUID.toString}")
+            val files = (middlePath / "part-*").list
+            val stream = new SequenceInputStream(files.view.map(_.open).iterator)
+            try {
+              Files.copy(stream, dstPath)
+            } catch {
+              case err: Throwable => println("eeeeeeeeeeeeeeeee", err)
+            } finally {
+              stream.close()
+            }
+            val df3 = source.sparkSession.read.parquet(s"${dataDir}/${e.gUID.toString}")
+            df3.show()
+          }
+          case _ => ???
+        })
+        future
       }
     }
   }
