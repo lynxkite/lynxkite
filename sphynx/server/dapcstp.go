af92925eb679d3a38f1517438623aff9aac94cae
Gabor Olah <gabor.olah@lynxanalytics.com>
2020-01-07 16:17:11 +0100

    Add dapcstp.go

---
+++ sphynx/server/dapcstp.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	operationRepository["Dapcstp"] = Operation{
+		execute: func(ea *EntityAccessor) error {
+			gain := ea.getDoubleAttribute("gain")
+			apcost := ea.getDoubleAttribute("apcost")
+			ap := ea.getDoubleAttribute("ap")
+			cost := ea.getDoubleAttribute("cost")
+			fmt.Printf("gain: %v\napcost: %v\nap: %vcost: %v\n", gain, apcost, ap, cost)
+			o := &DoubleAttribute{
+				Defined: make([]bool, len(cost.Values)),
+				Values:  make([]float64, len(cost.Values)),
+			}
+			for i := range o.Values {
+				o.Values[i] = float64(i % 2)
+				o.Defined[i] = true
+			}
+			ea.output("path", o)
+			return nil
+		},
+	}
+}
