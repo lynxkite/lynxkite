a26574e7f8f6c3c11047f0914e8a9fec3e5da460
Hanna Gábor <gahanna999@gmail.com>
2020-01-16 14:43:04 +0100

    Make DoulbeTuple2Attribute to have only one value field.

--- sphynx/server/unordered_disk_io.go
+++ sphynx/server/unordered_disk_io.go
@@ -163,9 +163,8 @@ func (s *Server) WriteToUnorderedDisk(ctx context.Context, in *pb.WriteToUnorder
 			if def {
 				sparkId := vs1.MappingToUnordered[sphynxId]
 				err := pw.Write(SingleDoubleTuple2Attribute{
-					Id:     sparkId,
-					Value1: e.Values1[sphynxId],
-					Value2: e.Values2[sphynxId],
+					Id:    sparkId,
+					Value: e.Values[sphynxId],
 				})
 				if err != nil {
 					return nil, fmt.Errorf("Failed to write parquet file: %v", err)
@@ -383,19 +382,16 @@ func (s *Server) ReadFromUnorderedDisk(
 				pr.ReadStop()
 				rawAttribute = append(rawAttribute, partialRawAttribute...)
 			}
-			values1 := make([]float64, numVS)
-			values2 := make([]float64, numVS)
+			values := make([]DoubleTuple2AttributeValue, numVS)
 			defined := make([]bool, numVS)
 			mappingToOrdered := vs.GetMappingToOrdered()
 			for _, singleAttr := range rawAttribute {
 				orderedId := mappingToOrdered[singleAttr.Id]
-				values1[orderedId] = singleAttr.Value1
-				values2[orderedId] = singleAttr.Value2
+				values[orderedId] = singleAttr.Value
 				defined[orderedId] = true
 			}
 			entity = &DoubleTuple2Attribute{
-				Values1: values1,
-				Values2: values2,
+				Values:  values,
 				Defined: defined,
 			}
 		default:
