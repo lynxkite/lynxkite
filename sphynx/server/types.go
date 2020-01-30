754649cccbf309f473e52b070715df45728b4af9
Daniel Darabos <darabos.daniel@gmail.com>
2020-01-30 14:28:16 +0100

    Start adding DoubleVectorAttribute.

--- sphynx/server/types.go
+++ sphynx/server/types.go
@@ -88,3 +88,9 @@ type DoubleTuple2Attribute struct {
 	Values  []DoubleTuple2AttributeValue
 	Defined []bool
 }
+
+type DoubleVectorAttributeValue []double
+type DoubleVectorAttribute struct {
+	Values  []DoubleVectorAttributeValue
+	Defined []bool
+}
