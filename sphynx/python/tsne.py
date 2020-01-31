e547fa5721644ac83b491495ad0b32354fb6b813
Daniel Darabos <darabos.daniel@gmail.com>
2020-01-31 17:31:29 +0100

    Struggle with Vector[Double] in PyArrow.

--- sphynx/python/tsne.py
+++ sphynx/python/tsne.py
@@ -3,6 +3,6 @@ from sklearn.manifold import TSNE
 from . import util
 
 op = util.Op()
-x = op.input('vector')
+x = op.input('vector', type=util.DoubleVectorAttribute)
 z = TSNE().fit_transform(x)
-op.output_tuple2('embedding', z)
+op.output('embedding', z, type=util.DoubleVectorAttribute)
