c4657c0c145002f84cdef2d63b7770d7d7c9908b
Daniel Darabos <darabos.daniel@gmail.com>
2020-02-05 19:14:03 +0100

    Box for importing PyTorch Geometric datasets.

---
+++ sphynx/python/datasets.py
@@ -0,0 +1,16 @@
+'''Creates a node embedding using PyTorch Geometric.'''
+import torch_geometric.datasets as ds
+from . import util
+
+op = util.Op()
+name = op.params["name"]
+print('loading dataset', name)
+if name == 'Karate Club':
+  data = ds.KarateClub().data
+else:
+  data = ds.Planetoid('/tmp/' + name, name).data
+
+op.output_vs('vs', len(data.x))
+op.output_es('es', data.edge_index)
+op.output('x', data.x, type=util.DoubleVectorAttribute)
+op.output('y', data.y, type=util.DoubleAttribute)
