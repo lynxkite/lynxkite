a630f245be7184b9d609cff938a0b5c69a919d23
Daniel Darabos <darabos.daniel@gmail.com>
2020-02-06 15:11:13 +0100

    Allow access without login.

--- app/com/lynxanalytics/biggraph/graph_util/HadoopFile.scala
+++ app/com/lynxanalytics/biggraph/graph_util/HadoopFile.scala
@@ -337,7 +337,7 @@ class HadoopFile private (
 
   def writeAllowedFrom(user: com.lynxanalytics.biggraph.serving.User): Boolean = {
     val acl = PrefixRepository.getWriteACL(prefixSymbol)
-    aclContains(acl, user)
+    user != com.lynxanalytics.biggraph.serving.User.notLoggedIn && aclContains(acl, user)
   }
 
 }
