b4626d1c336742b2e7a57c3bfdaf5579ae4e7df0
Hanna Gábor <gahanna999@gmail.com>
2020-02-20 15:58:41 +0100

    Store conv_layers in a nicer way.

--- sphynx/python/models.py
+++ sphynx/python/models.py
@@ -14,11 +14,13 @@ class GCNConvNet(torch.nn.Module):
     self.num_classes = num_classes
     self.conv_op = conv_op
     if conv_op == "GCNConv":
+      self.conv_layers = []
       sizes = [in_dim] + [hidden_size] * (num_conv_layers - 1) + [out_dim]
       zipped_sizes = zip(sizes[:-1], sizes[1:])
-      # To make the module aware of its parameters, we set them as attributes.
       for (i, (s1, s2)) in enumerate(zipped_sizes):
-        self.__setattr__(f'conv{i}', GCNConv(s1, s2))
+        conv = GCNConv(s1, s2)
+        self.conv_layers.append(conv)
+        self.add_module(f'conv{i}', conv)
     elif conv_op == "GatedGraphConv":
       self.gated_conv = GatedGraphConv(hidden_size, num_layers=num_conv_layers)
       self.lin1 = torch.nn.Linear(in_dim, hidden_size)
@@ -29,12 +31,11 @@ class GCNConvNet(torch.nn.Module):
   def forward(self, data):
     x, edge_index = data.x, data.edge_index
     if self.conv_op == "GCNConv":
-      for i in range(self.num_conv_layers - 1):
-        conv = self.__getattr__(f'conv{i}')
+      for conv in self.conv_layers[:-1]:
         x = conv(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
-      conv = self.__getattr__(f'conv{self.num_conv_layers - 1}')
+      conv = self.conv_layers[-1]
       x = conv(x, edge_index)
     elif self.conv_op == "GatedGraphConv":
       x = self.lin1(x)
@@ -52,11 +53,14 @@ class GCNConvNetForRegression(torch.nn.Module):
     self.num_conv_layers = num_conv_layers
     self.conv_op = conv_op
     if self.conv_op == "GCNConv":
+      self.conv_layers = []
       sizes = [in_dim] + [hidden_size] * num_conv_layers
       zipped_sizes = zip(sizes[:-1], sizes[1:])
       # To make the module aware of its parameters, we set them as attributes.
       for (i, (s1, s2)) in enumerate(zipped_sizes):
-        self.__setattr__(f'conv{i}', GCNConv(s1, s2))
+        conv = GCNConv(s1, s2)
+        self.conv_layers.append(conv)
+        self.add_module(f'conv{i}', conv)
     elif self.conv_op == "GatedGraphConv":
       self.gated_conv = GatedGraphConv(hidden_size, num_layers=num_conv_layers)
       self.lin1 = torch.nn.Linear(in_dim, hidden_size)
@@ -68,12 +72,11 @@ class GCNConvNetForRegression(torch.nn.Module):
   def forward(self, data):
     x, edge_index = data.x, data.edge_index
     if self.conv_op == "GCNConv":
-      for i in range(self.num_conv_layers - 1):
-        conv = self.__getattr__(f'conv{i}')
+      for conv in self.conv_layers[:-1]:
         x = conv(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
-      conv = self.__getattr__(f'conv{self.num_conv_layers - 1}')
+      conv = self.conv_layers[-1]
       x = conv(x, edge_index)
       x = F.relu(x)
       x = self.lin(x)
