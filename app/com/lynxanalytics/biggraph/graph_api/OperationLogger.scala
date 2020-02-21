acc2d320a307398837ddf8bc2124669f2a24db0c
Gabor <gabor.olah@lynxanalytics.com>
2020-02-21 10:39:32 +0100

    Log relocation times as well

--- app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
+++ app/com/lynxanalytics/biggraph/graph_api/OperationLogger.scala
@@ -2,14 +2,15 @@
 
 package com.lynxanalytics.biggraph.graph_api
 
+import java.util.UUID
+
 import com.lynxanalytics.biggraph.graph_util.{ KiteInstanceInfo, LoggedEnvironment }
 import com.lynxanalytics.biggraph.{ bigGraphLogger => log }
 import com.lynxanalytics.biggraph.graph_util.Timestamp
 import sun.misc.Signal
 import sun.misc.SignalHandler
 
-object OperationLogger {
-  private var unique = 0L
+object PerfLoggerPhase {
   private var phase = 0L
 
   def getPhase() = synchronized {
@@ -25,17 +26,33 @@ object OperationLogger {
   })
 }
 
+abstract class PerformanceLogger {
+  def write(): Unit
+  protected def elapsed() = System.currentTimeMillis() - startTime
+  protected val marker: String
+  protected val kiteVersion = KiteInstanceInfo.kiteVersion
+  protected val sparkVersion = KiteInstanceInfo.sparkVersion
+  protected val instanceName = KiteInstanceInfo.instanceName
+  protected val phase = PerfLoggerPhase.getPhase()
+  protected val startTime = System.currentTimeMillis()
+}
+
 class OperationLogger(
     instance: MetaGraphOperationInstance,
-    domainName: String) {
-  private val marker = "OPERATION_LOGGER_MARKER"
-  private val kiteVersion = KiteInstanceInfo.kiteVersion
-  private val sparkVersion = KiteInstanceInfo.sparkVersion
-  private val instanceName = KiteInstanceInfo.instanceName
-  private val phase = OperationLogger.getPhase()
-  private val startTime = System.currentTimeMillis()
+    domainName: String) extends PerformanceLogger {
+  protected val marker: String = "OPERATION_LOGGER_MARKER"
+
   def write(): Unit = {
-    val elapsed = System.currentTimeMillis() - startTime
-    log.info(s"${marker}\t${phase}\t${elapsed}\t${domainName}\t${instance}")
+    log.info(s"${marker}\t${phase}\t${elapsed()}\t${domainName}\t${instance}")
+  }
+}
+
+class RelocationLogger(
+    guid: UUID,
+    src: Domain,
+    dst: Domain) extends PerformanceLogger {
+  protected val marker = "RELOCATION_LOGGER_MARKER"
+  def write(): Unit = {
+    log.info(s"${marker}\t${phase}\t${elapsed()}\t${guid}\t${src}->${dst}")
   }
 }
