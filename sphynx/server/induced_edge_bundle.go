c3542b7966c39c646e872273d28f005d0524500e
Hanna Gábor <gahanna999@gmail.com>
2020-02-25 17:04:54 +0100

    Remove debug stuff.

--- sphynx/server/induced_edge_bundle.go
+++ sphynx/server/induced_edge_bundle.go
@@ -1,21 +1,12 @@
 package main
 
-import "fmt"
-
 func init() {
 	operationRepository["InducedEdgeBundle"] = Operation{
 		execute: func(ea *EntityAccessor) error {
-			// src := ea.getVertexSet("src")
-			// dst := ea.getVertexSet("dst")
 			induceSrc := ea.GetBoolParam("induceSrc", true)
-			fmt.Println("induceSrc")
-			fmt.Println(induceSrc)
 			induceDst := ea.GetBoolParam("induceDst", true)
-			fmt.Println("induceDst")
-			fmt.Println(induceDst)
 			var srcMapping map[int]int
 			if induceSrc {
-				// srcImage := ea.inputs["srcImage"]
 				srcMappingEB := ea.getEdgeBundle("srcMapping")
 				srcMapping = make(map[int]int, len(srcMappingEB.Src))
 				for i := range srcMappingEB.Src {
@@ -24,7 +15,6 @@ func init() {
 			}
 			var dstMapping map[int]int
 			if induceDst {
-				// dstImage := ea.inputs["dstImage"]
 				dstMappingEB := ea.getEdgeBundle("dstMapping")
 				dstMapping = make(map[int]int, len(dstMappingEB.Src))
 				for i := range dstMappingEB.Src {
@@ -32,19 +22,12 @@ func init() {
 				}
 			}
 			es := ea.getEdgeBundle("edges")
-
 			induced := &EdgeBundle{}
 			embedding := &EdgeBundle{}
 			approxLen := len(es.Src)
 			induced.Init(approxLen)
 			embedding.Init(approxLen)
 			numInducedEdges := 0
-			fmt.Println("srcMapping")
-			fmt.Println(srcMapping)
-			fmt.Println("dstMapping")
-			fmt.Println(dstMapping)
-			fmt.Println("es")
-			fmt.Println(es)
 			for i, src := range es.Src {
 				dst := es.Dst[i]
 				mappedSrc := src
@@ -57,10 +40,6 @@ func init() {
 				if induceDst {
 					mappedDst, dstExists = dstMapping[dst]
 				}
-				fmt.Println("mappedSrc")
-				fmt.Println(mappedSrc)
-				fmt.Println("mappedDst")
-				fmt.Println(mappedDst)
 				if srcExists && dstExists {
 					induced.Src = append(induced.Src, mappedSrc)
 					induced.Dst = append(induced.Dst, mappedDst)
@@ -71,7 +50,6 @@ func init() {
 					numInducedEdges += 1
 				}
 			}
-			fmt.Println(induced)
 			ea.output("induced", induced)
 			ea.output("embedding", embedding)
 			return nil
