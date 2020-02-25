f43b89b54cc46cf9664a74359e61c4ccc797819e
Daniel Darabos <darabos.daniel@gmail.com>
2020-02-25 16:34:53 +0100

    Hide table GUID.

--- web/app/scripts/sql/table-kind.js
+++ web/app/scripts/sql/table-kind.js
@@ -13,8 +13,7 @@ angular.module('biggraph').directive('tableKind', function(util) {
     templateUrl: 'scripts/sql/table-kind.html',
     link: function(scope) {
       scope.importBox = function() {
-        scope.disabled = true;
-        scope.error = undefined;
+        scope.inProgress = true;
         const box = angular.copy(scope.box.instance);
         box.parameters = scope.params;
         util.post('/ajax/importBox', {
@@ -22,13 +21,30 @@ angular.module('biggraph').directive('tableKind', function(util) {
           ref: scope.workspaceReference(),
         }).then(function success(response) {
           scope.guid = response.guid;
-          scope.params['last_settings'] = response.parameterSettings;
+          scope.params.last_settings = response.parameterSettings;
           scope.onBlur();
-        }, function error(error) {
-          scope.error = error;
         }).finally(function() {
-          scope.disabled = false;
+          scope.inProgress = false;
         });
+      };
+      scope.stale = function() {
+        const params = angular.copy(scope.params);
+        delete params.last_settings;
+        delete params.imported_table;
+        const fresh = scope.params.last_settings &&
+          angular.equals(params, JSON.parse(scope.params.last_settings));
+        return !fresh;
+      };
+      scope.tooltip = function() {
+        if (scope.inProgress) {
+          return '';
+        } else if (scope.stale()) {
+          return 'The parameters have changed. Please run the import.';
+        } else {
+          return `
+            The parameters have not changed since the last import.
+            You can re-run the import if the datasource has changed.`;
+        }
       };
     },
   };
