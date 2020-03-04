f17f449be59a85d03900c36ebd4cc2c27a500dd0
Gabor Olah <gabor.olah@lynxanalytics.com>
2020-03-04 16:38:39 +0100

    Fix out degree indexing

--- sphynx/server/out_degree.go
+++ sphynx/server/out_degree.go
@@ -6,11 +6,13 @@ func doOutDegree(es *EdgeBundle, src *VertexSet) *DoubleAttribute {
 		Values:  make([]float64, len(src.MappingToUnordered)),
 		Defined: make([]bool, len(src.MappingToUnordered)),
 	}
+	for i := 0; i < len(src.MappingToUnordered); i++ {
+		outDegree.Defined[i] = true
+	}
 
 	degree := make(map[int]int, len(src.MappingToUnordered))
 	for i := 0; i < len(es.Src); i++ {
 		degree[es.Src[i]]++
-		outDegree.Defined[i] = true
 	}
 	for i, cnt := range degree {
 		outDegree.Values[i] = float64(cnt)
