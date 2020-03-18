af54cfb0701302d1a6d2badd6624efe0148d21b1
Gabor Olah <gabor.olah@lynxanalytics.com>
2020-03-18 14:51:01 +0100

    Change comments

--- tools/log2graph.py
+++ tools/log2graph.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 '''
 Parses a LynxKite logfile and dumps the critical path from the first executed backend operation
-to the last created guid. Example output:
+to the last created guid. Example usage:
+
+./log2graph.py < application-20200318_101213.log
+
+Excerpt from a possible output:
 
 0 c6475919-831a-34ee-93fc-0a3631ac8ba4[OrderedSphynxDisk] -> DerivePython_start_0
 5016 DerivePython_start_0 -> DerivePython_end_0  DerivePython(matches = vs[vs.name.str.lower() ==...
@@ -22,18 +26,18 @@ Explanation:
 1. Guid c6475919-831a-34ee-93fc-0a3631ac8ba4[OrderedSphynxDisk] is input to DerivePython
    (no time is charged here).
 2. DerivePython runs in 5016 ms.
-3. In 36 ms, the resulting guid (99250d74-4e81-3f36-a02b-2a850bfcabce) is relocated to SphynxMemory
-4. Free of charge, 99250d74-4e81-3f36-a02b-2a850bfcabce is input by ShortestPath
-5. ShortestPath finished in 761 ms
-6. Its output 100b63fe-e164-3a1f-ae74-f81777d68a14 goes to SphynxMemory, 0 ms.
-7. In 91 ms, 100b63fe-e164-3a1f-ae74-f81777d68a14 goes to OrderedSphynxDisk
-8. And DerivePython can start working
+3. It's output  99250d74-4e81-3f36-a02b-2a850bfcabce is on OrderedSphynxDisk for in no time.
+4. In 36 ms, the guid 99250d74-4e81-3f36-a02b-2a850bfcabce is relocated to SphynxMemory
+5. Free of charge, 99250d74-4e81-3f36-a02b-2a850bfcabce is input by ShortestPath
+6. ShortestPath finished in 761 ms
+7. Its output 100b63fe-e164-3a1f-ae74-f81777d68a14 goes to SphynxMemory, 0 ms.
+8. In 91 ms, 100b63fe-e164-3a1f-ae74-f81777d68a14 goes to OrderedSphynxDisk
+9. And DerivePython can start working
 ...
 In the end, we see some SparkDomain operations for visualization.
 The last line is the total time taken. Probably there is some overhead, because because it is
 always noticeably less than the total length of the computation. (E.g., in the example above,
 the actual time was 22 seconds, but this path was only 17 seconds long.)
-
 
 TODO: Dump the whole graph for LK to import.
 '''
@@ -136,11 +140,10 @@ for v in VERTICES - TARGETS:
 for v in VERTICES - SOURCES:
   add_edge(v, 'END', 0)
 
-
-# Primitive longest path:
+# A simple critical path implementation.
 # Use compute shortest path on graph, with the
 # edge weights negated. This would not work with Dykstra, but
-# does work with Bellman-Ford.
+# does work with Bellman-Ford, since this graph is a DAG.
 
 DISTANCE = {}
 PRED = {}
