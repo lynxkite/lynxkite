1186b244c9436887850105588cdedb7abb0d0955
Hanna Gábor <gahanna999@gmail.com>
2020-04-01 10:45:50 +0200

    Use cpu-only torch libs.

--- sphynx/python/install-dependencies.sh
+++ sphynx/python/install-dependencies.sh
@@ -2,17 +2,10 @@
 # PyTorch Geometric installation is a nightmare because we have to get versions of
 # PyTorch, PyArrow and the PyTorch Geometric libraries that are all built with compatible
 # glibc++ and Boost libraries.
-conda install -y -c conda-forge boost pyarrow
-conda install -y -c pytorch pytorch
-git clone --depth 1 --branch 1.4.5 https://github.com/rusty1s/pytorch_cluster.git
-git clone --depth 1 --branch 2.0.2 https://github.com/rusty1s/pytorch_scatter.git
-git clone --depth 1 --branch 0.4.4 https://github.com/rusty1s/pytorch_sparse.git
-sed -i 's/extra_compile_args = .*/extra_compile_args = ["-D_GLIBCXX_USE_CXX11_ABI=1"]/' pytorch_cluster/setup.py
-sed -i 's/extra_compile_args = ..cxx.: ../extra_compile_args = {"cxx": ["-D_GLIBCXX_USE_CXX11_ABI=1"]/' pytorch_scatter/setup.py
-sed -i 's/extra_compile_args = ..cxx.: ../extra_compile_args = {"cxx": ["-D_GLIBCXX_USE_CXX11_ABI=1"]/' pytorch_sparse/setup.py
-pip install --no-cache-dir ./pytorch_cluster ./pytorch_scatter ./pytorch_sparse
+conda install -y pyarrow
+conda install -y pytorch torchvision cpuonly -c pytorch
+pip install --no-cache-dir torch-scatter==2.0.4+cpu -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+pip install --no-cache-dir torch-sparse==0.6.1+cpu -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+pip install --no-cache-dir torch-cluster==1.5.3+cpu -f https://pytorch-geometric.com/whl/torch-1.4.0.html
 pip install --no-cache-dir torch-geometric==1.4.2
-rm -r ./pytorch_cluster
-rm -r ./pytorch_sparse
-rm -r ./pytorch_scatter
 conda clean -a
