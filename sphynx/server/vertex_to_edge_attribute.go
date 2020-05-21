e777fd7d6e5d95da0901a4ff452631980ab23f9e
Hanna Gábor <gahanna999@gmail.com>
2020-05-21 17:51:04 +0200

    Implement VertexToEdgeAttribute in Go.

--- sphynx/server/vertex_to_edge_attribute.go
+++ sphynx/server/vertex_to_edge_attribute.go
@@ -2,19 +2,37 @@
 
 package main
 
-import "fmt"
+import (
+	"reflect"
+)
 
 func init() {
 	operationRepository["VertexToEdgeAttribute"] = Operation{
 		execute: func(ea *EntityAccessor) error {
-			// targetSrc := ea.getVertexSet("ignoredSrc")
-			// targetDst := ea.getVertexSet("ignoredDst")
-			// origAttr := ea.inputs["originalAttr"].(TabularEntity)
+			origAttr := ea.inputs["original"].(TabularEntity)
+			origValues := reflect.ValueOf(origAttr).Elem().FieldByName("Values")
+			origDefined := reflect.ValueOf(origAttr).Elem().FieldByName("Defined")
 			mapping := ea.getSparkIDVectorAttribute("mapping")
-			// target := ea.getEdgeBundle("target")
-			fmt.Println("aaaaaaaaaaaaaaaaa")
-			fmt.Println(mapping)
-			// ea.output("mappedAttribute", mappedAttribute)
+			target := ea.getEdgeBundle("target")
+			numEdges := len(target.EdgeMapping)
+
+			attrType := reflect.Indirect(reflect.ValueOf(origAttr)).Type()
+			edgeAttr := reflect.New(attrType)
+			InitializeAttribute(edgeAttr, numEdges)
+			edgeAttrValues := edgeAttr.Elem().FieldByName("Values")
+			edgeAttrDefined := edgeAttr.Elem().FieldByName("Defined")
+			for vertexID, edgeIDs := range mapping.Values {
+				value := origValues.Index(int(vertexID))
+				defined := origDefined.Index(int(vertexID))
+				if defined.Bool() {
+					for _, edgeID := range edgeIDs {
+						edgeAttrValues.Index(int(edgeID)).Set(value)
+						edgeAttrDefined.Index(int(edgeID)).Set(defined)
+					}
+				}
+			}
+			edgeAttrValue := edgeAttr.Interface()
+			ea.output("mappedAttribute", edgeAttrValue.(Entity))
 			return nil
 		},
 	}
