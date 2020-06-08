af4c41fc15fe5701e6ff9d21634aa1ed6854b5c9
Daniel Darabos <darabos.daniel@gmail.com>
2020-06-08 14:34:26 +0200

    Replace PCA and t-SNE boxes with one box with a drop-down.

--- sphynx/python/pca.py
+++ sphynx/python/pca.py
@@ -1,11 +1,11 @@
-'''Dimensionality reduction with t-SNE.'''
-from sklearn.manifold import TSNE
+'''Dimensionality reduction with PCA.'''
+from sklearn.decomposition import PCA
 from . import util
 
 op = util.Op()
 x = op.input_vector('vector')
 dim = op.params['dimensions']
-z = TSNE(n_components=dim, perplexity=op.params['perplexity']).fit_transform(x)
+z = PCA(n_components=dim).fit_transform(x)
 if dim == 2:
   op.output('embedding', z, type=util.DoubleTuple2Attribute)
 else:
