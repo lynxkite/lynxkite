ff9cc8efd0a26ebb89dd02d759136fb222829eba
Daniel Darabos <darabos.daniel@gmail.com>
2020-06-12 20:20:44 +0200

    Remove old unused files.

--- stage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xue
-
-echo 'You can also just run "make backend".'
-cd $(dirname $0)
-make backend
--- tools/lynxkite-vagrant-tools/status_kite.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-vagrant status
-echo "Whether the Kite VM is running is described above. Press any key to exit"
-pause > nul
-
