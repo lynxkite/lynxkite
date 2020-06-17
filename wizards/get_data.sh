809766017caf944aaabe9413862030f298f639c0
Daniel Darabos <darabos.daniel@gmail.com>
2020-06-17 21:56:14 +0200

    Copy wizards from biggraph/biggraph.

---
+++ wizards/get_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xue
+# We store the data files in this folder:
+# https://drive.google.com/drive/folders/1RyLzJ9cYg3bWBs0VMYRVxVhTJMS-7kaD
+# Add it to your Google Drive and then you can use this script.
+# (It's also okay to manually download the files.)
+skicka download '/try.lynxkite.com wizard data' data
---
+++ wizards/upload_all.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+'''Uploads the workspaces from the "workspaces" directory to try.lynxkite.com.'''
+import lynx.kite
+import os
+from ruamel import yaml
+import sys
+
+
+def str_representer(dumper, s):
+  if '\n' in s:
+    style = '|'
+  elif len(s) > 60:
+    style = '>'
+  else:
+    style = None
+  return dumper.represent_scalar('tag:yaml.org,2002:str', s, style=style)
+
+
+yaml.representer.SafeRepresenter.add_representer(str, str_representer)
+
+
+def fix_imports(filename, rewrite):
+  wn = ws_name(filename)
+  ws = lk.get_workspace(wn)
+  boxes = ws.workspace.boxes
+  outputs = {o.boxOutput.boxId: o for o in ws.outputs}
+  for b in boxes:
+    if b.operationId == 'Import CSV':
+      fn = b.parameters.filename
+      if fn and outputs[b.id].kind == 'error':
+        _, fn = fn.split('.', 1)
+        print(f'Uploading {fn} for {b.id} in {wn}.')
+        with open('data/' + fn) as f:
+          data = f.read()
+        b.parameters.filename = lk.upload(data, fn)
+        boxes = lk.import_box(boxes, b.id)
+  lk.save_workspace(wn, boxes)
+  if rewrite:
+    with open('workspaces/' + filename, 'w') as f:
+      yaml.safe_dump(lynx.kite.to_simple_dicts(boxes), f, default_flow_style=False)
+
+
+def mkdir(dn):
+  if dn:
+    try:
+      lk.create_dir(dn)
+    except lynx.kite.LynxException as e:
+      if not 'already exists' in e.error:
+        raise e
+
+
+def ws_name(filename):
+  filename, _ = os.path.splitext(filename)
+  return filename.replace('_', ' ').replace('custom boxes/', 'custom_boxes/')
+
+
+def upload(filename, rewrite):
+  print('Uploading', filename)
+  with open('workspaces/' + filename) as f:
+    ws = yaml.safe_load(f)
+  wn = ws_name(filename)
+  dn, _ = os.path.split(wn)
+  mkdir(dn)
+  lk.save_workspace(wn, ws)
+  fix_imports(filename, rewrite)
+
+
+def usage():
+  print('Usage:')
+  print()
+  print('upload_all.py [--remote|--local]')
+  print()
+  print('--remote uploads to https://try.lynxkite.com')
+  print('--local  uploads to your locally running lynxkite (set your LYNXKITE_ADDRESS)')
+  print()
+  sys.exit(0)
+
+
+os.chdir(os.path.dirname(__file__))
+
+if len(sys.argv) != 2:
+  usage()
+
+if sys.argv[1] == '--remote':
+  with open(os.path.expanduser('~/secrets/try.lynxkite.com.password')) as f:
+    password = f.read().strip()
+    lk = lynx.kite.LynxKite(
+        address='https://try.lynxkite.com/',
+        username='admin',
+        password=password)
+    lk._login()
+elif sys.argv[1] == '--local':
+  lk = lynx.kite.LynxKite()
+else:
+  usage()
+
+
+for root, dirs, files in os.walk('workspaces'):
+  for f in files:
+    if f.endswith('.yaml'):
+      upload((root + '/' + f)[len('workspaces/'):], sys.argv[1] == '--remote')
