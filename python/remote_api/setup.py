7cb6524b091308318b232632eed5ff2576adf2f2
Daniel Darabos <darabos.daniel@gmail.com>
2020-06-22 20:23:49 +0200

    Update license in Python package.

--- python/remote_api/setup.py
+++ python/remote_api/setup.py
@@ -19,14 +19,14 @@ setup(
     packages=find_namespace_packages('src'),
     package_dir={'': 'src'},
     author='Lynx Analytics',
-    author_email='lynxkite@lynxanalytics.com',
+    author_email='lynxkite@lynxkite.com',
     description='Python API for LynxKite',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     url='https://lynxkite.com/',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: Other/Proprietary License',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
     ],
 )
