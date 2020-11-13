a4fc672473d272eac81e3cf994b3d5261c479331
Daniel Darabos <darabos.daniel@gmail.com>
2020-11-13 20:50:09 +0100

    Scala for NetworKit ops that compute a numerical vertex attribute.

---
+++ app/com/lynxanalytics/biggraph/graph_operations/NetworKit.scala
@@ -0,0 +1,32 @@
+// Sphynx-only operations that use NetworKit.
+// Since NetworKit has so many algorithms we try to avoid having to add new
+// classes for each of them.
+package com.lynxanalytics.biggraph.graph_operations
+
+import play.api.libs.json
+import com.lynxanalytics.biggraph.graph_api._
+
+object NetworKitComputeAttribute extends OpFromJson {
+  def fromJson(j: json.JsValue) = NetworKitComputeAttribute(j.as[json.JsObject])
+  def run(vs: VertexSet, es: EdgeBundle, options: Map[String, Any])(
+    implicit
+    m: MetaGraphManager): Attribute[Double] = {
+    val j = json.JsObject(options.mapValues {
+      case v: String => json.Json.toJson(v)
+      case v: Int => json.Json.toJson(v)
+      case v: Double => json.Json.toJson(v)
+    }.toSeq)
+    val op = NetworKitComputeAttribute(j.as[json.JsObject])
+    import Scripting._
+    op(op.vs, vs)(op.es, es).result.attr
+  }
+}
+case class NetworKitComputeAttribute(j: json.JsObject)
+  extends TypedMetaGraphOp[GraphInput, AttributeOutput[Double]] {
+  @transient override lazy val inputs = new GraphInput()
+  def outputMeta(instance: MetaGraphOperationInstance) = {
+    implicit val i = instance
+    new AttributeOutput[Double](inputs.vs.entity)
+  }
+  override def toJson = j
+}
