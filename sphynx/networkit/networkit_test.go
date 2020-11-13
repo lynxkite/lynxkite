8f40ea91b177aae6839eafb9d0203302b7934869
Daniel Darabos <darabos.daniel@gmail.com>
2020-11-13 11:37:33 +0100

    Sphynx -> NetworKit graph passing.

--- sphynx/networkit/networkit_test.go
+++ sphynx/networkit/networkit_test.go
@@ -4,13 +4,34 @@ import (
 	"testing"
 )
 
-func TestNetworKit(t *testing.T) {
+type SphynxId uint32
+type EdgeBundle struct {
+	Src         []SphynxId
+	Dst         []SphynxId
+	EdgeMapping []int64
+}
+
+func TestBasicOps(t *testing.T) {
 	c := NewBarabasiAlbertGenerator(uint64(10), uint64(50))
 	g := c.Generate()
 	b := NewBetweenness(g)
 	b.Run()
 	if b.Maximum() != 1176 {
 		t.Errorf("Max betweenness is %v, expected 1176.", b.Maximum())
+	}
+}
+
+func TestGraphToNetworKit(t *testing.T) {
+	sphynxEB := EdgeBundle{[]SphynxId{0, 1, 2, 3, 4}, []SphynxId{1, 2, 3, 4, 1}, nil}
+	builder := NewGraphBuilder(uint64(5))
+	for i := range sphynxEB.Src {
+		builder.AddHalfEdge(uint64(sphynxEB.Src[i]), uint64(sphynxEB.Dst[i]))
+	}
+	g := builder.ToGraph(true)
+	b := NewBetweenness(g)
+	b.Run()
+	if b.Maximum() != 6 {
+		t.Errorf("Max betweenness is %v, expected 6.", b.Maximum())
 	}
 
 }
