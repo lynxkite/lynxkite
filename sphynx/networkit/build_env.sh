93a8d7b3a59e65a2c6c6fbcc23224931db4d7925
Daniel Darabos <darabos.daniel@gmail.com>
2020-12-01 12:13:23 +0100

    Download and build NetworKit automatically.

--- sphynx/networkit/build_env.sh
+++ sphynx/networkit/build_env.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -xue
+# Downloads and builds NetworKit and creates symlinks in this directory for the Go build.
 
-ln -s ~/networkit/include . || true
-ln -s ~/networkit/networkit/cpp . || true
-ln -s ~/networkit/extlibs/tlx/tlx . || true
-ln -s ~/networkit/build/libnetworkit.so . || true
+if [[ ! -e ~/networkit ]]; then
+  pushd ~
+  git clone https://github.com/networkit/networkit.git --branch 7.1 --single-branch --depth 1
+  mkdir networkit/build
+  cd networkit/build
+  git submodule update --init
+  cmake ..
+  make -j4
+  popd
+fi
+if [[ ! -e include ]]; then ln -s ~/networkit/include .; fi
+if [[ ! -e cpp ]]; then ln -s ~/networkit/networkit/cpp .; fi
+if [[ ! -e tlx ]]; then ln -s ~/networkit/extlibs/tlx/tlx .; fi
+if [[ ! -e libnetworkit.so ]]; then ln -s ~/networkit/build/libnetworkit.so .; fi
 export CPLUS_INCLUDE_PATH="$(pwd)/include"
 export LD_LIBRARY_PATH="$(pwd)"
 export CGO_LDFLAGS="-lnetworkit"
