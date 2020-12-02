2ff6ec4f0e16b3e066103cb77e4d3e44dfddb2bf
Daniel Darabos <darabos.daniel@gmail.com>
2020-12-02 15:18:15 +0100

    Panic if NETWORKIT_THREADS cannot be parsed.

--- sphynx/networkit/additions.go
+++ sphynx/networkit/additions.go
@@ -9,7 +9,11 @@ import (
 // Call this before using NetworKit if you want NETWORKIT_THREADS to apply.
 func SetThreadsFromEnv() {
 	s := os.Getenv("NETWORKIT_THREADS")
-	if t, err := strconv.Atoi(s); err == nil {
+	if len(s) > 0 {
+		t, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
 		SetNumberOfThreads(t)
 	}
 }
