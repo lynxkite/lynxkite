5cf22f9192e8feb6e8b560cea66c575a47b6419d
Daniel Darabos <darabos.daniel@gmail.com>
2022-06-02 19:30:54 +0200

    Always use the right NetworKit includes.

--- sphynx/networkit/build_env.sh
+++ sphynx/networkit/build_env.sh
@@ -1,6 +1,4 @@
-if [[ ! -e include ]]; then ln -s $CONDA_PREFIX/include .; fi
-#export CPLUS_INCLUDE_PATH="$(pwd)/include"
-#export LD_LIBRARY_PATH="$(pwd)"
-#export LIBRARY_PATH="$(pwd)"
+rm -f include
+ln -s $CONDA_PREFIX/include .
 export CGO_CXXFLAGS="-Wno-deprecated-declarations"
 export CGO_LDFLAGS="-lnetworkit -fopenmp"
