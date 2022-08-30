c9cecf926ec6c460cde72423e31e1868bc2cd158
Daniel Darabos <darabos.daniel@gmail.com>
2022-08-30 12:53:47 +0200

    Upgrade to Apache Spark 3.3.0.

--- app/com/lynxanalytics/biggraph/graph_operations/ExecuteSQL.scala
+++ app/com/lynxanalytics/biggraph/graph_operations/ExecuteSQL.scala
@@ -5,10 +5,7 @@ import com.lynxanalytics.biggraph.controllers.ProtoTable
 import com.lynxanalytics.biggraph.graph_api._
 import com.lynxanalytics.biggraph.spark_util.SQLHelper
 import org.apache.spark
-import org.apache.spark.sql.catalyst.analysis.UnresolvedException
-import org.apache.spark.sql.catalyst.optimizer.Optimizer
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.catalyst
 import org.apache.spark.sql.execution.SparkSqlParser
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types
@@ -46,16 +43,34 @@ object ExecuteSQL extends OpFromJson {
     catalog
   }
 
+  private def createTempViews(protoTables: Map[String, ProtoTable]): Unit = {
+    import spark.sql.catalyst.analysis._
+    for ((name, table) <- protoTables) {
+      // Based on SPARK-34700.
+      val identifier = catalyst.TableIdentifier(name)
+      val plan = table.relation
+      val metadata = catalyst.catalog.CatalogTable(
+        identifier = identifier,
+        tableType = catalyst.catalog.CatalogTableType.VIEW,
+        storage = catalyst.catalog.CatalogStorageFormat.empty,
+        schema = plan.schema,
+        properties = Map((catalyst.catalog.CatalogTable.VIEW_STORING_ANALYZED_PLAN, "true")),
+      )
+      catalog.createTempView(
+        name,
+        catalyst.catalog.TemporaryViewRelation(metadata, Some(plan)),
+        overrideIfExists = true)
+    }
+  }
+
   def getLogicalPlan(
       sqlQuery: String,
-      protoTables: Map[String, ProtoTable]): LogicalPlan = {
+      protoTables: Map[String, ProtoTable]): catalyst.plans.logical.LogicalPlan = {
     import spark.sql.catalyst.analysis._
     val analyzer = new Analyzer(catalog)
     val parsedPlan = parser.parsePlan(sqlQuery)
     synchronized {
-      for ((name, table) <- protoTables) {
-        catalog.createTempView(name, table.relation, overrideIfExists = true)
-      }
+      createTempViews(protoTables)
       try {
         analyzer.execute(parsedPlan)
       } finally {
@@ -98,13 +113,7 @@ object ExecuteSQL extends OpFromJson {
     val plan = getLogicalPlan(sqlQuery, protoTables)
     val minimizedProtoTables = ProtoTable.minimize(plan, protoTables)
     val tables = minimizedProtoTables.mapValues(protoTable => protoTable.toTable)
-    try {
-      ExecuteSQL.run(sqlQuery, SQLHelper.stripComment(plan.schema), tables)
-    } catch {
-      // The exception is thrown on the plan.schema call
-      case a: UnresolvedException[_] =>
-        throw UnresolvedColumnException(s"${a.treeString} column cannot be found", a)
-    }
+    ExecuteSQL.run(sqlQuery, SQLHelper.stripComment(plan.schema), tables)
   }
 
 }
