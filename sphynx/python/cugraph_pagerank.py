258ed8815dde0f30954a45131ea3232a1af8418a
Daniel Darabos <darabos.daniel@gmail.com>
2022-09-08 10:36:46 +0200

    Fixes for new cugraph.

--- sphynx/python/cugraph_pagerank.py
+++ sphynx/python/cugraph_pagerank.py
@@ -3,9 +3,9 @@ import cugraph
 from . import util
 
 op = util.Op()
-es = op.input_cudf('es')
+es = op.input_cudf('es').astype({'src': 'int32', 'dst': 'int32'})
 G = cugraph.Graph()
-G.from_cudf_edgelist(es, source='src', destination='dst')
+G.from_cudf_edgelist(es, source='src', destination='dst', renumber=False)
 res = cugraph.pagerank(G, alpha=op.params['dampingFactor'], max_iter=op.params['iterations'])
 res = res.sort_values('vertex').pagerank.to_arrow()
 op.output('pagerank', res, type=util.DoubleAttribute)
