96e9ee6a915758707d1615d58b88fabfdc1c3de4
Daniel Darabos <darabos.daniel@gmail.com>
2022-09-28 16:06:05 +0200

    Use placeholder for unused "secret".

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ tools/install_spark.sh
 make backend
 . ~/.kiterc
 $SPARK_HOME/bin/spark-submit \
-  --conf 'spark.driver.extraJavaOptions=-Dplay.http.secret.key=SECRET-TEST-TEST-TEST-TEST' \
   target/scala-2.12/lynxkite-0.1-SNAPSHOT.jar
