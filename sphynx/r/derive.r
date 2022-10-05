b599b36ec8d0f7e27a3bafdc3e051f7e59833780
Daniel Darabos <darabos.daniel@gmail.com>
2022-10-05 09:08:19 +0200

    Also support edges.

--- sphynx/r/derive.r
+++ sphynx/r/derive.r
@@ -1,29 +1,37 @@
-# Run user code.
+# Run user code on a graph.
 library('stringr')
 library('tibble')
 library('dplyr')
 source('r/util.r')
 
 # Create input tables.
-fields <- params[['inputFields']]
-ns <- fields[fields$parent == 'vs']$name
-if (length(names) != 0) {
-  columns <- input_table(paste('vs', ns, sep='.'))
-  vs <- tibble(!!!columns, .name_repair='minimal')
-  names(vs) <- ns
+gettable <- function(parent) {
+  fields <- params[['inputFields']]
+  ns <- fields[fields$parent == parent, ]$name
+  if (length(names) != 0) {
+    columns <- input_table(paste(parent, ns, sep='.'))
+    # .name_repair='minimal' allows duplicate column names. They are all called "values".
+    t <- tibble(!!!columns, .name_repair='minimal')
+    # We set the correct column names here.
+    names(t) <- ns
+  }
+  return(t)
 }
+vs <- gettable('vs')
+es <- gettable('es')
 
 # Run user code.
 code <- params[['code']]
 eval(parse(text = code))
-print('RESULT:')
-print(vs)
 
 # Save outputs.
-fields <- params[['outputFields']]
-fs <- fields[fields$parent == 'vs']
-if (length(fs) != 0) {
-  columns <- vs[[fs$name]]
-  print('OUTPUT')
-  output(paste('vs', fs$name, sep='.'), columns, fs$tpe$typename)
+save <- function(parent, t) {
+  fields <- params[['outputFields']]
+  fs <- fields[fields$parent == parent, ]
+  if (length(fs) != 0) {
+    columns <- t[[fs$name]]
+    output(paste(parent, fs$name, sep='.'), columns, fs$tpe$typename)
+  }
 }
+save('vs', vs)
+save('es', es)
