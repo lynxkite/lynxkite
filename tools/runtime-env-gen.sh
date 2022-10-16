350255eb65f737b066ed7af3cf48a56cd7814c26
Daniel Darabos <darabos.daniel@gmail.com>
2022-10-16 10:08:15 +0200

    Add zip and wget as build dependencies. (Used by Sphynx build.)

--- tools/runtime-env-gen.sh
+++ tools/runtime-env-gen.sh
@@ -2,5 +2,5 @@
 # Generates the runtime-env.yml configs based on conda-env.yml.
 
 cd $(dirname $0)
-cat ../conda-env.yml | grep -Pv -- '^#|- (make|sbt|nodejs|yarn|go|compilers|swig|autopep8|mypy)\b' > runtime-env-cuda.yml
+cat ../conda-env.yml | grep -Pv -- '^#|- (make|sbt|nodejs|yarn|go|compilers|swig|autopep8|mypy|zip|wget)\b' > runtime-env-cuda.yml
 cat runtime-env-cuda.yml | grep -Pv -- '- (rapidsai|nvidia|cugraph|cudatoolkit)\b' | sed 's/cuda\|cu113/cpu/g' > runtime-env.yml
