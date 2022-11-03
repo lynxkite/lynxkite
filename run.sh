1ee654a6f0ffd49ecb74f5dcfab5a2aaf5a73db5
Lynx <olena.herasymenko@lynxanalytics.com>
2022-11-03 14:44:59 +0100

    Use spark-submit in run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,4 @@ cd $(dirname $0)
 tools/install_spark.sh
 make backend
 . ~/.kiterc
-$SPARK_HOME/bin/spark-submit \
-  --conf 'spark.driver.extraJavaOptions=-Dplay.http.secret.key=SECRET-TEST-TEST-TEST-TEST' \
-  target/scala-2.12/lynxkite-0.1-SNAPSHOT.jar
+spark-submit --conf 'spark.driver.extraJavaOptions=-Dplay.http.secret.key=SECRET-TEST-TEST-TEST-TEST' target/scala-2.12/lynxkite-0.1-SNAPSHOT.jar
