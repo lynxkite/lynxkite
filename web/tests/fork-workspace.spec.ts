9ee24794ffca2f24385918c5a0dd73207f2bf853
Lena Gerasimenko <laae@mail.ru>
2023-01-22 17:44:59 +0100

    Rewrite fork workspace est

---
+++ web/tests/fork-workspace.spec.ts
@@ -0,0 +1,18 @@
+//Tests workspace fork
+import { test, expect } from '@playwright/test';
+import { Splash, Workspace } from './lynxkite';
+
+let splash: Splash;
+let workspace: Workspace;
+
+test('fork workspace', async function ({ browser }) {
+    splash = await Splash.open(browser);
+    workspace = await splash.openNewWorkspace('test-example');
+    await workspace.page.pause();
+    await workspace.page.locator('#save-workspace-as-starter-button').click();
+    await workspace.page.locator('#save-workspace-as-input').locator('input').fill('test-example-fork');
+    await workspace.page.locator('#save-workspace-as-input').locator('#ok').click();
+    await workspace.page.waitForURL('**/#/workspace/test-example-fork');
+    await workspace.close();
+    await splash.expectWorkspaceListed('test-example-fork');
+});
