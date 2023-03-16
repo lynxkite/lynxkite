2039ac025894dc6d532f9426823acae974f41b66
Daniel Darabos <darabos.daniel@gmail.com>
2023-03-16 10:33:47 +0100

    Convert operation-selector-test.js.

---
+++ web/tests/operation-selector.spec.ts
@@ -0,0 +1,11 @@
+// Tests the bar on the right side that lets you add boxes.
+import { test, expect } from '@playwright/test';
+import { Workspace } from './lynxkite';
+
+test('adding a box', async ({ page }) => {
+  const ws = await Workspace.empty(page, 'operation-selector');
+  await expect(page.locator('g.box')).toHaveCount(1);
+  await page.keyboard.type('/examp');
+  await page.keyboard.press('Enter');
+  await expect(page.locator('g.box')).toHaveCount(2);
+});
