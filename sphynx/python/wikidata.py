4ec4d0c0caa4c61828094c34bdca187d2a68710f
Daniel Darabos <darabos.daniel@gmail.com>
2023-04-21 14:17:43 +0200

    Get data for countries.

--- sphynx/python/wikidata.py
+++ sphynx/python/wikidata.py
@@ -64,12 +64,14 @@ def get_properties(entity_id):
   j = entity_data(entity_id)
   claims = j['claims']
   props = {}
-  # There can be multiple values for each property. We pick the most recent one. (Or the first one if there are no dates.)
+  # There can be multiple values for each property. We pick the most recent
+  # one. (Or the first one if there are no dates.)
   for p, vs in claims.items():
     besttime = None
     bestv = None
     for v in vs:
-      if v['mainsnak']['snaktype'] != 'value' or v['mainsnak']['datatype'] not in ['quantity', 'monolingualtext', 'wikibase-item']:
+      if v['mainsnak']['snaktype'] != 'value' or v['mainsnak']['datatype'] not in [
+              'quantity', 'monolingualtext', 'wikibase-item']:
         continue
       if bestv is None:
         bestv = v
@@ -106,8 +108,10 @@ def get_labels(ids):
   ids = remaining
   # The API limits us to 50 IDs at a time.
   for i in range(0, len(ids), 50):
-    res = api('wbgetentities', ids='|'.join(ids[i:i+50]), props='labels')
+    res = api('wbgetentities', ids='|'.join(ids[i:i + 50]), props='labels')
     for id, v in res['entities'].items():
+      if 'en' not in v['labels']:
+        continue
       label = v['labels']['en']['value']
       key = 'label-' + id
       shelf[key] = label
@@ -117,13 +121,15 @@ def get_labels(ids):
 def score_properties(eds):
   '''Returns a score for each property that reflects how much information it carries.'''
   sets = {}
+  counts = {}
   for e in eds:
     for p in e:
+      counts[p] = counts.get(p, 0) + 1
       v = e[p]
       if isinstance(v, dict):
         v = v['id']
       sets.setdefault(p, set()).add(v)
-  return {k: len(v) for k, v in sets.items()}
+  return {k: counts[k] if len(sets[k]) > 1 else 0 for k in counts}
 
 
 def enrich(df, key_col, limit_props=10):
@@ -157,7 +163,7 @@ def enrich(df, key_col, limit_props=10):
   for e in eds:
     e.properties = {
         labels[k]: labels[v['id']] if isinstance(v, dict) else v
-        for k, v in e.properties.items()}
+        for k, v in e.properties.items() if k in labels and (not isinstance(v, dict) or v['id'] in labels)}
   # Merge the data.
   for key, e in zip(keys, eds):
     e.properties['_key'] = key
@@ -170,6 +176,7 @@ if __name__ == '__main__':
   import pandas as pd
   pd.options.display.max_columns = 100
   pd.options.display.width = 1000
+  '''
   df = pd.DataFrame({'name':  ['New York', 'London', 'Paris']})
   print()
   print(enrich(df, 'name'))
@@ -190,3 +197,34 @@ if __name__ == '__main__':
   df = pd.DataFrame({'name':  ['Amazon', 'Google', 'Facebook', 'Microsoft']})
   print()
   print(enrich(df, 'name'))
+  '''
+  df = pd.DataFrame({'name': "Sweden;Montenegro;American Samoa;Isle of Man;France;Niger;Tuvalu;Suriname;Serbia;Mexico;Nepal;Paraguay;Iraq;Norfolk Island;Canada;Central African Republic;Egypt;Kiribati;Comoros;Turks and Caicos Islands;Oman;Portugal;Yemen;Sao Tome and Principe;East Timor;Djibouti;Ecuador;Germany;Monaco;Brazil;Luxembourg;Norway;Guernsey;Denmark;Estonia;Gabon;Guam;Nauru;South Korea;Slovenia;Czech Republic;Bolivia;Congo (Kinshasa);Guinea;Ghana;Nigeria;Chad;Barbados;Ethiopia;Bahrain;Cayman Islands;Saint Kitts and Nevis;China;Chile;Kenya;Kyrgyzstan;Cambodia;Lithuania;Finland;Antigua and Barbuda;Micronesia;Cyprus;Spain;Trinidad and Tobago;Singapore;Libya;Namibia;Bulgaria;Martinique;United States;Faroe Islands;Angola;Vanuatu;Zambia;French Guiana;Somalia;Tajikistan;Botswana;Tanzania;Malta;Uruguay;Bosnia and Herzegovina;Northern Mariana Islands;Madagascar;Nicaragua;Jordan;North Korea;Reunion;Christmas Island;El Salvador;Cape Verde;Dominica;Indonesia;Greece;Australia;Russia;Georgia;Ukraine;United Kingdom;Wallis and Futuna;Equatorial Guinea;Palau;Macedonia;Western Sahara;Bhutan;Netherlands;Laos;Lesotho;New Caledonia;Venezuela;Aruba;Netherlands Antilles;South Africa;Afghanistan;Poland;Gibraltar;Ireland;Kazakhstan;Saint Lucia;Congo (Brazzaville);Sierra Leone;Italy;Grenada;Azerbaijan;Uganda;Malaysia;Eritrea;Mozambique;Panama;Albania;Maldives;Hong Kong;Benin;Liberia;Japan;Saudi Arabia;Bangladesh;Mauritania;Romania;Virgin Islands;Burkina Faso;Moldova;Colombia;New Zealand;Iran;Greenland;Belarus;Macau;Cook Islands;Burma;Puerto Rico;Gambia;Costa Rica;Mali;Guatemala;Vietnam;Jamaica;Croatia;Honduras;Cote d'Ivoire;Kuwait;Saint Vincent and the Grenadines;Taiwan;Iceland;Anguilla;South Sudan;Seychelles;Switzerland;Turkmenistan;Belize;Slovakia;Cameroon;Sudan;Qatar;Sri Lanka;Tunisia;Solomon Islands;French Polynesia;Bermuda;Swaziland;Dominican Republic;Burundi;Mayotte;Jersey;Guyana;Thailand;Hungary;Belgium;Algeria;Zimbabwe;Tonga;Mauritius;Saint Pierre and Miquelon;Bahamas;Peru;Papua New Guinea;Uzbekistan;United Arab Emirates;Senegal;Samoa;Brunei;Morocco;India;Togo;Cuba;Marshall Islands;Guinea-Bissau;Austria;Argentina;Guadeloupe;Malawi;Haiti;Mongolia;Israel;Turkey;Philippines;British Virgin Islands;Cocos (Keeling) Islands;Pakistan;Fiji;Latvia;Falkland Islands;Armenia;Rwanda;Lebanon".split(';')})
+  # df = df.head(5)
+  df = enrich(df, 'name', limit_props=100)
+  print(df)
+  rates = {c: df[c].notnull().sum() / len(df) for c in df.columns}
+  for c in sorted(df.columns, key=lambda c: rates[c], reverse=True):
+    print(f'{c}: {rates[c]:.0%}')
+    print('- ' + ', '.join(df[c].astype(str).unique()[:5]))
+  df = pd.DataFrame({
+      'country': df['name'],
+      'country_area': df['area'],
+      'country_form': df['instance of'],
+      'currency': df['currency'],
+      'continent': df['continent'],
+      'language': df['official language'],
+      'country_tld': df['top-level Internet domain'],
+      'country_flag': df['short name'],
+      'country_demonym': df['demonym'],
+      'country_head_of_government': df['head of government'],
+      'country_anthem': df['anthem'],
+      'country_plug_type': df['electrical plug type'],
+      'country_voltage': df['mains voltage'],
+      'country_driving_side': df['driving side'],
+      'country_member_of': df['member of'],
+      'country_total_fertility_rate': df['total fertility rate'],
+      'country_life_expectancy': df['life expectancy'],
+      'country_part_of': df['part of'],
+      'country_human_development_index': df['Human Development Index'],
+  })
+  df.to_parquet('country-from-wikidata.parquet', index=False)
